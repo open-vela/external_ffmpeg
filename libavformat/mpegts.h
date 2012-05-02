b527aaf34357693da68bb4567bfaddec0c29ba20
openvela-robot <openvela-robot@xiaomi.com>
2012-05-02 16:34:45 +0800

    riff: don't overwrite bps from WAVEFORMATEX if EXTENSIBLE doesn't contain that data.
    According to the specification on the MSDN [1], 0 is valid for that
    particular field, and it should be ignored in that case.
    
    [1]: http://msdn.microsoft.com/en-us/library/windows/desktop/dd757714(v=vs.85).aspx
    
    Bug-Id: 950
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavformat/mpegts.h
+++ libavformat/mpegts.h
@@ -2,20 +2,20 @@
  * MPEG-2 transport stream defines
  * Copyright (c) 2003 Fabrice Bellard
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -51,7 +51,6 @@
 #define STREAM_TYPE_AUDIO_AAC       0x0f
 #define STREAM_TYPE_AUDIO_AAC_LATM  0x11
 #define STREAM_TYPE_VIDEO_MPEG4     0x10
-#define STREAM_TYPE_METADATA        0x15
 #define STREAM_TYPE_VIDEO_H264      0x1b
 #define STREAM_TYPE_VIDEO_HEVC      0x24
 #define STREAM_TYPE_VIDEO_CAVS      0x42
@@ -59,16 +58,14 @@
 #define STREAM_TYPE_VIDEO_DIRAC     0xd1
 
 #define STREAM_TYPE_AUDIO_AC3       0x81
-#define STREAM_TYPE_AUDIO_DTS       0x82
-#define STREAM_TYPE_AUDIO_TRUEHD    0x83
-#define STREAM_TYPE_AUDIO_EAC3      0x87
+#define STREAM_TYPE_AUDIO_DTS       0x8a
 
 typedef struct MpegTSContext MpegTSContext;
 
-MpegTSContext *avpriv_mpegts_parse_open(AVFormatContext *s);
-int avpriv_mpegts_parse_packet(MpegTSContext *ts, AVPacket *pkt,
-                               const uint8_t *buf, int len);
-void avpriv_mpegts_parse_close(MpegTSContext *ts);
+MpegTSContext *ff_mpegts_parse_open(AVFormatContext *s);
+int ff_mpegts_parse_packet(MpegTSContext *ts, AVPacket *pkt,
+                           const uint8_t *buf, int len);
+void ff_mpegts_parse_close(MpegTSContext *ts);
 
 typedef struct SLConfigDescr {
     int use_au_start;
@@ -107,11 +104,5 @@ int ff_parse_mpeg2_descriptor(AVFormatContext *fc, AVStream *st, int stream_type
                               const uint8_t **pp, const uint8_t *desc_list_end,
                               Mp4Descr *mp4_descr, int mp4_descr_count, int pid,
                               MpegTSContext *ts);
-
-/**
- * Check presence of H264 startcode
- * @return <0 to stop processing
- */
-int ff_check_h264_startcode(AVFormatContext *s, const AVStream *st, const AVPacket *pkt);
 
 #endif /* AVFORMAT_MPEGTS_H */
