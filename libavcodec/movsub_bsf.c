f8d31f05a4830211df333da289bb1311a8b3c4a2
openvela-robot <openvela-robot@xiaomi.com>
2013-01-31 04:20:24 +0800

    h264_cabac: Break infinite loops
    This fixes out of array reads and/or infinite loops.
    
    30 is the maximum number of bits that can be read into
    coeff_abs below.
    
    CC: libav-stable@libav.org
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/movsub_bsf.c
+++ libavcodec/movsub_bsf.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2008 Reimar Döffinger
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -29,14 +29,17 @@ static int text2movsub(AVBitStreamFilterContext *bsfc, AVCodecContext *avctx, co
     if (buf_size > 0xffff) return 0;
     *poutbuf_size = buf_size + 2;
     *poutbuf = av_malloc(*poutbuf_size + FF_INPUT_BUFFER_PADDING_SIZE);
+    if (!*poutbuf)
+        return AVERROR(ENOMEM);
     AV_WB16(*poutbuf, buf_size);
     memcpy(*poutbuf + 2, buf, buf_size);
     return 1;
 }
 
 AVBitStreamFilter ff_text2movsub_bsf={
-    .name   = "text2movsub",
-    .filter = text2movsub,
+    "text2movsub",
+    0,
+    text2movsub,
 };
 
 static int mov2textsub(AVBitStreamFilterContext *bsfc, AVCodecContext *avctx, const char *args,
@@ -45,11 +48,14 @@ static int mov2textsub(AVBitStreamFilterContext *bsfc, AVCodecContext *avctx, co
     if (buf_size < 2) return 0;
     *poutbuf_size = FFMIN(buf_size - 2, AV_RB16(buf));
     *poutbuf = av_malloc(*poutbuf_size + FF_INPUT_BUFFER_PADDING_SIZE);
+    if (!*poutbuf)
+        return AVERROR(ENOMEM);
     memcpy(*poutbuf, buf + 2, *poutbuf_size);
     return 1;
 }
 
 AVBitStreamFilter ff_mov2textsub_bsf={
-    .name   = "mov2textsub",
-    .filter = mov2textsub,
+    "mov2textsub",
+    0,
+    mov2textsub,
 };
