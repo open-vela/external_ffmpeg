428286292d828eccfc85460af4b991fc470cb251
openvela-robot <openvela-robot@xiaomi.com>
2013-11-29 18:12:26 +0800

    dcadec: Add some logging before returning on error
    Based on a patch by Michael Niedermayer.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavfilter/vf_copy.c
+++ libavfilter/vf_copy.c
@@ -1,18 +1,18 @@
 /*
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -37,7 +37,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         return AVERROR(ENOMEM);
     }
     av_frame_copy_props(out, in);
-    av_image_copy(out->data, out->linesize, (const uint8_t**) in->data, in->linesize,
+    av_image_copy(out->data, out->linesize, in->data, in->linesize,
                   in->format, in->width, in->height);
 
     av_frame_free(&in);
@@ -46,9 +46,10 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 
 static const AVFilterPad avfilter_vf_copy_inputs[] = {
     {
-        .name         = "default",
-        .type         = AVMEDIA_TYPE_VIDEO,
-        .filter_frame = filter_frame,
+        .name             = "default",
+        .type             = AVMEDIA_TYPE_VIDEO,
+        .get_video_buffer = ff_null_get_video_buffer,
+        .filter_frame     = filter_frame,
     },
     { NULL }
 };
@@ -62,8 +63,9 @@ static const AVFilterPad avfilter_vf_copy_outputs[] = {
 };
 
 AVFilter ff_vf_copy = {
-    .name        = "copy",
+    .name      = "copy",
     .description = NULL_IF_CONFIG_SMALL("Copy the input video unchanged to the output."),
-    .inputs      = avfilter_vf_copy_inputs,
-    .outputs     = avfilter_vf_copy_outputs,
+
+    .inputs    = avfilter_vf_copy_inputs,
+    .outputs   = avfilter_vf_copy_outputs,
 };
