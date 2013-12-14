cb803813679ac500128a7e15fe70ed0f839f8314
openvela-robot <openvela-robot@xiaomi.com>
2013-12-14 22:24:03 +0800

    Merge commit '6451c8853a07ff2e28bda950fb5e83fcf88c5cf4'
    * commit '6451c8853a07ff2e28bda950fb5e83fcf88c5cf4':
      sdp: Check theora colorspace before producing the configuration string
    
    See: db8a53d86a71c948bd41dd12d1ff1d3351ec2746
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/rtpdec_h264.c
+++ libavformat/rtpdec_h264.c
@@ -2,20 +2,20 @@
  * RTP H264 Protocol (RFC3984)
  * Copyright (c) 2006 Ryan Martell
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -248,8 +248,7 @@ static int h264_handle_packet(AVFormatContext *ctx, PayloadContext *data,
                 if (pass == 0) {
                     /* now we know the total size of the packet (with the
                      * start sequences added) */
-                    if ((result = av_new_packet(pkt, total_length)) < 0)
-                        return result;
+                    av_new_packet(pkt, total_length);
                     dst = pkt->data;
                 } else {
                     assert(dst - pkt->data == total_length);
