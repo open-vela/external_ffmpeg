3210a75c3ca59acf76182e50bd11551ed1d655cf
openvela-robot <openvela-robot@xiaomi.com>
2013-12-20 12:48:16 +0800

    Merge commit '8558595a59913a4667f57d5a4658b1340f1a4354'
    * commit '8558595a59913a4667f57d5a4658b1340f1a4354':
      configure: Express atomics/thread deps through the dependency system
    
    Conflicts:
    	configure
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/atomic.c
+++ libavutil/atomic.c
@@ -18,7 +18,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "config.h"
 #include "atomic.h"
 
 #if !HAVE_ATOMICS_NATIVE
@@ -103,7 +102,7 @@ void *avpriv_atomic_ptr_cas(void * volatile *ptr, void *oldval, void *newval)
 
 #endif /* HAVE_PTHREADS */
 
-#endif /* !HAVE_ATOMICS_NATIVE */
+#endif /* !HAVE_MEMORYBARRIER && !HAVE_SYNC_VAL_COMPARE_AND_SWAP && !HAVE_MACHINE_RW_BARRIER */
 
 #ifdef TEST
 #include "avassert.h"
