b1f026f0b7945a48e3d886816fac5a2920f076c3
openvela-robot <openvela-robot@xiaomi.com>
2013-12-21 17:27:18 +0800

    vc1: Fix intensity compensation performance regression
    Introduced by 28243b0d35b47bbf9abbd454fc444a6e0a9e7b71
    
    Intensity compensation is always used once it was encountered, because
    v->next_use_ic is never set back to zero.
    
    Reset v->next_use_ic, when resetting v->next_luty/uv.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/adx.c
+++ libavcodec/adx.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2011  Justin Ruggles
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -46,11 +46,6 @@ int avpriv_adx_decode_header(AVCodecContext *avctx, const uint8_t *buf,
     if (AV_RB16(buf) != 0x8000)
         return AVERROR_INVALIDDATA;
     offset = AV_RB16(buf + 2) + 4;
-
-    if (offset < 6) {
-        av_log(avctx, AV_LOG_ERROR, "offset is prior data\n");
-        return AVERROR_INVALIDDATA;
-    }
 
     /* if copyright string is within the provided data, validate it */
     if (bufsize >= offset && memcmp(buf + offset - 6, "(c)CRI", 6))
