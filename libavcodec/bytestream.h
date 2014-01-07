0e103401b13918762d4398b5a5812db89932e7c5
openvela-robot <openvela-robot@xiaomi.com>
2014-01-07 11:15:49 +0800

    arm: cosmetics: Reindent the h264dsp neon init function
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/bytestream.h
+++ libavcodec/bytestream.h
@@ -3,20 +3,20 @@
  * copyright (c) 2006 Baptiste Coudurier <baptiste.coudurier@free.fr>
  * Copyright (c) 2012 Aneesh Dogra (lionaneesh) <lionaneesh@gmail.com>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -26,7 +26,6 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "libavutil/avassert.h"
 #include "libavutil/common.h"
 #include "libavutil/intreadwrite.h"
 
@@ -132,7 +131,6 @@ static av_always_inline void bytestream2_init(GetByteContext *g,
                                               const uint8_t *buf,
                                               int buf_size)
 {
-    av_assert0(buf_size >= 0);
     g->buffer       = buf;
     g->buffer_start = buf;
     g->buffer_end   = buf + buf_size;
@@ -142,7 +140,6 @@ static av_always_inline void bytestream2_init_writer(PutByteContext *p,
                                                      uint8_t *buf,
                                                      int buf_size)
 {
-    av_assert0(buf_size >= 0);
     p->buffer       = buf;
     p->buffer_start = buf;
     p->buffer_end   = buf + buf_size;
@@ -191,16 +188,6 @@ static av_always_inline int bytestream2_tell(GetByteContext *g)
 static av_always_inline int bytestream2_tell_p(PutByteContext *p)
 {
     return (int)(p->buffer - p->buffer_start);
-}
-
-static av_always_inline int bytestream2_size(GetByteContext *g)
-{
-    return (int)(g->buffer_end - g->buffer_start);
-}
-
-static av_always_inline int bytestream2_size_p(PutByteContext *p)
-{
-    return (int)(p->buffer_end - p->buffer_start);
 }
 
 static av_always_inline int bytestream2_seek(GetByteContext *g,
