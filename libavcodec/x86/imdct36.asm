f2e765cb184994ba3af81f67bbef201b3f0bf3d4
openvela-robot <openvela-robot@xiaomi.com>
2014-01-19 19:22:55 +0800

    x86inc: Speed up assembling with Yasm
    Work around Yasm's inefficiency with handling large numbers of variables
    in the global scope.
    
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavcodec/x86/imdct36.asm
+++ libavcodec/x86/imdct36.asm
@@ -2,20 +2,20 @@
 ;* 36 point SSE-optimized IMDCT transform
 ;* Copyright (c) 2011 Vitor Sessak
 ;*
-;* This file is part of FFmpeg.
+;* This file is part of Libav.
 ;*
-;* FFmpeg is free software; you can redistribute it and/or
+;* Libav is free software; you can redistribute it and/or
 ;* modify it under the terms of the GNU Lesser General Public
 ;* License as published by the Free Software Foundation; either
 ;* version 2.1 of the License, or (at your option) any later version.
 ;*
-;* FFmpeg is distributed in the hope that it will be useful,
+;* Libav is distributed in the hope that it will be useful,
 ;* but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;* Lesser General Public License for more details.
 ;*
 ;* You should have received a copy of the GNU Lesser General Public
-;* License along with FFmpeg; if not, write to the Free Software
+;* License along with Libav; if not, write to the Free Software
 ;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 ;******************************************************************************
 
@@ -370,10 +370,8 @@ DEFINE_IMDCT
 INIT_XMM ssse3
 DEFINE_IMDCT
 
-%if HAVE_AVX_EXTERNAL
 INIT_XMM avx
 DEFINE_IMDCT
-%endif
 
 INIT_XMM sse
 
@@ -718,7 +716,5 @@ cglobal four_imdct36_float, 5,5,16, out, buf, in, win, tmp
 INIT_XMM sse
 DEFINE_FOUR_IMDCT
 
-%if HAVE_AVX_EXTERNAL
 INIT_XMM avx
 DEFINE_FOUR_IMDCT
-%endif
