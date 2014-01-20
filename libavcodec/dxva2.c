aaa3bcb52ab054cc5ad531a50243821f130a247c
openvela-robot <openvela-robot@xiaomi.com>
2014-01-20 15:22:09 +0800

    lavc: do not force the emu edge flag
    The default get_buffer2() implementation (and possibly some
    user ones) does not allocate edges when this flag is set, which may
    expose bugs in some decoders. Until the 10 release is out, it is safer
    to remove this part.

--- libavcodec/dxva2.c
+++ libavcodec/dxva2.c
@@ -3,25 +3,32 @@
  *
  * copyright (c) 2010 Laurent Aimar
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "dxva2_internal.h"
+#include <assert.h>
+#include <string.h>
+
+#include "libavutil/log.h"
 #include "libavutil/time.h"
+
+#include "avcodec.h"
+#include "mpegvideo.h"
+#include "dxva2_internal.h"
 
 void *ff_dxva2_get_surface(const Picture *picture)
 {
@@ -56,7 +63,7 @@ int ff_dxva2_commit_buffer(AVCodecContext *avctx,
     hr = IDirectXVideoDecoder_GetBuffer(ctx->decoder, type,
                                         &dxva_data, &dxva_size);
     if (FAILED(hr)) {
-        av_log(avctx, AV_LOG_ERROR, "Failed to get a buffer for %d: 0x%x\n",
+        av_log(avctx, AV_LOG_ERROR, "Failed to get a buffer for %u: 0x%lx\n",
                type, hr);
         return -1;
     }
@@ -70,14 +77,14 @@ int ff_dxva2_commit_buffer(AVCodecContext *avctx,
 
         result = 0;
     } else {
-        av_log(avctx, AV_LOG_ERROR, "Buffer for type %d was too small\n", type);
+        av_log(avctx, AV_LOG_ERROR, "Buffer for type %u was too small\n", type);
         result = -1;
     }
 
     hr = IDirectXVideoDecoder_ReleaseBuffer(ctx->decoder, type);
     if (FAILED(hr)) {
         av_log(avctx, AV_LOG_ERROR,
-               "Failed to release buffer type %d: 0x%x\n",
+               "Failed to release buffer type %u: 0x%lx\n",
                type, hr);
         result = -1;
     }
@@ -107,7 +114,7 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, Picture *pic,
     } while (hr == E_PENDING && ++runs < 50);
 
     if (FAILED(hr)) {
-        av_log(avctx, AV_LOG_ERROR, "Failed to begin frame: 0x%x\n", hr);
+        av_log(avctx, AV_LOG_ERROR, "Failed to begin frame: 0x%lx\n", hr);
         return -1;
     }
 
@@ -152,14 +159,14 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, Picture *pic,
     exec.pExtensionData      = NULL;
     hr = IDirectXVideoDecoder_Execute(ctx->decoder, &exec);
     if (FAILED(hr)) {
-        av_log(avctx, AV_LOG_ERROR, "Failed to execute: 0x%x\n", hr);
+        av_log(avctx, AV_LOG_ERROR, "Failed to execute: 0x%lx\n", hr);
         result = -1;
     }
 
 end:
     hr = IDirectXVideoDecoder_EndFrame(ctx->decoder, NULL);
     if (FAILED(hr)) {
-        av_log(avctx, AV_LOG_ERROR, "Failed to end frame: 0x%x\n", hr);
+        av_log(avctx, AV_LOG_ERROR, "Failed to end frame: 0x%lx\n", hr);
         result = -1;
     }
 
