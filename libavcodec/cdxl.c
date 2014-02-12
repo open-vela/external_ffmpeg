a540158f3ffe9def5d416f9ceed9a981f77d9381
openvela-robot <openvela-robot@xiaomi.com>
2014-02-13 00:44:08 +0800

    configure: do not link libraries against program-specific dependencies
    Bug-Id: 635

--- libavcodec/cdxl.c
+++ libavcodec/cdxl.c
@@ -2,30 +2,22 @@
  * CDXL video decoder
  * Copyright (c) 2011-2012 Paul B Mahol
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
-/**
- * @file
- * Commodore CDXL video decoder
- * @author Paul B Mahol
- */
-
-#define UNCHECKED_BITSTREAM_READER 1
 
 #include "libavutil/intreadwrite.h"
 #include "libavutil/imgutils.h"
@@ -34,8 +26,8 @@
 #include "internal.h"
 
 #define BIT_PLANAR   0x00
-#define CHUNKY       0x20
-#define BYTE_PLANAR  0x40
+#define BYTE_PLANAR  0x20
+#define CHUNKY       0x40
 #define BIT_LINE     0x80
 #define BYTE_LINE    0xC0
 
@@ -71,7 +63,7 @@ static void import_palette(CDXLVideoContext *c, uint32_t *new_palette)
         unsigned r   = ((rgb >> 8) & 0xF) * 0x11;
         unsigned g   = ((rgb >> 4) & 0xF) * 0x11;
         unsigned b   =  (rgb       & 0xF) * 0x11;
-        AV_WN32(&new_palette[i], (0xFFU << 24) | (r << 16) | (g << 8) | b);
+        AV_WN32(&new_palette[i], (r << 16) | (g << 8) | b);
     }
 }
 
@@ -123,7 +115,6 @@ static void cdxl_decode_rgb(CDXLVideoContext *c, AVFrame *frame)
 {
     uint32_t *new_palette = (uint32_t *)frame->data[1];
 
-    memset(frame->data[1], 0, AVPALETTE_SIZE);
     import_palette(c, new_palette);
     import_format(c, frame->linesize[0], frame->data[0]);
 }
@@ -264,8 +255,10 @@ static int cdxl_decode_frame(AVCodecContext *avctx, void *data,
         return AVERROR_PATCHWELCOME;
     }
 
-    if ((ret = ff_get_buffer(avctx, p, 0)) < 0)
+    if ((ret = ff_get_buffer(avctx, p, 0)) < 0) {
+        av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
         return ret;
+    }
     p->pict_type = AV_PICTURE_TYPE_I;
 
     if (encoding) {
@@ -289,7 +282,7 @@ static av_cold int cdxl_decode_end(AVCodecContext *avctx)
 {
     CDXLVideoContext *c = avctx->priv_data;
 
-    av_freep(&c->new_video);
+    av_free(c->new_video);
 
     return 0;
 }
