ab07b6db509b118bd9c494e7ae18f8eff2b68f29
openvela-robot <openvela-robot@xiaomi.com>
2014-02-12 14:41:24 +0800

    Merge commit '25a1ba814ad80056247fd357ec4c6911324a3f66'
    * commit '25a1ba814ad80056247fd357ec4c6911324a3f66':
      log: Have function parameter names match between .c and .h file
    
    Conflicts:
    	libavutil/log.c
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libx265.c
+++ libavcodec/libx265.c
@@ -3,20 +3,20 @@
  *
  * Copyright (c) 2013-2014 Derek Buitenhuis
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -139,7 +139,7 @@ static av_cold int libx265_encode_init(AVCodecContext *avctx)
     if (!ctx->encoder) {
         av_log(avctx, AV_LOG_ERROR, "Cannot open libx265 encoder.\n");
         libx265_encode_close(avctx);
-        return AVERROR_INVALIDDATA;
+        return AVERROR_EXTERNAL;
     }
 
     ret = x265_encoder_headers(ctx->encoder, &nal, &nnal);
