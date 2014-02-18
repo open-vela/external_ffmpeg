df0f095d6d6a90b110efcdc77aa1f57f5407332b
openvela-robot <openvela-robot@xiaomi.com>
2014-02-18 12:11:41 +0800

    dct-test: Reuse enum idct_permutation_type instead of duplicating it

--- libavcodec/h264_mp4toannexb_bsf.c
+++ libavcodec/h264_mp4toannexb_bsf.c
@@ -2,20 +2,20 @@
  * H.264 MP4 to Annex B byte stream format filter
  * Copyright (c) 2007 Benoit Fouet <benoit.fouet@free.fr>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -69,10 +69,17 @@ static int h264_extradata_to_annexb(AVCodecContext *avctx, const int padding)
     static const uint8_t nalu_header[4] = { 0, 0, 0, 1 };
     int length_size = (*extradata++ & 0x3) + 1; // retrieve length coded size
 
+    if (length_size == 3)
+        return AVERROR(EINVAL);
+
     /* retrieve sps and pps unit(s) */
     unit_nb = *extradata++ & 0x1f; /* number of sps unit(s) */
     if (!unit_nb) {
-        goto pps;
+        unit_nb = *extradata++; /* number of pps unit(s) */
+        sps_done++;
+
+        if (unit_nb)
+            pps_seen = 1;
     } else {
         sps_seen = 1;
     }
@@ -82,15 +89,9 @@ static int h264_extradata_to_annexb(AVCodecContext *avctx, const int padding)
 
         unit_size   = AV_RB16(extradata);
         total_size += unit_size + 4;
-        if (total_size > INT_MAX - padding) {
-            av_log(avctx, AV_LOG_ERROR,
-                   "Too big extradata size, corrupted stream or invalid MP4/AVCC bitstream\n");
-            av_free(out);
-            return AVERROR(EINVAL);
-        }
-        if (extradata + 2 + unit_size > avctx->extradata + avctx->extradata_size) {
-            av_log(avctx, AV_LOG_ERROR, "Packet header is not contained in global extradata, "
-                   "corrupted stream or invalid MP4/AVCC bitstream\n");
+        if (total_size > INT_MAX - padding ||
+            extradata + 2 + unit_size > avctx->extradata +
+            avctx->extradata_size) {
             av_free(out);
             return AVERROR(EINVAL);
         }
@@ -99,7 +100,7 @@ static int h264_extradata_to_annexb(AVCodecContext *avctx, const int padding)
         memcpy(out + total_size - unit_size - 4, nalu_header, 4);
         memcpy(out + total_size - unit_size, extradata + 2, unit_size);
         extradata += 2 + unit_size;
-pps:
+
         if (!unit_nb && !sps_done++) {
             unit_nb = *extradata++; /* number of pps unit(s) */
             if (unit_nb)
@@ -134,7 +135,6 @@ static int h264_mp4toannexb_filter(AVBitStreamFilterContext *bsfc,
                                    int keyframe)
 {
     H264BSFContext *ctx = bsfc->priv_data;
-    int i;
     uint8_t unit_type;
     int32_t nal_size;
     uint32_t cumul_size    = 0;
@@ -161,12 +161,15 @@ static int h264_mp4toannexb_filter(AVBitStreamFilterContext *bsfc,
     *poutbuf_size = 0;
     *poutbuf      = NULL;
     do {
-        ret= AVERROR(EINVAL);
         if (buf + ctx->length_size > buf_end)
             goto fail;
 
-        for (nal_size = 0, i = 0; i<ctx->length_size; i++)
-            nal_size = (nal_size << 8) | buf[i];
+        if (ctx->length_size == 1) {
+            nal_size = buf[0];
+        } else if (ctx->length_size == 2) {
+            nal_size = AV_RB16(buf);
+        } else
+            nal_size = AV_RB32(buf);
 
         buf      += ctx->length_size;
         unit_type = *buf & 0x1f;
@@ -175,15 +178,15 @@ static int h264_mp4toannexb_filter(AVBitStreamFilterContext *bsfc,
             goto fail;
 
         /* prepend only to the first type 5 NAL unit of an IDR picture */
-        if (ctx->first_idr && (unit_type == 5 || unit_type == 7 || unit_type == 8)) {
-            if ((ret=alloc_and_copy(poutbuf, poutbuf_size,
+        if (ctx->first_idr && unit_type == 5) {
+            if (alloc_and_copy(poutbuf, poutbuf_size,
                                avctx->extradata, avctx->extradata_size,
-                               buf, nal_size)) < 0)
+                               buf, nal_size) < 0)
                 goto fail;
             ctx->first_idr = 0;
         } else {
-            if ((ret=alloc_and_copy(poutbuf, poutbuf_size,
-                               NULL, 0, buf, nal_size)) < 0)
+            if (alloc_and_copy(poutbuf, poutbuf_size,
+                               NULL, 0, buf, nal_size) < 0)
                 goto fail;
             if (!ctx->first_idr && unit_type == 1)
                 ctx->first_idr = 1;
@@ -198,11 +201,11 @@ static int h264_mp4toannexb_filter(AVBitStreamFilterContext *bsfc,
 fail:
     av_freep(poutbuf);
     *poutbuf_size = 0;
-    return ret;
+    return AVERROR(EINVAL);
 }
 
 AVBitStreamFilter ff_h264_mp4toannexb_bsf = {
-    .name           = "h264_mp4toannexb",
-    .priv_data_size = sizeof(H264BSFContext),
-    .filter         = h264_mp4toannexb_filter,
+    "h264_mp4toannexb",
+    sizeof(H264BSFContext),
+    h264_mp4toannexb_filter,
 };
