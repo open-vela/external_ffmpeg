11d88f642c937e95046258e7459243e83848e457
openvela-robot <openvela-robot@xiaomi.com>
2014-02-22 22:28:24 +0800

    swresample: add swr_is_initialized()
    Idea-from/based-on: 7e86c27b4ee9e5a3fbe6cf5249b9d918b2a5e731
    Reviewed-by: ubitux
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/libwebpenc.c
+++ libavcodec/libwebpenc.c
@@ -2,20 +2,20 @@
  * WebP encoding support via libwebp
  * Copyright (c) 2013 Justin Ruggles <justin.ruggles@gmail.com>
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -161,7 +161,9 @@ static int libwebp_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
             ret = av_frame_get_buffer(alt_frame, 32);
             if (ret < 0)
                 goto end;
-            av_frame_copy(alt_frame, frame);
+            av_image_copy(alt_frame->data, alt_frame->linesize,
+                          frame->data, frame->linesize,
+                          avctx->pix_fmt, frame->width, frame->height);
             frame = alt_frame;
         }
         pic->use_argb  = 0;
