1258535ec1d775886cbadf228bfd94a6f4b61d99
openvela-robot <openvela-robot@xiaomi.com>
2014-02-26 22:02:19 +0800

    Merge remote-tracking branch 'qatar/master'
    * qatar/master:
      build: Do not redundantly specifiy H.263-related object files for MSMPEG4v*
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dcadsp.c
+++ libavcodec/dcadsp.c
@@ -2,20 +2,20 @@
  * Copyright (c) 2004 Gildas Bazin
  * Copyright (c) 2010 Mans Rullgard <mans@mansr.com>
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -36,22 +36,22 @@ static inline void
 dca_lfe_fir(float *out, const float *in, const float *coefs,
             int decifactor, float scale)
 {
-    float *out2 = out + decifactor;
+    float *out2 = out + 2*decifactor-1;
     const float *cf0 = coefs;
-    const float *cf1 = coefs + 256;
+    int num_coeffs = 256 / decifactor;
     int j, k;
 
     /* One decimated sample generates 2*decifactor interpolated ones */
     for (k = 0; k < decifactor; k++) {
         float v0 = 0.0;
         float v1 = 0.0;
-        for (j = 0; j < 256 / decifactor; j++) {
-            float s = in[-j];
-            v0 += s * *cf0++;
-            v1 += s * *--cf1;
+        for (j = 0; j < num_coeffs; j++, cf0++) {
+            v0 += in[-j] * *cf0;
+            v1 += in[1+j-num_coeffs] * *cf0;
+
         }
         *out++  = v0 * scale;
-        *out2++ = v1 * scale;
+        *out2-- = v1 * scale;
     }
 }
 
