1b62cf8d8b26ee2b987a07e235c301c8539cfa84
Lukasz Marek <lukasz.m.luki@gmail.com>
2014-02-28 00:36:07 +0100

    lavf/http: return error from seek on invalid whence
    Signed-off-by: Lukasz Marek <lukasz.m.luki@gmail.com>

--- libavformat/http.c
+++ libavformat/http.c
@@ -744,6 +744,8 @@ static int64_t http_seek(URLContext *h, int64_t off, int whence)
         off += s->off;
     else if (whence == SEEK_END)
         off += s->filesize;
+    else if (whence != SEEK_SET)
+        return AVERROR(EINVAL);
     if (off < 0)
         return AVERROR(EINVAL);
     s->off = off;
