69e108960a82de4ba676d7d1b306e269abe70a31
openvela-robot <openvela-robot@xiaomi.com>
2014-03-06 13:04:03 +0800

    build: Let the SVQ3 decoder depend on the H.264 decoder
    The SVQ3 decoder reuses large parts of the H.264 decoder so it
    makes no sense to enable the former but not the latter.
    
    Also drop unnecessary h263.o object from SVQ3 decoder object list.

--- libavcodec/libx265.c
+++ libavcodec/libx265.c
@@ -3,20 +3,20 @@
  *
  * Copyright (c) 2013-2014 Derek Buitenhuis
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -121,11 +121,11 @@ static av_cold int libx265_encode_init(AVCodecContext *avctx)
     av_reduce(&sar_num, &sar_den,
               avctx->sample_aspect_ratio.num,
               avctx->sample_aspect_ratio.den, 4096);
-    ctx->params->vui.bEnableVuiParametersPresentFlag = 1;
-    ctx->params->vui.bEnableAspectRatioIdc           = 1;
-    ctx->params->vui.aspectRatioIdc                  = 255;
-    ctx->params->vui.sarWidth                        = sar_num;
-    ctx->params->vui.sarHeight                       = sar_den;
+    ctx->params->bEnableVuiParametersPresentFlag = 1;
+    ctx->params->bEnableAspectRatioIdc           = 1;
+    ctx->params->aspectRatioIdc                  = 255;
+    ctx->params->sarWidth                        = sar_num;
+    ctx->params->sarHeight                       = sar_den;
 
     if (x265_max_bit_depth == 8)
         ctx->params->internalBitDepth = 8;
@@ -235,7 +235,7 @@ static int libx265_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
     ret = x265_encoder_encode(ctx->encoder, &nal, &nnal,
                               pic ? &x265pic : NULL, &x265pic_out);
     if (ret < 0)
-        return AVERROR_EXTERNAL;
+        return AVERROR_UNKNOWN;
 
     if (!nnal)
         return 0;
