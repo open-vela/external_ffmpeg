b9653bcb9030db3bb42c7b5a75965ab6e7076fdf
openvela-robot <openvela-robot@xiaomi.com>
2014-03-11 14:20:44 +0800

    Fix modplug linkage on Windows.
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/hnm.c
+++ libavformat/hnm.c
@@ -3,24 +3,22 @@
  *
  * Copyright (c) 2012 David Kment
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg .
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg  is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg  is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg ; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
-#include <inttypes.h>
 
 #include "libavutil/intreadwrite.h"
 #include "avformat.h"
@@ -150,10 +148,9 @@ static int hnm_read_packet(AVFormatContext *s, AVPacket *pkt)
     chunk_id = avio_rl16(pb);
     avio_skip(pb, 2);
 
-    if (chunk_size > hnm->superchunk_remaining) {
-        av_log(s, AV_LOG_ERROR,
-               "invalid chunk size: %"PRIu32", offset: %"PRId64"\n",
-               chunk_size, avio_tell(pb));
+    if (chunk_size > hnm->superchunk_remaining || !chunk_size) {
+        av_log(s, AV_LOG_ERROR, "invalid chunk size: %u, offset: %u\n",
+               chunk_size, (int) avio_tell(pb));
         avio_skip(pb, hnm->superchunk_remaining - 8);
         hnm->superchunk_remaining = 0;
     }
@@ -175,8 +172,8 @@ static int hnm_read_packet(AVFormatContext *s, AVPacket *pkt)
         break;
 
     default:
-        av_log(s, AV_LOG_WARNING, "unknown chunk found: %"PRIu16", offset: %"PRId64"\n",
-               chunk_id, avio_tell(pb));
+        av_log(s, AV_LOG_WARNING, "unknown chunk found: %d, offset: %d\n",
+               chunk_id, (int) avio_tell(pb));
         avio_skip(pb, chunk_size - 8);
         hnm->superchunk_remaining -= chunk_size;
         break;
