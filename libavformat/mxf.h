b37494c3d07f920db2e96d729447b046b5a9e096
openvela-robot <openvela-robot@xiaomi.com>
2014-03-19 17:57:02 +0800

    avcodec/mjpegdec: factorize parse_avid out
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mxf.h
+++ libavformat/mxf.h
@@ -2,20 +2,20 @@
  * MXF
  * Copyright (c) 2006 SmartJog S.A., Baptiste Coudurier <baptiste dot coudurier at smartjog dot com>
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 #ifndef AVFORMAT_MXF_H
@@ -48,11 +48,11 @@ enum MXFMetadataSetType {
 };
 
 enum MXFFrameLayout {
-     FullFrame = 0,
-     MixedFields,
-     OneField,
-     SegmentedFrame,
-     SeparateFields
+    FullFrame = 0,
+    SeparateFields,
+    OneField,
+    MixedFields,
+    SegmentedFrame,
 };
 
 typedef struct KLVPacket {
@@ -67,7 +67,7 @@ typedef struct MXFCodecUL {
     int id;
 } MXFCodecUL;
 
-typedef struct MXFSamplesPerFrame {
+typedef struct {
     struct AVRational time_base;
     int samples_per_frame[6];
 } MXFSamplesPerFrame;
@@ -79,32 +79,7 @@ extern const MXFCodecUL ff_mxf_pixel_format_uls[];
 int ff_mxf_decode_pixel_layout(const char pixel_layout[16], enum AVPixelFormat *pix_fmt);
 const MXFSamplesPerFrame *ff_mxf_get_samples_per_frame(AVFormatContext *s, AVRational time_base);
 
-#ifdef DEBUG
-#define PRINT_KEY(pc, s, x)                         \
-    av_log(pc, AV_LOG_VERBOSE,                      \
-           "%s "                                    \
-           "0x%02x,0x%02x,0x%02x,0x%02x,"           \
-           "0x%02x,0x%02x,0x%02x,0x%02x,"           \
-           "0x%02x,0x%02x,0x%02x,0x%02x,"           \
-           "0x%02x,0x%02x,0x%02x,0x%02x ",          \
-            s,                                      \
-            (x)[0],  (x)[1],  (x)[2],  (x)[3],      \
-            (x)[4],  (x)[5],  (x)[6],  (x)[7],      \
-            (x)[8],  (x)[9],  (x)[10], (x)[11],     \
-            (x)[12], (x)[13], (x)[14], (x)[15]);    \
-    av_log(pc, AV_LOG_INFO,                         \
-           "%s "                                    \
-           "%02x.%02x.%02x.%02x."                   \
-           "%02x.%02x.%02x.%02x."                   \
-           "%02x.%02x.%02x.%02x."                   \
-           "%02x.%02x.%02x.%02x\n",                 \
-            s,                                      \
-            (x)[0],  (x)[1],  (x)[2],  (x)[3],      \
-            (x)[4],  (x)[5],  (x)[6],  (x)[7],      \
-            (x)[8],  (x)[9],  (x)[10], (x)[11],     \
-            (x)[12], (x)[13], (x)[14], (x)[15])
-#else
-#define PRINT_KEY(pc, s, x)
-#endif
+#define PRINT_KEY(pc, s, x) av_dlog(pc, "%s %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X\n", s, \
+                             (x)[0], (x)[1], (x)[2], (x)[3], (x)[4], (x)[5], (x)[6], (x)[7], (x)[8], (x)[9], (x)[10], (x)[11], (x)[12], (x)[13], (x)[14], (x)[15])
 
 #endif /* AVFORMAT_MXF_H */
