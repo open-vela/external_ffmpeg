5bfc9df4be1a0db31d00d2dc8aea3093de52d9db
openvela-robot <openvela-robot@xiaomi.com>
2014-03-24 10:55:35 +0800

    lavd: Add QTKit input device.
    Signed-off-by: Thilo Borgmann <thilo.borgmann@mail.de>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/float_dsp.c
+++ libavutil/float_dsp.c
@@ -1,18 +1,21 @@
 /*
- * This file is part of Libav.
+ * Copyright 2005 Balatoni Denes
+ * Copyright 2006 Loren Merritt
  *
- * Libav is free software; you can redistribute it and/or
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -89,7 +92,7 @@ static void vector_fmul_reverse_c(float *dst, const float *src0,
         dst[i] = src0[i] * src1[-i];
 }
 
-static void butterflies_float_c(float *restrict v1, float *restrict v2,
+static void butterflies_float_c(float *av_restrict v1, float *av_restrict v2,
                                 int len)
 {
     int i;
@@ -132,6 +135,8 @@ av_cold void avpriv_float_dsp_init(AVFloatDSPContext *fdsp, int bit_exact)
         ff_float_dsp_init_ppc(fdsp, bit_exact);
     if (ARCH_X86)
         ff_float_dsp_init_x86(fdsp);
+    if (ARCH_MIPS)
+        ff_float_dsp_init_mips(fdsp);
 }
 
 #ifdef TEST
