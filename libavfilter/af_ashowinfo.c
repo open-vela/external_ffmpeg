209f7538149d698cdaa87053108d022c33367d7e
openvela-robot <openvela-robot@xiaomi.com>
2014-03-24 13:27:33 +0800

    Merge commit '0e2c3ee9a335d8a0a5edf0509e222e804d7b2619'
    * commit '0e2c3ee9a335d8a0a5edf0509e222e804d7b2619':
      lavc: give a typedeffed name to the AVPacket.side_data struct
    
    Conflicts:
    	doc/APIchanges
    	libavcodec/version.h
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavfilter/af_ashowinfo.c
+++ libavfilter/af_ashowinfo.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2011 Stefano Sabatini
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -30,10 +30,8 @@
 #include "libavutil/attributes.h"
 #include "libavutil/channel_layout.h"
 #include "libavutil/common.h"
-#include "libavutil/downmix_info.h"
-#include "libavutil/intreadwrite.h"
 #include "libavutil/mem.h"
-#include "libavutil/replaygain.h"
+#include "libavutil/timestamp.h"
 #include "libavutil/samplefmt.h"
 
 #include "audio.h"
@@ -45,121 +43,12 @@ typedef struct AShowInfoContext {
      * Scratch space for individual plane checksums for planar audio
      */
     uint32_t *plane_checksums;
-
-    /**
-     * Frame counter
-     */
-    uint64_t frame;
 } AShowInfoContext;
-
-static int config_input(AVFilterLink *inlink)
-{
-    AShowInfoContext *s = inlink->dst->priv;
-    int channels = av_get_channel_layout_nb_channels(inlink->channel_layout);
-    s->plane_checksums = av_malloc(channels * sizeof(*s->plane_checksums));
-    if (!s->plane_checksums)
-        return AVERROR(ENOMEM);
-
-    return 0;
-}
 
 static av_cold void uninit(AVFilterContext *ctx)
 {
     AShowInfoContext *s = ctx->priv;
     av_freep(&s->plane_checksums);
-}
-
-static void dump_matrixenc(AVFilterContext *ctx, AVFrameSideData *sd)
-{
-    enum AVMatrixEncoding enc;
-
-    av_log(ctx, AV_LOG_INFO, "matrix encoding: ");
-
-    if (sd->size < sizeof(enum AVMatrixEncoding)) {
-        av_log(ctx, AV_LOG_INFO, "invalid data");
-        return;
-    }
-
-    enc = *(enum AVMatrixEncoding *)sd->data;
-    switch (enc) {
-    case AV_MATRIX_ENCODING_NONE:           av_log(ctx, AV_LOG_INFO, "none");                break;
-    case AV_MATRIX_ENCODING_DOLBY:          av_log(ctx, AV_LOG_INFO, "Dolby");               break;
-    case AV_MATRIX_ENCODING_DPLII:          av_log(ctx, AV_LOG_INFO, "Dolby Pro Logic II");  break;
-    case AV_MATRIX_ENCODING_DPLIIX:         av_log(ctx, AV_LOG_INFO, "Dolby Pro Logic IIx"); break;
-    case AV_MATRIX_ENCODING_DPLIIZ:         av_log(ctx, AV_LOG_INFO, "Dolby Pro Logic IIz"); break;
-    case AV_MATRIX_ENCODING_DOLBYEX:        av_log(ctx, AV_LOG_INFO, "Dolby EX");            break;
-    case AV_MATRIX_ENCODING_DOLBYHEADPHONE: av_log(ctx, AV_LOG_INFO, "Dolby Headphone");     break;
-    default:                                av_log(ctx, AV_LOG_WARNING, "unknown");          break;
-    }
-}
-
-static void dump_downmix(AVFilterContext *ctx, AVFrameSideData *sd)
-{
-    AVDownmixInfo *di;
-
-    av_log(ctx, AV_LOG_INFO, "downmix: ");
-    if (sd->size < sizeof(*di)) {
-        av_log(ctx, AV_LOG_INFO, "invalid data");
-        return;
-    }
-
-    di = (AVDownmixInfo *)sd->data;
-
-    av_log(ctx, AV_LOG_INFO, "preferred downmix type - ");
-    switch (di->preferred_downmix_type) {
-    case AV_DOWNMIX_TYPE_LORO:    av_log(ctx, AV_LOG_INFO, "Lo/Ro");              break;
-    case AV_DOWNMIX_TYPE_LTRT:    av_log(ctx, AV_LOG_INFO, "Lt/Rt");              break;
-    case AV_DOWNMIX_TYPE_DPLII:   av_log(ctx, AV_LOG_INFO, "Dolby Pro Logic II"); break;
-    default:                      av_log(ctx, AV_LOG_WARNING, "unknown");         break;
-    }
-
-    av_log(ctx, AV_LOG_INFO, " Mix levels: center %f (%f ltrt) - "
-           "surround %f (%f ltrt) - lfe %f",
-           di->center_mix_level, di->center_mix_level_ltrt,
-           di->surround_mix_level, di->surround_mix_level_ltrt,
-           di->lfe_mix_level);
-}
-
-static void print_gain(AVFilterContext *ctx, const char *str, int32_t gain)
-{
-    av_log(ctx, AV_LOG_INFO, "%s - ", str);
-    if (gain == INT32_MIN)
-        av_log(ctx, AV_LOG_INFO, "unknown");
-    else
-        av_log(ctx, AV_LOG_INFO, "%f", gain / 100000.0f);
-    av_log(ctx, AV_LOG_INFO, ", ");
-}
-
-static void print_peak(AVFilterContext *ctx, const char *str, uint32_t peak)
-{
-    av_log(ctx, AV_LOG_INFO, "%s - ", str);
-    if (!peak)
-        av_log(ctx, AV_LOG_INFO, "unknown");
-    else
-        av_log(ctx, AV_LOG_INFO, "%f", (float)peak / UINT32_MAX);
-    av_log(ctx, AV_LOG_INFO, ", ");
-}
-
-static void dump_replaygain(AVFilterContext *ctx, AVFrameSideData *sd)
-{
-    AVReplayGain *rg;
-
-    av_log(ctx, AV_LOG_INFO, "replaygain: ");
-    if (sd->size < sizeof(*rg)) {
-        av_log(ctx, AV_LOG_INFO, "invalid data");
-        return;
-    }
-    rg = (AVReplayGain*)sd->data;
-
-    print_gain(ctx, "track gain", rg->track_gain);
-    print_peak(ctx, "track peak", rg->track_peak);
-    print_gain(ctx, "album gain", rg->album_gain);
-    print_peak(ctx, "album peak", rg->album_peak);
-}
-
-static void dump_unknown(AVFilterContext *ctx, AVFrameSideData *sd)
-{
-    av_log(ctx, AV_LOG_INFO, "unknown side data type: %d, size %d bytes", sd->type, sd->size);
 }
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *buf)
@@ -168,12 +57,17 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *buf)
     AShowInfoContext *s  = ctx->priv;
     char chlayout_str[128];
     uint32_t checksum = 0;
-    int channels    = av_get_channel_layout_nb_channels(buf->channel_layout);
+    int channels    = inlink->channels;
     int planar      = av_sample_fmt_is_planar(buf->format);
     int block_align = av_get_bytes_per_sample(buf->format) * (planar ? 1 : channels);
     int data_size   = buf->nb_samples * block_align;
     int planes      = planar ? channels : 1;
     int i;
+    void *tmp_ptr = av_realloc(s->plane_checksums, channels * sizeof(*s->plane_checksums));
+
+    if (!tmp_ptr)
+        return AVERROR(ENOMEM);
+    s->plane_checksums = tmp_ptr;
 
     for (i = 0; i < planes; i++) {
         uint8_t *data = buf->extended_data[i];
@@ -187,11 +81,13 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *buf)
                                  buf->channel_layout);
 
     av_log(ctx, AV_LOG_INFO,
-           "n:%"PRIu64" pts:%"PRId64" pts_time:%f "
-           "fmt:%s chlayout:%s rate:%d nb_samples:%d "
+           "n:%"PRId64" pts:%s pts_time:%s pos:%"PRId64" "
+           "fmt:%s channels:%d chlayout:%s rate:%d nb_samples:%d "
            "checksum:%08"PRIX32" ",
-           s->frame, buf->pts, buf->pts * av_q2d(inlink->time_base),
-           av_get_sample_fmt_name(buf->format), chlayout_str,
+           inlink->frame_count,
+           av_ts2str(buf->pts), av_ts2timestr(buf->pts, &inlink->time_base),
+           av_frame_get_pkt_pos(buf),
+           av_get_sample_fmt_name(buf->format), av_frame_get_channels(buf), chlayout_str,
            buf->sample_rate, buf->nb_samples,
            checksum);
 
@@ -200,33 +96,16 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *buf)
         av_log(ctx, AV_LOG_INFO, "%08"PRIX32" ", s->plane_checksums[i]);
     av_log(ctx, AV_LOG_INFO, "]\n");
 
-    for (i = 0; i < buf->nb_side_data; i++) {
-        AVFrameSideData *sd = buf->side_data[i];
-
-        av_log(ctx, AV_LOG_INFO, "  side data - ");
-        switch (sd->type) {
-        case AV_FRAME_DATA_MATRIXENCODING: dump_matrixenc (ctx, sd); break;
-        case AV_FRAME_DATA_DOWNMIX_INFO:   dump_downmix   (ctx, sd); break;
-        case AV_FRAME_DATA_REPLAYGAIN:     dump_replaygain(ctx, sd); break;
-        default:                           dump_unknown   (ctx, sd); break;
-        }
-
-        av_log(ctx, AV_LOG_INFO, "\n");
-    }
-
-    s->frame++;
     return ff_filter_frame(inlink->dst->outputs[0], buf);
 }
 
 static const AVFilterPad inputs[] = {
     {
-        .name       = "default",
-        .type             = AVMEDIA_TYPE_AUDIO,
-        .get_audio_buffer = ff_null_get_audio_buffer,
-        .config_props     = config_input,
-        .filter_frame     = filter_frame,
+        .name         = "default",
+        .type         = AVMEDIA_TYPE_AUDIO,
+        .filter_frame = filter_frame,
     },
-    { NULL },
+    { NULL }
 };
 
 static const AVFilterPad outputs[] = {
@@ -234,7 +113,7 @@ static const AVFilterPad outputs[] = {
         .name = "default",
         .type = AVMEDIA_TYPE_AUDIO,
     },
-    { NULL },
+    { NULL }
 };
 
 AVFilter ff_af_ashowinfo = {
--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -2,23 +2,24 @@
  * MP3 demuxer
  * Copyright (c) 2003 Fabrice Bellard
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "libavutil/opt.h"
 #include "libavutil/avstring.h"
 #include "libavutil/intreadwrite.h"
 #include "libavutil/dict.h"
@@ -27,8 +28,6 @@
 #include "internal.h"
 #include "id3v2.h"
 #include "id3v1.h"
-#include "replaygain.h"
-
 #include "libavcodec/mpegaudiodecheader.h"
 
 #define XING_FLAG_FRAMES 0x01
@@ -37,8 +36,15 @@
 
 #define XING_TOC_COUNT 100
 
-typedef struct MP3DecContext {
+typedef struct {
+    AVClass *class;
+    int64_t filesize;
+    int64_t header_filesize;
     int xing_toc;
+    int start_pad;
+    int end_pad;
+    int usetoc;
+    int is_cbr;
 } MP3DecContext;
 
 /* mp3 read */
@@ -48,7 +54,7 @@ static int mp3_read_probe(AVProbeData *p)
     int max_frames, first_frames = 0;
     int fsize, frames, sample_rate;
     uint32_t header;
-    uint8_t *buf, *buf0, *buf2, *end;
+    const uint8_t *buf, *buf0, *buf2, *end;
     AVCodecContext avctx;
 
     buf0 = p->buf;
@@ -61,6 +67,8 @@ static int mp3_read_probe(AVProbeData *p)
 
     for(; buf < end; buf= buf2+1) {
         buf2 = buf;
+        if(ff_mpa_check_header(AV_RB32(buf2)))
+            continue;
 
         for(frames = 0; buf2 < end; frames++) {
             header = AV_RB32(buf2);
@@ -75,31 +83,13 @@ static int mp3_read_probe(AVProbeData *p)
     }
     // keep this in sync with ac3 probe, both need to avoid
     // issues with MPEG-files!
-    if (first_frames >= 4) return AVPROBE_SCORE_EXTENSION + 1;
-
-    if (max_frames) {
-        int pes = 0, i;
-        unsigned int code = -1;
-
-#define VIDEO_ID 0x000001e0
-#define AUDIO_ID 0x000001c0
-        /* do a search for mpegps headers to be able to properly bias
-         * towards mpegps if we detect this stream as both. */
-        for (i = 0; i<p->buf_size; i++) {
-            code = (code << 8) + p->buf[i];
-            if ((code & 0xffffff00) == 0x100) {
-                if     ((code & 0x1f0) == VIDEO_ID) pes++;
-                else if((code & 0x1e0) == AUDIO_ID) pes++;
-            }
-        }
-
-        if (pes)
-            max_frames = (max_frames + pes - 1) / pes;
-    }
-    if      (max_frames >  500) return AVPROBE_SCORE_EXTENSION;
-    else if (max_frames >= 4)   return AVPROBE_SCORE_EXTENSION / 2;
-    else if (max_frames >= 1)   return 1;
-    else                        return 0;
+    if   (first_frames>=4) return AVPROBE_SCORE_EXTENSION + 1;
+    else if(max_frames>200)return AVPROBE_SCORE_EXTENSION;
+    else if(max_frames>=4) return AVPROBE_SCORE_EXTENSION / 2;
+    else if(ff_id3v2_match(buf0, ID3v2_DEFAULT_MAGIC) && 2*ff_id3v2_tag_len(buf0) >= p->buf_size)
+                           return p->buf_size < PROBE_BUF_MAX ? AVPROBE_SCORE_EXTENSION / 4 : AVPROBE_SCORE_EXTENSION - 2;
+    else if(max_frames>=1) return 1;
+    else                   return 0;
 //mpegps_mp3_unrecognized_format.mpg has max_frames=3
 }
 
@@ -107,22 +97,24 @@ static void read_xing_toc(AVFormatContext *s, int64_t filesize, int64_t duration
 {
     int i;
     MP3DecContext *mp3 = s->priv_data;
+    int fill_index = mp3->usetoc && duration > 0;
 
     if (!filesize &&
         !(filesize = avio_size(s->pb))) {
         av_log(s, AV_LOG_WARNING, "Cannot determine file size, skipping TOC table.\n");
-        return;
+        fill_index = 0;
     }
 
     for (i = 0; i < XING_TOC_COUNT; i++) {
         uint8_t b = avio_r8(s->pb);
-
-        av_add_index_entry(s->streams[0],
+        if (fill_index)
+            av_add_index_entry(s->streams[0],
                            av_rescale(b, filesize, 256),
                            av_rescale(i, duration, XING_TOC_COUNT),
                            0, 0, AVINDEX_KEYFRAME);
     }
-    mp3->xing_toc = 1;
+    if (fill_index)
+        mp3->xing_toc = 1;
 }
 
 /**
@@ -130,10 +122,11 @@ static void read_xing_toc(AVFormatContext *s, int64_t filesize, int64_t duration
  */
 static int mp3_parse_vbr_tags(AVFormatContext *s, AVStream *st, int64_t base)
 {
+    MP3DecContext *mp3 = s->priv_data;
     uint32_t v, spf;
     unsigned frames = 0; /* Total number of frames in file */
     unsigned size = 0; /* Total number of bytes in the stream */
-    const int64_t xing_offtbl[2][2] = {{32, 17}, {17,9}};
+    static const int64_t xing_offtbl[2][2] = {{32, 17}, {17,9}};
     MPADecodeHeader c;
     int vbrtag_size = 0;
     int is_cbr;
@@ -159,9 +152,25 @@ static int mp3_parse_vbr_tags(AVFormatContext *s, AVStream *st, int64_t base)
             frames = avio_rb32(s->pb);
         if(v & XING_FLAG_SIZE)
             size = avio_rb32(s->pb);
-        if (v & XING_FLAG_TOC && frames)
+        if (v & XING_FLAG_TOC)
             read_xing_toc(s, size, av_rescale_q(frames, (AVRational){spf, c.sample_rate},
                                     st->time_base));
+        if(v & 8)
+            avio_skip(s->pb, 4);
+
+        v = avio_rb32(s->pb);
+        if(v == MKBETAG('L', 'A', 'M', 'E') || v == MKBETAG('L', 'a', 'v', 'f')) {
+            avio_skip(s->pb, 21-4);
+            v= avio_rb24(s->pb);
+            mp3->start_pad = v>>12;
+            mp3->  end_pad = v&4095;
+            st->skip_samples = mp3->start_pad + 528 + 1;
+            if (!st->start_time)
+                st->start_time = av_rescale_q(st->skip_samples,
+                                              (AVRational){1, c.sample_rate},
+                                              st->time_base);
+            av_log(s, AV_LOG_DEBUG, "pad %d %d\n", mp3->start_pad, mp3->  end_pad);
+        }
     }
 
     /* Check for VBRI tag (always 32 bytes after end of mpegaudio header) */
@@ -189,14 +198,17 @@ static int mp3_parse_vbr_tags(AVFormatContext *s, AVStream *st, int64_t base)
     if (size && frames && !is_cbr)
         st->codec->bit_rate = av_rescale(size, 8 * c.sample_rate, frames * (int64_t)spf);
 
+    mp3->is_cbr          = is_cbr;
+    mp3->header_filesize = size;
+
     return 0;
 }
 
 static int mp3_read_header(AVFormatContext *s)
 {
+    MP3DecContext *mp3 = s->priv_data;
     AVStream *st;
     int64_t off;
-    int ret;
 
     st = avformat_new_stream(s, NULL);
     if (!st)
@@ -204,23 +216,23 @@ static int mp3_read_header(AVFormatContext *s)
 
     st->codec->codec_type = AVMEDIA_TYPE_AUDIO;
     st->codec->codec_id = AV_CODEC_ID_MP3;
-    st->need_parsing = AVSTREAM_PARSE_FULL;
+    st->need_parsing = AVSTREAM_PARSE_FULL_RAW;
     st->start_time = 0;
 
     // lcm of all mp3 sample rates
     avpriv_set_pts_info(st, 64, 1, 14112000);
 
+    s->pb->maxsize = -1;
     off = avio_tell(s->pb);
 
     if (!av_dict_get(s->metadata, "", NULL, AV_DICT_IGNORE_SUFFIX))
         ff_id3v1_read(s);
 
+    if(s->pb->seekable)
+        mp3->filesize = avio_size(s->pb);
+
     if (mp3_parse_vbr_tags(s, st, off) < 0)
         avio_seek(s->pb, off, SEEK_SET);
-
-    ret = ff_replaygain_export(st, s->metadata);
-    if (ret < 0)
-        return ret;
 
     /* the parameters will be extracted from the compressed bitstream */
     return 0;
@@ -230,15 +242,26 @@ static int mp3_read_header(AVFormatContext *s)
 
 static int mp3_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    int ret;
+    MP3DecContext *mp3 = s->priv_data;
+    int ret, size;
+    int64_t pos;
 
-    ret = av_get_packet(s->pb, pkt, MP3_PACKET_SIZE);
-    if (ret < 0)
-        return ret;
+    size= MP3_PACKET_SIZE;
+    pos = avio_tell(s->pb);
+    if(mp3->filesize > ID3v1_TAG_SIZE && pos < mp3->filesize)
+        size= FFMIN(size, mp3->filesize - pos);
 
+    ret= av_get_packet(s->pb, pkt, size);
+    if (ret <= 0) {
+        if(ret<0)
+            return ret;
+        return AVERROR_EOF;
+    }
+
+    pkt->flags &= ~AV_PKT_FLAG_CORRUPT;
     pkt->stream_index = 0;
 
-    if (ret > ID3v1_TAG_SIZE &&
+    if (ret >= ID3v1_TAG_SIZE &&
         memcmp(&pkt->data[ret - ID3v1_TAG_SIZE], "TAG", 3) == 0)
         ret -= ID3v1_TAG_SIZE;
 
@@ -248,37 +271,94 @@ static int mp3_read_packet(AVFormatContext *s, AVPacket *pkt)
     return ret;
 }
 
+static int check(AVFormatContext *s, int64_t pos)
+{
+    int64_t ret = avio_seek(s->pb, pos, SEEK_SET);
+    unsigned header;
+    MPADecodeHeader sd;
+    if (ret < 0)
+        return ret;
+    header = avio_rb32(s->pb);
+    if (ff_mpa_check_header(header) < 0)
+        return -1;
+    if (avpriv_mpegaudio_decode_header(&sd, header) == 1)
+        return -1;
+    return sd.frame_size;
+}
+
 static int mp3_seek(AVFormatContext *s, int stream_index, int64_t timestamp,
                     int flags)
 {
     MP3DecContext *mp3 = s->priv_data;
-    AVIndexEntry *ie;
+    AVIndexEntry *ie, ie1;
     AVStream *st = s->streams[0];
     int64_t ret  = av_index_search_timestamp(st, timestamp, flags);
-    uint32_t header = 0;
+    int i, j;
+    int dir = (flags&AVSEEK_FLAG_BACKWARD) ? -1 : 1;
 
-    if (!mp3->xing_toc)
-        return AVERROR(ENOSYS);
+    if (mp3->is_cbr && st->duration > 0 && mp3->header_filesize > s->data_offset) {
+        int64_t filesize = avio_size(s->pb);
+        int64_t duration;
+        if (filesize <= s->data_offset)
+            filesize = mp3->header_filesize;
+        filesize -= s->data_offset;
+        duration = av_rescale(st->duration, filesize, mp3->header_filesize - s->data_offset);
+        ie = &ie1;
+        timestamp = av_clip64(timestamp, 0, duration);
+        ie->timestamp = timestamp;
+        ie->pos       = av_rescale(timestamp, filesize, duration) + s->data_offset;
+    } else if (mp3->xing_toc) {
+        if (ret < 0)
+            return ret;
 
-    if (ret < 0)
-        return ret;
+        ie = &st->index_entries[ret];
+    } else {
+        st->skip_samples = timestamp <= 0 ? mp3->start_pad + 528 + 1 : 0;
 
-    ie = &st->index_entries[ret];
+        return -1;
+    }
+
+    if (dir < 0)
+        avio_seek(s->pb, FFMAX(ie->pos - 4096, 0), SEEK_SET);
     ret = avio_seek(s->pb, ie->pos, SEEK_SET);
     if (ret < 0)
         return ret;
 
-    while (!s->pb->eof_reached) {
-        header = (header << 8) + avio_r8(s->pb);
-        if (ff_mpa_check_header(header) >= 0) {
-            ff_update_cur_dts(s, st, ie->timestamp);
-            ret = avio_seek(s->pb, -4, SEEK_CUR);
-            return (ret >= 0) ? 0 : ret;
+#define MIN_VALID 3
+    for(i=0; i<4096; i++) {
+        int64_t pos = ie->pos + i*dir;
+        for(j=0; j<MIN_VALID; j++) {
+            ret = check(s, pos);
+            if(ret < 0)
+                break;
+            pos += ret;
         }
+        if(j==MIN_VALID)
+            break;
     }
+    if(j!=MIN_VALID)
+        i=0;
 
-    return AVERROR_EOF;
+    ret = avio_seek(s->pb, ie->pos + i*dir, SEEK_SET);
+    if (ret < 0)
+        return ret;
+    ff_update_cur_dts(s, st, ie->timestamp);
+    st->skip_samples = ie->timestamp <= 0 ? mp3->start_pad + 528 + 1 : 0;
+    return 0;
 }
+
+static const AVOption options[] = {
+    { "usetoc", "use table of contents", offsetof(MP3DecContext, usetoc), AV_OPT_TYPE_INT, {.i64 = -1}, -1, 1, AV_OPT_FLAG_DECODING_PARAM},
+    { NULL },
+};
+
+static const AVClass demuxer_class = {
+    .class_name = "mp3",
+    .item_name  = av_default_item_name,
+    .option     = options,
+    .version    = LIBAVUTIL_VERSION_INT,
+    .category   = AV_CLASS_CATEGORY_DEMUXER,
+};
 
 AVInputFormat ff_mp3_demuxer = {
     .name           = "mp3",
@@ -290,4 +370,5 @@ AVInputFormat ff_mp3_demuxer = {
     .priv_data_size = sizeof(MP3DecContext),
     .flags          = AVFMT_GENERIC_INDEX,
     .extensions     = "mp2,mp3,m2a,mpa", /* XXX: use probe */
+    .priv_class     = &demuxer_class,
 };
