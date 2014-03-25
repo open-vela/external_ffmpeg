7346828ef3d1c37522b5e18c065188ab58942b07
openvela-robot <openvela-robot@xiaomi.com>
2014-03-25 12:38:31 +0800

    avconv: simplify exit_program() by using more local vars

--- libavcodec/flvdec.c
+++ libavcodec/flvdec.c
@@ -1,19 +1,19 @@
 /*
  * FLV decoding.
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -22,11 +22,12 @@
 #include "flv.h"
 #include "libavutil/imgutils.h"
 
-void ff_flv2_decode_ac_esc(GetBitContext *gb, int *level, int *run, int *last){
+void ff_flv2_decode_ac_esc(GetBitContext *gb, int *level, int *run, int *last)
+{
     int is11 = get_bits1(gb);
     *last = get_bits1(gb);
-    *run = get_bits(gb, 6);
-    if(is11){
+    *run  = get_bits(gb, 6);
+    if (is11) {
         *level = get_sbits(gb, 11);
     } else {
         *level = get_sbits(gb, 7);
@@ -40,52 +41,52 @@ int ff_flv_decode_picture_header(MpegEncContext *s)
     /* picture header */
     if (get_bits_long(&s->gb, 17) != 1) {
         av_log(s->avctx, AV_LOG_ERROR, "Bad picture start code\n");
-        return AVERROR_INVALIDDATA;
+        return -1;
     }
     format = get_bits(&s->gb, 5);
     if (format != 0 && format != 1) {
         av_log(s->avctx, AV_LOG_ERROR, "Bad picture format\n");
-        return AVERROR_INVALIDDATA;
+        return -1;
     }
-    s->h263_flv = format+1;
+    s->h263_flv       = format + 1;
     s->picture_number = get_bits(&s->gb, 8); /* picture timestamp */
-    format = get_bits(&s->gb, 3);
+    format            = get_bits(&s->gb, 3);
     switch (format) {
     case 0:
-        width = get_bits(&s->gb, 8);
+        width  = get_bits(&s->gb, 8);
         height = get_bits(&s->gb, 8);
         break;
     case 1:
-        width = get_bits(&s->gb, 16);
+        width  = get_bits(&s->gb, 16);
         height = get_bits(&s->gb, 16);
         break;
     case 2:
-        width = 352;
+        width  = 352;
         height = 288;
         break;
     case 3:
-        width = 176;
+        width  = 176;
         height = 144;
         break;
     case 4:
-        width = 128;
+        width  = 128;
         height = 96;
         break;
     case 5:
-        width = 320;
+        width  = 320;
         height = 240;
         break;
     case 6:
-        width = 160;
+        width  = 160;
         height = 120;
         break;
     default:
         width = height = 0;
         break;
     }
-    if(av_image_check_size(width, height, 0, s->avctx))
-        return AVERROR(EINVAL);
-    s->width = width;
+    if (av_image_check_size(width, height, 0, s->avctx))
+        return -1;
+    s->width  = width;
     s->height = height;
 
     s->pict_type = AV_PICTURE_TYPE_I + get_bits(&s->gb, 2);
@@ -94,29 +95,25 @@ int ff_flv_decode_picture_header(MpegEncContext *s)
         s->pict_type = AV_PICTURE_TYPE_P;
 
     skip_bits1(&s->gb); /* deblocking flag */
-    s->chroma_qscale= s->qscale = get_bits(&s->gb, 5);
+    s->chroma_qscale = s->qscale = get_bits(&s->gb, 5);
 
     s->h263_plus = 0;
 
-    s->unrestricted_mv = 1;
+    s->unrestricted_mv   = 1;
     s->h263_long_vectors = 0;
 
     /* PEI */
-    if (skip_1stop_8data_bits(&s->gb) < 0)
-        return AVERROR_INVALIDDATA;
+    while (get_bits1(&s->gb) != 0)
+        skip_bits(&s->gb, 8);
     s->f_code = 1;
 
-    if (s->ehc_mode)
-        s->avctx->sample_aspect_ratio= (AVRational){1,2};
-
-    if(s->avctx->debug & FF_DEBUG_PICT_INFO){
+    if (s->avctx->debug & FF_DEBUG_PICT_INFO) {
         av_log(s->avctx, AV_LOG_DEBUG, "%c esc_type:%d, qp:%d num:%d\n",
                s->droppable ? 'D' : av_get_picture_type_char(s->pict_type),
                s->h263_flv - 1, s->qscale, s->picture_number);
     }
 
-    s->y_dc_scale_table=
-    s->c_dc_scale_table= ff_mpeg1_dc_scale_table;
+    s->y_dc_scale_table = s->c_dc_scale_table = ff_mpeg1_dc_scale_table;
 
     return 0;
 }
@@ -131,7 +128,6 @@ AVCodec ff_flv_decoder = {
     .close          = ff_h263_decode_end,
     .decode         = ff_h263_decode_frame,
     .capabilities   = CODEC_CAP_DRAW_HORIZ_BAND | CODEC_CAP_DR1,
-    .max_lowres     = 3,
     .pix_fmts       = (const enum AVPixelFormat[]) {
         AV_PIX_FMT_YUV420P,
         AV_PIX_FMT_NONE
--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -2,20 +2,20 @@
  * Apple HTTP Live Streaming segmenter
  * Copyright (c) 2012, Luca Barbato
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -33,7 +33,7 @@
 
 typedef struct ListEntry {
     char  name[1024];
-    double   duration;
+    int   duration;
     struct ListEntry *next;
 } ListEntry;
 
@@ -51,7 +51,7 @@ typedef struct HLSContext {
     int has_video;
     int64_t start_pts;
     int64_t end_pts;
-    double duration;      // last segment duration computed so far, in seconds
+    int64_t duration;      // last segment duration computed so far, in seconds
     int nb_entries;
     ListEntry *list;
     ListEntry *end_list;
@@ -72,7 +72,6 @@ static int hls_mux_init(AVFormatContext *s)
 
     oc->oformat            = hls->oformat;
     oc->interrupt_callback = s->interrupt_callback;
-    av_dict_copy(&oc->metadata, s->metadata, 0);
 
     for (i = 0; i < s->nb_streams; i++) {
         AVStream *st;
@@ -85,7 +84,7 @@ static int hls_mux_init(AVFormatContext *s)
     return 0;
 }
 
-static int append_entry(HLSContext *hls, double duration)
+static int append_entry(HLSContext *hls, uint64_t duration)
 {
     ListEntry *en = av_malloc(sizeof(*en));
 
@@ -104,7 +103,7 @@ static int append_entry(HLSContext *hls, double duration)
 
     hls->end_list = en;
 
-    if (hls->size && hls->nb_entries >= hls->size) {
+    if (hls->nb_entries >= hls->size) {
         en = hls->list;
         hls->list = en->next;
         av_free(en);
@@ -133,7 +132,7 @@ static int hls_window(AVFormatContext *s, int last)
     ListEntry *en;
     int target_duration = 0;
     int ret = 0;
-    int64_t sequence = FFMAX(hls->start_sequence, hls->sequence - hls->nb_entries);
+    int64_t sequence = FFMAX(hls->start_sequence, hls->sequence - hls->size);
 
     if ((ret = avio_open2(&hls->pb, s->filename, AVIO_FLAG_WRITE,
                           &s->interrupt_callback, NULL)) < 0)
@@ -141,7 +140,7 @@ static int hls_window(AVFormatContext *s, int last)
 
     for (en = hls->list; en; en = en->next) {
         if (target_duration < en->duration)
-            target_duration = (int) floor(en->duration + 0.5);
+            target_duration = en->duration;
     }
 
     avio_printf(hls->pb, "#EXTM3U\n");
@@ -153,7 +152,7 @@ static int hls_window(AVFormatContext *s, int last)
            sequence);
 
     for (en = hls->list; en; en = en->next) {
-        avio_printf(hls->pb, "#EXTINF:%f,\n", en->duration);
+        avio_printf(hls->pb, "#EXTINF:%d,\n", en->duration);
         if (hls->baseurl)
             avio_printf(hls->pb, "%s", hls->baseurl);
         avio_printf(hls->pb, "%s\n", en->name);
@@ -174,10 +173,8 @@ static int hls_start(AVFormatContext *s)
     int err = 0;
 
     if (av_get_frame_filename(oc->filename, sizeof(oc->filename),
-                              c->basename, c->wrap ? c->sequence % c->wrap : c->sequence) < 0) {
-        av_log(oc, AV_LOG_ERROR, "Invalid segment filename template '%s'\n", c->basename);
+                              c->basename, c->wrap ? c->sequence % c->wrap : c->sequence) < 0)
         return AVERROR(EINVAL);
-    }
     c->number++;
 
     if ((err = avio_open2(&oc->pb, oc->filename, AVIO_FLAG_WRITE,
@@ -259,7 +256,6 @@ static int hls_write_packet(AVFormatContext *s, AVPacket *pkt)
     AVFormatContext *oc = hls->avf;
     AVStream *st = s->streams[pkt->stream_index];
     int64_t end_pts = hls->recording_time * hls->number;
-    int is_ref_pkt = 1;
     int ret, can_split = 1;
 
     if (hls->start_pts == AV_NOPTS_VALUE) {
@@ -270,14 +266,12 @@ static int hls_write_packet(AVFormatContext *s, AVPacket *pkt)
     if (hls->has_video) {
         can_split = st->codec->codec_type == AVMEDIA_TYPE_VIDEO &&
                     pkt->flags & AV_PKT_FLAG_KEY;
-        is_ref_pkt = st->codec->codec_type == AVMEDIA_TYPE_VIDEO;
     }
     if (pkt->pts == AV_NOPTS_VALUE)
-        is_ref_pkt = can_split = 0;
-
-    if (is_ref_pkt)
-        hls->duration = (double)(pkt->pts - hls->end_pts)
-                                   * st->time_base.num / st->time_base.den;
+        can_split = 0;
+    else
+        hls->duration = av_rescale(pkt->pts - hls->end_pts,
+                                   st->time_base.num, st->time_base.den);
 
     if (can_split && av_compare_ts(pkt->pts - hls->start_pts, st->time_base,
                                    end_pts, AV_TIME_BASE_Q) >= 0) {
@@ -327,10 +321,10 @@ static int hls_write_trailer(struct AVFormatContext *s)
 #define OFFSET(x) offsetof(HLSContext, x)
 #define E AV_OPT_FLAG_ENCODING_PARAM
 static const AVOption options[] = {
-    {"start_number",  "set first number in the sequence",        OFFSET(start_sequence),AV_OPT_TYPE_INT64,  {.i64 = 0},     0, INT64_MAX, E},
-    {"hls_time",      "set segment length in seconds",           OFFSET(time),    AV_OPT_TYPE_FLOAT,  {.dbl = 2},     0, FLT_MAX, E},
-    {"hls_list_size", "set maximum number of playlist entries",  OFFSET(size),    AV_OPT_TYPE_INT,    {.i64 = 5},     0, INT_MAX, E},
-    {"hls_wrap",      "set number after which the index wraps",  OFFSET(wrap),    AV_OPT_TYPE_INT,    {.i64 = 0},     0, INT_MAX, E},
+    {"start_number",  "first number in the sequence",            OFFSET(start_sequence),AV_OPT_TYPE_INT64,  {.i64 = 0},     0, INT64_MAX, E},
+    {"hls_time",      "segment length in seconds",               OFFSET(time),    AV_OPT_TYPE_FLOAT,  {.dbl = 2},     0, FLT_MAX, E},
+    {"hls_list_size", "maximum number of playlist entries",      OFFSET(size),    AV_OPT_TYPE_INT,    {.i64 = 5},     0, INT_MAX, E},
+    {"hls_wrap",      "number after which the index wraps",      OFFSET(wrap),    AV_OPT_TYPE_INT,    {.i64 = 0},     0, INT_MAX, E},
     {"hls_base_url",  "url to prepend to each playlist entry",   OFFSET(baseurl), AV_OPT_TYPE_STRING, {.str = NULL},  0, 0,       E},
     { NULL },
 };
