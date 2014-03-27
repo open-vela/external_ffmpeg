88748d23cc22684dc1ac3782376a0228569623ee
openvela-robot <openvela-robot@xiaomi.com>
2014-03-27 13:44:20 +0800

    dshow: show device name when outputting buffer overflow log message
    Signed-off-by: rogerdpack <rogerpack2005@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/paf.c
+++ libavformat/paf.c
@@ -2,31 +2,29 @@
  * Packed Animation File demuxer
  * Copyright (c) 2012 Paul B Mahol
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include "libavutil/channel_layout.h"
-
+#include "libavcodec/paf.h"
 #include "avformat.h"
 #include "internal.h"
 
 #define MAGIC "Packed Animation File V1.0\n(c) 1992-96 Amazing Studio\x0a\x1a"
-#define PAF_SOUND_SAMPLES     2205
-#define PAF_SOUND_FRAME_SIZE  ((256 + PAF_SOUND_SAMPLES) * 2)
 
 typedef struct {
     uint32_t buffer_size;
@@ -46,13 +44,13 @@ typedef struct {
     uint32_t *blocks_offset_table;
 
     uint8_t  *video_frame;
-    int video_size;
+    int      video_size;
 
     uint8_t  *audio_frame;
     uint8_t  *temp_audio_frame;
-    int audio_size;
+    int      audio_size;
 
-    int got_audio;
+    int      got_audio;
 } PAFDemuxContext;
 
 static int read_probe(AVProbeData *p)
@@ -89,10 +87,10 @@ static void read_table(AVFormatContext *s, uint32_t *table, uint32_t count)
 
 static int read_header(AVFormatContext *s)
 {
-    PAFDemuxContext *p  = s->priv_data;
+    PAFDemuxContext *p = s->priv_data;
     AVIOContext     *pb = s->pb;
     AVStream        *ast, *vst;
-    int ret = 0;
+    int             ret = 0;
 
     avio_skip(pb, 132);
 
@@ -103,13 +101,11 @@ static int read_header(AVFormatContext *s)
     vst->start_time = 0;
     vst->nb_frames  =
     vst->duration   =
-    p->nb_frames    = avio_rl32(pb);
+    p->nb_frames = avio_rl32(pb);
     avio_skip(pb, 4);
-
     vst->codec->width  = avio_rl32(pb);
     vst->codec->height = avio_rl32(pb);
     avio_skip(pb, 4);
-
     vst->codec->codec_type = AVMEDIA_TYPE_VIDEO;
     vst->codec->codec_tag  = 0;
     vst->codec->codec_id   = AV_CODEC_ID_PAF_VIDEO;
@@ -119,13 +115,13 @@ static int read_header(AVFormatContext *s)
     if (!ast)
         return AVERROR(ENOMEM);
 
-    ast->start_time            = 0;
-    ast->codec->codec_type     = AVMEDIA_TYPE_AUDIO;
-    ast->codec->codec_tag      = 0;
-    ast->codec->codec_id       = AV_CODEC_ID_PAF_AUDIO;
-    ast->codec->channels       = 2;
+    ast->start_time         = 0;
+    ast->codec->codec_type  = AVMEDIA_TYPE_AUDIO;
+    ast->codec->codec_tag   = 0;
+    ast->codec->codec_id    = AV_CODEC_ID_PAF_AUDIO;
+    ast->codec->channels    = 2;
     ast->codec->channel_layout = AV_CH_LAYOUT_STEREO;
-    ast->codec->sample_rate    = 22050;
+    ast->codec->sample_rate = 22050;
     avpriv_set_pts_info(ast, 64, 1, 22050);
 
     p->buffer_size    = avio_rl32(pb);
@@ -147,19 +143,16 @@ static int read_header(AVFormatContext *s)
         p->frame_blks     > INT_MAX / sizeof(uint32_t))
         return AVERROR_INVALIDDATA;
 
-    p->blocks_count_table  = av_mallocz(p->nb_frames *
-                                        sizeof(*p->blocks_count_table));
-    p->frames_offset_table = av_mallocz(p->nb_frames *
-                                        sizeof(*p->frames_offset_table));
-    p->blocks_offset_table = av_mallocz(p->frame_blks *
-                                        sizeof(*p->blocks_offset_table));
+    p->blocks_count_table  = av_mallocz(p->nb_frames  * sizeof(uint32_t));
+    p->frames_offset_table = av_mallocz(p->nb_frames  * sizeof(uint32_t));
+    p->blocks_offset_table = av_mallocz(p->frame_blks * sizeof(uint32_t));
 
-    p->video_size  = p->max_video_blks * p->buffer_size;
-    p->video_frame = av_mallocz(p->video_size);
+    p->video_size          = p->max_video_blks * p->buffer_size;
+    p->video_frame         = av_mallocz(p->video_size);
 
-    p->audio_size       = p->max_audio_blks * p->buffer_size;
-    p->audio_frame      = av_mallocz(p->audio_size);
-    p->temp_audio_frame = av_mallocz(p->audio_size);
+    p->audio_size          = p->max_audio_blks * p->buffer_size;
+    p->audio_frame         = av_mallocz(p->audio_size);
+    p->temp_audio_frame    = av_mallocz(p->audio_size);
 
     if (!p->blocks_count_table  ||
         !p->frames_offset_table ||
@@ -193,12 +186,15 @@ fail:
 
 static int read_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    PAFDemuxContext *p  = s->priv_data;
+    PAFDemuxContext *p = s->priv_data;
     AVIOContext     *pb = s->pb;
-    uint32_t count, offset;
-    int size, i;
+    uint32_t        count, offset;
+    int             size, i;
 
-    if (p->current_frame >= p->nb_frames || pb->eof_reached)
+    if (p->current_frame >= p->nb_frames)
+        return AVERROR_EOF;
+
+    if (url_feof(pb))
         return AVERROR_EOF;
 
     if (p->got_audio) {
@@ -213,8 +209,7 @@ static int read_packet(AVFormatContext *s, AVPacket *pkt)
         return pkt->size;
     }
 
-    count = (p->current_frame == 0) ? p->preload_count
-                                    : p->blocks_count_table[p->current_frame - 1];
+    count = (p->current_frame == 0) ? p->preload_count : p->blocks_count_table[p->current_frame - 1];
     for (i = 0; i < count; i++) {
         if (p->current_frame_block >= p->frame_blks)
             return AVERROR_INVALIDDATA;
@@ -250,7 +245,7 @@ static int read_packet(AVFormatContext *s, AVPacket *pkt)
     pkt->duration     = 1;
     memcpy(pkt->data, p->video_frame + p->frames_offset_table[p->current_frame], size);
     if (pkt->data[0] & 0x20)
-        pkt->flags |= AV_PKT_FLAG_KEY;
+        pkt->flags   |= AV_PKT_FLAG_KEY;
     p->current_frame++;
 
     return pkt->size;
