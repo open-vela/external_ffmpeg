8c0caa0adbe0000a680089085f08c6e11a538d46
openvela-robot <openvela-robot@xiaomi.com>
2014-04-11 14:04:10 +0800

    Merge commit '89ef08c992c484a46711b1a68a988303679c288e'
    * commit '89ef08c992c484a46711b1a68a988303679c288e':
      DNxHD: K&R formatting cosmetics
    
    Conflicts:
    	libavcodec/dnxhd_parser.c
    	libavcodec/dnxhddec.c
    	libavcodec/dnxhdenc.c
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mvi.c
+++ libavformat/mvi.c
@@ -2,20 +2,20 @@
  * Motion Pixels MVI Demuxer
  * Copyright (c) 2008 Gregory Montoir (cyx@users.sourceforge.net)
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -54,8 +54,8 @@ static int read_header(AVFormatContext *s)
     if (!vst)
         return AVERROR(ENOMEM);
 
-    vst->codec->extradata_size = 2;
-    vst->codec->extradata = av_mallocz(2 + FF_INPUT_BUFFER_PADDING_SIZE);
+    if (ff_alloc_extradata(vst->codec, 2))
+        return AVERROR(ENOMEM);
 
     version                  = avio_r8(pb);
     vst->codec->extradata[0] = avio_r8(pb);
