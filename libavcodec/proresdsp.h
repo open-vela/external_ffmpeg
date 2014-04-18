475afca6ea80ea51a9b2060b3408698cda685803
openvela-robot <openvela-robot@xiaomi.com>
2014-04-18 23:40:59 +0800

    configure: Handle armcc 5.0
    The first line of the "armcc --vsn" output in one version is
    "Product: ARM Compiler 5.04".
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/proresdsp.h
+++ libavcodec/proresdsp.h
@@ -3,20 +3,20 @@
  *
  * Copyright (c) 2010-2011 Maxim Poliakovski
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -24,7 +24,6 @@
 #define AVCODEC_PRORESDSP_H
 
 #include <stdint.h>
-#include "dsputil.h"
 
 #define PRORES_BITS_PER_SAMPLE 10 ///< output precision of prores decoder
 
@@ -34,8 +33,8 @@ typedef struct ProresDSPContext {
     void (* idct_put) (uint16_t *out, int linesize, int16_t *block, const int16_t *qmat);
 } ProresDSPContext;
 
-void ff_proresdsp_init(ProresDSPContext *dsp, AVCodecContext *avctx);
+void ff_proresdsp_init(ProresDSPContext *dsp);
 
-void ff_proresdsp_init_x86(ProresDSPContext *dsp, AVCodecContext *avctx);
+void ff_proresdsp_init_x86(ProresDSPContext *dsp);
 
 #endif /* AVCODEC_PRORESDSP_H */
