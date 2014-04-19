47e399a403c15813031e2b7a26e7cacb2591c9c0
openvela-robot <openvela-robot@xiaomi.com>
2014-04-19 22:43:16 +0800

    Merge commit '911fa05b514e1be009e00b79d7004b93717c023b'
    * commit '911fa05b514e1be009e00b79d7004b93717c023b':
      mvc: Specify the pixel format for the mv-mvc* tests
    
    Conflicts:
    	tests/fate/video.mak
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/mvdec.c
+++ libavformat/mvdec.c
@@ -2,20 +2,20 @@
  * Silicon Graphics Movie demuxer
  * Copyright (c) 2012 Peter Ross
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -261,7 +261,7 @@ static int mv_read_header(AVFormatContext *avctx)
 {
     MvContext *mv = avctx->priv_data;
     AVIOContext *pb = avctx->pb;
-    AVStream *ast = NULL, *vst = NULL;
+    AVStream *ast = NULL, *vst = NULL; //initialization to suppress warning
     int version, i;
 
     avio_skip(pb, 4);
