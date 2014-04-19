905099ec8a6921a7c8eba2fccdf1882f83473aaa
openvela-robot <openvela-robot@xiaomi.com>
2014-04-19 18:51:38 +0800

    mvc: Specify the pixel format for the mv-mvc* tests
    Also set the RGBA pixel format correctly as the native endian format,
    which is what it returns.
    
    This fixes the tests on big endian.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/mvcdec.c
+++ libavcodec/mvcdec.c
@@ -2,20 +2,20 @@
  * Silicon Graphics Motion Video Compressor 1 & 2 decoder
  * Copyright (c) 2012 Peter Ross
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -52,7 +52,7 @@ static av_cold int mvc_decode_init(AVCodecContext *avctx)
         return ret;
 
     avctx->pix_fmt = (avctx->codec_id == AV_CODEC_ID_MVC1) ? AV_PIX_FMT_RGB555
-                                                           : AV_PIX_FMT_BGRA;
+                                                           : AV_PIX_FMT_RGB32;
     s->frame       = av_frame_alloc();
     if (!s->frame)
         return AVERROR(ENOMEM);
