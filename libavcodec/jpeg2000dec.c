90634e631bbf096453bc88528ef5f8f7f0796e99
openvela-robot <openvela-robot@xiaomi.com>
2014-04-24 21:27:09 +0800

    Merge commit '374fdc8c071dcd96422378b0a1a0d453336d8a01'
    * commit '374fdc8c071dcd96422378b0a1a0d453336d8a01':
      flv: Improve log messages
    
    Conflicts:
    	libavformat/flvdec.c
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -782,7 +782,7 @@ static int jpeg2000_decode_packet(Jpeg2000DecoderContext *s,
                 return ret;
             if (ret > sizeof(cblk->data)) {
                 avpriv_request_sample(s->avctx,
-                                      "Block with lengthinc greater than %"SIZE_SPECIFIER"",
+                                      "Block with lengthinc greater than %zu",
                                       sizeof(cblk->data));
                 return AVERROR_PATCHWELCOME;
             }
--- libavutil/internal.h
+++ libavutil/internal.h
@@ -93,13 +93,6 @@
 // to be forced to tokenize __VA_ARGS__
 #define E1(x) x
 
-/* Check if the hard coded offset of a struct member still matches reality.
- * Induce a compilation failure if not.
- */
-#define AV_CHECK_OFFSET(s, m, o) struct check_##o {    \
-        int x_##o[offsetof(s, m) == o? 1: -1];         \
-    }
-
 #define LOCAL_ALIGNED_A(a, t, v, s, o, ...)             \
     uint8_t la_##v[sizeof(t s o) + (a)];                \
     t (*v) o = (void *)FFALIGN((uintptr_t)la_##v, a)
@@ -238,11 +231,6 @@ void avpriv_request_sample(void *avc,
 
 #if HAVE_LIBC_MSVCRT
 #define avpriv_open ff_open
-#define PTRDIFF_SPECIFIER "Id"
-#define SIZE_SPECIFIER "Iu"
-#else
-#define PTRDIFF_SPECIFIER "td"
-#define SIZE_SPECIFIER "zu"
 #endif
 
 /**
