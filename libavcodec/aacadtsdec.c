120a5b9900f9fb38766d2483f96d00b8219d6354
openvela-robot <openvela-robot@xiaomi.com>
2014-05-01 14:14:10 +0800

    Merge commit '1c0210c7981b6a61043d9171f506b435ff5a1f5e'
    * commit '1c0210c7981b6a61043d9171f506b435ff5a1f5e':
      lavfi: add Bauer stereo-to-binaural audio filter
    
    Conflicts:
    	Changelog
    	configure
    	doc/filters.texi
    	libavfilter/Makefile
    	libavfilter/allfilters.c
    	libavfilter/version.h
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/aacadtsdec.c
+++ libavcodec/aacadtsdec.c
@@ -4,20 +4,20 @@
  * Copyright (c) 2003 Michael Niedermayer
  * Copyright (c) 2009 Alex Converse
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -31,7 +31,7 @@ int avpriv_aac_parse_header(GetBitContext *gbc, AACADTSHeaderInfo *hdr)
     int size, rdb, ch, sr;
     int aot, crc_abs;
 
-    if (get_bits(gbc, 12) != 0xfff)
+    if(get_bits(gbc, 12) != 0xfff)
         return AAC_AC3_PARSE_ERROR_SYNC;
 
     skip_bits1(gbc);             /* id */
@@ -39,10 +39,10 @@ int avpriv_aac_parse_header(GetBitContext *gbc, AACADTSHeaderInfo *hdr)
     crc_abs = get_bits1(gbc);    /* protection_absent */
     aot     = get_bits(gbc, 2);  /* profile_objecttype */
     sr      = get_bits(gbc, 4);  /* sample_frequency_index */
-    if (!avpriv_mpeg4audio_sample_rates[sr])
+    if(!avpriv_mpeg4audio_sample_rates[sr])
         return AAC_AC3_PARSE_ERROR_SAMPLE_RATE;
     skip_bits1(gbc);             /* private_bit */
-    ch = get_bits(gbc, 3);       /* channel_configuration */
+    ch      = get_bits(gbc, 3);  /* channel_configuration */
 
     skip_bits1(gbc);             /* original/copy */
     skip_bits1(gbc);             /* home */
@@ -50,8 +50,8 @@ int avpriv_aac_parse_header(GetBitContext *gbc, AACADTSHeaderInfo *hdr)
     /* adts_variable_header */
     skip_bits1(gbc);             /* copyright_identification_bit */
     skip_bits1(gbc);             /* copyright_identification_start */
-    size = get_bits(gbc, 13);    /* aac_frame_length */
-    if (size < AAC_ADTS_HEADER_SIZE)
+    size    = get_bits(gbc, 13); /* aac_frame_length */
+    if(size < AAC_ADTS_HEADER_SIZE)
         return AAC_AC3_PARSE_ERROR_FRAME_SIZE;
 
     skip_bits(gbc, 11);          /* adts_buffer_fullness */
