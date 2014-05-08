118410557cbceef4081bc5a477863ec33c1b2b8a
openvela-robot <openvela-robot@xiaomi.com>
2014-05-08 18:19:38 +0800

    avformat/rtmpproto: add FFMIN() to av_strlcpy()
    Suggested-by: Reimar Döffinger <Reimar.Doeffinger@gmx.de>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/rtmpproto.c
+++ libavformat/rtmpproto.c
@@ -2487,8 +2487,7 @@ reconnect:
         // Strip any further query parameters from fname
         amp = strchr(fname, '&');
         if (amp) {
-            av_strlcpy(fname_buffer, fname, FFMIN(amp - fname + 1,
-                                                  sizeof(fname_buffer)));
+            av_strlcpy(fname_buffer, fname, FFMIN(amp - fname + 1, sizeof(fname_buffer)));
             fname = fname_buffer;
         }
     } else if (!strncmp(path, "/ondemand/", 10)) {
