779ca1f936829d94b92f0e67881bf1dc7983c21b
openvela-robot <openvela-robot@xiaomi.com>
2014-06-10 17:41:57 +0800

    avconv: make -shortest work with streamcopy
    CC: libav-stable@libav.org

--- libavutil/ppc/intreadwrite.h
+++ libavutil/ppc/intreadwrite.h
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2008 Mans Rullgard <mans@mansr.com>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -26,60 +26,46 @@
 
 #if HAVE_XFORM_ASM
 
-#if HAVE_BIGENDIAN
-#define AV_RL16 AV_RX16
-#define AV_WL16 AV_WX16
-#define AV_RL32 AV_RX32
-#define AV_WL32 AV_WX32
-#else
-#define AV_RB16 AV_RX16
-#define AV_WB16 AV_WX16
-#define AV_RB32 AV_RX32
-#define AV_WB32 AV_WX32
-#endif
-
-static av_always_inline uint16_t AV_RX16(const void *p)
+#define AV_RL16 AV_RL16
+static av_always_inline uint16_t AV_RL16(const void *p)
 {
     uint16_t v;
     __asm__ ("lhbrx   %0, %y1" : "=r"(v) : "Z"(*(const uint16_t*)p));
     return v;
 }
 
-static av_always_inline void AV_WX16(void *p, uint16_t v)
+#define AV_WL16 AV_WL16
+static av_always_inline void AV_WL16(void *p, uint16_t v)
 {
     __asm__ ("sthbrx  %1, %y0" : "=Z"(*(uint16_t*)p) : "r"(v));
 }
 
-static av_always_inline uint32_t AV_RX32(const void *p)
+#define AV_RL32 AV_RL32
+static av_always_inline uint32_t AV_RL32(const void *p)
 {
     uint32_t v;
     __asm__ ("lwbrx   %0, %y1" : "=r"(v) : "Z"(*(const uint32_t*)p));
     return v;
 }
 
-static av_always_inline void AV_WX32(void *p, uint32_t v)
+#define AV_WL32 AV_WL32
+static av_always_inline void AV_WL32(void *p, uint32_t v)
 {
     __asm__ ("stwbrx  %1, %y0" : "=Z"(*(uint32_t*)p) : "r"(v));
 }
 
 #if HAVE_LDBRX
 
-#if HAVE_BIGENDIAN
-#define AV_RL64 AV_RX64
-#define AV_WL64 AV_WX64
-#else
-#define AV_RB64 AV_RX64
-#define AV_WB64 AV_WX64
-#endif
-
-static av_always_inline uint64_t AV_RX64(const void *p)
+#define AV_RL64 AV_RL64
+static av_always_inline uint64_t AV_RL64(const void *p)
 {
     uint64_t v;
     __asm__ ("ldbrx   %0, %y1" : "=r"(v) : "Z"(*(const uint64_t*)p));
     return v;
 }
 
-static av_always_inline void AV_WX64(void *p, uint64_t v)
+#define AV_WL64 AV_WL64
+static av_always_inline void AV_WL64(void *p, uint64_t v)
 {
     __asm__ ("stdbrx  %1, %y0" : "=Z"(*(uint64_t*)p) : "r"(v));
 }
@@ -116,9 +102,7 @@ static av_always_inline void AV_WL64(void *p, uint64_t v)
  * default, so we override it here.
  */
 
-#if HAVE_BIGENDIAN
 #define AV_RB64(p) (*(const uint64_t *)(p))
 #define AV_WB64(p, v) (*(uint64_t *)(p) = (v))
-#endif
 
 #endif /* AVUTIL_PPC_INTREADWRITE_H */
