d71dfa69102c9d14759906a3af45ffa9539107a5
openvela-robot <openvela-robot@xiaomi.com>
2014-06-23 18:50:34 +0800

    apichanges: fill in changes for lavu 51.19 and 51.20

--- libavcodec/x86/huffyuvdsp.h
+++ libavcodec/x86/huffyuvdsp.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -21,8 +21,10 @@
 
 #include <stdint.h>
 
+void ff_add_bytes_mmx(uint8_t *dst, uint8_t *src, int w);
+
 void ff_add_hfyu_median_pred_cmov(uint8_t *dst, const uint8_t *top,
-                                  const uint8_t *diff, intptr_t w,
+                                  const uint8_t *diff, int w,
                                   int *left, int *left_top);
 
 #endif /* AVCODEC_X86_HUFFYUVDSP_H */
