e34afb564f71d1f2db15be1195be64f4b7df48d3
openvela-robot <openvela-robot@xiaomi.com>
2014-07-05 03:41:35 +0800

    Merge commit 'e0bfe34ea8ccf333ec5b17961fd58eb575e74f8b'
    * commit 'e0bfe34ea8ccf333ec5b17961fd58eb575e74f8b':
      libfdk-aacdec: Reduce the default decoder delay by one frame
    
    Conflicts:
    	libavcodec/version.h
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/bmp.c
+++ libavcodec/bmp.c
@@ -128,7 +128,6 @@ static int bmp_decode_frame(AVCodecContext *avctx,
         rgb[0] = bytestream_get_le32(&buf);
         rgb[1] = bytestream_get_le32(&buf);
         rgb[2] = bytestream_get_le32(&buf);
-        if (ihsize > 40)
         alpha = bytestream_get_le32(&buf);
     }
 
@@ -248,8 +247,6 @@ static int bmp_decode_frame(AVCodecContext *avctx,
             } else if (t) {
                 colors = t;
             }
-        } else {
-            colors = FFMIN(256, (hsize-ihsize-14) / 3);
         }
         buf = buf0 + 14 + ihsize; //palette location
         // OS/2 bitmap, 3 bytes per palette entry
