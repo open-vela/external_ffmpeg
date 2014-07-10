aec2f537a7acc69af0972df302916b4a366f10e6
openvela-robot <openvela-robot@xiaomi.com>
2014-07-11 00:12:31 +0800

    armv6: Accelerate ff_imdct_half for general case (mdct_bits != 6)
    The previous implementation targeted DTS Coherent Acoustics, which only
    requires mdct_bits == 6. This relatively small size lent itself to
    unrolling the loops a small number of times, and encoding offsets
    calculated at assembly time within the load/store instructions of each
    iteration.
    
    In the more general case (codecs such as AAC and AC3) much larger arrays
    are used - mdct_bits == [8, 9, 11]. The old method does not scale for
    these cases, so more integer registers are used with non-unrolled versions
    of the loops (and with some stack spillage). The postrotation filter loop
    is still unrolled by a factor of 2 to permit the double-buffering of some
    VFP registers to facilitate overlap of neighbouring iterations.
    
    I benchmarked the result by measuring the number of gperftools samples
    that hit anywhere in the AAC decoder (starting from aac_decode_frame())
    or specifically in ff_imdct_half_c / ff_imdct_half_vfp, for the same
    example AAC stream:
    
                      Before          After
                      Mean   StdDev   Mean   StdDev  Confidence  Change
    aac_decode_frame  2368.1 35.8     2117.2 35.3    100.0%      +11.8%
    ff_imdct_half_*   457.5  22.4     251.2  16.2    100.0%      +82.1%
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/atomic.c
+++ libavutil/atomic.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2012 Ronald S. Bultje <rsbultje@gmail.com>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -109,7 +109,7 @@ void *avpriv_atomic_ptr_cas(void * volatile *ptr, void *oldval, void *newval)
 #endif /* !HAVE_ATOMICS_NATIVE */
 
 #ifdef TEST
-#include "avassert.h"
+#include <assert.h>
 
 int main(void)
 {
@@ -117,10 +117,10 @@ int main(void)
     int res;
 
     res = avpriv_atomic_int_add_and_fetch(&val, 1);
-    av_assert0(res == 2);
+    assert(res == 2);
     avpriv_atomic_int_set(&val, 3);
     res = avpriv_atomic_int_get(&val);
-    av_assert0(res == 3);
+    assert(res == 3);
 
     return 0;
 }
