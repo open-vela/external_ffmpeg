b5b7938541784793b147d7791676b33499b86b52
openvela-robot <openvela-robot@xiaomi.com>
2014-07-22 10:57:34 +0800

    x86: hevcdsp: align
    Reviewed-by: Mickaël Raulet <mraulet@gmail.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/fft-test.c
+++ libavcodec/fft-test.c
@@ -1,20 +1,20 @@
 /*
  * (c) 2002 Fabrice Bellard
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -23,8 +23,14 @@
  * FFT and MDCT tests.
  */
 
-#include "config.h"
-
+#include "libavutil/cpu.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/lfg.h"
+#include "libavutil/log.h"
+#include "libavutil/time.h"
+#include "fft.h"
+#include "dct.h"
+#include "rdft.h"
 #include <math.h>
 #if HAVE_UNISTD_H
 #include <unistd.h>
@@ -33,57 +39,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "libavutil/cpu.h"
-#include "libavutil/lfg.h"
-#include "libavutil/log.h"
-#include "libavutil/mathematics.h"
-#include "libavutil/time.h"
-
-#include "dct.h"
-#include "fft.h"
-#include "rdft.h"
-
 /* reference fft */
 
-#define MUL16(a, b) ((a) * (b))
+#define MUL16(a,b) ((a) * (b))
 
-#define CMAC(pre, pim, are, aim, bre, bim)          \
-    {                                               \
-        pre += (MUL16(are, bre) - MUL16(aim, bim)); \
-        pim += (MUL16(are, bim) + MUL16(bre, aim)); \
-    }
+#define CMAC(pre, pim, are, aim, bre, bim) \
+{\
+   pre += (MUL16(are, bre) - MUL16(aim, bim));\
+   pim += (MUL16(are, bim) + MUL16(bre, aim));\
+}
 
 #if FFT_FLOAT
-#define RANGE 1.0
-#define REF_SCALE(x, bits)  (x)
-#define FMT "%10.6f"
+#   define RANGE 1.0
+#   define REF_SCALE(x, bits)  (x)
+#   define FMT "%10.6f"
+#elif FFT_FIXED_32
+#   define RANGE 8388608
+#   define REF_SCALE(x, bits) (x)
+#   define FMT "%6d"
 #else
-#define RANGE 16384
-#define REF_SCALE(x, bits) ((x) / (1 << (bits)))
-#define FMT "%6d"
+#   define RANGE 16384
+#   define REF_SCALE(x, bits) ((x) / (1<<(bits)))
+#   define FMT "%6d"
 #endif
 
 static struct {
     float re, im;
 } *exptab;
 
-static int fft_ref_init(int nbits, int inverse)
+static void fft_ref_init(int nbits, int inverse)
 {
     int i, n = 1 << nbits;
 
-    exptab = av_malloc((n / 2) * sizeof(*exptab));
-    if (!exptab)
-        return AVERROR(ENOMEM);
+    exptab = av_malloc_array((n / 2), sizeof(*exptab));
 
-    for (i = 0; i < (n / 2); i++) {
-        double alpha = 2 * M_PI * (float) i / (float) n;
+    for (i = 0; i < (n/2); i++) {
+        double alpha = 2 * M_PI * (float)i / (float)n;
         double c1 = cos(alpha), s1 = sin(alpha);
         if (!inverse)
             s1 = -s1;
         exptab[i].re = c1;
         exptab[i].im = s1;
     }
-    return 0;
 }
 
 static void fft_ref(FFTComplex *tabr, FFTComplex *tab, int nbits)
@@ -120,9 +117,9 @@ static void imdct_ref(FFTSample *out, FFTSample *in, int nbits)
 
     for (i = 0; i < n; i++) {
         double sum = 0;
-        for (k = 0; k < n / 2; k++) {
+        for (k = 0; k < n/2; k++) {
             int a = (2 * i + 1 + (n / 2)) * (2 * k + 1);
-            double f = cos(M_PI * a / (double) (2 * n));
+            double f = cos(M_PI * a / (double)(2 * n));
             sum += f * in[k];
         }
         out[i] = REF_SCALE(-sum, nbits - 2);
@@ -135,7 +132,7 @@ static void mdct_ref(FFTSample *output, FFTSample *input, int nbits)
     int i, k, n = 1 << nbits;
 
     /* do it by hand */
-    for (k = 0; k < n / 2; k++) {
+    for (k = 0; k < n/2; k++) {
         double s = 0;
         for (i = 0; i < n; i++) {
             double a = (2 * M_PI * (2 * i + 1 + n / 2) * (2 * k + 1) / (4 * n));
@@ -148,7 +145,7 @@ static void mdct_ref(FFTSample *output, FFTSample *input, int nbits)
 
 #if FFT_FLOAT
 #if CONFIG_DCT
-static void idct_ref(float *output, float *input, int nbits)
+static void idct_ref(FFTSample *output, FFTSample *input, int nbits)
 {
     int i, k, n = 1 << nbits;
 
@@ -162,8 +159,7 @@ static void idct_ref(float *output, float *input, int nbits)
         output[i] = 2 * s / n;
     }
 }
-
-static void dct_ref(float *output, float *input, int nbits)
+static void dct_ref(FFTSample *output, FFTSample *input, int nbits)
 {
     int i, k, n = 1 << nbits;
 
@@ -180,9 +176,10 @@ static void dct_ref(float *output, float *input, int nbits)
 #endif /* CONFIG_DCT */
 #endif /* FFT_FLOAT */
 
+
 static FFTSample frandom(AVLFG *prng)
 {
-    return (int16_t) av_lfg_get(prng) / 32768.0 * RANGE;
+    return (int16_t)av_lfg_get(prng) / 32768.0 * RANGE;
 }
 
 static int check_diff(FFTSample *tab1, FFTSample *tab2, int n, double scale)
@@ -197,18 +194,17 @@ static int check_diff(FFTSample *tab1, FFTSample *tab2, int n, double scale)
                    i, tab1[i], tab2[i]);
             err = 1;
         }
-        error += e * e;
-        if (e > max)
-            max = e;
+        error+= e*e;
+        if(e>max) max= e;
     }
-    av_log(NULL, AV_LOG_INFO, "max:%f e:%g\n", max, sqrt(error) / n);
+    av_log(NULL, AV_LOG_INFO, "max:%f e:%g\n", max, sqrt(error/n));
     return err;
 }
 
+
 static void help(void)
 {
-    av_log(NULL, AV_LOG_INFO,
-           "usage: fft-test [-h] [-s] [-i] [-n b]\n"
+    av_log(NULL, AV_LOG_INFO,"usage: fft-test [-h] [-s] [-i] [-n b]\n"
            "-h     print this help\n"
            "-s     speed test\n"
            "-m     (I)MDCT test\n"
@@ -216,7 +212,8 @@ static void help(void)
            "-r     (I)RDFT test\n"
            "-i     inverse transform test\n"
            "-n b   set the transform size to 2^b\n"
-           "-f x   set scale factor for output data of (I)MDCT to x\n");
+           "-f x   set scale factor for output data of (I)MDCT to x\n"
+           );
 }
 
 enum tf_transform {
@@ -248,11 +245,11 @@ int main(int argc, char **argv)
 
     av_lfg_init(&prng, 1);
 
-    for (;;) {
+    for(;;) {
         int c = getopt(argc, argv, "hsimrdn:f:c:");
         if (c == -1)
             break;
-        switch (c) {
+        switch(c) {
         case 'h':
             help();
             return 1;
@@ -279,71 +276,68 @@ int main(int argc, char **argv)
             break;
         case 'c':
         {
-            int cpuflags = av_parse_cpu_flags(optarg);
-            if (cpuflags < 0)
+            int cpuflags = av_get_cpu_flags();
+
+            if (av_parse_cpu_caps(&cpuflags, optarg) < 0)
                 return 1;
-            av_set_cpu_flags_mask(cpuflags);
+
+            av_force_cpu_flags(cpuflags);
             break;
         }
         }
     }
 
     fft_size = 1 << fft_nbits;
-    tab      = av_malloc(fft_size * sizeof(FFTComplex));
-    tab1     = av_malloc(fft_size * sizeof(FFTComplex));
-    tab_ref  = av_malloc(fft_size * sizeof(FFTComplex));
-    tab2     = av_malloc(fft_size * sizeof(FFTSample));
-
-    if (!(tab && tab1 && tab_ref && tab2))
-        goto cleanup;
+    tab = av_malloc_array(fft_size, sizeof(FFTComplex));
+    tab1 = av_malloc_array(fft_size, sizeof(FFTComplex));
+    tab_ref = av_malloc_array(fft_size, sizeof(FFTComplex));
+    tab2 = av_malloc_array(fft_size, sizeof(FFTSample));
 
     switch (transform) {
 #if CONFIG_MDCT
     case TRANSFORM_MDCT:
-        av_log(NULL, AV_LOG_INFO, "Scale factor is set to %f\n", scale);
+        av_log(NULL, AV_LOG_INFO,"Scale factor is set to %f\n", scale);
         if (do_inverse)
-            av_log(NULL, AV_LOG_INFO, "IMDCT");
+            av_log(NULL, AV_LOG_INFO,"IMDCT");
         else
-            av_log(NULL, AV_LOG_INFO, "MDCT");
+            av_log(NULL, AV_LOG_INFO,"MDCT");
         ff_mdct_init(&m, fft_nbits, do_inverse, scale);
         break;
 #endif /* CONFIG_MDCT */
     case TRANSFORM_FFT:
         if (do_inverse)
-            av_log(NULL, AV_LOG_INFO, "IFFT");
+            av_log(NULL, AV_LOG_INFO,"IFFT");
         else
-            av_log(NULL, AV_LOG_INFO, "FFT");
+            av_log(NULL, AV_LOG_INFO,"FFT");
         ff_fft_init(&s, fft_nbits, do_inverse);
-        if (err = fft_ref_init(fft_nbits, do_inverse) < 0)
-            goto cleanup;
+        fft_ref_init(fft_nbits, do_inverse);
         break;
 #if FFT_FLOAT
-#if CONFIG_RDFT
+#    if CONFIG_RDFT
     case TRANSFORM_RDFT:
         if (do_inverse)
-            av_log(NULL, AV_LOG_INFO, "IDFT_C2R");
+            av_log(NULL, AV_LOG_INFO,"IDFT_C2R");
         else
-            av_log(NULL, AV_LOG_INFO, "DFT_R2C");
+            av_log(NULL, AV_LOG_INFO,"DFT_R2C");
         ff_rdft_init(&r, fft_nbits, do_inverse ? IDFT_C2R : DFT_R2C);
-        if (err = fft_ref_init(fft_nbits, do_inverse) < 0)
-            goto cleanup;
+        fft_ref_init(fft_nbits, do_inverse);
         break;
-#endif /* CONFIG_RDFT */
-#if CONFIG_DCT
+#    endif /* CONFIG_RDFT */
+#    if CONFIG_DCT
     case TRANSFORM_DCT:
         if (do_inverse)
-            av_log(NULL, AV_LOG_INFO, "DCT_III");
+            av_log(NULL, AV_LOG_INFO,"DCT_III");
         else
-            av_log(NULL, AV_LOG_INFO, "DCT_II");
+            av_log(NULL, AV_LOG_INFO,"DCT_II");
         ff_dct_init(&d, fft_nbits, do_inverse ? DCT_III : DCT_II);
         break;
-#endif /* CONFIG_DCT */
+#    endif /* CONFIG_DCT */
 #endif /* FFT_FLOAT */
     default:
         av_log(NULL, AV_LOG_ERROR, "Requested transform not supported\n");
-        goto cleanup;
+        return 1;
     }
-    av_log(NULL, AV_LOG_INFO, " %d test\n", fft_size);
+    av_log(NULL, AV_LOG_INFO," %d test\n", fft_size);
 
     /* generate random data */
 
@@ -353,21 +347,21 @@ int main(int argc, char **argv)
     }
 
     /* checking result */
-    av_log(NULL, AV_LOG_INFO, "Checking...\n");
+    av_log(NULL, AV_LOG_INFO,"Checking...\n");
 
     switch (transform) {
 #if CONFIG_MDCT
     case TRANSFORM_MDCT:
         if (do_inverse) {
-            imdct_ref((FFTSample *) tab_ref, (FFTSample *) tab1, fft_nbits);
-            m.imdct_calc(&m, tab2, (FFTSample *) tab1);
-            err = check_diff((FFTSample *) tab_ref, tab2, fft_size, scale);
+            imdct_ref((FFTSample *)tab_ref, (FFTSample *)tab1, fft_nbits);
+            m.imdct_calc(&m, tab2, (FFTSample *)tab1);
+            err = check_diff((FFTSample *)tab_ref, tab2, fft_size, scale);
         } else {
-            mdct_ref((FFTSample *) tab_ref, (FFTSample *) tab1, fft_nbits);
+            mdct_ref((FFTSample *)tab_ref, (FFTSample *)tab1, fft_nbits);
 
-            m.mdct_calc(&m, tab2, (FFTSample *) tab1);
+            m.mdct_calc(&m, tab2, (FFTSample *)tab1);
 
-            err = check_diff((FFTSample *) tab_ref, tab2, fft_size / 2, scale);
+            err = check_diff((FFTSample *)tab_ref, tab2, fft_size / 2, scale);
         }
         break;
 #endif /* CONFIG_MDCT */
@@ -377,8 +371,7 @@ int main(int argc, char **argv)
         s.fft_calc(&s, tab);
 
         fft_ref(tab_ref, tab1, fft_nbits);
-        err = check_diff((FFTSample *) tab_ref, (FFTSample *) tab,
-                         fft_size * 2, 1.0);
+        err = check_diff((FFTSample *)tab_ref, (FFTSample *)tab, fft_size * 2, 1.0);
         break;
 #if FFT_FLOAT
 #if CONFIG_RDFT
@@ -386,11 +379,11 @@ int main(int argc, char **argv)
     {
         int fft_size_2 = fft_size >> 1;
         if (do_inverse) {
-            tab1[0].im          = 0;
+            tab1[         0].im = 0;
             tab1[fft_size_2].im = 0;
             for (i = 1; i < fft_size_2; i++) {
-                tab1[fft_size_2 + i].re =  tab1[fft_size_2 - i].re;
-                tab1[fft_size_2 + i].im = -tab1[fft_size_2 - i].im;
+                tab1[fft_size_2+i].re =  tab1[fft_size_2-i].re;
+                tab1[fft_size_2+i].im = -tab1[fft_size_2-i].im;
             }
 
             memcpy(tab2, tab1, fft_size * sizeof(FFTSample));
@@ -402,8 +395,7 @@ int main(int argc, char **argv)
                 tab[i].re = tab2[i];
                 tab[i].im = 0;
             }
-            err = check_diff((float *) tab_ref, (float *) tab,
-                             fft_size * 2, 0.5);
+            err = check_diff((float *)tab_ref, (float *)tab, fft_size * 2, 0.5);
         } else {
             for (i = 0; i < fft_size; i++) {
                 tab2[i]    = tab1[i].re;
@@ -412,7 +404,7 @@ int main(int argc, char **argv)
             r.rdft_calc(&r, tab2);
             fft_ref(tab_ref, tab1, fft_nbits);
             tab_ref[0].im = tab_ref[fft_size_2].re;
-            err = check_diff((float *) tab_ref, (float *) tab2, fft_size, 1.0);
+            err = check_diff((float *)tab_ref, (float *)tab2, fft_size, 1.0);
         }
         break;
     }
@@ -420,12 +412,13 @@ int main(int argc, char **argv)
 #if CONFIG_DCT
     case TRANSFORM_DCT:
         memcpy(tab, tab1, fft_size * sizeof(FFTComplex));
-        d.dct_calc(&d, &tab->re);
-        if (do_inverse)
-            idct_ref(&tab_ref->re, &tab1->re, fft_nbits);
-        else
-            dct_ref(&tab_ref->re, &tab1->re, fft_nbits);
-        err = check_diff((float *) tab_ref, (float *) tab, fft_size, 1.0);
+        d.dct_calc(&d, (FFTSample *)tab);
+        if (do_inverse) {
+            idct_ref((FFTSample*)tab_ref, (FFTSample *)tab1, fft_nbits);
+        } else {
+            dct_ref((FFTSample*)tab_ref, (FFTSample *)tab1, fft_nbits);
+        }
+        err = check_diff((float *)tab_ref, (float *)tab, fft_size, 1.0);
         break;
 #endif /* CONFIG_DCT */
 #endif /* FFT_FLOAT */
@@ -437,18 +430,19 @@ int main(int argc, char **argv)
         int64_t time_start, duration;
         int nb_its;
 
-        av_log(NULL, AV_LOG_INFO, "Speed test...\n");
+        av_log(NULL, AV_LOG_INFO,"Speed test...\n");
         /* we measure during about 1 seconds */
         nb_its = 1;
-        for (;;) {
-            time_start = av_gettime();
+        for(;;) {
+            time_start = av_gettime_relative();
             for (it = 0; it < nb_its; it++) {
                 switch (transform) {
                 case TRANSFORM_MDCT:
-                    if (do_inverse)
-                        m.imdct_calc(&m, (FFTSample *) tab, (FFTSample *) tab1);
-                    else
-                        m.mdct_calc(&m, (FFTSample *) tab, (FFTSample *) tab1);
+                    if (do_inverse) {
+                        m.imdct_calc(&m, (FFTSample *)tab, (FFTSample *)tab1);
+                    } else {
+                        m.mdct_calc(&m, (FFTSample *)tab, (FFTSample *)tab1);
+                    }
                     break;
                 case TRANSFORM_FFT:
                     memcpy(tab, tab1, fft_size * sizeof(FFTComplex));
@@ -466,15 +460,14 @@ int main(int argc, char **argv)
 #endif /* FFT_FLOAT */
                 }
             }
-            duration = av_gettime() - time_start;
+            duration = av_gettime_relative() - time_start;
             if (duration >= 1000000)
                 break;
             nb_its *= 2;
         }
-        av_log(NULL, AV_LOG_INFO,
-               "time: %0.1f us/transform [total time=%0.2f s its=%d]\n",
-               (double) duration / nb_its,
-               (double) duration / 1000000.0,
+        av_log(NULL, AV_LOG_INFO,"time: %0.1f us/transform [total time=%0.2f s its=%d]\n",
+               (double)duration / nb_its,
+               (double)duration / 1000000.0,
                nb_its);
     }
 
@@ -488,20 +481,19 @@ int main(int argc, char **argv)
         ff_fft_end(&s);
         break;
 #if FFT_FLOAT
-#if CONFIG_RDFT
+#    if CONFIG_RDFT
     case TRANSFORM_RDFT:
         ff_rdft_end(&r);
         break;
-#endif /* CONFIG_RDFT */
-#if CONFIG_DCT
+#    endif /* CONFIG_RDFT */
+#    if CONFIG_DCT
     case TRANSFORM_DCT:
         ff_dct_end(&d);
         break;
-#endif /* CONFIG_DCT */
+#    endif /* CONFIG_DCT */
 #endif /* FFT_FLOAT */
     }
 
-cleanup:
     av_free(tab);
     av_free(tab1);
     av_free(tab2);
--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -2,20 +2,20 @@
  * Apple HTTP Live Streaming segmenter
  * Copyright (c) 2012, Luca Barbato
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -33,7 +33,7 @@
 
 typedef struct ListEntry {
     char  name[1024];
-    int   duration;
+    double   duration;
     struct ListEntry *next;
 } ListEntry;
 
@@ -51,7 +51,7 @@ typedef struct HLSContext {
     int has_video;
     int64_t start_pts;
     int64_t end_pts;
-    int64_t duration;      // last segment duration computed so far, in seconds
+    double duration;      // last segment duration computed so far, in seconds
     int nb_entries;
     ListEntry *list;
     ListEntry *end_list;
@@ -72,6 +72,7 @@ static int hls_mux_init(AVFormatContext *s)
 
     oc->oformat            = hls->oformat;
     oc->interrupt_callback = s->interrupt_callback;
+    av_dict_copy(&oc->metadata, s->metadata, 0);
 
     for (i = 0; i < s->nb_streams; i++) {
         AVStream *st;
@@ -84,7 +85,7 @@ static int hls_mux_init(AVFormatContext *s)
     return 0;
 }
 
-static int append_entry(HLSContext *hls, uint64_t duration)
+static int append_entry(HLSContext *hls, double duration)
 {
     ListEntry *en = av_malloc(sizeof(*en));
 
@@ -103,7 +104,7 @@ static int append_entry(HLSContext *hls, uint64_t duration)
 
     hls->end_list = en;
 
-    if (hls->nb_entries >= hls->size) {
+    if (hls->size && hls->nb_entries >= hls->size) {
         en = hls->list;
         hls->list = en->next;
         av_free(en);
@@ -132,7 +133,7 @@ static int hls_window(AVFormatContext *s, int last)
     ListEntry *en;
     int target_duration = 0;
     int ret = 0;
-    int64_t sequence = FFMAX(hls->start_sequence, hls->sequence - hls->size);
+    int64_t sequence = FFMAX(hls->start_sequence, hls->sequence - hls->nb_entries);
 
     if ((ret = avio_open2(&hls->pb, s->filename, AVIO_FLAG_WRITE,
                           &s->interrupt_callback, NULL)) < 0)
@@ -140,7 +141,7 @@ static int hls_window(AVFormatContext *s, int last)
 
     for (en = hls->list; en; en = en->next) {
         if (target_duration < en->duration)
-            target_duration = en->duration;
+            target_duration = ceil(en->duration);
     }
 
     avio_printf(hls->pb, "#EXTM3U\n");
@@ -152,7 +153,7 @@ static int hls_window(AVFormatContext *s, int last)
            sequence);
 
     for (en = hls->list; en; en = en->next) {
-        avio_printf(hls->pb, "#EXTINF:%d,\n", en->duration);
+        avio_printf(hls->pb, "#EXTINF:%f,\n", en->duration);
         if (hls->baseurl)
             avio_printf(hls->pb, "%s", hls->baseurl);
         avio_printf(hls->pb, "%s\n", en->name);
@@ -173,8 +174,10 @@ static int hls_start(AVFormatContext *s)
     int err = 0;
 
     if (av_get_frame_filename(oc->filename, sizeof(oc->filename),
-                              c->basename, c->wrap ? c->sequence % c->wrap : c->sequence) < 0)
+                              c->basename, c->wrap ? c->sequence % c->wrap : c->sequence) < 0) {
+        av_log(oc, AV_LOG_ERROR, "Invalid segment filename template '%s'\n", c->basename);
         return AVERROR(EINVAL);
+    }
     c->number++;
 
     if ((err = avio_open2(&oc->pb, oc->filename, AVIO_FLAG_WRITE,
@@ -256,6 +259,7 @@ static int hls_write_packet(AVFormatContext *s, AVPacket *pkt)
     AVFormatContext *oc = hls->avf;
     AVStream *st = s->streams[pkt->stream_index];
     int64_t end_pts = hls->recording_time * hls->number;
+    int is_ref_pkt = 1;
     int ret, can_split = 1;
 
     if (hls->start_pts == AV_NOPTS_VALUE) {
@@ -266,12 +270,14 @@ static int hls_write_packet(AVFormatContext *s, AVPacket *pkt)
     if (hls->has_video) {
         can_split = st->codec->codec_type == AVMEDIA_TYPE_VIDEO &&
                     pkt->flags & AV_PKT_FLAG_KEY;
+        is_ref_pkt = st->codec->codec_type == AVMEDIA_TYPE_VIDEO;
     }
     if (pkt->pts == AV_NOPTS_VALUE)
-        can_split = 0;
-    else
-        hls->duration = av_rescale(pkt->pts - hls->end_pts,
-                                   st->time_base.num, st->time_base.den);
+        is_ref_pkt = can_split = 0;
+
+    if (is_ref_pkt)
+        hls->duration = (double)(pkt->pts - hls->end_pts)
+                                   * st->time_base.num / st->time_base.den;
 
     if (can_split && av_compare_ts(pkt->pts - hls->start_pts, st->time_base,
                                    end_pts, AV_TIME_BASE_Q) >= 0) {
@@ -321,10 +327,10 @@ static int hls_write_trailer(struct AVFormatContext *s)
 #define OFFSET(x) offsetof(HLSContext, x)
 #define E AV_OPT_FLAG_ENCODING_PARAM
 static const AVOption options[] = {
-    {"start_number",  "first number in the sequence",            OFFSET(start_sequence),AV_OPT_TYPE_INT64,  {.i64 = 0},     0, INT64_MAX, E},
-    {"hls_time",      "segment length in seconds",               OFFSET(time),    AV_OPT_TYPE_FLOAT,  {.dbl = 2},     0, FLT_MAX, E},
-    {"hls_list_size", "maximum number of playlist entries",      OFFSET(size),    AV_OPT_TYPE_INT,    {.i64 = 5},     0, INT_MAX, E},
-    {"hls_wrap",      "number after which the index wraps",      OFFSET(wrap),    AV_OPT_TYPE_INT,    {.i64 = 0},     0, INT_MAX, E},
+    {"start_number",  "set first number in the sequence",        OFFSET(start_sequence),AV_OPT_TYPE_INT64,  {.i64 = 0},     0, INT64_MAX, E},
+    {"hls_time",      "set segment length in seconds",           OFFSET(time),    AV_OPT_TYPE_FLOAT,  {.dbl = 2},     0, FLT_MAX, E},
+    {"hls_list_size", "set maximum number of playlist entries",  OFFSET(size),    AV_OPT_TYPE_INT,    {.i64 = 5},     0, INT_MAX, E},
+    {"hls_wrap",      "set number after which the index wraps",  OFFSET(wrap),    AV_OPT_TYPE_INT,    {.i64 = 0},     0, INT_MAX, E},
     {"hls_base_url",  "url to prepend to each playlist entry",   OFFSET(baseurl), AV_OPT_TYPE_STRING, {.str = NULL},  0, 0,       E},
     { NULL },
 };
