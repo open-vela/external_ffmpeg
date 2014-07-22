43082cda5959da863c041d440cb0ad6f6ffe1e50
openvela-robot <openvela-robot@xiaomi.com>
2014-07-22 16:37:09 +0800

    http: Use a constant for the supported header size

--- libavformat/http.h
+++ libavformat/http.h
@@ -2,20 +2,20 @@
  * HTTP definitions
  * Copyright (c) 2010 Josh Allmann
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -23,6 +23,8 @@
 #define AVFORMAT_HTTP_H
 
 #include "url.h"
+
+#define HTTP_HEADERS_SIZE 1024
 
 /**
  * Initialize the authentication state based on another HTTP URLContext.
@@ -38,7 +40,7 @@ void ff_http_init_auth_state(URLContext *dest, const URLContext *src);
 /**
  * Send a new HTTP request, reusing the old connection.
  *
- * @param h pointer to the resource
+ * @param h pointer to the ressource
  * @param uri uri used to perform the request
  * @return a negative value if an error condition occurred, 0
  * otherwise
