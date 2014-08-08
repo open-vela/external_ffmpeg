e7404697f199d739ab2fcc9b0255dc78c48cb39d
openvela-robot <openvela-robot@xiaomi.com>
2014-08-08 21:25:32 +0800

    Merge commit '454697603e4efdfc04fadec40518d56c7dc1e5dd'
    * commit '454697603e4efdfc04fadec40518d56c7dc1e5dd':
      mpegts: Use av_free() to free memory allocated by av_strdup()
    
    Conflicts:
    	libavformat/mpegtsenc.c
    
    See: 92deb28945a5f2b58908d383f183cfc1bc1d7fae
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/idctdsp_init.c
+++ libavcodec/x86/idctdsp_init.c
@@ -1,18 +1,18 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -37,8 +37,6 @@ static const uint8_t simple_mmx_permutation[64] = {
     0x32, 0x3A, 0x36, 0x3B, 0x33, 0x3E, 0x37, 0x3F,
 };
 
-static const uint8_t idct_sse2_row_perm[8] = { 0, 4, 1, 5, 2, 6, 3, 7 };
-
 av_cold int ff_init_scantable_permutation_x86(uint8_t *idct_permutation,
                                               enum idct_permutation_type perm_type)
 {
@@ -48,10 +46,6 @@ av_cold int ff_init_scantable_permutation_x86(uint8_t *idct_permutation,
     case FF_IDCT_PERM_SIMPLE:
         for (i = 0; i < 64; i++)
             idct_permutation[i] = simple_mmx_permutation[i];
-        return 1;
-    case FF_IDCT_PERM_SSE2:
-        for (i = 0; i < 64; i++)
-            idct_permutation[i] = (i & 0x38) | idct_sse2_row_perm[i & 7];
         return 1;
     }
 
@@ -65,16 +59,23 @@ av_cold void ff_idctdsp_init_x86(IDCTDSPContext *c, AVCodecContext *avctx,
 
     if (INLINE_MMX(cpu_flags)) {
         c->put_pixels_clamped        = ff_put_pixels_clamped_mmx;
-        c->put_signed_pixels_clamped = ff_put_signed_pixels_clamped_mmx;
         c->add_pixels_clamped        = ff_add_pixels_clamped_mmx;
 
         if (!high_bit_depth &&
+            avctx->lowres == 0 &&
             (avctx->idct_algo == FF_IDCT_AUTO ||
+             avctx->idct_algo == FF_IDCT_SIMPLEAUTO ||
              avctx->idct_algo == FF_IDCT_SIMPLEMMX)) {
                 c->idct_put  = ff_simple_idct_put_mmx;
                 c->idct_add  = ff_simple_idct_add_mmx;
                 c->idct      = ff_simple_idct_mmx;
                 c->perm_type = FF_IDCT_PERM_SIMPLE;
         }
+    }
+    if (EXTERNAL_MMX(cpu_flags)) {
+        c->put_signed_pixels_clamped = ff_put_signed_pixels_clamped_mmx;
+    }
+    if (EXTERNAL_SSE2(cpu_flags)) {
+        c->put_signed_pixels_clamped = ff_put_signed_pixels_clamped_sse2;
     }
 }
--- libavcodec/x86/xvididct_init.c
+++ libavcodec/x86/xvididct_init.c
@@ -1,18 +1,18 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -25,6 +25,17 @@
 #include "idct_xvid.h"
 #include "idctdsp.h"
 
+static const uint8_t idct_sse2_row_perm[8] = { 0, 4, 1, 5, 2, 6, 3, 7 };
+
+static av_cold void init_scantable_permutation_sse2(uint8_t *idct_permutation,
+                                                    enum idct_permutation_type perm_type)
+{
+    int i;
+
+    for (i = 0; i < 64; i++)
+        idct_permutation[i] = (i & 0x38) | idct_sse2_row_perm[i & 7];
+}
+
 av_cold void ff_xvididct_init_x86(IDCTDSPContext *c)
 {
     int cpu_flags = av_get_cpu_flags();
@@ -33,14 +44,12 @@ av_cold void ff_xvididct_init_x86(IDCTDSPContext *c)
         c->idct_put  = ff_idct_xvid_mmx_put;
         c->idct_add  = ff_idct_xvid_mmx_add;
         c->idct      = ff_idct_xvid_mmx;
-        c->perm_type = FF_IDCT_PERM_NONE;
     }
 
     if (INLINE_MMXEXT(cpu_flags)) {
         c->idct_put  = ff_idct_xvid_mmxext_put;
         c->idct_add  = ff_idct_xvid_mmxext_add;
         c->idct      = ff_idct_xvid_mmxext;
-        c->perm_type = FF_IDCT_PERM_NONE;
     }
 
     if (INLINE_SSE2(cpu_flags)) {
@@ -48,5 +57,7 @@ av_cold void ff_xvididct_init_x86(IDCTDSPContext *c)
         c->idct_add  = ff_idct_xvid_sse2_add;
         c->idct      = ff_idct_xvid_sse2;
         c->perm_type = FF_IDCT_PERM_SSE2;
+
+        init_scantable_permutation_sse2(c->idct_permutation, c->perm_type);
     }
 }
