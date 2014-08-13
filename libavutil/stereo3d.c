3380f00e56ca7b69c5a81153f2efe51fb0a45e9b
openvela-robot <openvela-robot@xiaomi.com>
2014-08-13 22:22:52 +0800

    Merge commit 'afbd4b7e093adf6d7a830b32759ca3ba8500363d'
    * commit 'afbd4b7e093adf6d7a830b32759ca3ba8500363d':
      lavf: add AVFormatContext/AVStream fields for signaling to the user when events happen.
    
    Conflicts:
    	libavformat/avformat.h
    	libavformat/version.h
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavutil/stereo3d.c
+++ libavutil/stereo3d.c
@@ -1,25 +1,24 @@
 /*
  * Copyright (c) 2013 Vittorio Giovara <vittorio.giovara@gmail.com>
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include <stdint.h>
-#include <string.h>
 
 #include "mem.h"
 #include "stereo3d.h"
@@ -36,8 +35,6 @@ AVStereo3D *av_stereo3d_create_side_data(AVFrame *frame)
                                                         sizeof(AVStereo3D));
     if (!side_data)
         return NULL;
-
-    memset(side_data->data, 0, sizeof(AVStereo3D));
 
     return (AVStereo3D *)side_data->data;
 }
