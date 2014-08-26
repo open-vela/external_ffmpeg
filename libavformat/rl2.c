ba98bcabefdcd8c15cf61556c81d38ffd8a720f3
openvela-robot <openvela-robot@xiaomi.com>
2014-08-27 04:43:47 +0800

    x11grab: K&R formatting cosmetics
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavformat/rl2.c
+++ libavformat/rl2.c
@@ -2,20 +2,20 @@
  * RL2 Format Demuxer
  * Copyright (c) 2008 Sascha Sommer (saschasommer@freenet.de)
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -109,6 +109,10 @@ static av_cold int rl2_read_header(AVFormatContext *s)
     rate = avio_rl16(pb);
     channels = avio_rl16(pb);
     def_sound_size = avio_rl16(pb);
+    if (!channels || channels > 42) {
+        av_log(s, AV_LOG_ERROR, "Invalid number of channels: %d\n", channels);
+        return AVERROR_INVALIDDATA;
+    }
 
     /** setup video stream */
     st = avformat_new_stream(s, NULL);
@@ -127,16 +131,17 @@ static av_cold int rl2_read_header(AVFormatContext *s)
     if(signature == RLV3_TAG && back_size > 0)
         st->codec->extradata_size += back_size;
 
-    if(ff_get_extradata(st->codec, pb, st->codec->extradata_size) < 0)
+    st->codec->extradata = av_mallocz(st->codec->extradata_size +
+                                          FF_INPUT_BUFFER_PADDING_SIZE);
+    if(!st->codec->extradata)
         return AVERROR(ENOMEM);
+
+    if(avio_read(pb,st->codec->extradata,st->codec->extradata_size) !=
+                      st->codec->extradata_size)
+        return AVERROR(EIO);
 
     /** setup audio stream if present */
     if(sound_rate){
-        if (!channels || channels > 42) {
-            av_log(s, AV_LOG_ERROR, "Invalid number of channels: %d\n", channels);
-            return AVERROR_INVALIDDATA;
-        }
-
         pts_num = def_sound_size;
         pts_den = rate;
 
@@ -230,7 +235,7 @@ static int rl2_read_packet(AVFormatContext *s,
     }
 
     if(stream_id == -1)
-        return AVERROR_EOF;
+        return AVERROR(EIO);
 
     ++rl2->index_pos[stream_id];
 
