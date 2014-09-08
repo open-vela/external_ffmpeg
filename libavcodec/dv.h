dded0d8e9f71c874400f72e01fd82399a35ba702
openvela-robot <openvela-robot@xiaomi.com>
2014-09-08 13:40:28 +0800

    dv: K&R formatting cosmetics
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavcodec/dv.h
+++ libavcodec/dv.h
@@ -2,20 +2,20 @@
  * Constants for DV codec
  * Copyright (c) 2002 Fabrice Bellard
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -28,13 +28,13 @@
 #define AVCODEC_DV_H
 
 #include "avcodec.h"
-#include "me_cmp.h"
-#include "get_bits.h"
 #include "dv_profile.h"
+#include "get_bits.h"
+#include "me_cmp.h"
 
 typedef struct DVwork_chunk {
-    uint16_t  buf_offset;
-    uint16_t  mb_coordinates[5];
+    uint16_t buf_offset;
+    uint16_t mb_coordinates[5];
 } DVwork_chunk;
 
 typedef struct DVVideoContext {
@@ -43,9 +43,9 @@ typedef struct DVVideoContext {
     AVCodecContext  *avctx;
     uint8_t         *buf;
 
-    uint8_t  dv_zigzag[2][64];
+    uint8_t dv_zigzag[2][64];
 
-    void (*get_pixels)(int16_t *block, const uint8_t *pixels, ptrdiff_t line_size);
+    void (*get_pixels)(int16_t *block, const uint8_t *pixels, int line_size);
     void (*fdct[2])(int16_t *block);
     void (*idct_put[2])(uint8_t *dest, int line_size, int16_t *block);
     me_cmp_func ildct_cmp;
@@ -54,32 +54,35 @@ typedef struct DVVideoContext {
 } DVVideoContext;
 
 enum dv_section_type {
-     dv_sect_header  = 0x1f,
-     dv_sect_subcode = 0x3f,
-     dv_sect_vaux    = 0x56,
-     dv_sect_audio   = 0x76,
-     dv_sect_video   = 0x96,
+    dv_sect_header  = 0x1f,
+    dv_sect_subcode = 0x3f,
+    dv_sect_vaux    = 0x56,
+    dv_sect_audio   = 0x76,
+    dv_sect_video   = 0x96,
 };
 
 enum dv_pack_type {
-     dv_header525     = 0x3f, /* see dv_write_pack for important details on */
-     dv_header625     = 0xbf, /* these two packs */
-     dv_timecode      = 0x13,
-     dv_audio_source  = 0x50,
-     dv_audio_control = 0x51,
-     dv_audio_recdate = 0x52,
-     dv_audio_rectime = 0x53,
-     dv_video_source  = 0x60,
-     dv_video_control = 0x61,
-     dv_video_recdate = 0x62,
-     dv_video_rectime = 0x63,
-     dv_unknown_pack  = 0xff,
+    dv_header525     = 0x3f,  /* see dv_write_pack for important details on */
+    dv_header625     = 0xbf,  /* these two packs */
+    dv_timecode      = 0x13,
+    dv_audio_source  = 0x50,
+    dv_audio_control = 0x51,
+    dv_audio_recdate = 0x52,
+    dv_audio_rectime = 0x53,
+    dv_video_source  = 0x60,
+    dv_video_control = 0x61,
+    dv_video_recdate = 0x62,
+    dv_video_rectime = 0x63,
+    dv_unknown_pack  = 0xff,
 };
 
 #define DV_PROFILE_IS_HD(p) ((p)->video_stype & 0x10)
 #define DV_PROFILE_IS_1080i50(p) (((p)->video_stype == 0x14) && ((p)->dsf == 1))
 #define DV_PROFILE_IS_720p50(p)  (((p)->video_stype == 0x18) && ((p)->dsf == 1))
 
+/* minimum number of bytes to read from a DV stream in order to
+ * determine the profile */
+#define DV_PROFILE_BYTES (6 * 80) /* 6 DIF blocks */
 
 /**
  * largest possible DV frame, in bytes (1080i50)
@@ -100,23 +103,26 @@ int ff_dvvideo_init(AVCodecContext *avctx);
 
 static inline int dv_work_pool_size(const AVDVProfile *d)
 {
-    int size = d->n_difchan*d->difseg_size*27;
+    int size = d->n_difchan * d->difseg_size * 27;
     if (DV_PROFILE_IS_1080i50(d))
-        size -= 3*27;
+        size -= 3 * 27;
     if (DV_PROFILE_IS_720p50(d))
-        size -= 4*27;
+        size -= 4 * 27;
     return size;
 }
 
-static inline void dv_calculate_mb_xy(DVVideoContext *s, DVwork_chunk *work_chunk, int m, int *mb_x, int *mb_y)
+static inline void dv_calculate_mb_xy(DVVideoContext *s,
+                                      DVwork_chunk *work_chunk,
+                                      int m, int *mb_x, int *mb_y)
 {
-     *mb_x = work_chunk->mb_coordinates[m] & 0xff;
-     *mb_y = work_chunk->mb_coordinates[m] >> 8;
+    *mb_x = work_chunk->mb_coordinates[m] & 0xff;
+    *mb_y = work_chunk->mb_coordinates[m] >> 8;
 
-     /* We work with 720p frames split in half. The odd half-frame (chan==2,3) is displaced :-( */
-     if (s->sys->height == 720 && !(s->buf[1]&0x0C)) {
-         *mb_y -= (*mb_y>17)?18:-72; /* shifting the Y coordinate down by 72/2 macro blocks */
-     }
+    /* We work with 720p frames split in half.
+     * The odd half-frame (chan == 2,3) is displaced :-( */
+    if (s->sys->height == 720 && !(s->buf[1] & 0x0C))
+        /* shifting the Y coordinate down by 72/2 macro blocks */
+        *mb_y -= (*mb_y > 17) ? 18 : -72;
 }
 
 #endif /* AVCODEC_DV_H */
--- libavcodec/dv_tablegen.h
+++ libavcodec/dv_tablegen.h
@@ -3,20 +3,20 @@
  *
  * Copyright (c) 2010 Reimar Döffinger <Reimar.Doeffinger@gmx.de>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -24,22 +24,21 @@
 #define AVCODEC_DV_TABLEGEN_H
 
 #include <stdint.h>
-#include "libavutil/attributes.h"
 
 #include "dvdata.h"
 
 #if CONFIG_SMALL
-#define DV_VLC_MAP_RUN_SIZE 15
-#define DV_VLC_MAP_LEV_SIZE 23
+#define DV_VLC_MAP_RUN_SIZE  15
+#define DV_VLC_MAP_LEV_SIZE  23
 #else
 #define DV_VLC_MAP_RUN_SIZE  64
-#define DV_VLC_MAP_LEV_SIZE 512 //FIXME sign was removed so this should be /2 but needs check
+#define DV_VLC_MAP_LEV_SIZE 512 // FIXME sign was removed so this should be /2 but needs check
 #endif
 
 /* VLC encoding lookup table */
 typedef struct dv_vlc_pair {
-   uint32_t vlc;
-   uint32_t size;
+    uint32_t vlc;
+    uint32_t size;
 } dv_vlc_pair;
 
 #if CONFIG_HARDCODED_TABLES
@@ -48,48 +47,48 @@ typedef struct dv_vlc_pair {
 #else
 static struct dv_vlc_pair dv_vlc_map[DV_VLC_MAP_RUN_SIZE][DV_VLC_MAP_LEV_SIZE];
 
-static av_cold void dv_vlc_map_tableinit(void)
+static void dv_vlc_map_tableinit(void)
 {
     int i, j;
     for (i = 0; i < NB_DV_VLC - 1; i++) {
-       if (ff_dv_vlc_run[i] >= DV_VLC_MAP_RUN_SIZE)
-           continue;
+        if (ff_dv_vlc_run[i] >= DV_VLC_MAP_RUN_SIZE)
+            continue;
 #if CONFIG_SMALL
-       if (ff_dv_vlc_level[i] >= DV_VLC_MAP_LEV_SIZE)
-           continue;
+        if (ff_dv_vlc_level[i] >= DV_VLC_MAP_LEV_SIZE)
+            continue;
 #endif
 
-       if (dv_vlc_map[ff_dv_vlc_run[i]][ff_dv_vlc_level[i]].size != 0)
-           continue;
+        if (dv_vlc_map[ff_dv_vlc_run[i]][ff_dv_vlc_level[i]].size != 0)
+            continue;
 
-       dv_vlc_map[ff_dv_vlc_run[i]][ff_dv_vlc_level[i]].vlc  =
-           ff_dv_vlc_bits[i] << (!!ff_dv_vlc_level[i]);
-       dv_vlc_map[ff_dv_vlc_run[i]][ff_dv_vlc_level[i]].size =
-           ff_dv_vlc_len[i] + (!!ff_dv_vlc_level[i]);
+        dv_vlc_map[ff_dv_vlc_run[i]][ff_dv_vlc_level[i]].vlc  =
+            ff_dv_vlc_bits[i] << (!!ff_dv_vlc_level[i]);
+        dv_vlc_map[ff_dv_vlc_run[i]][ff_dv_vlc_level[i]].size =
+            ff_dv_vlc_len[i]   + (!!ff_dv_vlc_level[i]);
     }
     for (i = 0; i < DV_VLC_MAP_RUN_SIZE; i++) {
 #if CONFIG_SMALL
-       for (j = 1; j < DV_VLC_MAP_LEV_SIZE; j++) {
-          if (dv_vlc_map[i][j].size == 0) {
-              dv_vlc_map[i][j].vlc = dv_vlc_map[0][j].vlc |
-                        (dv_vlc_map[i-1][0].vlc << (dv_vlc_map[0][j].size));
-              dv_vlc_map[i][j].size = dv_vlc_map[i-1][0].size +
-                                      dv_vlc_map[0][j].size;
-          }
-       }
+        for (j = 1; j < DV_VLC_MAP_LEV_SIZE; j++) {
+            if (dv_vlc_map[i][j].size == 0) {
+                dv_vlc_map[i][j].vlc  = dv_vlc_map[0][j].vlc |
+                                        (dv_vlc_map[i - 1][0].vlc <<
+                                         dv_vlc_map[0][j].size);
+                dv_vlc_map[i][j].size = dv_vlc_map[i - 1][0].size +
+                                        dv_vlc_map[0][j].size;
+            }
+        }
 #else
-       for (j = 1; j < DV_VLC_MAP_LEV_SIZE/2; j++) {
-          if (dv_vlc_map[i][j].size == 0) {
-              dv_vlc_map[i][j].vlc = dv_vlc_map[0][j].vlc |
-                        (dv_vlc_map[i-1][0].vlc << (dv_vlc_map[0][j].size));
-              dv_vlc_map[i][j].size = dv_vlc_map[i-1][0].size +
-                                      dv_vlc_map[0][j].size;
-          }
-          dv_vlc_map[i][((uint16_t)(-j))&0x1ff].vlc =
-                                        dv_vlc_map[i][j].vlc | 1;
-          dv_vlc_map[i][((uint16_t)(-j))&0x1ff].size =
-                                        dv_vlc_map[i][j].size;
-       }
+        for (j = 1; j < DV_VLC_MAP_LEV_SIZE / 2; j++) {
+            if (dv_vlc_map[i][j].size == 0) {
+                dv_vlc_map[i][j].vlc  = dv_vlc_map[0][j].vlc |
+                                        (dv_vlc_map[i - 1][0].vlc <<
+                                         dv_vlc_map[0][j].size);
+                dv_vlc_map[i][j].size = dv_vlc_map[i - 1][0].size +
+                                        dv_vlc_map[0][j].size;
+            }
+            dv_vlc_map[i][((uint16_t) (-j)) & 0x1ff].vlc  = dv_vlc_map[i][j].vlc | 1;
+            dv_vlc_map[i][((uint16_t) (-j)) & 0x1ff].size = dv_vlc_map[i][j].size;
+        }
 #endif
     }
 }
