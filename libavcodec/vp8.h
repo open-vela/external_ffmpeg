c3ccf55664bb698b88f35fea88b1a8c9b6adaf9a
openvela-robot <openvela-robot@xiaomi.com>
2014-09-10 20:50:32 +0800

    avcodec/mpegvideo: Use FF_ALLOCZ_ARRAY_OR_GOTO()
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/vp8.h
+++ libavcodec/vp8.h
@@ -6,20 +6,20 @@
  * Copyright (C) 2010 Fiona Glaser
  * Copyright (C) 2012 Daniel Kang
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -35,6 +35,8 @@
 
 #if HAVE_PTHREADS
 #   include <pthread.h>
+#elif HAVE_OS2THREADS
+#   include "compat/os2threads.h"
 #elif HAVE_W32THREADS
 #   include "compat/w32pthreads.h"
 #endif
@@ -271,6 +273,11 @@ typedef struct VP8Context {
      * 1 -> Macroblocks for entire frame alloced (sliced thread).
      */
     int mb_layout;
+
+    void (*decode_mb_row_no_filter)(AVCodecContext *avctx, void *tdata, int jobnr, int threadnr);
+    void (*filter_mb_row)(AVCodecContext *avctx, void *tdata, int jobnr, int threadnr);
+
+    int vp7;
 
     /**
      * Fade bit present in bitstream (VP7)
