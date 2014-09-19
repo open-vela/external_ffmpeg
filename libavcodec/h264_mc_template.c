1ab256fd54eb34aca32e0c1d75f4e5ed8ae4be1a
openvela-robot <openvela-robot@xiaomi.com>
2014-09-19 10:01:28 +0800

    hlsenc: Add parameter -hls_allow_cache
    The -hls_allow_cache parameter enables explicitly setting the
    EXT-X-ALLOW-CACHE tag in the manifest file. That tag indicates
    whether the client MAY or MUST NOT cache downloaded media
    segments for later replay.
    
    Valid values are 1 (=YES) or 0 (=NO) and the EXT-X-ALLOW-CACHE
    will not show in the manifest for other values (or if
    -hls_allow_cache is not used.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/h264_mc_template.c
+++ libavcodec/h264_mc_template.c
@@ -2,20 +2,20 @@
  * H.26L/H.264/AVC/JVT/14496-10/... decoder
  * Copyright (c) 2003 Michael Niedermayer <michaelni@gmx.at>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -72,7 +72,7 @@ static void MCFUNC(hl_motion)(H264Context *h, uint8_t *dest_y,
     const int mb_xy   = h->mb_xy;
     const int mb_type = h->cur_pic.mb_type[mb_xy];
 
-    av_assert2(IS_INTER(mb_type));
+    assert(IS_INTER(mb_type));
 
     if (HAVE_THREADS && (h->avctx->active_thread_type & FF_THREAD_FRAME))
         await_references(h);
@@ -104,7 +104,7 @@ static void MCFUNC(hl_motion)(H264Context *h, uint8_t *dest_y,
     } else {
         int i;
 
-        av_assert2(IS_8X8(mb_type));
+        assert(IS_8X8(mb_type));
 
         for (i = 0; i < 4; i++) {
             const int sub_mb_type = h->sub_mb_type[i];
@@ -142,7 +142,7 @@ static void MCFUNC(hl_motion)(H264Context *h, uint8_t *dest_y,
                         IS_DIR(sub_mb_type, 0, 0), IS_DIR(sub_mb_type, 0, 1));
             } else {
                 int j;
-                av_assert2(IS_SUB_4X4(sub_mb_type));
+                assert(IS_SUB_4X4(sub_mb_type));
                 for (j = 0; j < 4; j++) {
                     int sub_x_offset = x_offset + 2 * (j & 1);
                     int sub_y_offset = y_offset + (j & 2);
