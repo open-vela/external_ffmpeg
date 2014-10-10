a5256ed5bb1d991b6bcd804c2c3f222a7fe1563d
openvela-robot <openvela-robot@xiaomi.com>
2014-10-10 12:08:48 +0800

    Merge commit '73ea3ffcd55b0b1d23ba4769d65996a8d58ffa11'
    * commit '73ea3ffcd55b0b1d23ba4769d65996a8d58ffa11':
      w32pthreads: use the CONDITION_VARIABLE typedef if available
    
    Conflicts:
    	compat/w32pthreads.h
    	configure
    
    See: 0c0694931442ef9ea91eace120e0629e0ac01b76
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libswscale/version.h
+++ libswscale/version.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -27,8 +27,8 @@
 #include "libavutil/version.h"
 
 #define LIBSWSCALE_VERSION_MAJOR 3
-#define LIBSWSCALE_VERSION_MINOR 0
-#define LIBSWSCALE_VERSION_MICRO 0
+#define LIBSWSCALE_VERSION_MINOR 1
+#define LIBSWSCALE_VERSION_MICRO 100
 
 #define LIBSWSCALE_VERSION_INT  AV_VERSION_INT(LIBSWSCALE_VERSION_MAJOR, \
                                                LIBSWSCALE_VERSION_MINOR, \
