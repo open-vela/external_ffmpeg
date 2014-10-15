a80ed6760055ef04c366503db87fe883b504cf64
openvela-robot <openvela-robot@xiaomi.com>
2014-10-15 21:22:06 +0800

    Merge commit '1ac5a29b2e5ddeae068deb9d6e0e803a91941d4d'
    * commit '1ac5a29b2e5ddeae068deb9d6e0e803a91941d4d':
      imc: fix order of operations in coefficients read
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/rtpdec_hevc.c
+++ libavformat/rtpdec_hevc.c
@@ -109,7 +109,7 @@ static av_cold int hevc_sdp_parse_fmtp_config(AVFormatContext *s,
         while (*value) {
             char base64packet[1024];
             uint8_t decoded_packet[1024];
-            int decoded_packet_size;
+            int packet_size;
             char *dst = base64packet;
 
             while (*value && *value != ',' &&
@@ -121,24 +121,28 @@ static av_cold int hevc_sdp_parse_fmtp_config(AVFormatContext *s,
             if (*value == ',')
                 value++;
 
-            decoded_packet_size = av_base64_decode(decoded_packet, base64packet,
-                                                   sizeof(decoded_packet));
-            if (decoded_packet_size > 0) {
-                uint8_t *tmp = av_realloc(*data_ptr, decoded_packet_size +
-                                          sizeof(start_sequence) + *size_ptr);
-                if (!tmp) {
+            packet_size = av_base64_decode(decoded_packet, base64packet,
+                                           sizeof(decoded_packet));
+            if (packet_size > 0) {
+                uint8_t *dest = av_malloc(packet_size + sizeof(start_sequence) +
+                                          *size_ptr);
+                if (!dest) {
                     av_log(s, AV_LOG_ERROR,
                            "Unable to allocate memory for extradata!\n");
                     return AVERROR(ENOMEM);
                 }
-                *data_ptr = tmp;
+                if (*size_ptr) {
+                    memcpy(dest, *data_ptr, *size_ptr);
+                    av_free(*data_ptr);
+                }
 
-                memcpy(*data_ptr + *size_ptr, start_sequence,
+                memcpy(dest + *size_ptr, start_sequence,
                        sizeof(start_sequence));
-                memcpy(*data_ptr + *size_ptr + sizeof(start_sequence),
-                       decoded_packet, decoded_packet_size);
+                memcpy(dest + *size_ptr + sizeof(start_sequence),
+                       decoded_packet, packet_size);
 
-                *size_ptr += sizeof(start_sequence) + decoded_packet_size;
+                *data_ptr  = dest;
+                *size_ptr += sizeof(start_sequence) + packet_size;
             }
         }
     }
