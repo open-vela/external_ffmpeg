4f2e03ad0e01583bcadff0d0158bbd9e388f07bf
openvela-robot <openvela-robot@xiaomi.com>
2014-10-22 12:14:37 +0800

    lavu: Add av_gettime_relative
    Since av_gettime() is used in a number of places where actual
    real time clock is required, the monotonic clock introduced in
    ebef9f5a5 would have consequences that are hard to handle. Instead
    split it into a separate function that can be used in the cases
    where only relative time is desired.
    
    On platform where no monotonic clock is available, the difference
    between the two av_gettime functions is not clear, and one could
    mistakenly use the relative clock where an absolute one is
    required. Therefore add an offset, to make it evident that the
    time returned from av_gettime_relative never is actual current
    real time, even though it is based on av_gettime.
    
    Based on a patch by Olivier Langlois.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/time.h
+++ libavutil/time.h
@@ -1,20 +1,18 @@
 /*
- * Copyright (c) 2000-2003 Fabrice Bellard
+ * This file is part of Libav.
  *
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -32,16 +30,10 @@ int64_t av_gettime(void);
  * Get the current time in microseconds since some unspecified starting point.
  * On platforms that support it, the time comes from a monotonic clock
  * This property makes this time source ideal for measuring relative time.
- * If a monotonic clock is not available on the targeted platform, the
- * implementation fallsback on using av_gettime().
+ * The returned values may not be monotonic on platforms where a monotonic
+ * clock is not available.
  */
 int64_t av_gettime_relative(void);
-
-/**
- * Indicates with a boolean result if the av_gettime_relative() time source
- * is monotonic.
- */
-int av_gettime_relative_is_monotonic(void);
 
 /**
  * Sleep for a period of time.  Although the duration is expressed in
