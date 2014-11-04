7b1305bfa7c582e4c183c95c8fb2c2866b7a034c
openvela-robot <openvela-robot@xiaomi.com>
2014-11-04 22:33:09 +0800

    Merge commit '77ab341c0c6cdf2bd437bb48d429e797d1e60da2'
    * commit '77ab341c0c6cdf2bd437bb48d429e797d1e60da2':
      aacdec: add default case in channel layout
    
    Conflicts:
    	libavcodec/aacdec.c
    
    Note, the default case is currently unreachable
    
    See: a48b890392aa22033f182421ba9e3f3b3256461d
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/utils.c
+++ libavcodec/utils.c
@@ -1210,11 +1210,6 @@ int ff_get_format(AVCodecContext *avctx, const enum AVPixelFormat *fmt)
     memcpy(choices, fmt, (n + 1) * sizeof(*choices));
 
     for (;;) {
-        if (avctx->hwaccel && avctx->hwaccel->uninit)
-            avctx->hwaccel->uninit(avctx);
-        av_freep(&avctx->internal->hwaccel_priv_data);
-        avctx->hwaccel = NULL;
-
         ret = avctx->get_format(avctx, choices);
 
         desc = av_pix_fmt_desc_get(ret);
@@ -1222,6 +1217,11 @@ int ff_get_format(AVCodecContext *avctx, const enum AVPixelFormat *fmt)
             ret = AV_PIX_FMT_NONE;
             break;
         }
+
+        if (avctx->hwaccel && avctx->hwaccel->uninit)
+            avctx->hwaccel->uninit(avctx);
+        av_freep(&avctx->internal->hwaccel_priv_data);
+        avctx->hwaccel = NULL;
 
         if (!(desc->flags & AV_PIX_FMT_FLAG_HWACCEL))
             break;
