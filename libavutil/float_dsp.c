0f4507a7477ce71788928445c4bca8d11f2ea387
openvela-robot <openvela-robot@xiaomi.com>
2014-11-10 10:14:45 +0800

    libavcodec: Unconditionally build xiph.o
    vorbis_parser.o is built unconditionally since 5e80fb7ff, and the
    unconditionally built parts of it depend on xiph.o.
    
    This fixes builds with --disable-everything.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavutil/float_dsp.c
+++ libavutil/float_dsp.c
@@ -1,21 +1,18 @@
 /*
- * Copyright 2005 Balatoni Denes
- * Copyright 2006 Loren Merritt
+ * This file is part of Libav.
  *
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -92,7 +89,7 @@ static void vector_fmul_reverse_c(float *dst, const float *src0,
         dst[i] = src0[i] * src1[-i];
 }
 
-static void butterflies_float_c(float *av_restrict v1, float *av_restrict v2,
+static void butterflies_float_c(float *restrict v1, float *restrict v2,
                                 int len)
 {
     int i;
@@ -135,8 +132,6 @@ av_cold void avpriv_float_dsp_init(AVFloatDSPContext *fdsp, int bit_exact)
         ff_float_dsp_init_ppc(fdsp, bit_exact);
     if (ARCH_X86)
         ff_float_dsp_init_x86(fdsp);
-    if (ARCH_MIPS)
-        ff_float_dsp_init_mips(fdsp);
 }
 
 #ifdef TEST
@@ -146,12 +141,6 @@ av_cold void avpriv_float_dsp_init(AVFloatDSPContext *fdsp, int bit_exact)
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#if HAVE_UNISTD_H
-#include <unistd.h> /* for getopt */
-#endif
-#if !HAVE_GETOPT
-#include "compat/getopt.c"
-#endif
 
 #include "common.h"
 #include "cpu.h"
@@ -375,7 +364,7 @@ static int test_scalarproduct_float(AVFloatDSPContext *fdsp, AVFloatDSPContext *
 
 int main(int argc, char **argv)
 {
-    int ret = 0, seeded = 0;
+    int ret = 0;
     uint32_t seed;
     AVFloatDSPContext fdsp, cdsp;
     AVLFG lfg;
@@ -386,31 +375,12 @@ int main(int argc, char **argv)
     LOCAL_ALIGNED(32, double, dbl_src0, [LEN]);
     LOCAL_ALIGNED(32, double, dbl_src1, [LEN]);
 
-    for (;;) {
-        int arg = getopt(argc, argv, "s:c:");
-        if (arg == -1)
-            break;
-        switch (arg) {
-        case 's':
-            seed = strtoul(optarg, NULL, 10);
-            seeded = 1;
-            break;
-        case 'c':
-        {
-            int cpuflags = av_get_cpu_flags();
-
-            if (av_parse_cpu_caps(&cpuflags, optarg) < 0)
-                return 1;
-
-            av_force_cpu_flags(cpuflags);
-            break;
-        }
-        }
-    }
-    if (!seeded)
+    if (argc > 2 && !strcmp(argv[1], "-s"))
+        seed = strtoul(argv[2], NULL, 10);
+    else
         seed = av_get_random_seed();
 
-    av_log(NULL, AV_LOG_INFO, "float_dsp-test: %s %u\n", seeded ? "seed" : "random seed", seed);
+    av_log(NULL, AV_LOG_INFO, "float_dsp-test: random seed %u\n", seed);
 
     av_lfg_init(&lfg, seed);
 
