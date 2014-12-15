431b6805304e48e30075dbe18175bd58c1482c2b
Michael Niedermayer <michaelni@gmx.at>
2014-12-15 15:47:54 +0100

    avformat/xmv: use av_freep() to avoid leaving stale extradata pointer
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavformat/xmv.c
+++ libavformat/xmv.c
@@ -390,7 +390,7 @@ static int xmv_process_packet_header(AVFormatContext *s)
                 assert(xmv->video.stream_index < s->nb_streams);
 
                 if (vst->codec->extradata_size < 4) {
-                    av_free(vst->codec->extradata);
+                    av_freep(&vst->codec->extradata);
 
                     vst->codec->extradata =
                         av_malloc(4 + FF_INPUT_BUFFER_PADDING_SIZE);
