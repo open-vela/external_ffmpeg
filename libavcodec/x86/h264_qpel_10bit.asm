20113f8fa09651fec7db8ee02b18e1f691a3ba1f
openvela-robot <openvela-robot@xiaomi.com>
2015-02-01 13:22:21 +0800

    Merge commit 'f726fc21ef76a8ba3445448066f7b2a687fbca16'
    * commit 'f726fc21ef76a8ba3445448066f7b2a687fbca16':
      ogg: Provide an option to offset the serial number
    
    Conflicts:
    	libavformat/oggenc.c
    
    Merged-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/x86/h264_qpel_10bit.asm
+++ libavcodec/x86/h264_qpel_10bit.asm
@@ -5,20 +5,20 @@
 ;*
 ;* Authors: Daniel Kang <daniel.d.kang@gmail.com>
 ;*
-;* This file is part of Libav.
+;* This file is part of FFmpeg.
 ;*
-;* Libav is free software; you can redistribute it and/or
+;* FFmpeg is free software; you can redistribute it and/or
 ;* modify it under the terms of the GNU Lesser General Public
 ;* License as published by the Free Software Foundation; either
 ;* version 2.1 of the License, or (at your option) any later version.
 ;*
-;* Libav is distributed in the hope that it will be useful,
+;* FFmpeg is distributed in the hope that it will be useful,
 ;* but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;* Lesser General Public License for more details.
 ;*
 ;* You should have received a copy of the GNU Lesser General Public
-;* License along with Libav; if not, write to the Free Software
+;* License along with FFmpeg; if not, write to the Free Software
 ;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 ;******************************************************************************
 
@@ -28,7 +28,7 @@ SECTION_RODATA 32
 
 cextern pw_16
 cextern pw_1
-cextern pb_0
+pb_0: times 32 db 0 ; we do not use cextern here as old llvm-gcc fails to align it correctly
 
 pw_pixel_max: times 8 dw ((1 << 10)-1)
 
