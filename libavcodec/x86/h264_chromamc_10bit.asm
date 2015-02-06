abffd9eb0e455ce42e3e3f8f4cc27aed445c8c4e
openvela-robot <openvela-robot@xiaomi.com>
2015-02-06 14:53:39 +0800

    lavf: remove unused code
    Nothing uses it, and it provides no public API.
    
    Archeological finds:
    
    Commit 101036adb9 added the API.
    Commit a8dd8dc6e9 made mpegts.c use it.
    Commit af8aae3fa3 disabled it by default in mpegts.c.
    Commit ae2bb52cd2 removed all uses of this from mpegts.c.
    
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavcodec/x86/h264_chromamc_10bit.asm
+++ libavcodec/x86/h264_chromamc_10bit.asm
@@ -5,20 +5,20 @@
 ;*
 ;* Authors: Daniel Kang <daniel.d.kang@gmail.com>
 ;*
-;* This file is part of FFmpeg.
+;* This file is part of Libav.
 ;*
-;* FFmpeg is free software; you can redistribute it and/or
+;* Libav is free software; you can redistribute it and/or
 ;* modify it under the terms of the GNU Lesser General Public
 ;* License as published by the Free Software Foundation; either
 ;* version 2.1 of the License, or (at your option) any later version.
 ;*
-;* FFmpeg is distributed in the hope that it will be useful,
+;* Libav is distributed in the hope that it will be useful,
 ;* but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;* Lesser General Public License for more details.
 ;*
 ;* You should have received a copy of the GNU Lesser General Public
-;* License along with FFmpeg; if not, write to the Free Software
+;* License along with Libav; if not, write to the Free Software
 ;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 ;******************************************************************************
 
@@ -252,10 +252,8 @@ cglobal %1_h264_chroma_mc2_10, 6,7
 %define CHROMAMC_AVG  NOTHING
 INIT_XMM sse2
 CHROMA_MC8 put
-%if HAVE_AVX_EXTERNAL
 INIT_XMM avx
 CHROMA_MC8 put
-%endif
 INIT_MMX mmxext
 CHROMA_MC4 put
 CHROMA_MC2 put
@@ -263,10 +261,8 @@ CHROMA_MC2 put
 %define CHROMAMC_AVG  AVG
 INIT_XMM sse2
 CHROMA_MC8 avg
-%if HAVE_AVX_EXTERNAL
 INIT_XMM avx
 CHROMA_MC8 avg
-%endif
 INIT_MMX mmxext
 CHROMA_MC4 avg
 CHROMA_MC2 avg
