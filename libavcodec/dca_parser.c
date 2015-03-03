913c91fe85add68f83ee00864a1a34df2c397497
openvela-robot <openvela-robot@xiaomi.com>
2015-03-03 11:05:15 +0800

    vorbis: Check the vlc value in setup_classifs
    The valid returned values are always at most 11bit.
    Remove the previous check that assumed larger values plausible and
    use a signed integer to check get_vlc2 return values.
    
    CC: libav-stable@libav.org

--- libavcodec/dca_parser.c
+++ libavcodec/dca_parser.c
@@ -5,20 +5,20 @@
  * Copyright (C) 2006 Benjamin Larsson
  * Copyright (C) 2007 Konstantin Shishkov
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -38,7 +38,7 @@ typedef struct DCAParseContext {
 #define IS_MARKER(state, i, buf, buf_size) \
     ((state == DCA_SYNCWORD_CORE_14B_LE && (i < buf_size - 2) && (buf[i + 1] & 0xF0) == 0xF0 &&  buf[i + 2]         == 0x07) || \
      (state == DCA_SYNCWORD_CORE_14B_BE && (i < buf_size - 2) &&  buf[i + 1]         == 0x07 && (buf[i + 2] & 0xF0) == 0xF0) || \
-      state == DCA_SYNCWORD_CORE_LE || state == DCA_SYNCWORD_CORE_BE || state == DCA_SYNCWORD_SUBSTREAM)
+      state == DCA_SYNCWORD_CORE_LE || state == DCA_SYNCWORD_CORE_BE)
 
 /**
  * Find the end of the current frame in the bitstream.
@@ -59,7 +59,11 @@ static int dca_find_frame_end(DCAParseContext *pc1, const uint8_t *buf,
         for (i = 0; i < buf_size; i++) {
             state = (state << 8) | buf[i];
             if (IS_MARKER(state, i, buf, buf_size)) {
-                if (!pc1->lastmarker || state == pc1->lastmarker || pc1->lastmarker == DCA_SYNCWORD_SUBSTREAM) {
+                if (pc1->lastmarker && state == pc1->lastmarker) {
+                    start_found = 1;
+                    i++;
+                    break;
+                } else if (!pc1->lastmarker) {
                     start_found     = 1;
                     pc1->lastmarker = state;
                     i++;
@@ -74,7 +78,7 @@ static int dca_find_frame_end(DCAParseContext *pc1, const uint8_t *buf,
             state = (state << 8) | buf[i];
             if (state == DCA_SYNCWORD_SUBSTREAM && !pc1->hd_pos)
                 pc1->hd_pos = pc1->size;
-            if (IS_MARKER(state, i, buf, buf_size) && (state == pc1->lastmarker || pc1->lastmarker == DCA_SYNCWORD_SUBSTREAM)) {
+            if (state == pc1->lastmarker && IS_MARKER(state, i, buf, buf_size)) {
                 if (pc1->framesize > pc1->size)
                     continue;
                 pc->frame_start_found = 0;
@@ -107,7 +111,7 @@ static int dca_parse_params(const uint8_t *buf, int buf_size, int *duration,
     if (buf_size < 12)
         return AVERROR_INVALIDDATA;
 
-    if ((ret = avpriv_dca_convert_bitstream(buf, 12, hdr, 12)) < 0)
+    if ((ret = ff_dca_convert_bitstream(buf, 12, hdr, 12)) < 0)
         return ret;
 
     init_get_bits(&gb, hdr, 96);
