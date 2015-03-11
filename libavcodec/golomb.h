2e2985b3d48dc919c9f49ad9cd535e0c8644084b
Michael Niedermayer <michaelni@gmx.at>
2015-03-11 16:47:13 +0100

    avcodec/golomb: Fix undefined shifts in unsigned rice decoding code
    Found-by: Clang -fsanitize=shift
    Reported-by: Thierry Foucu <tfoucu@google.com>
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/golomb.h
+++ libavcodec/golomb.h
@@ -285,7 +285,7 @@ static inline int get_ur_golomb(GetBitContext *gb, int k, int limit,
 
     if (log > 31 - limit) {
         buf >>= log - k;
-        buf  += (30 - log) << k;
+        buf  += (30U - log) << k;
         LAST_SKIP_BITS(re, gb, 32 + k - log);
         CLOSE_READER(re, gb);
 
@@ -321,7 +321,7 @@ static inline int get_ur_golomb_jpegls(GetBitContext *gb, int k, int limit,
     if (log - k >= 32 - MIN_CACHE_BITS + (MIN_CACHE_BITS == 32) &&
         32 - log < limit) {
         buf >>= log - k;
-        buf  += (30 - log) << k;
+        buf  += (30U - log) << k;
         LAST_SKIP_BITS(re, gb, 32 + k - log);
         CLOSE_READER(re, gb);
 
