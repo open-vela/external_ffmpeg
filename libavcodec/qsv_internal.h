61317f1755ecc385005cc575c6c0dc43703ff591
openvela-robot <openvela-robot@xiaomi.com>
2015-03-28 00:16:24 +0800

    fate: simplify filter-pp tests
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/qsv_internal.h
+++ libavcodec/qsv_internal.h
@@ -1,29 +1,37 @@
 /*
- * Intel MediaSDK QSV encoder/decoder shared code
+ * Intel MediaSDK QSV utility functions
  *
- * This file is part of Libav.
+ * copyright (c) 2013 Luca Barbato
  *
- * Libav is free software; you can redistribute it and/or
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifndef AVCODEC_QSV_INTERNAL_H
 #define AVCODEC_QSV_INTERNAL_H
 
+#include <stdint.h>
+#include <sys/types.h>
+
 #include <mfx/mfxvideo.h>
 
 #include "libavutil/frame.h"
+#include "libavutil/pixfmt.h"
+
+#include "avcodec.h"
 
 #define QSV_VERSION_MAJOR 1
 #define QSV_VERSION_MINOR 1
@@ -39,13 +47,40 @@ typedef struct QSVFrame {
     struct QSVFrame *next;
 } QSVFrame;
 
+typedef struct QSVContext {
+    // the session used for decoding
+    mfxSession session;
+
+    // the session we allocated internally, in case the caller did not provide
+    // one
+    mfxSession internal_session;
+
+    /**
+     * a linked list of frames currently being used by QSV
+     */
+    QSVFrame *work_frames;
+
+    // options set by the caller
+    int async_depth;
+    int iopattern;
+
+    mfxExtBuffer **ext_buffers;
+    int         nb_ext_buffers;
+} QSVContext;
+
 /**
- * Convert a libmfx error code into a libav error code.
+ * Convert a libmfx error code into a ffmpeg error code.
  */
 int ff_qsv_error(int mfx_err);
 
-int ff_qsv_codec_id_to_mfx(enum AVCodecID codec_id);
+int ff_qsv_map_pixfmt(enum AVPixelFormat format);
 
-int ff_qsv_init_internal_session(AVCodecContext *avctx, mfxSession *session);
+int ff_qsv_init(AVCodecContext *s, QSVContext *q, mfxSession session);
+
+int ff_qsv_decode(AVCodecContext *s, QSVContext *q,
+                  AVFrame *frame, int *got_frame,
+                  AVPacket *avpkt);
+
+int ff_qsv_close(QSVContext *q);
 
 #endif /* AVCODEC_QSV_INTERNAL_H */
