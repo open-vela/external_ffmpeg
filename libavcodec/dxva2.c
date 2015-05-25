b1e7385388a8733e77dcf5b80590395901ab8119
openvela-robot <openvela-robot@xiaomi.com>
2015-05-25 12:13:53 +0800

    ffmpeg_opt: Set the video VBV parameters only for the video stream from -target
    Signed-off-by: Michael Niedermayer <michaelni@gmx.at>

--- libavcodec/dxva2.c
+++ libavcodec/dxva2.c
@@ -3,20 +3,20 @@
  *
  * copyright (c) 2010 Laurent Aimar
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -35,15 +35,14 @@ void *ff_dxva2_get_surface(const AVFrame *frame)
     return frame->data[3];
 }
 
-unsigned ff_dxva2_get_surface_index(const AVCodecContext *avctx,
-                                    const AVDXVAContext *ctx,
+unsigned ff_dxva2_get_surface_index(const struct dxva_context *ctx,
                                     const AVFrame *frame)
 {
     void *surface = ff_dxva2_get_surface(frame);
     unsigned i;
 
-    for (i = 0; i < DXVA_CONTEXT_COUNT(avctx, ctx); i++)
-        if (DXVA_CONTEXT_SURFACE(avctx, ctx, i) == surface)
+    for (i = 0; i < ctx->surface_count; i++)
+        if (ctx->surface[i] == surface)
             return i;
 
     assert(0);
@@ -51,8 +50,8 @@ unsigned ff_dxva2_get_surface_index(const AVCodecContext *avctx,
 }
 
 int ff_dxva2_commit_buffer(AVCodecContext *avctx,
-                           AVDXVAContext *ctx,
-                           DECODER_BUFFER_DESC *dsc,
+                           struct dxva_context *ctx,
+                           DXVA2_DecodeBufferDesc *dsc,
                            unsigned type, const void *data, unsigned size,
                            unsigned mb_count)
 {
@@ -61,18 +60,8 @@ int ff_dxva2_commit_buffer(AVCodecContext *avctx,
     int      result;
     HRESULT hr;
 
-#if CONFIG_D3D11VA
-    if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD)
-        hr = ID3D11VideoContext_GetDecoderBuffer(D3D11VA_CONTEXT(ctx)->video_context,
-                                                 D3D11VA_CONTEXT(ctx)->decoder,
-                                                 type,
-                                                 &dxva_size, &dxva_data);
-#endif
-#if CONFIG_DXVA2
-    if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD)
-        hr = IDirectXVideoDecoder_GetBuffer(DXVA2_CONTEXT(ctx)->decoder, type,
-                                            &dxva_data, &dxva_size);
-#endif
+    hr = IDirectXVideoDecoder_GetBuffer(ctx->decoder, type,
+                                        &dxva_data, &dxva_size);
     if (FAILED(hr)) {
         av_log(avctx, AV_LOG_ERROR, "Failed to get a buffer for %u: 0x%lx\n",
                type, hr);
@@ -81,24 +70,10 @@ int ff_dxva2_commit_buffer(AVCodecContext *avctx,
     if (size <= dxva_size) {
         memcpy(dxva_data, data, size);
 
-#if CONFIG_D3D11VA
-        if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD) {
-            D3D11_VIDEO_DECODER_BUFFER_DESC *dsc11 = dsc;
-            memset(dsc11, 0, sizeof(*dsc11));
-            dsc11->BufferType           = type;
-            dsc11->DataSize             = size;
-            dsc11->NumMBsInBuffer       = mb_count;
-        }
-#endif
-#if CONFIG_DXVA2
-        if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD) {
-            DXVA2_DecodeBufferDesc *dsc2 = dsc;
-            memset(dsc2, 0, sizeof(*dsc2));
-            dsc2->CompressedBufferType = type;
-            dsc2->DataSize             = size;
-            dsc2->NumMBsInBuffer       = mb_count;
-        }
-#endif
+        memset(dsc, 0, sizeof(*dsc));
+        dsc->CompressedBufferType = type;
+        dsc->DataSize             = size;
+        dsc->NumMBsInBuffer       = mb_count;
 
         result = 0;
     } else {
@@ -106,14 +81,7 @@ int ff_dxva2_commit_buffer(AVCodecContext *avctx,
         result = -1;
     }
 
-#if CONFIG_D3D11VA
-    if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD)
-        hr = ID3D11VideoContext_ReleaseDecoderBuffer(D3D11VA_CONTEXT(ctx)->video_context, D3D11VA_CONTEXT(ctx)->decoder, type);
-#endif
-#if CONFIG_DXVA2
-    if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD)
-        hr = IDirectXVideoDecoder_ReleaseBuffer(DXVA2_CONTEXT(ctx)->decoder, type);
-#endif
+    hr = IDirectXVideoDecoder_ReleaseBuffer(ctx->decoder, type);
     if (FAILED(hr)) {
         av_log(avctx, AV_LOG_ERROR,
                "Failed to release buffer type %u: 0x%lx\n",
@@ -127,31 +95,20 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, AVFrame *frame,
                               const void *pp, unsigned pp_size,
                               const void *qm, unsigned qm_size,
                               int (*commit_bs_si)(AVCodecContext *,
-                                                  DECODER_BUFFER_DESC *bs,
-                                                  DECODER_BUFFER_DESC *slice))
+                                                  DXVA2_DecodeBufferDesc *bs,
+                                                  DXVA2_DecodeBufferDesc *slice))
 {
-    AVDXVAContext *ctx = avctx->hwaccel_context;
+    struct dxva_context *ctx = avctx->hwaccel_context;
     unsigned               buffer_count = 0;
-    D3D11_VIDEO_DECODER_BUFFER_DESC buffer11[4];
-    DXVA2_DecodeBufferDesc          buffer2[4];
-    DECODER_BUFFER_DESC             *buffer,*buffer_slice;
+    DXVA2_DecodeBufferDesc buffer[4];
+    DXVA2_DecodeExecuteParams exec = { 0 };
     int result, runs = 0;
     HRESULT hr;
-    unsigned type;
 
     do {
-#if CONFIG_D3D11VA
-        if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD)
-            hr = ID3D11VideoContext_DecoderBeginFrame(D3D11VA_CONTEXT(ctx)->video_context, D3D11VA_CONTEXT(ctx)->decoder,
-                                                      ff_dxva2_get_surface(frame),
-                                                      0, NULL);
-#endif
-#if CONFIG_DXVA2
-        if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD)
-            hr = IDirectXVideoDecoder_BeginFrame(DXVA2_CONTEXT(ctx)->decoder,
-                                                 ff_dxva2_get_surface(frame),
-                                                 NULL);
-#endif
+        hr = IDirectXVideoDecoder_BeginFrame(ctx->decoder,
+                                             ff_dxva2_get_surface(frame),
+                                             NULL);
         if (hr == E_PENDING)
             av_usleep(2000);
     } while (hr == E_PENDING && ++runs < 50);
@@ -161,20 +118,8 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, AVFrame *frame,
         return -1;
     }
 
-#if CONFIG_D3D11VA
-    if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD) {
-        buffer = &buffer11[buffer_count];
-        type = D3D11_VIDEO_DECODER_BUFFER_PICTURE_PARAMETERS;
-    }
-#endif
-#if CONFIG_DXVA2
-    if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD) {
-        buffer = &buffer2[buffer_count];
-        type = DXVA2_PictureParametersBufferType;
-    }
-#endif
-    result = ff_dxva2_commit_buffer(avctx, ctx, buffer,
-                                    type,
+    result = ff_dxva2_commit_buffer(avctx, ctx, &buffer[buffer_count],
+                                    DXVA2_PictureParametersBufferType,
                                     pp, pp_size, 0);
     if (result) {
         av_log(avctx, AV_LOG_ERROR,
@@ -184,20 +129,8 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, AVFrame *frame,
     buffer_count++;
 
     if (qm_size > 0) {
-#if CONFIG_D3D11VA
-        if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD) {
-            buffer = &buffer11[buffer_count];
-            type = D3D11_VIDEO_DECODER_BUFFER_INVERSE_QUANTIZATION_MATRIX;
-        }
-#endif
-#if CONFIG_DXVA2
-        if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD) {
-            buffer = &buffer2[buffer_count];
-            type = DXVA2_InverseQuantizationMatrixBufferType;
-        }
-#endif
-        result = ff_dxva2_commit_buffer(avctx, ctx, buffer,
-                                        type,
+        result = ff_dxva2_commit_buffer(avctx, ctx, &buffer[buffer_count],
+                                        DXVA2_InverseQuantizationMatrixBufferType,
                                         qm, qm_size, 0);
         if (result) {
             av_log(avctx, AV_LOG_ERROR,
@@ -207,22 +140,9 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, AVFrame *frame,
         buffer_count++;
     }
 
-#if CONFIG_D3D11VA
-    if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD) {
-        buffer       = &buffer11[buffer_count + 0];
-        buffer_slice = &buffer11[buffer_count + 1];
-    }
-#endif
-#if CONFIG_DXVA2
-    if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD) {
-        buffer       = &buffer2[buffer_count + 0];
-        buffer_slice = &buffer2[buffer_count + 1];
-    }
-#endif
-
     result = commit_bs_si(avctx,
-                          buffer,
-                          buffer_slice);
+                          &buffer[buffer_count + 0],
+                          &buffer[buffer_count + 1]);
     if (result) {
         av_log(avctx, AV_LOG_ERROR,
                "Failed to add bitstream or slice control buffer\n");
@@ -234,36 +154,17 @@ int ff_dxva2_common_end_frame(AVCodecContext *avctx, AVFrame *frame,
 
     assert(buffer_count == 1 + (qm_size > 0) + 2);
 
-#if CONFIG_D3D11VA
-    if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD)
-        hr = ID3D11VideoContext_SubmitDecoderBuffers(D3D11VA_CONTEXT(ctx)->video_context,
-                                                     D3D11VA_CONTEXT(ctx)->decoder,
-                                                     buffer_count, buffer11);
-#endif
-#if CONFIG_DXVA2
-    if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD) {
-        DXVA2_DecodeExecuteParams exec = {
-            .NumCompBuffers     = buffer_count,
-            .pCompressedBuffers = buffer2,
-            .pExtensionData     = NULL,
-        };
-        hr = IDirectXVideoDecoder_Execute(DXVA2_CONTEXT(ctx)->decoder, &exec);
-    }
-#endif
+    exec.NumCompBuffers      = buffer_count;
+    exec.pCompressedBuffers  = buffer;
+    exec.pExtensionData      = NULL;
+    hr = IDirectXVideoDecoder_Execute(ctx->decoder, &exec);
     if (FAILED(hr)) {
         av_log(avctx, AV_LOG_ERROR, "Failed to execute: 0x%lx\n", hr);
         result = -1;
     }
 
 end:
-#if CONFIG_D3D11VA
-    if (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD)
-        hr = ID3D11VideoContext_DecoderEndFrame(D3D11VA_CONTEXT(ctx)->video_context, D3D11VA_CONTEXT(ctx)->decoder);
-#endif
-#if CONFIG_DXVA2
-    if (avctx->pix_fmt == AV_PIX_FMT_DXVA2_VLD)
-        hr = IDirectXVideoDecoder_EndFrame(DXVA2_CONTEXT(ctx)->decoder, NULL);
-#endif
+    hr = IDirectXVideoDecoder_EndFrame(ctx->decoder, NULL);
     if (FAILED(hr)) {
         av_log(avctx, AV_LOG_ERROR, "Failed to end frame: 0x%lx\n", hr);
         result = -1;
--- libavcodec/dxva2_internal.h
+++ libavcodec/dxva2_internal.h
@@ -3,20 +3,20 @@
  *
  * copyright (c) 2010 Laurent Aimar
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -28,7 +28,6 @@
 #include "config.h"
 
 #include "dxva2.h"
-#include "d3d11va.h"
 #if HAVE_DXVA_H
 #include <dxva.h>
 #endif
@@ -36,34 +35,13 @@
 #include "avcodec.h"
 #include "mpegvideo.h"
 
-typedef void DECODER_BUFFER_DESC;
-
-typedef union {
-    struct AVD3D11VAContext  d3d11va;
-    struct dxva_context      dxva2;
-} AVDXVAContext;
-
-#define D3D11VA_CONTEXT(ctx) (&ctx->d3d11va)
-#define DXVA2_CONTEXT(ctx)   (&ctx->dxva2)
-
-#define DXVA_CONTEXT_WORKAROUND(avctx, ctx)     (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.workaround : ctx->dxva2.workaround)
-#define DXVA_CONTEXT_COUNT(avctx, ctx)          (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.surface_count : ctx->dxva2.surface_count)
-#define DXVA_CONTEXT_SURFACE(avctx, ctx, i)     (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.surface[i] : ctx->dxva2.surface[i])
-#define DXVA_CONTEXT_DECODER(avctx, ctx)        (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.decoder : ctx->dxva2.decoder)
-#define DXVA_CONTEXT_REPORT_ID(avctx, ctx)      (*(avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? &ctx->d3d11va.report_id : &ctx->dxva2.report_id))
-#define DXVA_CONTEXT_CFG(avctx, ctx)            (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.cfg : ctx->dxva2.cfg)
-#define DXVA_CONTEXT_CFG_BITSTREAM(avctx, ctx)  (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.cfg->ConfigBitstreamRaw : ctx->dxva2.cfg->ConfigBitstreamRaw)
-#define DXVA_CONTEXT_CFG_INTRARESID(avctx, ctx) (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.cfg->ConfigIntraResidUnsigned : ctx->dxva2.cfg->ConfigIntraResidUnsigned)
-#define DXVA_CONTEXT_CFG_RESIDACCEL(avctx, ctx) (avctx->pix_fmt == AV_PIX_FMT_D3D11VA_VLD ? ctx->d3d11va.cfg->ConfigResidDiffAccelerator : ctx->dxva2.cfg->ConfigResidDiffAccelerator)
-
 void *ff_dxva2_get_surface(const AVFrame *frame);
 
-unsigned ff_dxva2_get_surface_index(const AVCodecContext *avctx,
-                                    const AVDXVAContext *,
+unsigned ff_dxva2_get_surface_index(const struct dxva_context *,
                                     const AVFrame *frame);
 
-int ff_dxva2_commit_buffer(AVCodecContext *, AVDXVAContext *,
-                           DECODER_BUFFER_DESC *,
+int ff_dxva2_commit_buffer(AVCodecContext *, struct dxva_context *,
+                           DXVA2_DecodeBufferDesc *,
                            unsigned type, const void *data, unsigned size,
                            unsigned mb_count);
 
@@ -72,7 +50,7 @@ int ff_dxva2_common_end_frame(AVCodecContext *, AVFrame *,
                               const void *pp, unsigned pp_size,
                               const void *qm, unsigned qm_size,
                               int (*commit_bs_si)(AVCodecContext *,
-                                                  DECODER_BUFFER_DESC *bs,
-                                                  DECODER_BUFFER_DESC *slice));
+                                                  DXVA2_DecodeBufferDesc *bs,
+                                                  DXVA2_DecodeBufferDesc *slice));
 
 #endif /* AVCODEC_DXVA_INTERNAL_H */
