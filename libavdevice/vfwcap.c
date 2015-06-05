be9e4aa4526b3e1cbffff2bb44ef0eae492d695b
openvela-robot <openvela-robot@xiaomi.com>
2015-06-05 15:07:06 +0800

    configure: Support MSVC 2015
    The C runtime C99 compatibility had been improved a lot and it now
    rejects some of the compatibility defines provided for the older
    versions.
    
    Many thanks to Ray for the time spent testing.
    
    Bug-Id: 864
    CC: libav-stable@libav.org

--- libavdevice/vfwcap.c
+++ libavdevice/vfwcap.c
@@ -2,20 +2,20 @@
  * VFW capture interface
  * Copyright (c) 2006-2008 Ramiro Polla
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -24,6 +24,7 @@
 #include "libavutil/opt.h"
 #include "libavutil/parseutils.h"
 
+#include "libavformat/avformat.h"
 #include "libavformat/internal.h"
 
 // windows.h must no be included before winsock2.h, and libavformat internal
@@ -31,8 +32,6 @@
 #include <windows.h>
 // windows.h needs to be included before vfw.h
 #include <vfw.h>
-
-#include "avdevice.h"
 
 /* Some obsolete versions of MinGW32 before 4.0.0 lack this. */
 #ifndef HWND_MESSAGE
@@ -161,7 +160,7 @@ static void dump_bih(AVFormatContext *s, BITMAPINFOHEADER *bih)
 static int shall_we_drop(AVFormatContext *s)
 {
     struct vfw_ctx *ctx = s->priv_data;
-    static const uint8_t dropscore[] = {62, 75, 87, 100};
+    const uint8_t dropscore[] = {62, 75, 87, 100};
     const int ndropscores = FF_ARRAY_ELEMS(dropscore);
     unsigned int buffer_fullness = (ctx->curbufsize*100)/s->max_picture_buffer;
 
@@ -249,7 +248,7 @@ static int vfw_read_header(AVFormatContext *s)
     AVStream *st;
     int devnum;
     int bisize;
-    BITMAPINFO *bi = NULL;
+    BITMAPINFO *bi;
     CAPTUREPARMS cparms;
     DWORD biCompression;
     WORD biBitCount;
@@ -295,7 +294,7 @@ static int vfw_read_header(AVFormatContext *s)
                       (LPARAM) videostream_cb);
     if(!ret) {
         av_log(s, AV_LOG_ERROR, "Could not set video stream callback.\n");
-        goto fail;
+        goto fail_io;
     }
 
     SetWindowLongPtr(ctx->hwnd, GWLP_USERDATA, (LONG_PTR) s);
@@ -309,7 +308,7 @@ static int vfw_read_header(AVFormatContext *s)
     /* Set video format */
     bisize = SendMessage(ctx->hwnd, WM_CAP_GET_VIDEOFORMAT, 0, 0);
     if(!bisize)
-        goto fail;
+        goto fail_io;
     bi = av_malloc(bisize);
     if(!bi) {
         vfw_read_close(s);
@@ -317,21 +316,16 @@ static int vfw_read_header(AVFormatContext *s)
     }
     ret = SendMessage(ctx->hwnd, WM_CAP_GET_VIDEOFORMAT, bisize, (LPARAM) bi);
     if(!ret)
-        goto fail;
+        goto fail_bi;
 
     dump_bih(s, &bi->bmiHeader);
 
-    ret = av_parse_video_rate(&framerate_q, ctx->framerate);
-    if (ret < 0) {
-        av_log(s, AV_LOG_ERROR, "Could not parse framerate '%s'.\n", ctx->framerate);
-        goto fail;
-    }
 
     if (ctx->video_size) {
         ret = av_parse_video_size(&bi->bmiHeader.biWidth, &bi->bmiHeader.biHeight, ctx->video_size);
         if (ret < 0) {
             av_log(s, AV_LOG_ERROR, "Couldn't parse video size.\n");
-            goto fail;
+            goto fail_bi;
         }
     }
 
@@ -350,17 +344,19 @@ static int vfw_read_header(AVFormatContext *s)
     ret = SendMessage(ctx->hwnd, WM_CAP_SET_VIDEOFORMAT, bisize, (LPARAM) bi);
     if(!ret) {
         av_log(s, AV_LOG_ERROR, "Could not set Video Format.\n");
-        goto fail;
+        goto fail_bi;
     }
 
     biCompression = bi->bmiHeader.biCompression;
     biBitCount = bi->bmiHeader.biBitCount;
 
+    av_free(bi);
+
     /* Set sequence setup */
     ret = SendMessage(ctx->hwnd, WM_CAP_GET_SEQUENCE_SETUP, sizeof(cparms),
                       (LPARAM) &cparms);
     if(!ret)
-        goto fail;
+        goto fail_io;
 
     dump_captureparms(s, &cparms);
 
@@ -375,10 +371,10 @@ static int vfw_read_header(AVFormatContext *s)
     ret = SendMessage(ctx->hwnd, WM_CAP_SET_SEQUENCE_SETUP, sizeof(cparms),
                       (LPARAM) &cparms);
     if(!ret)
-        goto fail;
+        goto fail_io;
 
     codec = st->codec;
-    codec->time_base = av_inv_q(framerate_q);
+    codec->time_base = (AVRational){framerate_q.den, framerate_q.num};
     codec->codec_type = AVMEDIA_TYPE_VIDEO;
     codec->width  = bi->bmiHeader.biWidth;
     codec->height = bi->bmiHeader.biHeight;
@@ -404,31 +400,31 @@ static int vfw_read_header(AVFormatContext *s)
         }
     }
 
-    av_freep(&bi);
-
     avpriv_set_pts_info(st, 32, 1, 1000);
 
     ctx->mutex = CreateMutex(NULL, 0, NULL);
     if(!ctx->mutex) {
         av_log(s, AV_LOG_ERROR, "Could not create Mutex.\n" );
-        goto fail;
+        goto fail_io;
     }
     ctx->event = CreateEvent(NULL, 1, 0, NULL);
     if(!ctx->event) {
         av_log(s, AV_LOG_ERROR, "Could not create Event.\n" );
-        goto fail;
+        goto fail_io;
     }
 
     ret = SendMessage(ctx->hwnd, WM_CAP_SEQUENCE_NOFILE, 0, 0);
     if(!ret) {
         av_log(s, AV_LOG_ERROR, "Could not start capture sequence.\n" );
-        goto fail;
+        goto fail_io;
     }
 
     return 0;
 
-fail:
-    av_freep(&bi);
+fail_bi:
+    av_free(bi);
+
+fail_io:
     vfw_read_close(s);
     return AVERROR(EIO);
 }
@@ -475,7 +471,6 @@ static const AVClass vfw_class = {
     .item_name  = av_default_item_name,
     .option     = options,
     .version    = LIBAVUTIL_VERSION_INT,
-    .category   = AV_CLASS_CATEGORY_DEVICE_VIDEO_INPUT
 };
 
 AVInputFormat ff_vfwcap_demuxer = {
