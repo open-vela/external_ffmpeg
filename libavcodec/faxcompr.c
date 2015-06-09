67e4b4ff60566e52cebc49c8482a96aa27a78ab8
openvela-robot <openvela-robot@xiaomi.com>
2015-06-09 12:27:47 +0800

    movenc: fixes a questionable valgrind uninitialized value warning
    display_matrix_size is only initialized when av_stream_get_side_data()
    returns a side data pointer. The code is safe since the only effect this
    has is setting the display_matrix pointer to NULL which it was already
    anyway.

--- libavcodec/faxcompr.c
+++ libavcodec/faxcompr.c
@@ -2,20 +2,20 @@
  * CCITT Fax Group 3 and 4 decompression
  * Copyright (c) 2008 Konstantin Shishkov
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -165,6 +165,8 @@ static int decode_group3_2d_line(AVCodecContext *avctx, GetBitContext *gb,
     int run_off       = *ref++;
     unsigned int offs = 0, run = 0;
 
+    runend--; // for the last written 0
+
     while (offs < width) {
         int cmode = get_vlc2(gb, ccitt_group3_2d_vlc.table, 9, 1);
         if (cmode == -1) {
@@ -172,12 +174,10 @@ static int decode_group3_2d_line(AVCodecContext *avctx, GetBitContext *gb,
             return AVERROR_INVALIDDATA;
         }
         if (!cmode) { //pass mode
-            if (run_off < width)
-                run_off += *ref++;
+            run_off += *ref++;
             run      = run_off - offs;
             offs     = run_off;
-            if (run_off < width)
-                run_off += *ref++;
+            run_off += *ref++;
             if (offs > width) {
                 av_log(avctx, AV_LOG_ERROR, "Run went out of bounds\n");
                 return AVERROR_INVALIDDATA;
@@ -230,19 +230,13 @@ static int decode_group3_2d_line(AVCodecContext *avctx, GetBitContext *gb,
             mode      = !mode;
         }
         //sync line pointers
-        while (offs < width && run_off <= offs) {
+        while (run_off <= offs) {
             run_off += *ref++;
             run_off += *ref++;
         }
     }
     *runs++ = saved_run;
-    if (saved_run) {
-        if (runs >= runend) {
-            av_log(avctx, AV_LOG_ERROR, "Run overrun\n");
-            return -1;
-        }
-        *runs++ = 0;
-    }
+    *runs++ = 0;
     return 0;
 }
 
@@ -251,7 +245,7 @@ static void put_line(uint8_t *dst, int size, int width, const int *runs)
     PutBitContext pb;
     int run, mode = ~0, pix_left = width, run_idx = 0;
 
-    init_put_bits(&pb, dst, size);
+    init_put_bits(&pb, dst, size * 8);
     while (pix_left > 0) {
         run       = runs[run_idx++];
         mode      = ~mode;
@@ -285,10 +279,9 @@ int ff_ccitt_unpack(AVCodecContext *avctx, const uint8_t *src, int srcsize,
     int *runs, *ref = NULL, *runend;
     int ret;
     int runsize = avctx->width + 2;
-    int has_eol;
 
-    runs = av_malloc_array(runsize, sizeof(runs[0]));
-    ref  = av_malloc_array(runsize, sizeof(ref[0]));
+    runs = av_malloc(runsize * sizeof(runs[0]));
+    ref  = av_malloc(runsize * sizeof(ref[0]));
     if (!runs || !ref) {
         ret = AVERROR(ENOMEM);
         goto fail;
@@ -296,10 +289,7 @@ int ff_ccitt_unpack(AVCodecContext *avctx, const uint8_t *src, int srcsize,
     ref[0] = avctx->width;
     ref[1] = 0;
     ref[2] = 0;
-    if ((ret = init_get_bits8(&gb, src, srcsize)) < 0)
-        goto fail;
-    has_eol = show_bits(&gb, 12) == 1 || show_bits(&gb, 16) == 1;
-
+    init_get_bits(&gb, src, srcsize * 8);
     for (j = 0; j < height; j++) {
         runend = runs + runsize;
         if (compr == TIFF_G4) {
@@ -310,7 +300,6 @@ int ff_ccitt_unpack(AVCodecContext *avctx, const uint8_t *src, int srcsize,
         } else {
             int g3d1 = (compr == TIFF_G3) && !(opts & 1);
             if (compr != TIFF_CCITT_RLE &&
-                has_eol &&
                 find_group3_syncmarker(&gb, srcsize * 8) < 0)
                 break;
             if (compr == TIFF_CCITT_RLE || g3d1 || get_bits1(&gb))
