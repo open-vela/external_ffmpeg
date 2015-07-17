623ee158fe80228552b4557376ddee681d8281c5
openvela-robot <openvela-robot@xiaomi.com>
2015-07-17 20:27:25 +0800

    avfilter/af_aresample: Check ff_all_* for allocation failures
    Fixes: signal_sigabrt_7ffff70eccc9_498_divx502.avi with memlimit 1572864
    
    Found-by: Samuel Groß, Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tests/checkasm/checkasm.h
+++ tests/checkasm/checkasm.h
@@ -3,20 +3,20 @@
  * Copyright (c) 2015 Henrik Gramner
  * Copyright (c) 2008 Loren Merritt
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or modify
+ * FFmpeg is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with Libav; if not, write to the Free Software Foundation, Inc.,
+ * with FFmpeg; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
@@ -29,7 +29,6 @@
 #include "libavutil/lfg.h"
 #include "libavutil/timer.h"
 
-void checkasm_check_bswapdsp(void);
 void checkasm_check_h264pred(void);
 void checkasm_check_h264qpel(void);
 
@@ -111,7 +110,7 @@ void checkasm_stack_clobber(uint64_t clobber, ...);
         }\
     } while (0)
 #else
-#define bench_new(...)
+#define bench_new(...) while(0)
 #endif
 
 #endif
