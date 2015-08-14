36fc02c0a1ba5c73ed322807cd8efa8b17151029
openvela-robot <openvela-robot@xiaomi.com>
2015-08-14 11:05:30 +0800

    libfdk-aacdec: Always decode into an intermediate buffer
    For ADTS streams, the output format (number of channels, frame size)
    can change at any point (with the latest version of fdk-aac, the decoder
    seems to change format after a handful of frames, not outputting the
    right format immediately, for cases that worked fine with the earlier
    version of the lib).
    
    Previously, the decoder decoded straight into the output frame once the
    number of channels and frame size was known. This obviously does not
    work if the number of channels or frame size changes.
    
    The alternative would be to allocate the AVFrame with the maximum number
    of channels and frame size, and change them afterward decoding into it,
    but that may cause confusion to users e.g. of the get_buffer callback.
    This solution should be more robust.
    
    CC: libav-stable@libav.org
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/riffdec.c
+++ libavformat/riffdec.c
@@ -2,20 +2,20 @@
  * RIFF demuxing functions and data
  * Copyright (c) 2000 Fabrice Bellard
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -29,17 +29,13 @@
 #include "avio_internal.h"
 #include "riff.h"
 
-int ff_get_guid(AVIOContext *s, ff_asf_guid *g)
-{
-    int ret;
-    av_assert0(sizeof(*g) == 16); //compiler will optimize this out
-    ret = avio_read(s, *g, sizeof(*g));
-    if (ret < (int)sizeof(*g)) {
-        memset(*g, 0, sizeof(*g));
-        return ret < 0 ? ret : AVERROR_INVALIDDATA;
-    }
-    return 0;
-}
+const AVCodecGuid ff_codec_wav_guids[] = {
+    { AV_CODEC_ID_AC3,      { 0x2C, 0x80, 0x6D, 0xE0, 0x46, 0xDB, 0xCF, 0x11, 0xB4, 0xD1, 0x00, 0x80, 0x5F, 0x6C, 0xBB, 0xEA } },
+    { AV_CODEC_ID_ATRAC3P,  { 0xBF, 0xAA, 0x23, 0xE9, 0x58, 0xCB, 0x71, 0x44, 0xA1, 0x19, 0xFF, 0xFA, 0x01, 0xE4, 0xCE, 0x62 } },
+    { AV_CODEC_ID_EAC3,     { 0xAF, 0x87, 0xFB, 0xA7, 0x02, 0x2D, 0xFB, 0x42, 0xA4, 0xD4, 0x05, 0xCD, 0x93, 0x84, 0x3B, 0xDD } },
+    { AV_CODEC_ID_MP2,      { 0x2B, 0x80, 0x6D, 0xE0, 0x46, 0xDB, 0xCF, 0x11, 0xB4, 0xD1, 0x00, 0x80, 0x5F, 0x6C, 0xBB, 0xEA } },
+    { AV_CODEC_ID_NONE }
+};
 
 enum AVCodecID ff_codec_guid_get_id(const AVCodecGuid *guids, ff_asf_guid guid)
 {
@@ -61,10 +57,7 @@ enum AVCodecID ff_codec_guid_get_id(const AVCodecGuid *guids, ff_asf_guid guid)
 static void parse_waveformatex(AVIOContext *pb, AVCodecContext *c)
 {
     ff_asf_guid subformat;
-    int bps = avio_rl16(pb);
-    if (bps)
-        c->bits_per_coded_sample = bps;
-
+    c->bits_per_coded_sample = avio_rl16(pb);
     c->channel_layout        = avio_rl32(pb); /* dwChannelMask */
 
     ff_get_guid(pb, &subformat);
@@ -82,41 +75,25 @@ static void parse_waveformatex(AVIOContext *pb, AVCodecContext *c)
     }
 }
 
-/* "big_endian" values are needed for RIFX file format */
 int ff_get_wav_header(AVFormatContext *s, AVIOContext *pb,
-                      AVCodecContext *codec, int size, int big_endian)
+                      AVCodecContext *codec, int size)
 {
     int id;
     uint64_t bitrate;
 
-    if (size < 14) {
-        avpriv_request_sample(codec, "wav header size < 14");
+    if (size < 14)
         return AVERROR_INVALIDDATA;
-    }
 
+    id                 = avio_rl16(pb);
     codec->codec_type  = AVMEDIA_TYPE_AUDIO;
-    if (!big_endian) {
-        id                 = avio_rl16(pb);
-        codec->channels    = avio_rl16(pb);
-        codec->sample_rate = avio_rl32(pb);
-        bitrate            = avio_rl32(pb) * 8LL;
-        codec->block_align = avio_rl16(pb);
-    } else {
-        id                 = avio_rb16(pb);
-        codec->channels    = avio_rb16(pb);
-        codec->sample_rate = avio_rb32(pb);
-        bitrate            = avio_rb32(pb) * 8LL;
-        codec->block_align = avio_rb16(pb);
-    }
+    codec->channels    = avio_rl16(pb);
+    codec->sample_rate = avio_rl32(pb);
+    bitrate            = avio_rl32(pb) * 8;
+    codec->block_align = avio_rl16(pb);
     if (size == 14) {  /* We're dealing with plain vanilla WAVEFORMAT */
         codec->bits_per_coded_sample = 8;
-    } else {
-        if (!big_endian) {
-            codec->bits_per_coded_sample = avio_rl16(pb);
-        } else {
-            codec->bits_per_coded_sample = avio_rb16(pb);
-        }
-    }
+    } else
+        codec->bits_per_coded_sample = avio_rl16(pb);
     if (id == 0xFFFE) {
         codec->codec_tag = 0;
     } else {
@@ -126,10 +103,6 @@ int ff_get_wav_header(AVFormatContext *s, AVIOContext *pb,
     }
     if (size >= 18) {  /* We're obviously dealing with WAVEFORMATEX */
         int cbSize = avio_rl16(pb); /* cbSize */
-        if (big_endian) {
-            avpriv_report_missing_feature(codec, "WAVEFORMATEX support for RIFX files\n");
-            return AVERROR_PATCHWELCOME;
-        }
         size  -= 18;
         cbSize = FFMIN(size, cbSize);
         if (cbSize >= 22 && id == 0xfffe) { /* WAVEFORMATEXTENSIBLE */
@@ -137,10 +110,14 @@ int ff_get_wav_header(AVFormatContext *s, AVIOContext *pb,
             cbSize -= 22;
             size   -= 22;
         }
+        codec->extradata_size = cbSize;
         if (cbSize > 0) {
-            av_freep(&codec->extradata);
-            if (ff_get_extradata(codec, pb, cbSize) < 0)
+            av_free(codec->extradata);
+            codec->extradata = av_mallocz(codec->extradata_size +
+                                          AV_INPUT_BUFFER_PADDING_SIZE);
+            if (!codec->extradata)
                 return AVERROR(ENOMEM);
+            avio_read(pb, codec->extradata, codec->extradata_size);
             size -= cbSize;
         }
 
@@ -177,7 +154,7 @@ int ff_get_wav_header(AVFormatContext *s, AVIOContext *pb,
         codec->sample_rate = 0;
     }
     /* override bits_per_coded_sample for G.726 */
-    if (codec->codec_id == AV_CODEC_ID_ADPCM_G726 && codec->sample_rate)
+    if (codec->codec_id == AV_CODEC_ID_ADPCM_G726)
         codec->bits_per_coded_sample = codec->bit_rate / codec->sample_rate;
 
     return 0;
@@ -200,11 +177,10 @@ enum AVCodecID ff_wav_codec_get_id(unsigned int tag, int bps)
     return id;
 }
 
-int ff_get_bmp_header(AVIOContext *pb, AVStream *st, unsigned *esize)
+int ff_get_bmp_header(AVIOContext *pb, AVStream *st)
 {
     int tag1;
-    if(esize) *esize  = avio_rl32(pb);
-    else                avio_rl32(pb);
+    avio_rl32(pb); /* size */
     st->codec->width  = avio_rl32(pb);
     st->codec->height = (int32_t)avio_rl32(pb);
     avio_rl16(pb); /* planes */
@@ -235,23 +211,12 @@ int ff_read_riff_info(AVFormatContext *s, int64_t size)
 
         chunk_code = avio_rl32(pb);
         chunk_size = avio_rl32(pb);
-        if (avio_feof(pb)) {
-            if (chunk_code || chunk_size) {
-                av_log(s, AV_LOG_WARNING, "INFO subchunk truncated\n");
-                return AVERROR_INVALIDDATA;
-            }
-            return AVERROR_EOF;
-        }
+
         if (chunk_size > end ||
             end - chunk_size < cur ||
             chunk_size == UINT_MAX) {
-            avio_seek(pb, -9, SEEK_CUR);
-            chunk_code = avio_rl32(pb);
-            chunk_size = avio_rl32(pb);
-            if (chunk_size > end || end - chunk_size < cur || chunk_size == UINT_MAX) {
-                av_log(s, AV_LOG_WARNING, "too big INFO subchunk\n");
-                return AVERROR_INVALIDDATA;
-            }
+            av_log(s, AV_LOG_WARNING, "too big INFO subchunk\n");
+            break;
         }
 
         chunk_size += (chunk_size & 1);
@@ -266,7 +231,7 @@ int ff_read_riff_info(AVFormatContext *s, int64_t size)
             continue;
         }
 
-        value = av_mallocz(chunk_size + 1);
+        value = av_malloc(chunk_size + 1);
         if (!value) {
             av_log(s, AV_LOG_ERROR,
                    "out of memory, unable to read INFO tag\n");
@@ -276,9 +241,13 @@ int ff_read_riff_info(AVFormatContext *s, int64_t size)
         AV_WL32(key, chunk_code);
 
         if (avio_read(pb, value, chunk_size) != chunk_size) {
+            av_free(value);
             av_log(s, AV_LOG_WARNING,
                    "premature end of file while reading INFO tag\n");
+            break;
         }
+
+        value[chunk_size] = 0;
 
         av_dict_set(&s->metadata, key, value, AV_DICT_DONT_STRDUP_VAL);
     }
--- libavformat/rtpdec_xiph.c
+++ libavformat/rtpdec_xiph.c
@@ -3,20 +3,20 @@
  * Copyright (c) 2009 Colin McQuillian
  * Copyright (c) 2010 Josh Allmann
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -28,13 +28,13 @@
  */
 
 #include "libavutil/attributes.h"
-#include "libavutil/avassert.h"
 #include "libavutil/avstring.h"
 #include "libavutil/base64.h"
 #include "libavcodec/bytestream.h"
 
+#include <assert.h>
+
 #include "avio_internal.h"
-#include "internal.h"
 #include "rtpdec.h"
 #include "rtpdec_formats.h"
 
@@ -53,7 +53,7 @@ struct PayloadContext {
 static void xiph_close_context(PayloadContext * data)
 {
     ffio_free_dyn_buf(&data->fragment);
-    av_freep(&data->split_buf);
+    av_free(data->split_buf);
 }
 
 
@@ -73,7 +73,7 @@ static int xiph_handle_packet(AVFormatContext *ctx, PayloadContext *data,
         }
         pkt_len = AV_RB16(data->split_buf + data->split_pos);
         data->split_pos += 2;
-        if (pkt_len > data->split_buf_len - data->split_pos) {
+        if (data->split_pos + pkt_len > data->split_buf_len) {
             av_log(ctx, AV_LOG_ERROR, "Not enough data to return\n");
             return AVERROR_INVALIDDATA;
         }
@@ -88,7 +88,7 @@ static int xiph_handle_packet(AVFormatContext *ctx, PayloadContext *data,
         return data->split_pkts > 0;
     }
 
-    if (len < 6 || len > INT_MAX/2) {
+    if (len < 6) {
         av_log(ctx, AV_LOG_ERROR, "Invalid %d byte packet\n", len);
         return AVERROR_INVALIDDATA;
     }
@@ -168,7 +168,7 @@ static int xiph_handle_packet(AVFormatContext *ctx, PayloadContext *data,
         data->timestamp = *timestamp;
 
     } else {
-        av_assert1(fragmented < 4);
+        assert(fragmented < 4);
         if (data->timestamp != *timestamp) {
             // skip if fragmented timestamp is incorrect;
             // a start packet has been lost somewhere
@@ -232,7 +232,7 @@ parse_packed_headers(const uint8_t * packed_headers,
 
     if (packed_headers_end - packed_headers < 9) {
         av_log(codec, AV_LOG_ERROR,
-               "Invalid %"PTRDIFF_SPECIFIER" byte packed header.",
+               "Invalid %td byte packed header.",
                packed_headers_end - packed_headers);
         return AVERROR_INVALIDDATA;
     }
@@ -254,7 +254,7 @@ parse_packed_headers(const uint8_t * packed_headers,
     if (packed_headers_end - packed_headers != length ||
         length1 > length || length2 > length - length1) {
         av_log(codec, AV_LOG_ERROR,
-               "Bad packed header lengths (%d,%d,%"PTRDIFF_SPECIFIER",%d)\n", length1,
+               "Bad packed header lengths (%d,%d,%td,%d)\n", length1,
                length2, packed_headers_end - packed_headers, length);
         return AVERROR_INVALIDDATA;
     }
@@ -265,11 +265,11 @@ parse_packed_headers(const uint8_t * packed_headers,
      * -- AV_INPUT_BUFFER_PADDING_SIZE required */
     extradata_alloc = length + length/255 + 3 + AV_INPUT_BUFFER_PADDING_SIZE;
 
-    if (ff_alloc_extradata(codec, extradata_alloc)) {
+    ptr = codec->extradata = av_malloc(extradata_alloc);
+    if (!ptr) {
         av_log(codec, AV_LOG_ERROR, "Out of memory\n");
         return AVERROR(ENOMEM);
     }
-    ptr = codec->extradata;
     *ptr++ = 2;
     ptr += av_xiphlacing(ptr, length1);
     ptr += av_xiphlacing(ptr, length2);
