926bee85941c673abdef03760b3a6f2cf96d5b45
openvela-robot <openvela-robot@xiaomi.com>
2015-09-09 15:32:42 +0800

    libopenh264enc: Use av_log() to log messages
    File libopenh264enc.c has been modified so that the encoder uses av_log()
    to log messages (error, warning, info, etc.) instead of logging them
    directly to stderr.  At the time the encoder is created, the current
    libav log level is mapped to an equivalent libopenh264 log level.  This
    log level, and a message logging function that invokes av_log() to
    actually log messages, are then set on the encoder.
    
    This contains further changes and simplifications by Michael Niedermayer
    and Martin Storsjö.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/qsv.h
+++ libavcodec/qsv.h
@@ -1,20 +1,20 @@
 /*
  * Intel MediaSDK QSV public API
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -23,10 +23,29 @@
 
 #include <mfx/mfxvideo.h>
 
+/**
+ * This struct is used for communicating QSV parameters between libavcodec and
+ * the caller. It is managed by the caller and must be assigned to
+ * AVCodecContext.hwaccel_context.
+ * - decoding: hwaccel_context must be set on return from the get_format()
+ *             callback
+ * - encoding: hwaccel_context must be set before avcodec_open2()
+ */
 typedef struct AVQSVContext {
+    /**
+     * If non-NULL, the session to use for encoding or decoding.
+     * Otherwise, libavcodec will try to create an internal session.
+     */
     mfxSession session;
+
+    /**
+     * The IO pattern to use.
+     */
     int iopattern;
 
+    /**
+     * Extra buffers to pass to encoder or decoder initialization.
+     */
     mfxExtBuffer **ext_buffers;
     int         nb_ext_buffers;
 } AVQSVContext;
--- libavcodec/qsvenc_mpeg2.c
+++ libavcodec/qsvenc_mpeg2.c
@@ -1,20 +1,20 @@
 /*
  * Intel MediaSDK QSV based MPEG-2 encoder
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -108,10 +108,11 @@ AVCodec ff_mpeg2_qsv_encoder = {
     .init           = qsv_enc_init,
     .encode2        = qsv_enc_frame,
     .close          = qsv_enc_close,
-    .capabilities   = AV_CODEC_CAP_DELAY,
+    .capabilities   = CODEC_CAP_DELAY,
     .pix_fmts       = (const enum AVPixelFormat[]){ AV_PIX_FMT_NV12,
                                                     AV_PIX_FMT_QSV,
                                                     AV_PIX_FMT_NONE },
     .priv_class     = &class,
     .defaults       = qsv_enc_defaults,
+    .caps_internal  = FF_CODEC_CAP_INIT_CLEANUP,
 };
