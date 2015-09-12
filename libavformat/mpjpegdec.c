dfdff20d38610e559c505b40db234818ca99335c
openvela-robot <openvela-robot@xiaomi.com>
2015-09-12 22:32:44 +0800

    avfilter/vsrc_mandelbrot: use the name 's' for the pointer to the private context
    Signed-off-by: Ganesh Ajjanagadde <gajjanagadde@gmail.com>

--- libavformat/mpjpegdec.c
+++ libavformat/mpjpegdec.c
@@ -2,20 +2,20 @@
  * Multipart JPEG format
  * Copyright (c) 2015 Luca Barbato
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -164,8 +164,11 @@ static int parse_multipart_header(AVFormatContext *s)
         char *tag, *value;
 
         ret = get_line(s->pb, line, sizeof(line));
-        if (ret < 0)
+        if (ret < 0) {
+            if (ret==AVERROR_EOF)
+                break;
             return ret;
+        }
 
         if (line[0] == '\0')
             break;
