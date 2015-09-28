faa71afa021da38c01b574cf9d533e46f914e57b
openvela-robot <openvela-robot@xiaomi.com>
2015-09-28 11:40:09 +0800

    lavu: Deprecate AVFrame.error[]
    These field are difficult to interpret, and are provided by a single
    encoder (mpegvideoenc). In general they do not belong to a structure
    containing raw data only, so remove them from AVFrame.
    
    Mpegvideoenc now uses a private field in Picture for its internal
    computations.
    
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/huffman.c
+++ libavcodec/huffman.c
@@ -2,20 +2,20 @@
  * Copyright (c) 2006 Konstantin Shishkov
  * Copyright (c) 2007 Loren Merritt
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -52,31 +52,18 @@ static void heap_sift(HeapElem *h, int root, int size)
     }
 }
 
-int ff_huff_gen_len_table(uint8_t *dst, const uint64_t *stats, int stats_size, int skip0)
+void ff_huff_gen_len_table(uint8_t *dst, const uint64_t *stats)
 {
-    HeapElem *h  = av_malloc_array(sizeof(*h), stats_size);
-    int *up      = av_malloc_array(sizeof(*up) * 2, stats_size);
-    uint8_t *len = av_malloc_array(sizeof(*len) * 2, stats_size);
-    uint16_t *map= av_malloc_array(sizeof(*map), stats_size);
+    HeapElem h[256];
+    int up[2*256];
+    int len[2*256];
     int offset, i, next;
-    int size = 0;
-    int ret = 0;
-
-    if (!h || !up || !len || !map) {
-        ret = AVERROR(ENOMEM);
-        goto end;
-    }
-
-    for (i = 0; i<stats_size; i++) {
-        dst[i] = 255;
-        if (stats[i] || !skip0)
-            map[size++] = i;
-    }
+    int size = 256;
 
     for (offset = 1; ; offset <<= 1) {
         for (i=0; i < size; i++) {
             h[i].name = i;
-            h[i].val = (stats[map[i]] << 14) + offset;
+            h[i].val = (stats[i] << 8) + offset;
         }
         for (i = size / 2 - 1; i >= 0; i--)
             heap_sift(h, i, size);
@@ -97,17 +84,11 @@ int ff_huff_gen_len_table(uint8_t *dst, const uint64_t *stats, int stats_size, i
         for (i = 2 * size - 3; i >= size; i--)
             len[i] = len[up[i]] + 1;
         for (i = 0; i < size; i++) {
-            dst[map[i]] = len[up[i]] + 1;
-            if (dst[map[i]] >= 32) break;
+            dst[i] = len[up[i]] + 1;
+            if (dst[i] >= 32) break;
         }
         if (i==size) break;
     }
-end:
-    av_free(h);
-    av_free(up);
-    av_free(len);
-    av_free(map);
-    return ret;
 }
 
 static void get_tree_codes(uint32_t *bits, int16_t *lens, uint8_t *xlat,
@@ -174,19 +155,18 @@ int ff_huff_build_tree(AVCodecContext *avctx, VLC *vlc, int nb_codes, int nb_bit
     cur_node = nb_codes;
     nodes[nb_codes*2-1].count = 0;
     for (i = 0; i < nb_codes * 2 - 1; i += 2) {
-        uint32_t cur_count = nodes[i].count + nodes[i+1].count;
-        // find correct place to insert new node, and
-        // make space for the new node while at it
-        for(j = cur_node; j > i + 2; j--){
-            if(cur_count > nodes[j-1].count ||
-               (cur_count == nodes[j-1].count &&
-                !(flags & FF_HUFFMAN_FLAG_HNODE_FIRST)))
+        nodes[cur_node].sym = HNODE;
+        nodes[cur_node].count = nodes[i].count + nodes[i + 1].count;
+        nodes[cur_node].n0 = i;
+        for (j = cur_node; j > 0; j--) {
+            if (nodes[j].count > nodes[j - 1].count ||
+                (nodes[j].count == nodes[j - 1].count &&
+                 (!(flags & FF_HUFFMAN_FLAG_HNODE_FIRST) ||
+                  nodes[j].n0 == j - 1 || nodes[j].n0 == j - 2 ||
+                  (nodes[j].sym!=HNODE && nodes[j-1].sym!=HNODE))))
                 break;
-            nodes[j] = nodes[j - 1];
+            FFSWAP(Node, nodes[j], nodes[j - 1]);
         }
-        nodes[j].sym = HNODE;
-        nodes[j].count = cur_count;
-        nodes[j].n0 = i;
         cur_node++;
     }
     if (build_huff_tree(vlc, nodes, nb_codes * 2 - 2, flags, nb_bits) < 0) {
