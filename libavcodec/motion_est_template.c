4a57a13e9371ed52df69c520ff0a1e040d3d2b0a
openvela-robot <openvela-robot@xiaomi.com>
2015-09-29 14:33:35 +0800

    Merge commit '1016a75cf3170648dc9b59fdef170cbfc142f8ad'
    * commit '1016a75cf3170648dc9b59fdef170cbfc142f8ad':
      configure: mips: Support mips r6, r2 and r1
    
    Not merge, as MIPS support in ffmpeg is handled
    quite differently and I do not have an env to test.
    
    Merged-by: Hendrik Leppkes <h.leppkes@gmail.com>

--- libavcodec/motion_est_template.c
+++ libavcodec/motion_est_template.c
@@ -2,20 +2,20 @@
  * Motion estimation
  * Copyright (c) 2002-2004 Michael Niedermayer
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -91,19 +91,18 @@ static int hpel_motion_search(MpegEncContext * s,
         const int b= score_map[(index+(1<<ME_MAP_SHIFT))&(ME_MAP_SIZE-1)]
                      + (mv_penalty[bx   - pred_x] + mv_penalty[by+2 - pred_y])*c->penalty_factor;
 
+#if defined(ASSERT_LEVEL) && ASSERT_LEVEL > 1
         unsigned key;
         unsigned map_generation= c->map_generation;
-#ifndef NDEBUG
-        uint32_t *map= c->map;
-#endif
         key= ((my-1)<<ME_MAP_MV_BITS) + (mx) + map_generation;
-        assert(map[(index-(1<<ME_MAP_SHIFT))&(ME_MAP_SIZE-1)] == key);
+        av_assert2(c->map[(index-(1<<ME_MAP_SHIFT))&(ME_MAP_SIZE-1)] == key);
         key= ((my+1)<<ME_MAP_MV_BITS) + (mx) + map_generation;
-        assert(map[(index+(1<<ME_MAP_SHIFT))&(ME_MAP_SIZE-1)] == key);
+        av_assert2(c->map[(index+(1<<ME_MAP_SHIFT))&(ME_MAP_SIZE-1)] == key);
         key= ((my)<<ME_MAP_MV_BITS) + (mx+1) + map_generation;
-        assert(map[(index+1)&(ME_MAP_SIZE-1)] == key);
+        av_assert2(c->map[(index+1)&(ME_MAP_SIZE-1)] == key);
         key= ((my)<<ME_MAP_MV_BITS) + (mx-1) + map_generation;
-        assert(map[(index-1)&(ME_MAP_SIZE-1)] == key);
+        av_assert2(c->map[(index-1)&(ME_MAP_SIZE-1)] == key);
+#endif
         if(t<=b){
             CHECK_HALF_MV(0, 1, mx  ,my-1)
             if(l<=r){
@@ -143,7 +142,7 @@ static int hpel_motion_search(MpegEncContext * s,
             }
             CHECK_HALF_MV(0, 1, mx  , my)
         }
-        assert(bx >= xmin*2 && bx <= xmax*2 && by >= ymin*2 && by <= ymax*2);
+        av_assert2(bx >= xmin*2 && bx <= xmax*2 && by >= ymin*2 && by <= ymax*2);
     }
 
     *mx_ptr = bx;
@@ -180,9 +179,6 @@ static inline int get_mb_score(MpegEncContext *s, int mx, int my,
 
     cmp_sub        = s->mecc.mb_cmp[size];
     chroma_cmp_sub = s->mecc.mb_cmp[size + 1];
-
-//    assert(!c->skip);
-//    assert(c->avctx->me_sub_cmp != c->avctx->mb_cmp);
 
     d= cmp(s, mx>>(qpel+1), my>>(qpel+1), mx&mask, my&mask, size, h, ref_index, src_index, cmp_sub, chroma_cmp_sub, flags);
     //FIXME check cbp before adding penalty for (0,0) vector
@@ -310,11 +306,11 @@ static int qpel_motion_search(MpegEncContext * s,
 
             cxy= 2*tl + (cx + cy)/4 - (cx2 + cy2) - 2*c;
 
-            assert(16*cx2 + 4*cx + 32*c == 32*r);
-            assert(16*cx2 - 4*cx + 32*c == 32*l);
-            assert(16*cy2 + 4*cy + 32*c == 32*b);
-            assert(16*cy2 - 4*cy + 32*c == 32*t);
-            assert(16*cxy + 16*cy2 + 16*cx2 - 4*cy - 4*cx + 32*c == 32*tl);
+            av_assert2(16*cx2 + 4*cx + 32*c == 32*r);
+            av_assert2(16*cx2 - 4*cx + 32*c == 32*l);
+            av_assert2(16*cy2 + 4*cy + 32*c == 32*b);
+            av_assert2(16*cy2 - 4*cy + 32*c == 32*t);
+            av_assert2(16*cxy + 16*cy2 + 16*cx2 - 4*cy - 4*cx + 32*c == 32*tl);
 
             for(ny= -3; ny <= 3; ny++){
                 for(nx= -3; nx <= 3; nx++){
@@ -347,7 +343,7 @@ static int qpel_motion_search(MpegEncContext * s,
             CHECK_QUARTER_MV(nx&3, ny&3, nx>>2, ny>>2)
         }
 
-        assert(bx >= xmin*4 && bx <= xmax*4 && by >= ymin*4 && by <= ymax*4);
+        av_assert2(bx >= xmin*4 && bx <= xmax*4 && by >= ymin*4 && by <= ymax*4);
 
         *mx_ptr = bx;
         *my_ptr = by;
@@ -362,17 +358,17 @@ static int qpel_motion_search(MpegEncContext * s,
 
 #define CHECK_MV(x,y)\
 {\
-    const unsigned key = ((y)<<ME_MAP_MV_BITS) + (x) + map_generation;\
-    const int index= (((y)<<ME_MAP_SHIFT) + (x))&(ME_MAP_SIZE-1);\
-    assert((x) >= xmin);\
-    assert((x) <= xmax);\
-    assert((y) >= ymin);\
-    assert((y) <= ymax);\
+    const unsigned key = ((unsigned)(y)<<ME_MAP_MV_BITS) + (x) + map_generation;\
+    const int index= (((unsigned)(y)<<ME_MAP_SHIFT) + (x))&(ME_MAP_SIZE-1);\
+    av_assert2((x) >= xmin);\
+    av_assert2((x) <= xmax);\
+    av_assert2((y) >= ymin);\
+    av_assert2((y) <= ymax);\
     if(map[index]!=key){\
         d= cmp(s, x, y, 0, 0, size, h, ref_index, src_index, cmpf, chroma_cmpf, flags);\
         map[index]= key;\
         score_map[index]= d;\
-        d += (mv_penalty[((x)<<shift)-pred_x] + mv_penalty[((y)<<shift)-pred_y])*penalty_factor;\
+        d += (mv_penalty[((x)*(1<<shift))-pred_x] + mv_penalty[((y)*(1<<shift))-pred_y])*penalty_factor;\
         COPY3_IF_LT(dmin, d, best[0], x, best[1], y)\
     }\
 }
@@ -388,13 +384,13 @@ static int qpel_motion_search(MpegEncContext * s,
 
 #define CHECK_MV_DIR(x,y,new_dir)\
 {\
-    const unsigned key = ((y)<<ME_MAP_MV_BITS) + (x) + map_generation;\
-    const int index= (((y)<<ME_MAP_SHIFT) + (x))&(ME_MAP_SIZE-1);\
+    const unsigned key = ((unsigned)(y)<<ME_MAP_MV_BITS) + (x) + map_generation;\
+    const int index= (((unsigned)(y)<<ME_MAP_SHIFT) + (x))&(ME_MAP_SIZE-1);\
     if(map[index]!=key){\
         d= cmp(s, x, y, 0, 0, size, h, ref_index, src_index, cmpf, chroma_cmpf, flags);\
         map[index]= key;\
         score_map[index]= d;\
-        d += (mv_penalty[((x)<<shift)-pred_x] + mv_penalty[((y)<<shift)-pred_y])*penalty_factor;\
+        d += (mv_penalty[(int)((unsigned)(x)<<shift)-pred_x] + mv_penalty[(int)((unsigned)(y)<<shift)-pred_y])*penalty_factor;\
         if(d<dmin){\
             best[0]=x;\
             best[1]=y;\
@@ -405,10 +401,10 @@ static int qpel_motion_search(MpegEncContext * s,
 }
 
 #define check(x,y,S,v)\
-if( (x)<(xmin<<(S)) ) printf("%d %d %d %d %d xmin" #v, xmin, (x), (y), s->mb_x, s->mb_y);\
-if( (x)>(xmax<<(S)) ) printf("%d %d %d %d %d xmax" #v, xmax, (x), (y), s->mb_x, s->mb_y);\
-if( (y)<(ymin<<(S)) ) printf("%d %d %d %d %d ymin" #v, ymin, (x), (y), s->mb_x, s->mb_y);\
-if( (y)>(ymax<<(S)) ) printf("%d %d %d %d %d ymax" #v, ymax, (x), (y), s->mb_x, s->mb_y);\
+if( (x)<(xmin<<(S)) ) av_log(NULL, AV_LOG_ERROR, "%d %d %d %d %d xmin" #v, xmin, (x), (y), s->mb_x, s->mb_y);\
+if( (x)>(xmax<<(S)) ) av_log(NULL, AV_LOG_ERROR, "%d %d %d %d %d xmax" #v, xmax, (x), (y), s->mb_x, s->mb_y);\
+if( (y)<(ymin<<(S)) ) av_log(NULL, AV_LOG_ERROR, "%d %d %d %d %d ymin" #v, ymin, (x), (y), s->mb_x, s->mb_y);\
+if( (y)>(ymax<<(S)) ) av_log(NULL, AV_LOG_ERROR, "%d %d %d %d %d ymax" #v, ymax, (x), (y), s->mb_x, s->mb_y);\
 
 #define LOAD_COMMON2\
     uint32_t *map= c->map;\
@@ -430,8 +426,8 @@ static av_always_inline int small_diamond_search(MpegEncContext * s, int *best, 
     chroma_cmpf = s->mecc.me_cmp[size + 1];
 
     { /* ensure that the best point is in the MAP as h/qpel refinement needs it */
-        const unsigned key = (best[1]<<ME_MAP_MV_BITS) + best[0] + map_generation;
-        const int index= ((best[1]<<ME_MAP_SHIFT) + best[0])&(ME_MAP_SIZE-1);
+        const unsigned key = ((unsigned)best[1]<<ME_MAP_MV_BITS) + best[0] + map_generation;
+        const int index= (((unsigned)best[1]<<ME_MAP_SHIFT) + best[0])&(ME_MAP_SIZE-1);
         if(map[index]!=key){ //this will be executed only very rarey
             score_map[index]= cmp(s, best[0], best[1], 0, 0, size, h, ref_index, src_index, cmpf, chroma_cmpf, flags);
             map[index]= key;
@@ -693,6 +689,8 @@ static int sab_diamond_search(MpegEncContext * s, int *best, int dmin,
     LOAD_COMMON2
     unsigned map_generation = c->map_generation;
 
+    av_assert1(minima_count <= MAX_SAB_SIZE);
+
     cmpf        = s->mecc.me_cmp[size];
     chroma_cmpf = s->mecc.me_cmp[size + 1];
 
@@ -704,8 +702,7 @@ static int sab_diamond_search(MpegEncContext * s, int *best, int dmin,
 
         key += (1<<(ME_MAP_MV_BITS-1)) + (1<<(2*ME_MAP_MV_BITS-1));
 
-        if ((key & (-(1 << (2 * ME_MAP_MV_BITS)))) != map_generation)
-            continue;
+        if((key&(-(1<<(2*ME_MAP_MV_BITS)))) != map_generation) continue;
 
         minima[j].height= score_map[i];
         minima[j].x= key & ((1<<ME_MAP_MV_BITS)-1); key>>=ME_MAP_MV_BITS;
@@ -890,7 +887,7 @@ static av_always_inline int epzs_motion_search_internal(MpegEncContext * s, int 
 
     map_generation= update_map_generation(c);
 
-    assert(cmpf);
+    av_assert2(cmpf);
     dmin= cmp(s, 0, 0, 0, 0, size, h, ref_index, src_index, cmpf, chroma_cmpf, flags);
     map[0]= map_generation;
     score_map[0]= dmin;
--- libavfilter/vf_framepack.c
+++ libavfilter/vf_framepack.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2013 Vittorio Giovara
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -61,8 +61,10 @@ static const enum AVPixelFormat formats_supported[] = {
 static int query_formats(AVFilterContext *ctx)
 {
     // this will ensure that formats are the same on all pads
-    ff_set_common_formats(ctx, ff_make_format_list(formats_supported));
-    return 0;
+    AVFilterFormats *fmts_list = ff_make_format_list(formats_supported);
+    if (!fmts_list)
+        return AVERROR(ENOMEM);
+    return ff_set_common_formats(ctx, fmts_list);
 }
 
 static av_cold void framepack_uninit(AVFilterContext *ctx)
@@ -82,6 +84,7 @@ static int config_output(AVFilterLink *outlink)
     int width            = ctx->inputs[LEFT]->w;
     int height           = ctx->inputs[LEFT]->h;
     AVRational time_base = ctx->inputs[LEFT]->time_base;
+    AVRational frame_rate = ctx->inputs[LEFT]->frame_rate;
 
     // check size and fps match on the other input
     if (width  != ctx->inputs[RIGHT]->w ||
@@ -93,10 +96,17 @@ static int config_output(AVFilterLink *outlink)
         return AVERROR_INVALIDDATA;
     } else if (av_cmp_q(time_base, ctx->inputs[RIGHT]->time_base) != 0) {
         av_log(ctx, AV_LOG_ERROR,
-               "Left and right framerates differ (%d/%d vs %d/%d).\n",
+               "Left and right time bases differ (%d/%d vs %d/%d).\n",
                time_base.num, time_base.den,
                ctx->inputs[RIGHT]->time_base.num,
                ctx->inputs[RIGHT]->time_base.den);
+        return AVERROR_INVALIDDATA;
+    } else if (av_cmp_q(frame_rate, ctx->inputs[RIGHT]->frame_rate) != 0) {
+        av_log(ctx, AV_LOG_ERROR,
+               "Left and right framerates differ (%d/%d vs %d/%d).\n",
+               frame_rate.num, frame_rate.den,
+               ctx->inputs[RIGHT]->frame_rate.num,
+               ctx->inputs[RIGHT]->frame_rate.den);
         return AVERROR_INVALIDDATA;
     }
 
@@ -108,6 +118,8 @@ static int config_output(AVFilterLink *outlink)
     switch (s->format) {
     case AV_STEREO3D_FRAMESEQUENCE:
         time_base.den *= 2;
+        frame_rate.num *= 2;
+
         s->double_pts = AV_NOPTS_VALUE;
         break;
     case AV_STEREO3D_COLUMNS:
@@ -126,131 +138,97 @@ static int config_output(AVFilterLink *outlink)
     outlink->w         = width;
     outlink->h         = height;
     outlink->time_base = time_base;
+    outlink->frame_rate= frame_rate;
 
     return 0;
 }
 
-static void horizontal_frame_pack(AVFilterLink *outlink,
-                                  AVFrame *out,
+static void horizontal_frame_pack(FramepackContext *s,
+                                  AVFrame *dst,
                                   int interleaved)
 {
-    AVFilterContext *ctx = outlink->src;
-    FramepackContext *s = ctx->priv;
-    int i, plane;
+    int plane, i;
+    int length = dst->width / 2;
+    int lines  = dst->height;
 
-    if (interleaved) {
-        const uint8_t *leftp  = s->input_views[LEFT]->data[0];
-        const uint8_t *rightp = s->input_views[RIGHT]->data[0];
-        uint8_t *dstp         = out->data[0];
-        int length = out->width / 2;
-        int lines  = out->height;
+    for (plane = 0; plane < s->pix_desc->nb_components; plane++) {
+        const uint8_t *leftp  = s->input_views[LEFT]->data[plane];
+        const uint8_t *rightp = s->input_views[RIGHT]->data[plane];
+        uint8_t *dstp         = dst->data[plane];
 
-        for (plane = 0; plane < s->pix_desc->nb_components; plane++) {
-            if (plane == 1 || plane == 2) {
-                length = -(-(out->width / 2) >> s->pix_desc->log2_chroma_w);
-                lines  = -(-(out->height)    >> s->pix_desc->log2_chroma_h);
-            }
+        if (plane == 1 || plane == 2) {
+            length = FF_CEIL_RSHIFT(dst->width / 2, s->pix_desc->log2_chroma_w);
+            lines  = FF_CEIL_RSHIFT(dst->height,    s->pix_desc->log2_chroma_h);
+        }
+
+        if (interleaved) {
             for (i = 0; i < lines; i++) {
                 int j;
-                leftp  = s->input_views[LEFT]->data[plane] +
-                         s->input_views[LEFT]->linesize[plane] * i;
-                rightp = s->input_views[RIGHT]->data[plane] +
-                         s->input_views[RIGHT]->linesize[plane] * i;
-                dstp   = out->data[plane] + out->linesize[plane] * i;
+                int k = 0;
+
                 for (j = 0; j < length; j++) {
-                    // interpolate chroma as necessary
-                    if ((s->pix_desc->log2_chroma_w ||
-                         s->pix_desc->log2_chroma_h) &&
-                        (plane == 1 || plane == 2)) {
-                        *dstp++ = (*leftp + *rightp) / 2;
-                        *dstp++ = (*leftp + *rightp) / 2;
-                    } else {
-                        *dstp++ = *leftp;
-                        *dstp++ = *rightp;
-                    }
-                    leftp += 1;
-                    rightp += 1;
+                    dstp[k++] = leftp[j];
+                    dstp[k++] = rightp[j];
                 }
+
+                dstp   += dst->linesize[plane];
+                leftp  += s->input_views[LEFT]->linesize[plane];
+                rightp += s->input_views[RIGHT]->linesize[plane];
             }
-        }
-    } else {
-        for (i = 0; i < 2; i++) {
-            const uint8_t *src[4];
-            uint8_t *dst[4];
-            int sub_w = s->input_views[i]->width >> s->pix_desc->log2_chroma_w;
-
-            src[0] = s->input_views[i]->data[0];
-            src[1] = s->input_views[i]->data[1];
-            src[2] = s->input_views[i]->data[2];
-
-            dst[0] = out->data[0] + i * s->input_views[i]->width;
-            dst[1] = out->data[1] + i * sub_w;
-            dst[2] = out->data[2] + i * sub_w;
-
-            av_image_copy(dst, out->linesize, src, s->input_views[i]->linesize,
-                          s->input_views[i]->format,
-                          s->input_views[i]->width,
-                          s->input_views[i]->height);
+        } else {
+            av_image_copy_plane(dst->data[plane], dst->linesize[plane],
+                                leftp, s->input_views[LEFT]->linesize[plane],
+                                length, lines);
+            av_image_copy_plane(dst->data[plane] + length, dst->linesize[plane],
+                                rightp, s->input_views[RIGHT]->linesize[plane],
+                                length, lines);
         }
     }
 }
 
-static void vertical_frame_pack(AVFilterLink *outlink,
-                                AVFrame *out,
+static void vertical_frame_pack(FramepackContext *s,
+                                AVFrame *dst,
                                 int interleaved)
 {
-    AVFilterContext *ctx = outlink->src;
-    FramepackContext *s = ctx->priv;
-    int i;
+    int plane, offset;
+    int length = dst->width;
+    int lines  = dst->height / 2;
 
-    for (i = 0; i < 2; i++) {
-        const uint8_t *src[4];
-        uint8_t *dst[4];
-        int linesizes[4];
-        int sub_h = s->input_views[i]->height >> s->pix_desc->log2_chroma_h;
+    for (plane = 0; plane < s->pix_desc->nb_components; plane++) {
+        if (plane == 1 || plane == 2) {
+            length = -(-(dst->width)      >> s->pix_desc->log2_chroma_w);
+            lines  = -(-(dst->height / 2) >> s->pix_desc->log2_chroma_h);
+        }
 
-        src[0] = s->input_views[i]->data[0];
-        src[1] = s->input_views[i]->data[1];
-        src[2] = s->input_views[i]->data[2];
+        offset = interleaved ? dst->linesize[plane] : dst->linesize[plane] * lines;
 
-        dst[0] = out->data[0] + i * out->linesize[0] *
-                 (interleaved + s->input_views[i]->height * (1 - interleaved));
-        dst[1] = out->data[1] + i * out->linesize[1] *
-                 (interleaved + sub_h * (1 - interleaved));
-        dst[2] = out->data[2] + i * out->linesize[2] *
-                 (interleaved + sub_h * (1 - interleaved));
-
-        linesizes[0] = out->linesize[0] +
-                       interleaved * out->linesize[0];
-        linesizes[1] = out->linesize[1] +
-                       interleaved * out->linesize[1];
-        linesizes[2] = out->linesize[2] +
-                       interleaved * out->linesize[2];
-
-        av_image_copy(dst, linesizes, src, s->input_views[i]->linesize,
-                      s->input_views[i]->format,
-                      s->input_views[i]->width,
-                      s->input_views[i]->height);
+        av_image_copy_plane(dst->data[plane],
+                            dst->linesize[plane] << interleaved,
+                            s->input_views[LEFT]->data[plane],
+                            s->input_views[LEFT]->linesize[plane],
+                            length, lines);
+        av_image_copy_plane(dst->data[plane] + offset,
+                            dst->linesize[plane] << interleaved,
+                            s->input_views[RIGHT]->data[plane],
+                            s->input_views[RIGHT]->linesize[plane],
+                            length, lines);
     }
 }
 
-static av_always_inline void spatial_frame_pack(AVFilterLink *outlink,
-                                                AVFrame *dst)
+static av_always_inline void spatial_frame_pack(FramepackContext *s, AVFrame *dst)
 {
-    AVFilterContext *ctx = outlink->src;
-    FramepackContext *s = ctx->priv;
     switch (s->format) {
     case AV_STEREO3D_SIDEBYSIDE:
-        horizontal_frame_pack(outlink, dst, 0);
+        horizontal_frame_pack(s, dst, 0);
         break;
     case AV_STEREO3D_COLUMNS:
-        horizontal_frame_pack(outlink, dst, 1);
+        horizontal_frame_pack(s, dst, 1);
         break;
     case AV_STEREO3D_TOPBOTTOM:
-        vertical_frame_pack(outlink, dst, 0);
+        vertical_frame_pack(s, dst, 0);
         break;
     case AV_STEREO3D_LINES:
-        vertical_frame_pack(outlink, dst, 1);
+        vertical_frame_pack(s, dst, 1);
         break;
     }
 }
@@ -311,7 +289,7 @@ static int request_frame(AVFilterLink *outlink)
         if (!dst)
             return AVERROR(ENOMEM);
 
-        spatial_frame_pack(outlink, dst);
+        spatial_frame_pack(s, dst);
 
         // get any property from the original frame
         ret = av_frame_copy_props(dst, s->input_views[LEFT]);
@@ -337,7 +315,7 @@ static int request_frame(AVFilterLink *outlink)
 
 #define OFFSET(x) offsetof(FramepackContext, x)
 #define V AV_OPT_FLAG_VIDEO_PARAM
-static const AVOption options[] = {
+static const AVOption framepack_options[] = {
     { "format", "Frame pack output format", OFFSET(format), AV_OPT_TYPE_INT,
         { .i64 = AV_STEREO3D_SIDEBYSIDE }, 0, INT_MAX, .flags = V, .unit = "format" },
     { "sbs", "Views are packed next to each other", 0, AV_OPT_TYPE_CONST,
@@ -353,12 +331,7 @@ static const AVOption options[] = {
     { NULL },
 };
 
-static const AVClass framepack_class = {
-    .class_name = "framepack",
-    .item_name  = av_default_item_name,
-    .option     = options,
-    .version    = LIBAVUTIL_VERSION_INT,
-};
+AVFILTER_DEFINE_CLASS(framepack);
 
 static const AVFilterPad framepack_inputs[] = {
     {
