70d4e6084f89185f68cc77b12ccc775952c040ba
openvela-robot <openvela-robot@xiaomi.com>
2015-11-05 14:49:57 +0800

    swscale/arm: add ff_nv{12,21}_to_{argb,rgba,abgr,bgra}_neon
    Signed-off-by: Matthieu Bouron <matthieu.bouron@stupeflix.com>
    Signed-off-by: Clément Bœsch <clement@stupeflix.com>

--- libavformat/oggparsespeex.c
+++ libavformat/oggparsespeex.c
@@ -47,8 +47,6 @@ static int speex_header(AVFormatContext *s, int idx) {
 
     if (!spxp) {
         spxp = av_mallocz(sizeof(*spxp));
-        if (!spxp)
-            return AVERROR(ENOMEM);
         os->private = spxp;
     }
 
@@ -59,6 +57,11 @@ static int speex_header(AVFormatContext *s, int idx) {
         int frames_per_packet;
         st->codec->codec_type = AVMEDIA_TYPE_AUDIO;
         st->codec->codec_id = AV_CODEC_ID_SPEEX;
+
+        if (os->psize < 68) {
+            av_log(s, AV_LOG_ERROR, "speex packet too small\n");
+            return AVERROR_INVALIDDATA;
+        }
 
         st->codec->sample_rate = AV_RL32(p + 36);
         st->codec->channels = AV_RL32(p + 48);
@@ -74,9 +77,8 @@ static int speex_header(AVFormatContext *s, int idx) {
         if (frames_per_packet)
             spxp->packet_size *= frames_per_packet;
 
-        st->codec->extradata_size = os->psize;
-        st->codec->extradata = av_malloc(st->codec->extradata_size
-                                         + AV_INPUT_BUFFER_PADDING_SIZE);
+        if (ff_alloc_extradata(st->codec, os->psize) < 0)
+            return AVERROR(ENOMEM);
         memcpy(st->codec->extradata, p, st->codec->extradata_size);
 
         avpriv_set_pts_info(st, 64, 1, st->codec->sample_rate);
