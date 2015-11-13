0eb62370476482110e5ba85d4422a8820339f3fd
Michael Niedermayer <michael@niedermayer.cc>
2015-11-14 00:51:56 +0100

    avcodec/jpeg2000dec: Check for duplicate SIZ marker
    Fixes: 0231a17345734228011c6f35a64e4594/asan_heap-oob_1d92a72_3218_1213809a9e3affec77e4c191fdfdc0a9.mov
    
    Found-by: Mateusz "j00ru" Jurczyk and Gynvael Coldwind
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -1223,6 +1223,7 @@ static void jpeg2000_dec_cleanup(Jpeg2000DecoderContext *s)
     }
     av_freep(&s->tile);
     s->numXtiles = s->numYtiles = 0;
+    s->ncomponents = 0;
 }
 
 static int jpeg2000_read_main_headers(Jpeg2000DecoderContext *s)
@@ -1273,6 +1274,10 @@ static int jpeg2000_read_main_headers(Jpeg2000DecoderContext *s)
 
         switch (marker) {
         case JPEG2000_SIZ:
+            if (s->ncomponents) {
+                av_log(s->avctx, AV_LOG_ERROR, "Duplicate SIZ\n");
+                return AVERROR_INVALIDDATA;
+            }
             ret = get_siz(s);
             break;
         case JPEG2000_COC:
