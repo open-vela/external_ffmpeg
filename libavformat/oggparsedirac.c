f2126e3e2d48004019b86f01208d7f5d20b1ca3d
openvela-robot <openvela-robot@xiaomi.com>
2015-12-04 01:03:26 +0800

    movenc-test: add a missing va_end call
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/oggparsedirac.c
+++ libavformat/oggparsedirac.c
@@ -1,24 +1,24 @@
 /*
  * Copyright (C) 2008  David Conrad
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavcodec/get_bits.h"
+#include "libavutil/intreadwrite.h"
 #include "libavcodec/dirac.h"
 #include "avformat.h"
 #include "internal.h"
@@ -29,26 +29,34 @@ static int dirac_header(AVFormatContext *s, int idx)
     struct ogg *ogg = s->priv_data;
     struct ogg_stream *os = ogg->streams + idx;
     AVStream *st = s->streams[idx];
-    dirac_source_params source;
-    GetBitContext gb;
+    AVDiracSeqHeader *dsh;
     int ret;
 
     // already parsed the header
     if (st->codec->codec_id == AV_CODEC_ID_DIRAC)
         return 0;
 
-    ret = init_get_bits8(&gb, os->buf + os->pstart + 13, (os->psize - 13));
+    ret = av_dirac_parse_sequence_header(&dsh, os->buf + os->pstart + 13, (os->psize - 13) * 8, s);
     if (ret < 0)
         return ret;
 
-    ret = avpriv_dirac_parse_sequence_header(st->codec, &gb, &source);
-    if (ret < 0)
-        return ret;
+    st->codec->codec_type      = AVMEDIA_TYPE_VIDEO;
+    st->codec->codec_id        = AV_CODEC_ID_DIRAC;
+    st->codec->width           = dsh->width;
+    st->codec->height          = dsh->height;
+    st->codec->pix_fmt         = dsh->pix_fmt;
+    st->codec->color_range     = dsh->color_range;
+    st->codec->color_trc       = dsh->color_trc;
+    st->codec->color_primaries = dsh->color_primaries;
+    st->codec->colorspace      = dsh->colorspace;
+    st->codec->profile         = dsh->profile;
+    st->codec->level           = dsh->level;
 
-    st->codec->codec_type = AVMEDIA_TYPE_VIDEO;
-    st->codec->codec_id = AV_CODEC_ID_DIRAC;
     // dirac in ogg always stores timestamps as though the video were interlaced
-    avpriv_set_pts_info(st, 64, st->codec->framerate.den, 2*st->codec->framerate.num);
+    avpriv_set_pts_info(st, 64, dsh->framerate.den, 2 * dsh->framerate.num);
+
+    av_freep(&dsh);
+
     return 1;
 }
 
