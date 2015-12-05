35b5f4911f6ece44186db69654a4f93515b78df3
openvela-robot <openvela-robot@xiaomi.com>
2015-12-05 13:31:38 +0800

    imgconvert: Re-enable the deprecation warnings
    The end-marked was typoed in
    
    f7edcac040f73635fc1127489c9bb29ca8b43532

--- libavcodec/libdcadec.c
+++ libavcodec/libdcadec.c
@@ -2,20 +2,20 @@
  * libdcadec decoder wrapper
  * Copyright (C) 2015 Hendrik Leppkes
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -34,7 +34,6 @@ typedef struct DCADecContext {
     struct dcadec_context *ctx;
     uint8_t *buffer;
     int buffer_size;
-    int downmix_warned;
 } DCADecContext;
 
 static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
@@ -42,7 +41,6 @@ static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
 {
     DCADecContext *s = avctx->priv_data;
     AVFrame *frame = data;
-    struct dcadec_exss_info *exss;
     int ret, i, k;
     int **samples, nsamples, channel_mask, sample_rate, bits_per_sample, profile;
     uint32_t mrk;
@@ -60,10 +58,7 @@ static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
         if (!s->buffer)
             return AVERROR(ENOMEM);
 
-        for (i = 0, ret = AVERROR_INVALIDDATA; i < input_size - 3 && ret < 0; i++)
-            ret = avpriv_dca_convert_bitstream(input + i, input_size - i, s->buffer, s->buffer_size);
-
-        if (ret < 0)
+        if ((ret = ff_dca_convert_bitstream(avpkt->data, avpkt->size, s->buffer, s->buffer_size)) < 0)
             return ret;
 
         input      = s->buffer;
@@ -72,14 +67,12 @@ static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
 
     if ((ret = dcadec_context_parse(s->ctx, input, input_size)) < 0) {
         av_log(avctx, AV_LOG_ERROR, "dcadec_context_parse() failed: %d (%s)\n", -ret, dcadec_strerror(ret));
-        return AVERROR_EXTERNAL;
+        return AVERROR_UNKNOWN;
     }
     if ((ret = dcadec_context_filter(s->ctx, &samples, &nsamples, &channel_mask,
                                      &sample_rate, &bits_per_sample, &profile)) < 0) {
         av_log(avctx, AV_LOG_ERROR, "dcadec_context_filter() failed: %d (%s)\n", -ret, dcadec_strerror(ret));
-        return AVERROR_EXTERNAL;
-    } else if (ret > 0) {
-        av_log(avctx, AV_LOG_WARNING, "dcadec_context_filter() warning: %d (%s)\n", ret, dcadec_strerror(ret));
+        return AVERROR_UNKNOWN;
     }
 
     avctx->channels       = av_get_channel_layout_nb_channels(channel_mask);
@@ -88,7 +81,7 @@ static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
 
     if (bits_per_sample == 16)
         avctx->sample_fmt = AV_SAMPLE_FMT_S16P;
-    else if (bits_per_sample > 16 && bits_per_sample <= 24)
+    else if (bits_per_sample <= 24)
         avctx->sample_fmt = AV_SAMPLE_FMT_S32P;
     else {
         av_log(avctx, AV_LOG_ERROR, "Unsupported number of bits per sample: %d\n",
@@ -130,35 +123,6 @@ static int dcadec_decode_frame(AVCodecContext *avctx, void *data,
         dcadec_context_free_core_info(info);
     } else
         avctx->bit_rate = 0;
-
-    if (exss = dcadec_context_get_exss_info(s->ctx)) {
-        enum AVMatrixEncoding matrix_encoding = AV_MATRIX_ENCODING_NONE;
-
-        if (!s->downmix_warned) {
-            uint64_t layout = avctx->request_channel_layout;
-
-            if (((layout == AV_CH_LAYOUT_STEREO_DOWNMIX || layout == AV_CH_LAYOUT_STEREO) && !exss->embedded_stereo) ||
-                ( layout == AV_CH_LAYOUT_5POINT1 && !exss->embedded_6ch))
-                av_log(avctx, AV_LOG_WARNING, "%s downmix was requested but no custom coefficients are available, "
-                                              "this may result in clipping\n",
-                                              layout == AV_CH_LAYOUT_5POINT1 ? "5.1" : "Stereo");
-            s->downmix_warned = 1;
-        }
-
-        switch(exss->matrix_encoding) {
-        case DCADEC_MATRIX_ENCODING_SURROUND:
-            matrix_encoding = AV_MATRIX_ENCODING_DOLBY;
-            break;
-        case DCADEC_MATRIX_ENCODING_HEADPHONE:
-            matrix_encoding = AV_MATRIX_ENCODING_DOLBYHEADPHONE;
-            break;
-        }
-        dcadec_context_free_exss_info(exss);
-
-        if (matrix_encoding != AV_MATRIX_ENCODING_NONE &&
-            (ret = ff_side_data_update_matrix_encoding(frame, matrix_encoding)) < 0)
-            return ret;
-    }
 
     frame->nb_samples = nsamples;
     if ((ret = ff_get_buffer(avctx, frame, 0)) < 0)
@@ -203,34 +167,8 @@ static av_cold int dcadec_close(AVCodecContext *avctx)
 static av_cold int dcadec_init(AVCodecContext *avctx)
 {
     DCADecContext *s = avctx->priv_data;
-    int flags = 0;
 
-    /* Affects only lossy DTS profiles. DTS-HD MA is always bitexact */
-    if (avctx->flags & AV_CODEC_FLAG_BITEXACT)
-        flags |= DCADEC_FLAG_CORE_BIT_EXACT;
-
-    if (avctx->request_channel_layout > 0 && avctx->request_channel_layout != AV_CH_LAYOUT_NATIVE) {
-        switch (avctx->request_channel_layout) {
-        case AV_CH_LAYOUT_STEREO:
-        case AV_CH_LAYOUT_STEREO_DOWNMIX:
-            /* libdcadec ignores the 2ch flag if used alone when no custom downmix coefficients
-               are available, silently outputting a 5.1 downmix if possible instead.
-               Using both the 2ch and 6ch flags together forces a 2ch downmix using default
-               coefficients in such cases. This matches the behavior of the 6ch flag when used
-               alone, where a 5.1 downmix is generated if possible, regardless of custom
-               coefficients being available or not. */
-            flags |= DCADEC_FLAG_KEEP_DMIX_2CH | DCADEC_FLAG_KEEP_DMIX_6CH;
-            break;
-        case AV_CH_LAYOUT_5POINT1:
-            flags |= DCADEC_FLAG_KEEP_DMIX_6CH;
-            break;
-        default:
-            av_log(avctx, AV_LOG_WARNING, "Invalid request_channel_layout\n");
-            break;
-        }
-    }
-
-    s->ctx = dcadec_context_create(flags);
+    s->ctx = dcadec_context_create(0);
     if (!s->ctx)
         return AVERROR(ENOMEM);
 
