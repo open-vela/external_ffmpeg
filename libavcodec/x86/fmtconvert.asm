7da14ae33ccc62a24ed00b4037138472b6e85dcd
Janne Grunau <janne-libav@jannau.net>
2015-12-22 22:45:42 +0100

    x86: zero extend the 32-bit length in int32_to_float_fmul_scalar implicitly
    This reverts commit 5dfe4edad63971d669ae456b0bc40ef9364cca80.

--- libavcodec/x86/fmtconvert.asm
+++ libavcodec/x86/fmtconvert.asm
@@ -39,7 +39,7 @@ cglobal int32_to_float_fmul_scalar, 4, 4, %1, dst, src, mul, len
     movss   m0, mulm
 %endif
     SPLATD  m0
-    shl     lenq, 2
+    shl     lend, 2
     add     srcq, lenq
     add     dstq, lenq
     neg     lenq
