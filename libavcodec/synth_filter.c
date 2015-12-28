3e7d1570349148749c48f15e2d6d79506ff83cfa
openvela-robot <openvela-robot@xiaomi.com>
2015-12-28 23:04:08 +0800

    avcodec: Use get_ue_golomb_long() when needed
    get_ue_golomb() cannot decode values larger than 8190 (the maximum
    value that can be golomb encoded in 25 bits) and produces the error
    "Invalid UE golomb code" if a larger value is encountered.  Use
    get_ue_golomb_long() instead (which supports 63 bits, up to 4294967294)
    when valid h264/hevc values can exceed 8190.
    
    This updates decoding of the following values:   (maximum)
      first_mb_in_slice                                36863* for level 5.2
      abs_diff_pic_num_minus1                         131071
      difference_of_pic_nums_minus1                   131071
      idr_pic_id                                       65535
      recovery_frame_cnt                               65535
      frame_packing_arrangement_id                4294967294
      frame_packing_arrangement_repetition_period      16384
      display_orientation_repetition_period            16384
    
    An alternative would be to modify get_ue_golomb() to handle encoded
    values of up to 49 bits as was done for get_se_golomb() in a92816c.
    In that case get_ue_golomb() could continue to be used for all of
    these except frame_packing_arrangement_id.
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/synth_filter.c
+++ libavcodec/synth_filter.c
@@ -1,20 +1,20 @@
 /*
  * copyright (c) 2008 Michael Niedermayer <michaelni@gmx.at>
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -60,10 +60,6 @@ av_cold void ff_synth_filter_init(SynthFilterContext *c)
 {
     c->synth_filter_float = synth_filter_float;
 
-    if (ARCH_AARCH64)
-        ff_synth_filter_init_aarch64(c);
-    if (ARCH_ARM)
-        ff_synth_filter_init_arm(c);
-    if (ARCH_X86)
-        ff_synth_filter_init_x86(c);
+    if (ARCH_ARM) ff_synth_filter_init_arm(c);
+    if (ARCH_X86) ff_synth_filter_init_x86(c);
 }
--- libavformat/qtpalette.h
+++ libavformat/qtpalette.h
@@ -3,27 +3,28 @@
  *  Automatically generated from a utility derived from XAnim:
  *  http://xanim.va.pubnix.com/home.html
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifndef AVFORMAT_QTPALETTE_H
 #define AVFORMAT_QTPALETTE_H
 
-#include <inttypes.h>
+#include <stdint.h>
+#include "avformat.h"
 
 static const uint8_t ff_qt_default_palette_4[4 * 3] = {
   0x93, 0x65, 0x5E,
@@ -309,5 +310,16 @@ static const uint8_t ff_qt_default_palette_256[256 * 3] = {
   /* 254, 0xFE */  0x11, 0x11, 0x11,
   /* 255, 0xFF */  0x00, 0x00, 0x00
 };
+
+/**
+ * Retrieve the palette (or "color table" in QuickTime terms), either
+ * from the video sample description, or from the default Macintosh
+ * palette.
+ *
+ * The file offset of the AVIOContext pointed to by the 'pb' variable
+ * should be the start of the video sample description (the sample
+ * description size and the data format).
+ */
+int ff_get_qtpalette(int codec_id, AVIOContext *pb, uint32_t *palette);
 
 #endif /* AVFORMAT_QTPALETTE_H */
