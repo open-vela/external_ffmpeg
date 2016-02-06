eac2ef1c4d4ddc3020d78ed4529f4866b51467a6
openvela-robot <openvela-robot@xiaomi.com>
2016-02-06 13:15:37 +0800

    mkv: Force the full parsing of mp3
    Some muxer might or might not fit incomplete mp3 frames in
    their packets.
    
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/rpza.c
+++ libavcodec/rpza.c
@@ -1,21 +1,21 @@
 /*
  * Quicktime Video (RPZA) Video Decoder
- * Copyright (c) 2003 The FFmpeg Project
+ * Copyright (C) 2003 the ffmpeg project
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -96,12 +96,8 @@ static int rpza_decode_stream(RpzaContext *s)
     chunk_size = bytestream2_get_be32(&s->gb) & 0x00FFFFFF;
 
     /* If length mismatch use size from MOV file and try to decode anyway */
-    if (chunk_size != bytestream2_get_bytes_left(&s->gb) + 4)
-        av_log(s->avctx, AV_LOG_WARNING,
-               "MOV chunk size %d != encoded chunk size %d\n",
-               chunk_size,
-               bytestream2_get_bytes_left(&s->gb) + 4
-              );
+    if (chunk_size != bytestream2_get_bytes_left(&s->gb) - 4)
+        av_log(s->avctx, AV_LOG_WARNING, "MOV chunk size != encoded chunk size\n");
 
     /* Number of 4x4 blocks in frame. */
     total_blocks = ((s->avctx->width + 3) / 4) * ((s->avctx->height + 3) / 4);
@@ -256,8 +252,10 @@ static int rpza_decode_frame(AVCodecContext *avctx,
 
     bytestream2_init(&s->gb, avpkt->data, avpkt->size);
 
-    if ((ret = ff_reget_buffer(avctx, s->frame)) < 0)
+    if ((ret = ff_reget_buffer(avctx, s->frame)) < 0) {
+        av_log(avctx, AV_LOG_ERROR, "reget_buffer() failed\n");
         return ret;
+    }
 
     ret = rpza_decode_stream(s);
     if (ret < 0)
