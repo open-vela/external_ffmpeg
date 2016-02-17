968f4440b4c0c5bc2c6a8610a84d64d575068657
openvela-robot <openvela-robot@xiaomi.com>
2016-02-17 23:34:54 +0800

    Revert 4 commits to configure which broke dependency handling
    Revert "configure: Don't enable examples when --disable-everything is used"
    reverted as the problematic commit will be reverted too
    This reverts commit 02dfa64c088c87367c298a3f648454204656734f.
    
    Revert "Merge commit 'a2bb771a3cded8a05137c0effb34f61a2bc78e22'"
    This reverts commit e8ebcb0034c5d4e5df8ff407a5c28d8c53823236, reversing
    changes made to 3bff005be8ea213c23160ee0ac286634a80a10e1.
    
    Revert "Merge commit '21c750f240b9d0c41a258d1adee2d9f75ff378b6'"
    This reverts commit 470bfab470893d45328f4e6e25d60b89c9af1abe, reversing
    changes made to f97ee815cf25580cbb9d6bed304d9c22895f7074.
    
    Revert "Revert "configure: Revert recent changes to disable-everything""
    This reverts commit f97ee815cf25580cbb9d6bed304d9c22895f7074.
    
    Reviewed-by: Hendrik Leppkes <h.leppkes@gmail.com>
    Immediate commit suggested by BBB

--- libavutil/hwcontext_internal.h
+++ libavutil/hwcontext_internal.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -86,7 +86,6 @@ struct AVHWFramesInternal {
     AVBufferPool *pool_internal;
 };
 
-extern const HWContextType ff_hwcontext_type_cuda;
 extern const HWContextType ff_hwcontext_type_vdpau;
 
 #endif /* AVUTIL_HWCONTEXT_INTERNAL_H */
