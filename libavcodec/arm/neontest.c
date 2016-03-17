374fb35779b6d0348d6516f74c2a134618d99192
openvela-robot <openvela-robot@xiaomi.com>
2016-03-17 14:21:24 +0800

    svq3: Convert to the new bitstream reader

--- libavcodec/arm/neontest.c
+++ libavcodec/arm/neontest.c
@@ -2,20 +2,20 @@
  * check NEON registers for clobbers
  * Copyright (c) 2013 Martin Storsjo
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -83,9 +83,9 @@ wrap(avcodec_send_packet(AVCodecContext *avctx, const AVPacket *avpkt))
     testneonclobbers(avcodec_send_packet, avctx, avpkt);
 }
 
-wrap(avcodec_receive_frame(AVCodecContext *avctx, AVFrame *frame))
+wrap(avcodec_receive_packet(AVCodecContext *avctx, AVPacket *avpkt))
 {
-    testneonclobbers(avcodec_receive_frame, avctx, frame);
+    testneonclobbers(avcodec_receive_packet, avctx, avpkt);
 }
 
 wrap(avcodec_send_frame(AVCodecContext *avctx, const AVFrame *frame))
@@ -93,7 +93,7 @@ wrap(avcodec_send_frame(AVCodecContext *avctx, const AVFrame *frame))
     testneonclobbers(avcodec_send_frame, avctx, frame);
 }
 
-wrap(avcodec_receive_packet(AVCodecContext *avctx, AVPacket *avpkt))
+wrap(avcodec_receive_frame(AVCodecContext *avctx, AVFrame *frame))
 {
-    testneonclobbers(avcodec_receive_packet, avctx, avpkt);
+    testneonclobbers(avcodec_receive_frame, avctx, frame);
 }
