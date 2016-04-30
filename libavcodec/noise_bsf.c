9d71b578649f3c1ddddbd320e7523adc7b6c502b
openvela-robot <openvela-robot@xiaomi.com>
2016-04-30 16:05:32 +0800

    Revert "mmaldec: autodetect by default" since it breaks linking on systems without mmal libraries
    This reverts commit 33ac77e850efdfd0e8835950c3d947baffd4df45.

--- libavcodec/noise_bsf.c
+++ libavcodec/noise_bsf.c
@@ -1,20 +1,20 @@
 /*
  * copyright (c) 2006 Michael Niedermayer <michaelni@gmx.at>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -40,9 +40,6 @@ static int noise(AVBSFContext *ctx, AVPacket *out)
     AVPacket *in;
     int amount = s->amount > 0 ? s->amount : (s->state % 10001 + 1);
     int i, ret = 0;
-
-    if (amount <= 0)
-        return AVERROR(EINVAL);
 
     ret = ff_bsf_get_packet(ctx, &in);
     if (ret < 0)
