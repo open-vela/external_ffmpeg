d0bf24759214d9d59cb7670ae95a9bf6d1717e09
Michael Niedermayer <michael@niedermayer.cc>
2016-05-04 21:31:18 +0200

    avformat/utils: Do not detect video codecs when audio is expected
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -167,6 +167,9 @@ static int set_codec_from_probe_data(AVFormatContext *s, AVStream *st,
                fmt->name, score);
         for (i = 0; fmt_id_type[i].name; i++) {
             if (!strcmp(fmt->name, fmt_id_type[i].name)) {
+                if (fmt_id_type[i].type != AVMEDIA_TYPE_AUDIO &&
+                    st->codecpar->sample_rate)
+                    continue;
                 st->codecpar->codec_id   = fmt_id_type[i].id;
                 st->codecpar->codec_type = fmt_id_type[i].type;
 #if FF_API_LAVF_AVCTX
