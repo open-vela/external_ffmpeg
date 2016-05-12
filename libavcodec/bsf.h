3394ade8081b3747f3cf62020b79106f42ab097a
openvela-robot <openvela-robot@xiaomi.com>
2016-05-12 15:34:58 +0800

    avpacket: fix setting AVPacket.data in av_packet_ref()
    The data field does not necessarily point to the beginning of the
    underlying AVBuffer.
    
    CC: libav-stable@libav.org

--- libavcodec/bsf.h
+++ libavcodec/bsf.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -22,7 +22,7 @@
 #include "avcodec.h"
 
 /**
- * Called by the biststream filters to get the next packet for filtering.
+ * Called by the bitstream filters to get the next packet for filtering.
  * The filter is responsible for either freeing the packet or passing it to the
  * caller.
  */
--- libavcodec/flvenc.c
+++ libavcodec/flvenc.c
@@ -1,20 +1,20 @@
 /*
  * FLV Encoding specific code.
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -31,7 +31,7 @@ void ff_flv_encode_picture_header(MpegEncContext *s, int picture_number)
     avpriv_align_put_bits(&s->pb);
 
     put_bits(&s->pb, 17, 1);
-    /* 0: h263 escape codes 1: 11-bit escape codes */
+    /* 0: H.263 escape codes 1: 11-bit escape codes */
     put_bits(&s->pb, 5, (s->h263_flv - 1));
     put_bits(&s->pb, 8,
              (((int64_t) s->picture_number * 30 * s->avctx->time_base.num) /   // FIXME use timestamp
