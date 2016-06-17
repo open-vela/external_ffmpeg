204f4cf3768c8e8a9ec782d26b4c4acffb5efaa2
openvela-robot <openvela-robot@xiaomi.com>
2016-06-17 14:29:05 +0800

    fate/h264: add test for ticket #631

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -1,6 +1,7 @@
-LIBAVCODEC_MAJOR {
-    global:
-        av*;
-    local:
-        *;
+LIBAVCODEC_$MAJOR {
+        global: av*;
+                #deprecated, remove after next bump
+                audio_resample;
+                audio_resample_close;
+        local:  *;
 };
