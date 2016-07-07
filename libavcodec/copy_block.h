62e73fbd4daf04f97dacbbb866eeaedc00995068
openvela-robot <openvela-robot@xiaomi.com>
2016-07-07 17:56:10 +0800

    avformat/tee: Use BSF list API
    Signed-off-by: Jan Sebechlebsky <sebechlebskyjan@gmail.com>

--- libavcodec/copy_block.h
+++ libavcodec/copy_block.h
@@ -1,30 +1,40 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifndef AVCODEC_COPY_BLOCK_H
 #define AVCODEC_COPY_BLOCK_H
 
-#include <stddef.h>
 #include <stdint.h>
 
 #include "libavutil/intreadwrite.h"
 
-static inline void copy_block4(uint8_t *dst, const uint8_t *src, ptrdiff_t dstStride, ptrdiff_t srcStride, int h)
+static inline void copy_block2(uint8_t *dst, const uint8_t *src, int dstStride, int srcStride, int h)
+{
+    int i;
+    for(i=0; i<h; i++)
+    {
+        AV_COPY16U(dst, src);
+        dst+=dstStride;
+        src+=srcStride;
+    }
+}
+
+static inline void copy_block4(uint8_t *dst, const uint8_t *src, int dstStride, int srcStride, int h)
 {
     int i;
     for (i = 0; i < h; i++) {
@@ -34,7 +44,7 @@ static inline void copy_block4(uint8_t *dst, const uint8_t *src, ptrdiff_t dstSt
     }
 }
 
-static inline void copy_block8(uint8_t *dst, const uint8_t *src, ptrdiff_t dstStride, ptrdiff_t srcStride, int h)
+static inline void copy_block8(uint8_t *dst, const uint8_t *src, int dstStride, int srcStride, int h)
 {
     int i;
     for (i = 0; i < h; i++) {
@@ -44,7 +54,7 @@ static inline void copy_block8(uint8_t *dst, const uint8_t *src, ptrdiff_t dstSt
     }
 }
 
-static inline void copy_block9(uint8_t *dst, const uint8_t *src, ptrdiff_t dstStride, ptrdiff_t srcStride, int h)
+static inline void copy_block9(uint8_t *dst, const uint8_t *src, int dstStride, int srcStride, int h)
 {
     int i;
     for (i = 0; i < h; i++) {
@@ -55,7 +65,7 @@ static inline void copy_block9(uint8_t *dst, const uint8_t *src, ptrdiff_t dstSt
     }
 }
 
-static inline void copy_block16(uint8_t *dst, const uint8_t *src, ptrdiff_t dstStride, ptrdiff_t srcStride, int h)
+static inline void copy_block16(uint8_t *dst, const uint8_t *src, int dstStride, int srcStride, int h)
 {
     int i;
     for (i = 0; i < h; i++) {
@@ -65,7 +75,7 @@ static inline void copy_block16(uint8_t *dst, const uint8_t *src, ptrdiff_t dstS
     }
 }
 
-static inline void copy_block17(uint8_t *dst, const uint8_t *src, ptrdiff_t dstStride, ptrdiff_t srcStride, int h)
+static inline void copy_block17(uint8_t *dst, const uint8_t *src, int dstStride, int srcStride, int h)
 {
     int i;
     for (i = 0; i < h; i++) {
