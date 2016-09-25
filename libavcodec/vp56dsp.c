020febff035eb3b99a2329f7f348c0c96179a1a1
openvela-robot <openvela-robot@xiaomi.com>
2016-09-25 17:09:57 +0800

    swscale: Consistently order input YUV pixel formats
    Follow a 420, 422, 444 order instead of a random one.
    This simplifies double-checking additions of new formats.
    
    Signed-off-by: Diego Biurrun <diego@biurrun.de>

--- libavcodec/vp56dsp.c
+++ libavcodec/vp56dsp.c
@@ -2,20 +2,20 @@
  * Copyright (c) 2006 Aurelien Jacobs <aurel@gnuage.org>
  * Copyright (c) 2010 Mans Rullgard <mans@mansr.com>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -26,6 +26,23 @@
 #include "vp56dsp.h"
 #include "libavutil/common.h"
 
+#define VP56_EDGE_FILTER(pfx, suf, pix_inc, line_inc)                   \
+static void pfx ## _edge_filter_ ## suf(uint8_t *yuv, ptrdiff_t stride, \
+                                        int t)                          \
+{                                                                       \
+    int pix2_inc = 2 * pix_inc;                                         \
+    int i, v;                                                           \
+                                                                        \
+    for (i=0; i<12; i++) {                                              \
+        v = (yuv[-pix2_inc] + 3*(yuv[0]-yuv[-pix_inc]) - yuv[pix_inc] + 4)>>3;\
+        v = pfx##_adjust(v, t);                                         \
+        yuv[-pix_inc] = av_clip_uint8(yuv[-pix_inc] + v);               \
+        yuv[0] = av_clip_uint8(yuv[0] - v);                             \
+        yuv += line_inc;                                                \
+    }                                                                   \
+}
+
+#if CONFIG_VP5_DECODER
 /* Gives very similar result than the vp6 version except in a few cases */
 static int vp5_adjust(int v, int t)
 {
@@ -43,6 +60,18 @@ static int vp5_adjust(int v, int t)
     return v;
 }
 
+
+VP56_EDGE_FILTER(vp5, hor, 1, stride)
+VP56_EDGE_FILTER(vp5, ver, stride, 1)
+
+av_cold void ff_vp5dsp_init(VP56DSPContext *s)
+{
+    s->edge_filter_hor = vp5_edge_filter_hor;
+    s->edge_filter_ver = vp5_edge_filter_ver;
+}
+#endif /* CONFIG_VP5_DECODER */
+
+#if CONFIG_VP6_DECODER
 static int vp6_adjust(int v, int t)
 {
     int V = v, s = v >> 31;
@@ -56,43 +85,19 @@ static int vp6_adjust(int v, int t)
     return V;
 }
 
-
-#define VP56_EDGE_FILTER(pfx, suf, pix_inc, line_inc)                   \
-static void pfx##_edge_filter_##suf(uint8_t *yuv, int stride, int t)    \
-{                                                                       \
-    int pix2_inc = 2 * pix_inc;                                         \
-    int i, v;                                                           \
-                                                                        \
-    for (i=0; i<12; i++) {                                              \
-        v = (yuv[-pix2_inc] + 3*(yuv[0]-yuv[-pix_inc]) - yuv[pix_inc] + 4)>>3;\
-        v = pfx##_adjust(v, t);                                         \
-        yuv[-pix_inc] = av_clip_uint8(yuv[-pix_inc] + v);               \
-        yuv[0] = av_clip_uint8(yuv[0] - v);                             \
-        yuv += line_inc;                                                \
-    }                                                                   \
-}
-
-VP56_EDGE_FILTER(vp5, hor, 1, stride)
-VP56_EDGE_FILTER(vp5, ver, stride, 1)
 VP56_EDGE_FILTER(vp6, hor, 1, stride)
 VP56_EDGE_FILTER(vp6, ver, stride, 1)
 
-av_cold void ff_vp56dsp_init(VP56DSPContext *s, enum AVCodecID codec)
+av_cold void ff_vp6dsp_init(VP56DSPContext *s)
 {
-    if (codec == AV_CODEC_ID_VP5) {
-        s->edge_filter_hor = vp5_edge_filter_hor;
-        s->edge_filter_ver = vp5_edge_filter_ver;
-    } else {
-        s->edge_filter_hor = vp6_edge_filter_hor;
-        s->edge_filter_ver = vp6_edge_filter_ver;
+    s->edge_filter_hor = vp6_edge_filter_hor;
+    s->edge_filter_ver = vp6_edge_filter_ver;
 
-        if (CONFIG_VP6_DECODER) {
-            s->vp6_filter_diag4 = ff_vp6_filter_diag4_c;
+    s->vp6_filter_diag4 = ff_vp6_filter_diag4_c;
 
-            if (ARCH_ARM)
-                ff_vp6dsp_init_arm(s, codec);
-            if (ARCH_X86)
-                ff_vp6dsp_init_x86(s, codec);
-        }
-    }
+    if (ARCH_ARM)
+        ff_vp6dsp_init_arm(s);
+    if (ARCH_X86)
+        ff_vp6dsp_init_x86(s);
 }
+#endif /* CONFIG_VP6_DECODER */
