a8cce89cbf2d45246feec9fa8ce3f165264ce47a
openvela-robot <openvela-robot@xiaomi.com>
2016-10-21 13:29:46 +0800

    avcodec/dnxhdenc: Move allocation out of radix_sort()
    Its slow, its not checked, FPU state is not clean either currently there
    
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavutil/hwcontext_qsv.c
+++ libavutil/hwcontext_qsv.c
@@ -1,18 +1,18 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -90,7 +90,6 @@ static const struct {
     uint32_t           fourcc;
 } supported_pixel_formats[] = {
     { AV_PIX_FMT_NV12, MFX_FOURCC_NV12 },
-    { AV_PIX_FMT_P010, MFX_FOURCC_P010 },
 };
 
 static int qsv_device_init(AVHWDeviceContext *ctx)
