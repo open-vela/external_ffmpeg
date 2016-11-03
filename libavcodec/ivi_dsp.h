da5687e9718297bc8583cd127ab478807af241fa
openvela-robot <openvela-robot@xiaomi.com>
2016-11-03 14:55:56 +0800

    ffmpeg: Fix bsf corrupting merged side data
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavcodec/ivi_dsp.h
+++ libavcodec/ivi_dsp.h
@@ -3,20 +3,20 @@
  *
  * Copyright (c) 2009-2011 Maxim Poliakovski
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -29,7 +29,6 @@
 #ifndef AVCODEC_IVI_DSP_H
 #define AVCODEC_IVI_DSP_H
 
-#include <stddef.h>
 #include <stdint.h>
 
 #include "ivi.h"
@@ -42,7 +41,7 @@
  *  @param[in]   dst_pitch    pitch of the destination buffer
  */
 void ff_ivi_recompose53(const IVIPlaneDesc *plane, uint8_t *dst,
-                        const ptrdiff_t dst_pitch);
+                        const int dst_pitch);
 
 /**
  *  Haar wavelet recomposition filter for Indeo 4
@@ -52,7 +51,7 @@ void ff_ivi_recompose53(const IVIPlaneDesc *plane, uint8_t *dst,
  *  @param[in]  dst_pitch    pitch of the destination buffer
  */
 void ff_ivi_recompose_haar(const IVIPlaneDesc *plane, uint8_t *dst,
-                           const ptrdiff_t dst_pitch);
+                           const int dst_pitch);
 
 /**
  *  two-dimensional inverse Haar 8x8 transform for Indeo 4
@@ -64,7 +63,11 @@ void ff_ivi_recompose_haar(const IVIPlaneDesc *plane, uint8_t *dst,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_inverse_haar_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_inverse_haar_8x8(const int32_t *in, int16_t *out, uint32_t pitch,
+                             const uint8_t *flags);
+void ff_ivi_inverse_haar_8x1(const int32_t *in, int16_t *out, uint32_t pitch,
+                             const uint8_t *flags);
+void ff_ivi_inverse_haar_1x8(const int32_t *in, int16_t *out, uint32_t pitch,
                              const uint8_t *flags);
 
 /**
@@ -77,7 +80,7 @@ void ff_ivi_inverse_haar_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_row_haar8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_row_haar8(const int32_t *in, int16_t *out, uint32_t pitch,
                       const uint8_t *flags);
 
 /**
@@ -90,7 +93,7 @@ void ff_ivi_row_haar8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_col_haar8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_col_haar8(const int32_t *in, int16_t *out, uint32_t pitch,
                       const uint8_t *flags);
 
 /**
@@ -103,7 +106,7 @@ void ff_ivi_col_haar8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_inverse_haar_4x4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_inverse_haar_4x4(const int32_t *in, int16_t *out, uint32_t pitch,
                              const uint8_t *flags);
 
 /**
@@ -116,7 +119,7 @@ void ff_ivi_inverse_haar_4x4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_row_haar4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_row_haar4(const int32_t *in, int16_t *out, uint32_t pitch,
                       const uint8_t *flags);
 
 /**
@@ -129,7 +132,7 @@ void ff_ivi_row_haar4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_col_haar4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_col_haar4(const int32_t *in, int16_t *out, uint32_t pitch,
                       const uint8_t *flags);
 
 /**
@@ -142,7 +145,7 @@ void ff_ivi_col_haar4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *  @param[in]  pitch       pitch to move to the next y line
  *  @param[in]  blk_size    transform block size
  */
-void ff_ivi_dc_haar_2d(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_dc_haar_2d(const int32_t *in, int16_t *out, uint32_t pitch,
                        int blk_size);
 
 /**
@@ -155,7 +158,7 @@ void ff_ivi_dc_haar_2d(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_inverse_slant_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_inverse_slant_8x8(const int32_t *in, int16_t *out, uint32_t pitch,
                               const uint8_t *flags);
 
 /**
@@ -168,7 +171,7 @@ void ff_ivi_inverse_slant_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_inverse_slant_4x4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_inverse_slant_4x4(const int32_t *in, int16_t *out, uint32_t pitch,
                               const uint8_t *flags);
 
 /**
@@ -182,7 +185,7 @@ void ff_ivi_inverse_slant_4x4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *  @param[in]    pitch       pitch to move to the next y line
  *  @param[in]    blk_size    transform block size
  */
-void ff_ivi_dc_slant_2d(const int32_t *in, int16_t *out, ptrdiff_t pitch, int blk_size);
+void ff_ivi_dc_slant_2d(const int32_t *in, int16_t *out, uint32_t pitch, int blk_size);
 
 /**
  *  inverse 1D row slant transform
@@ -192,7 +195,7 @@ void ff_ivi_dc_slant_2d(const int32_t *in, int16_t *out, ptrdiff_t pitch, int bl
  *  @param[in]    pitch   pitch to move to the next y line
  *  @param[in]    flags   pointer to the array of column flags (unused here)
  */
-void ff_ivi_row_slant8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_row_slant8(const int32_t *in, int16_t *out, uint32_t pitch,
                        const uint8_t *flags);
 
 /**
@@ -205,7 +208,7 @@ void ff_ivi_row_slant8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_col_slant8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_col_slant8(const int32_t *in, int16_t *out, uint32_t pitch,
                        const uint8_t *flags);
 
 /**
@@ -216,7 +219,7 @@ void ff_ivi_col_slant8(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *  @param[in]    pitch   pitch to move to the next y line
  *  @param[in]    flags   pointer to the array of column flags (unused here)
  */
-void ff_ivi_row_slant4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_row_slant4(const int32_t *in, int16_t *out, uint32_t pitch,
                        const uint8_t *flags);
 
 /**
@@ -229,29 +232,29 @@ void ff_ivi_row_slant4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
  *                        != 0 - non_empty column, 0 - empty one
  *                        (this array must be filled by caller)
  */
-void ff_ivi_col_slant4(const int32_t *in, int16_t *out, ptrdiff_t pitch,
+void ff_ivi_col_slant4(const int32_t *in, int16_t *out, uint32_t pitch,
                        const uint8_t *flags);
 
 /**
  *  DC-only inverse row slant transform
  */
-void ff_ivi_dc_row_slant(const int32_t *in, int16_t *out, ptrdiff_t pitch, int blk_size);
+void ff_ivi_dc_row_slant(const int32_t *in, int16_t *out, uint32_t pitch, int blk_size);
 
 /**
  *  DC-only inverse column slant transform
  */
-void ff_ivi_dc_col_slant(const int32_t *in, int16_t *out, ptrdiff_t pitch, int blk_size);
+void ff_ivi_dc_col_slant(const int32_t *in, int16_t *out, uint32_t pitch, int blk_size);
 
 /**
  *  Copy the pixels into the frame buffer.
  */
-void ff_ivi_put_pixels_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch, const uint8_t *flags);
+void ff_ivi_put_pixels_8x8(const int32_t *in, int16_t *out, uint32_t pitch, const uint8_t *flags);
 
 /**
  *  Copy the DC coefficient into the first pixel of the block and
  *  zero all others.
  */
-void ff_ivi_put_dc_pixel_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch, int blk_size);
+void ff_ivi_put_dc_pixel_8x8(const int32_t *in, int16_t *out, uint32_t pitch, int blk_size);
 
 /**
  *  8x8 block motion compensation with adding delta
@@ -261,7 +264,7 @@ void ff_ivi_put_dc_pixel_8x8(const int32_t *in, int16_t *out, ptrdiff_t pitch, i
  *  @param[in]       pitch    pitch for moving to the next y line
  *  @param[in]       mc_type  interpolation type
  */
-void ff_ivi_mc_8x8_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitch, int mc_type);
+void ff_ivi_mc_8x8_delta(int16_t *buf, const int16_t *ref_buf, uint32_t pitch, int mc_type);
 
 /**
  *  4x4 block motion compensation with adding delta
@@ -271,7 +274,7 @@ void ff_ivi_mc_8x8_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitch, 
  *  @param[in]       pitch    pitch for moving to the next y line
  *  @param[in]       mc_type  interpolation type
  */
-void ff_ivi_mc_4x4_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitch, int mc_type);
+void ff_ivi_mc_4x4_delta(int16_t *buf, const int16_t *ref_buf, uint32_t pitch, int mc_type);
 
 /**
  *  motion compensation without adding delta
@@ -281,7 +284,7 @@ void ff_ivi_mc_4x4_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitch, 
  *  @param[in]      pitch    pitch for moving to the next y line
  *  @param[in]      mc_type  interpolation type
  */
-void ff_ivi_mc_8x8_no_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitch, int mc_type);
+void ff_ivi_mc_8x8_no_delta(int16_t *buf, const int16_t *ref_buf, uint32_t pitch, int mc_type);
 
 /**
  *  4x4 block motion compensation without adding delta
@@ -291,7 +294,7 @@ void ff_ivi_mc_8x8_no_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitc
  *  @param[in]      pitch    pitch for moving to the next y line
  *  @param[in]      mc_type  interpolation type
  */
-void ff_ivi_mc_4x4_no_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitch, int mc_type);
+void ff_ivi_mc_4x4_no_delta(int16_t *buf, const int16_t *ref_buf, uint32_t pitch, int mc_type);
 
 /**
  *  8x8 block motion compensation with adding delta
@@ -303,7 +306,7 @@ void ff_ivi_mc_4x4_no_delta(int16_t *buf, const int16_t *ref_buf, ptrdiff_t pitc
  *  @param[in]      mc_type  interpolation type for backward reference
  *  @param[in]      mc_type2 interpolation type for forward reference
  */
-void ff_ivi_mc_avg_8x8_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, ptrdiff_t pitch, int mc_type, int mc_type2);
+void ff_ivi_mc_avg_8x8_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, uint32_t pitch, int mc_type, int mc_type2);
 
 /**
  *  4x4 block motion compensation with adding delta
@@ -315,7 +318,7 @@ void ff_ivi_mc_avg_8x8_delta(int16_t *buf, const int16_t *ref_buf, const int16_t
  *  @param[in]      mc_type  interpolation type for backward reference
  *  @param[in]      mc_type2 interpolation type for forward reference
  */
-void ff_ivi_mc_avg_4x4_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, ptrdiff_t pitch, int mc_type, int mc_type2);
+void ff_ivi_mc_avg_4x4_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, uint32_t pitch, int mc_type, int mc_type2);
 
 /**
  *  motion compensation without adding delta for B-frames
@@ -327,7 +330,7 @@ void ff_ivi_mc_avg_4x4_delta(int16_t *buf, const int16_t *ref_buf, const int16_t
  *  @param[in]      mc_type  interpolation type for backward reference
  *  @param[in]      mc_type2 interpolation type for forward reference
  */
-void ff_ivi_mc_avg_8x8_no_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, ptrdiff_t pitch, int mc_type, int mc_type2);
+void ff_ivi_mc_avg_8x8_no_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, uint32_t pitch, int mc_type, int mc_type2);
 
 /**
  *  4x4 block motion compensation without adding delta for B-frames
@@ -339,6 +342,6 @@ void ff_ivi_mc_avg_8x8_no_delta(int16_t *buf, const int16_t *ref_buf, const int1
  *  @param[in]      mc_type  interpolation type for backward reference
  *  @param[in]      mc_type2 interpolation type for forward reference
  */
-void ff_ivi_mc_avg_4x4_no_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, ptrdiff_t pitch, int mc_type, int mc_type2);
+void ff_ivi_mc_avg_4x4_no_delta(int16_t *buf, const int16_t *ref_buf, const int16_t *ref_buf2, uint32_t pitch, int mc_type, int mc_type2);
 
 #endif /* AVCODEC_IVI_DSP_H */
--- tests/checkasm/h264dsp.c
+++ tests/checkasm/h264dsp.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2016 Martin Storsjo
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or modify
+ * FFmpeg is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with Libav; if not, write to the Free Software Foundation, Inc.,
+ * with FFmpeg; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
@@ -34,7 +34,6 @@ static const uint32_t pixel_mask[3] = { 0xffffffff, 0x01ff01ff, 0x03ff03ff };
 
 #define randomize_buffers()                                                  \
     do {                                                                     \
-        int x, y;                                                            \
         uint32_t mask = pixel_mask[bit_depth - 8];                           \
         for (y = 0; y < sz; y++) {                                           \
             for (x = 0; x < PIXEL_STRIDE; x += 4) {                          \
@@ -179,7 +178,8 @@ static void check_idct(void)
     LOCAL_ALIGNED_16(int16_t, subcoef0, [8 * 8 * 2]);
     LOCAL_ALIGNED_16(int16_t, subcoef1, [8 * 8 * 2]);
     H264DSPContext h;
-    int bit_depth, sz, align, dc;
+    int bit_depth, sz, align;
+    int x, y, dc;
     declare_func_emms(AV_CPU_FLAG_MMX, void, uint8_t *dst, int16_t *block, int stride);
 
     for (bit_depth = 8; bit_depth <= 10; bit_depth++) {
