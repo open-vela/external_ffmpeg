ca4aa573655f997516212e3ad63aa1eb56fcb594
openvela-robot <openvela-robot@xiaomi.com>
2016-11-13 20:52:02 +0800

    softfloat: handle -INT_MAX correctly
    This is similar to commit 9ac61e73d0843ec4b83f4e3d47eded73234e406e.
    
    Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
    Signed-off-by: Andreas Cadhalpun <Andreas.Cadhalpun@googlemail.com>

--- libavcodec/aarch64/mpegaudiodsp_init.c
+++ libavcodec/aarch64/mpegaudiodsp_init.c
@@ -1,22 +1,21 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <stddef.h>
 #include <stdint.h>
 
 #include "libavutil/attributes.h"
@@ -25,9 +24,9 @@
 #include "config.h"
 
 void ff_mpadsp_apply_window_fixed_neon(int32_t *synth_buf, int32_t *window,
-                                       int *dither, int16_t *samples, ptrdiff_t incr);
+                                       int *dither, int16_t *samples, int incr);
 void ff_mpadsp_apply_window_float_neon(float *synth_buf, float *window,
-                                       int *dither, float *samples, ptrdiff_t incr);
+                                       int *dither, float *samples, int incr);
 
 av_cold void ff_mpadsp_init_aarch64(MPADSPContext *s)
 {
