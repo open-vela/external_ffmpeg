97b4881967ca98ffde7d04e8f750db5b923d56bc
openvela-robot <openvela-robot@xiaomi.com>
2016-12-09 15:22:52 +0800

    libavcodec/tests: Add avpacket test to .gitignore
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- libavutil/tests/cpu_init.c
+++ libavutil/tests/cpu_init.c
@@ -1,18 +1,18 @@
 /*
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -24,9 +24,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "config.h"
+
 #include "libavutil/cpu.h"
 #include "libavutil/thread.h"
 
+#if HAVE_PTHREADS
 static void *thread_main(void *arg)
 {
     int *flags = arg;
@@ -34,9 +37,12 @@ static void *thread_main(void *arg)
     *flags = av_get_cpu_flags();
     return NULL;
 }
+#endif
 
-int main(void)
+
+int main(int argc, char **argv)
 {
+#if HAVE_PTHREADS
     int cpu_flags1;
     int cpu_flags2;
     int ret;
@@ -60,6 +66,7 @@ int main(void)
         return 2;
     if (cpu_flags1 != cpu_flags2)
         return 3;
+#endif
 
     return 0;
 }
