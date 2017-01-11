14f8b0924cb5ee9498fb1c852a56987516a010ca
openvela-robot <openvela-robot@xiaomi.com>
2017-01-11 13:12:19 +0800

    hevc: Mark as having threadsafe init
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>
    Signed-off-by: Anton Khirnov <anton@khirnov.net>

--- libavutil/time_internal.h
+++ libavutil/time_internal.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -22,7 +22,7 @@
 #include <time.h>
 #include "config.h"
 
-#if !HAVE_GMTIME_R && !defined(gmtime_r)
+#if !HAVE_GMTIME_R
 static inline struct tm *gmtime_r(const time_t* clock, struct tm *result)
 {
     struct tm *ptr = gmtime(clock);
@@ -33,7 +33,7 @@ static inline struct tm *gmtime_r(const time_t* clock, struct tm *result)
 }
 #endif
 
-#if !HAVE_LOCALTIME_R && !defined(localtime_r)
+#if !HAVE_LOCALTIME_R
 static inline struct tm *localtime_r(const time_t* clock, struct tm *result)
 {
     struct tm *ptr = localtime(clock);
