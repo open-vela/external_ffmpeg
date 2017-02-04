a020ff7f249af6321d7455b7f95bbe3c1cb30e2d
openvela-robot <openvela-robot@xiaomi.com>
2017-02-05 01:16:29 +0800

    ffplay: change keyboard volume control to logarithmic
    The command line parameter remains linear.
    
    Signed-off-by: Marton Balint <cus@passwd.hu>

--- libavformat/rtmpdh.h
+++ libavformat/rtmpdh.h
@@ -2,20 +2,20 @@
  * RTMP Diffie-Hellmann utilities
  * Copyright (c) 2012 Samuel Pitoiset
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
- * Libav is free software; you can redistribute it and/or
+ * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Libav; if not, write to the Free Software
+ * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -26,15 +26,15 @@
 
 #include "config.h"
 
+#if CONFIG_GMP || CONFIG_GCRYPT
 #if CONFIG_GMP
 #include <gmp.h>
 
 typedef mpz_ptr FFBigNum;
-#elif CONFIG_OPENSSL
-#include <openssl/bn.h>
-#include <openssl/dh.h>
+#elif CONFIG_GCRYPT
+#include <gcrypt.h>
 
-typedef BIGNUM *FFBigNum;
+typedef gcry_mpi_t FFBigNum;
 #endif
 
 typedef struct FF_DH {
@@ -45,6 +45,13 @@ typedef struct FF_DH {
     long length;
 } FF_DH;
 
+#elif CONFIG_OPENSSL
+#include <openssl/bn.h>
+#include <openssl/dh.h>
+
+typedef BIGNUM *FFBigNum;
+typedef DH FF_DH;
+#endif
 
 /**
  * Initialize a Diffie-Hellmann context.
