068acbc36e034fe40563d62b109e43b6e113b49b
Petri Hintukainen <phintuka@gmail.com>
2017-02-06 09:41:03 +0200

    matroska: demux BluRay text subtitles
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- libavformat/matroska.c
+++ libavformat/matroska.c
@@ -65,6 +65,7 @@ const CodecTags ff_mkv_codec_tags[]={
     {"S_VOBSUB"         , AV_CODEC_ID_DVD_SUBTITLE},
     {"S_DVBSUB"         , AV_CODEC_ID_DVB_SUBTITLE},
     {"S_HDMV/PGS"       , AV_CODEC_ID_HDMV_PGS_SUBTITLE},
+    {"S_HDMV/TEXTST"    , AV_CODEC_ID_HDMV_TEXT_SUBTITLE},
 
     {"V_AV1"            , AV_CODEC_ID_AV1},
     {"V_DIRAC"          , AV_CODEC_ID_DIRAC},
