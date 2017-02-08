2fe1a1b662172a5f72e36ad7f79afad3b824e475
openvela-robot <openvela-robot@xiaomi.com>
2017-02-08 18:06:34 +0800

    configure: Correctly recurse in do_check_deps()
    Fixes all sorts of configuration problems introducec by dad7a9c7c0ae
    on non-Linux or non-vanilla configs. Also removes a line made redundant
    in that commit.

--- libavcodec/zerocodec.c
+++ libavcodec/zerocodec.c
@@ -59,8 +59,10 @@ static int zerocodec_decode_frame(AVCodecContext *avctx, void *data,
         return AVERROR_INVALIDDATA;
     }
 
-    if ((ret = ff_get_buffer(avctx, pic, AV_GET_BUFFER_FLAG_REF)) < 0)
-        return ret;
+    if (ff_get_buffer(avctx, pic, AV_GET_BUFFER_FLAG_REF) < 0) {
+        av_log(avctx, AV_LOG_ERROR, "Could not allocate buffer.\n");
+        return AVERROR(ENOMEM);
+    }
 
     zstream->next_in  = avpkt->data;
     zstream->avail_in = avpkt->size;
@@ -149,5 +151,4 @@ AVCodec ff_zerocodec_decoder = {
     .decode         = zerocodec_decode_frame,
     .close          = zerocodec_decode_close,
     .capabilities   = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
