4dbeceea4b60d481d78926e6ee8a9fd8d05507b7
openvela-robot <openvela-robot@xiaomi.com>
2017-02-21 10:48:37 +0800

    avcodec/h264, videotoolbox: fix crash after VT decoder fails
    The way videotoolbox hooks in as a hwaccel is pretty hacky. The VT decode
    API is not invoked until end_frame(), so alloc_frame() returns a dummy
    frame with a 1-byte buffer. When end_frame() is eventually called, the
    dummy buffer is replaced with the actual decoded data from
    VTDecompressionSessionDecodeFrame().
    
    When the VT decoder fails, the frame returned to the h264 decoder from
    alloc_frame() remains invalid and should not be used. Before
    9747219958060d8c4f697df62e7f172c2a77e6c7, it was accidentally being
    returned all the way up to the API user. After that commit, the dummy
    frame was unref'd so the user received an error.
    
    However, since that commit, VT hwaccel failures started causing random
    segfaults in the h264 decoder. This happened more often on iOS where the
    VT implementation is more likely to throw errors on bitstream anomolies.
    A recent report of this issue can be see in
    http://ffmpeg.org/pipermail/libav-user/2016-November/009831.html
    
    The issue here is that the dummy frame is still referenced internally by the
    h264 decoder, as part of the reflist and cur_pic_ptr. Deallocating the
    frame causes assertions like this one to trip later on during decoding:
    
      Assertion h->cur_pic_ptr->f->buf[0] failed at src/libavcodec/h264_slice.c:1340
    
    With this commit, we leave the dummy 1-byte frame intact, but avoid returning it
    to the user.
    
    This reverts commit 9747219958060d8c4f697df62e7f172c2a77e6c7.
    
    Signed-off-by: wm4 <nfxjfg@googlemail.com>

--- libavcodec/libfdk-aacdec.c
+++ libavcodec/libfdk-aacdec.c
@@ -2,7 +2,7 @@
  * AAC decoder wrapper
  * Copyright (c) 2012 Martin Storsjo
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -44,7 +44,7 @@ typedef struct FDKAACDecContext {
     uint8_t *decoder_buffer;
     int decoder_buffer_size;
     uint8_t *anc_buffer;
-    enum ConcealMethod conceal_method;
+    int conceal_method;
     int drc_level;
     int drc_boost;
     int drc_heavy;
@@ -199,8 +199,8 @@ static av_cold int fdk_aac_decode_close(AVCodecContext *avctx)
 
     if (s->handle)
         aacDecoder_Close(s->handle);
-    av_free(s->decoder_buffer);
-    av_free(s->anc_buffer);
+    av_freep(&s->decoder_buffer);
+    av_freep(&s->anc_buffer);
 
     return 0;
 }
@@ -341,10 +341,9 @@ static int fdk_aac_decode_frame(AVCodecContext *avctx, void *data,
         goto end;
     frame->nb_samples = avctx->frame_size;
 
-    if ((ret = ff_get_buffer(avctx, frame, 0)) < 0) {
-        av_log(avctx, AV_LOG_ERROR, "ff_get_buffer() failed\n");
+    if ((ret = ff_get_buffer(avctx, frame, 0)) < 0)
         goto end;
-    }
+
     memcpy(frame->extended_data[0], s->decoder_buffer,
            avctx->channels * avctx->frame_size *
            av_get_bytes_per_sample(avctx->sample_fmt));
