b01540eb9da64cb9eab83f8294660c883512fd1f
openvela-robot <openvela-robot@xiaomi.com>
2017-03-23 12:20:50 +0800

    avplay: Use the named syntax for buffersrc arguments
    Avoid confusion.

--- libavfilter/audio.c
+++ libavfilter/audio.c
@@ -1,34 +1,27 @@
 /*
- * Copyright (c) Stefano Sabatini | stefasab at gmail.com
- * Copyright (c) S.N. Hemanth Meenakshisundaram | smeenaks at ucsd.edu
+ * This file is part of Libav.
  *
- * This file is part of FFmpeg.
- *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavutil/avassert.h"
 #include "libavutil/channel_layout.h"
 #include "libavutil/common.h"
 
 #include "audio.h"
 #include "avfilter.h"
 #include "internal.h"
-
-#define BUFFER_ALIGN 0
-
 
 AVFrame *ff_null_get_audio_buffer(AVFilterLink *link, int nb_samples)
 {
@@ -37,48 +30,26 @@ AVFrame *ff_null_get_audio_buffer(AVFilterLink *link, int nb_samples)
 
 AVFrame *ff_default_get_audio_buffer(AVFilterLink *link, int nb_samples)
 {
-    AVFrame *frame = NULL;
-    int channels = link->channels;
+    AVFrame *frame = av_frame_alloc();
+    int channels = av_get_channel_layout_nb_channels(link->channel_layout);
+    int ret;
 
-    av_assert0(channels == av_get_channel_layout_nb_channels(link->channel_layout) || !av_get_channel_layout_nb_channels(link->channel_layout));
-
-    if (!link->frame_pool) {
-        link->frame_pool = ff_frame_pool_audio_init(av_buffer_allocz, channels,
-                                                    nb_samples, link->format, BUFFER_ALIGN);
-        if (!link->frame_pool)
-            return NULL;
-    } else {
-        int pool_channels = 0;
-        int pool_nb_samples = 0;
-        int pool_align = 0;
-        enum AVSampleFormat pool_format = AV_SAMPLE_FMT_NONE;
-
-        if (ff_frame_pool_get_audio_config(link->frame_pool,
-                                           &pool_channels, &pool_nb_samples,
-                                           &pool_format, &pool_align) < 0) {
-            return NULL;
-        }
-
-        if (pool_channels != channels || pool_nb_samples < nb_samples ||
-            pool_format != link->format || pool_align != BUFFER_ALIGN) {
-
-            ff_frame_pool_uninit((FFFramePool **)&link->frame_pool);
-            link->frame_pool = ff_frame_pool_audio_init(av_buffer_allocz, channels,
-                                                        nb_samples, link->format, BUFFER_ALIGN);
-            if (!link->frame_pool)
-                return NULL;
-        }
-    }
-
-    frame = ff_frame_pool_get(link->frame_pool);
     if (!frame)
         return NULL;
 
-    frame->nb_samples = nb_samples;
+    frame->nb_samples     = nb_samples;
+    frame->format         = link->format;
     frame->channel_layout = link->channel_layout;
-    frame->sample_rate = link->sample_rate;
+    frame->sample_rate    = link->sample_rate;
+    ret = av_frame_get_buffer(frame, 0);
+    if (ret < 0) {
+        av_frame_free(&frame);
+        return NULL;
+    }
 
-    av_samples_set_silence(frame->extended_data, 0, nb_samples, channels, link->format);
+    av_samples_set_silence(frame->extended_data, 0, nb_samples, channels,
+                           link->format);
+
 
     return frame;
 }
