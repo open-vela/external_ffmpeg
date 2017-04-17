c566449ce3c037e4d9836e5df9ad237ce4e52f2c
openvela-robot <openvela-robot@xiaomi.com>
2017-04-17 12:53:14 +0800

    dca: Account for lfe when checking for the channel count
    Bug-Id: 1037
    CC: libav-stable@libav.org

--- libavcodec/ppc/hevcdsp_template.c
+++ libavcodec/ppc/hevcdsp_template.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) Alexandra Hajkova
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -22,9 +22,9 @@ static void FUNC(ff_hevc_idct_4x4, BIT_DEPTH)(int16_t *coeffs, int col_limit)
 {
     const int shift = 7;
     const int shift2 = 20 - BIT_DEPTH;
-    vector int16_t src_01, src_23;
-    vector int32_t res[4];
-    vector int16_t res_packed[2];
+    vec_s16 src_01, src_23;
+    vec_s32 res[4];
+    vec_s16 res_packed[2];
 
     src_01 = vec_ld(0, coeffs);
     src_23 = vec_ld(16, coeffs);
