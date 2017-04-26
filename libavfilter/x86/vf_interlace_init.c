e8162bf00edf671c1a8e6cf7ca7c882593227ae0
openvela-robot <openvela-robot@xiaomi.com>
2017-04-26 13:35:57 +0800

    Merge commit '86157e6db2c7a9222f77fa7e7f50fb9aebc3aa81'
    * commit '86157e6db2c7a9222f77fa7e7f50fb9aebc3aa81':
      hevc: decouple calling get_format() from exporting the SPS parameters
    
    See 786032cad8ecabe577d9cff0356da6e9e9488a2d (which has been reverted
    and replaced with Anton's version to reduce diffs between the two
    projects).
    
    Merged-by: Clément Bœsch <u@pkh.me>

--- libavfilter/x86/vf_interlace_init.c
+++ libavfilter/x86/vf_interlace_init.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2014 Kieran Kunhya <kierank@obe.tv>
  *
- * This file is part of Libav.
+ * This file is part of FFmpeg.
  *
-  * Libav is free software; you can redistribute it and/or modify
+  * FFmpeg is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Libav is distributed in the hope that it will be useful,
+ * FFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with Libav; if not, write to the Free Software Foundation, Inc.,
+ * with FFmpeg; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
@@ -28,12 +28,10 @@
 
 void ff_lowpass_line_sse2(uint8_t *dstp, ptrdiff_t linesize,
                           const uint8_t *srcp,
-                          const uint8_t *srcp_above,
-                          const uint8_t *srcp_below);
+                          ptrdiff_t mref, ptrdiff_t pref);
 void ff_lowpass_line_avx (uint8_t *dstp, ptrdiff_t linesize,
                           const uint8_t *srcp,
-                          const uint8_t *srcp_above,
-                          const uint8_t *srcp_below);
+                          ptrdiff_t mref, ptrdiff_t pref);
 
 av_cold void ff_interlace_init_x86(InterlaceContext *s)
 {
