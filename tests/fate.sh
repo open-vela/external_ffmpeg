c7a7d4cdd59c3aaa84f489e1d2d841f4f7aa1881
openvela-robot <openvela-robot@xiaomi.com>
2017-05-04 15:24:46 +0800

    avcodec/msvideo1: Check buffer size before re-getting the frame
    Fixes timeout
    Fixes: 1306/clusterfuzz-testcase-minimized-6152296217968640
    
    Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/targets/ffmpeg
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- tests/fate.sh
+++ tests/fate.sh
@@ -37,17 +37,18 @@ checkout(){
 update()(
     cd ${src} || return
     case "$repo" in
-        git:*) git fetch --quiet --force; git reset --quiet --hard "origin/$branch" ;;
+        git:*) git fetch --quiet --force && git reset --quiet --hard "origin/$branch" ;;
     esac
 )
 
 configure()(
     cd ${build} || return
-    ${src}/configure                                                    \
+    ${shell} ${src}/configure                                           \
         --prefix="${inst}"                                              \
         --samples="${samples}"                                          \
         --enable-gpl                                                    \
-        ${ignore_tests:+--ignore-tests="$ignore_tests"}                 \
+        --enable-memory-poisoning                                       \
+        --enable-avresample                                             \
         ${arch:+--arch=$arch}                                           \
         ${cpu:+--cpu="$cpu"}                                            \
         ${toolchain:+--toolchain="$toolchain"}                          \
@@ -84,7 +85,8 @@ clean(){
 report(){
     date=$(date -u +%Y%m%d%H%M%S)
     echo "fate:1:${date}:${slot}:${version}:$1:$2:${branch}:${comment}" >report
-    cat ${build}/avbuild/config.fate ${build}/tests/data/fate/*.rep >> report 2> /dev/null
+    cat ${build}/ffbuild/config.fate >>report
+    cat ${build}/tests/data/fate/*.rep >>report 2>/dev/null || for i in ${build}/tests/data/fate/*.rep ; do cat "$i" >>report 2>/dev/null; done
     test -n "$fate_recv" && $tar report *.log | gzip | $fate_recv
 }
 
@@ -106,15 +108,15 @@ test -d "$src" && update || checkout || die "Error fetching source"
 
 cd ${workdir}
 
-version=$(${src}/avbuild/version.sh ${src})
+version=$(${src}/ffbuild/version.sh ${src})
 test "$version" = "$(cat version-$slot 2>/dev/null)" && exit 0
 echo ${version} >version-$slot
 
 rm -rf "${build}" *.log
 mkdir -p ${build}
 
-configure >configure.log 2>&1 || fail $? "error configuring"
-compile   >compile.log   2>&1 || fail $? "error compiling"
-fate      >test.log      2>&1 || fail $? "error testing"
+configure >configure.log 2>&1 || fail 3 "error configuring"
+compile   >compile.log   2>&1 || fail 2 "error compiling"
+fate      >test.log      2>&1 || fail 1 "error testing"
 report 0 success
 clean
