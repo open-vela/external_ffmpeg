d15b6e7ea6cf79cae9ac3753e0174b82ff0433d3
openvela-robot <openvela-robot@xiaomi.com>
2017-05-31 13:08:29 +0800

    os_support: Remove the dynamic loading of getaddrinfo from the fallback getaddrinfo
    If we for some unexplicable reason didn't pick up getaddrinfo
    at configure, the default, IPv4-only, fallback should be good enough.
    
    This effectively reverts 6023d84a2b7.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavcodec/vp9data.h
+++ libavcodec/vp9data.h
@@ -2,20 +2,20 @@
  * Copyright (C) 2013 Ronald S. Bultje <rsbultje gmail com>
  * Copyright (C) 2013 Clément Bœsch <u pkh me>
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -24,9 +24,8 @@
 
 #include <stdint.h>
 
-#include "vp9dec.h"
+#include "vp9.h"
 
-extern const uint8_t ff_vp9_bwh_tab[2][N_BS_SIZES][2];
 extern const int8_t ff_vp9_partition_tree[3][2];
 extern const uint8_t ff_vp9_default_kf_partition_probs[4][4][3];
 extern const int8_t ff_vp9_segmentation_tree[7][2];
@@ -36,8 +35,8 @@ extern const uint8_t ff_vp9_default_kf_uvmode_probs[10][9];
 extern const int8_t ff_vp9_inter_mode_tree[3][2];
 extern const int8_t ff_vp9_filter_tree[2][2];
 extern const enum FilterMode ff_vp9_filter_lut[3];
-extern const int16_t ff_vp9_dc_qlookup[3][256];
-extern const int16_t ff_vp9_ac_qlookup[3][256];
+extern const int16_t ff_vp9_dc_qlookup[256];
+extern const int16_t ff_vp9_ac_qlookup[256];
 extern const enum TxfmType ff_vp9_intra_txfm_type[14];
 extern const int16_t ff_vp9_default_scan_4x4[16];
 extern const int16_t ff_vp9_col_scan_4x4[16];
