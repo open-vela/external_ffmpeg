5e3edcdb97603fe4dec17bc72c7ee50246ebc2b7
openvela-robot <openvela-robot@xiaomi.com>
2017-06-09 12:05:00 +0800

    d3d11va: Link directly to dxgi.dll and d3d11.dll functions if LoadLibrary is unavailable
    When targeting the UWP API subset, the LoadLibrary function is not
    available (and the fallback, LoadPackagedLibrary, can't be used to
    load system DLLs). In these cases, link directly to the functions
    in the DLLs instead of trying to load them dynamically at runtime.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/apetag.c
+++ libavformat/apetag.c
@@ -3,20 +3,20 @@
  * Copyright (c) 2007 Benjamin Zores <ben@geexbox.org>
  *  based upon libdemac from Dave Chapman.
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -29,6 +29,9 @@
 #include "apetag.h"
 #include "internal.h"
 
+#define APE_TAG_VERSION               2000
+#define APE_TAG_FOOTER_BYTES          32
+#define APE_TAG_HEADER_BYTES          32
 #define APE_TAG_FLAG_CONTAINS_HEADER  (1 << 31)
 #define APE_TAG_FLAG_LACKS_FOOTER     (1 << 30)
 #define APE_TAG_FLAG_IS_HEADER        (1 << 29)
@@ -62,19 +65,15 @@ static int ape_tag_read_field(AVFormatContext *s)
     if (flags & APE_TAG_FLAG_IS_BINARY) {
         uint8_t filename[1024];
         enum AVCodecID id;
-        int ret;
         AVStream *st = avformat_new_stream(s, NULL);
         if (!st)
             return AVERROR(ENOMEM);
 
-        ret = avio_get_str(pb, size, filename, sizeof(filename));
-        if (ret < 0)
-            return ret;
-        if (size <= ret) {
+        size -= avio_get_str(pb, size, filename, sizeof(filename));
+        if (size <= 0) {
             av_log(s, AV_LOG_WARNING, "Skipping binary tag '%s'.\n", key);
             return 0;
         }
-        size -= ret;
 
         av_dict_set(&st->metadata, key, filename, 0);
 
@@ -96,8 +95,14 @@ static int ape_tag_read_field(AVFormatContext *s)
             st->attached_pic.stream_index = st->index;
             st->attached_pic.flags       |= AV_PKT_FLAG_KEY;
         } else {
-            if (ff_get_extradata(s, st->codecpar, s->pb, size) < 0)
+            st->codecpar->extradata = av_malloc(size + AV_INPUT_BUFFER_PADDING_SIZE);
+            if (!st->codecpar->extradata)
                 return AVERROR(ENOMEM);
+            if (avio_read(pb, st->codecpar->extradata, size) != size) {
+                av_freep(&st->codecpar->extradata);
+                return AVERROR(EIO);
+            }
+            st->codecpar->extradata_size = size;
             st->codecpar->codec_type = AVMEDIA_TYPE_ATTACHMENT;
         }
     } else {
@@ -130,7 +135,7 @@ int64_t ff_ape_parse_tag(AVFormatContext *s)
     avio_seek(pb, file_size - APE_TAG_FOOTER_BYTES, SEEK_SET);
 
     avio_read(pb, buf, 8);     /* APETAGEX */
-    if (strncmp(buf, APE_TAG_PREAMBLE, 8)) {
+    if (strncmp(buf, "APETAGEX", 8)) {
         return 0;
     }
 
@@ -176,61 +181,42 @@ int64_t ff_ape_parse_tag(AVFormatContext *s)
     return tag_start;
 }
 
-static int string_is_ascii(const uint8_t *str)
-{
-    while (*str && *str >= 0x20 && *str <= 0x7e ) str++;
-    return !*str;
-}
-
 int ff_ape_write_tag(AVFormatContext *s)
 {
     AVDictionaryEntry *e = NULL;
-    int size, ret, count = 0;
-    AVIOContext *dyn_bc = NULL;
-    uint8_t *dyn_buf = NULL;
+    int64_t start, end;
+    int size, count = 0;
 
-    if ((ret = avio_open_dyn_buf(&dyn_bc)) < 0)
-        goto end;
+    if (!(s->pb->seekable & AVIO_SEEKABLE_NORMAL))
+        return 0;
 
-    ff_standardize_creation_time(s);
-    while ((e = av_dict_get(s->metadata, "", e, AV_DICT_IGNORE_SUFFIX))) {
-        int val_len;
-
-        if (!string_is_ascii(e->key)) {
-            av_log(s, AV_LOG_WARNING, "Non ASCII keys are not allowed\n");
-            continue;
-        }
-
-        val_len = strlen(e->value);
-        avio_wl32(dyn_bc, val_len);            // value length
-        avio_wl32(dyn_bc, 0);                  // item flags
-        avio_put_str(dyn_bc, e->key);          // key
-        avio_write(dyn_bc, e->value, val_len); // value
-        count++;
-    }
-    if (!count)
-        goto end;
-
-    size = avio_close_dyn_buf(dyn_bc, &dyn_buf);
-    if (size <= 0)
-        goto end;
-    size += APE_TAG_FOOTER_BYTES;
+    start = avio_tell(s->pb);
 
     // header
     avio_write(s->pb, "APETAGEX", 8);   // id
-    avio_wl32(s->pb, APE_TAG_VERSION);  // version
-    avio_wl32(s->pb, size);
-    avio_wl32(s->pb, count);
+    avio_wl32 (s->pb, APE_TAG_VERSION); // version
+    avio_wl32(s->pb, 0);                // reserve space for size
+    avio_wl32(s->pb, 0);                // reserve space for tag count
 
     // flags
     avio_wl32(s->pb, APE_TAG_FLAG_CONTAINS_HEADER | APE_TAG_FLAG_IS_HEADER);
     ffio_fill(s->pb, 0, 8);             // reserved
 
-    avio_write(s->pb, dyn_buf, size - APE_TAG_FOOTER_BYTES);
+    while ((e = av_dict_get(s->metadata, "", e, AV_DICT_IGNORE_SUFFIX))) {
+        int val_len = strlen(e->value);
+
+        avio_wl32(s->pb, val_len);            // value length
+        avio_wl32(s->pb, 0);                  // item flags
+        avio_put_str(s->pb, e->key);          // key
+        avio_write(s->pb, e->value, val_len); // value
+        count++;
+    }
+
+    size = avio_tell(s->pb) - start;
 
     // footer
     avio_write(s->pb, "APETAGEX", 8);   // id
-    avio_wl32(s->pb, APE_TAG_VERSION);  // version
+    avio_wl32 (s->pb, APE_TAG_VERSION); // version
     avio_wl32(s->pb, size);             // size
     avio_wl32(s->pb, count);            // tag count
 
@@ -238,10 +224,12 @@ int ff_ape_write_tag(AVFormatContext *s)
     avio_wl32(s->pb, APE_TAG_FLAG_CONTAINS_HEADER);
     ffio_fill(s->pb, 0, 8);             // reserved
 
-end:
-    if (dyn_bc && !dyn_buf)
-        avio_close_dyn_buf(dyn_bc, &dyn_buf);
-    av_freep(&dyn_buf);
+    // update values in the header
+    end = avio_tell(s->pb);
+    avio_seek(s->pb, start + 12, SEEK_SET);
+    avio_wl32(s->pb, size);
+    avio_wl32(s->pb, count);
+    avio_seek(s->pb, end, SEEK_SET);
 
-    return ret;
+    return 0;
 }
