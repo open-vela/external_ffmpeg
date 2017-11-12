ef0187a8e3c00de4ec5a40e07dee226ac969dba1
openvela-robot <openvela-robot@xiaomi.com>
2017-11-12 22:56:45 +0800

    configure: fix writing library dependencies to config.sh
    Signed-off-by: James Almer <jamrial@gmail.com>

--- libavformat/network.h
+++ libavformat/network.h
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2007 The FFmpeg Project
+ * Copyright (c) 2007 The Libav Project
  *
- * This file is part of FFmpeg.
+ * This file is part of Libav.
  *
- * FFmpeg is free software; you can redistribute it and/or
+ * Libav is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * FFmpeg is distributed in the hope that it will be useful,
+ * Libav is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with FFmpeg; if not, write to the Free Software
+ * License along with Libav; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
@@ -27,7 +27,6 @@
 #include "config.h"
 #include "libavutil/error.h"
 #include "os_support.h"
-#include "avio.h"
 #include "url.h"
 
 #if HAVE_UNISTD_H
@@ -78,22 +77,10 @@ extern int ff_network_inited_globally;
 int ff_network_init(void);
 void ff_network_close(void);
 
-int ff_tls_init(void);
+void ff_tls_init(void);
 void ff_tls_deinit(void);
 
 int ff_network_wait_fd(int fd, int write);
-
-/**
- * This works similarly to ff_network_wait_fd, but waits up to 'timeout' microseconds
- * Uses ff_network_wait_fd in a loop
- *
- * @fd Socket descriptor
- * @write Set 1 to wait for socket able to be read, 0 to be written
- * @timeout Timeout interval, in microseconds. Actual precision is 100000 mcs, due to ff_network_wait_fd usage
- * @param int_cb Interrupt callback, is checked before each ff_network_wait_fd call
- * @return 0 if data can be read/written, AVERROR(ETIMEDOUT) if timeout expired, or negative error code
- */
-int ff_network_wait_fd_timeout(int fd, int write, int64_t timeout, AVIOInterruptCB *int_cb);
 
 int ff_inet_aton (const char * str, struct in_addr * add);
 
@@ -110,14 +97,6 @@ struct sockaddr_storage {
     char ss_pad2[112];
 };
 #endif /* !HAVE_STRUCT_SOCKADDR_STORAGE */
-
-typedef union sockaddr_union {
-    struct sockaddr_storage storage;
-    struct sockaddr_in in;
-#if HAVE_STRUCT_SOCKADDR_IN6
-    struct sockaddr_in6 in6;
-#endif
-} sockaddr_union;
 
 #ifndef MSG_NOSIGNAL
 #define MSG_NOSIGNAL 0
@@ -253,26 +232,6 @@ int ff_is_multicast_address(struct sockaddr *addr);
 int ff_listen_bind(int fd, const struct sockaddr *addr,
                    socklen_t addrlen, int timeout,
                    URLContext *h);
-
-/**
- * Bind to a file descriptor to an address without accepting connections.
- * @param fd      First argument of bind().
- * @param addr    Second argument of bind().
- * @param addrlen Third argument of bind().
- * @return        0 on success or an AVERROR on failure.
- */
-int ff_listen(int fd, const struct sockaddr *addr, socklen_t addrlen);
-
-/**
- * Poll for a single connection on the passed file descriptor.
- * @param fd      The listening socket file descriptor.
- * @param timeout Polling timeout in milliseconds.
- * @param h       URLContext providing interrupt check
- *                callback and logging context.
- * @return        A non-blocking file descriptor on success
- *                or an AVERROR on failure.
- */
-int ff_accept(int fd, int timeout, URLContext *h);
 
 /**
  * Connect to a file descriptor and poll for result.
