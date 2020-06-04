372ce7582e891c74e8d35a7db281eb07c9734ac5
liujinye <liujinye@xiaomi.com>
2020-06-04 12:01:51 +0800

    pthread_frame: change the way delay is set
    It is a constant known at codec init, so set it in
    ff_frame_thread_init(). Also, only set it for video, since the meaning
    of this field is not well-defined for audio with frame threading.
    
    Fixes availability of delay in callbacks invoked from the per-thread
    contexts after 1f4cf92cfbd3accbae582ac63126ed5570ddfd37.
    
    (cherry picked from commit 6943ab688d0c75dbab3222b5b80457ab72a0615f)

--- libavcodec/smvjpegdec.c
+++ libavcodec/smvjpegdec.c
@@ -84,7 +84,8 @@ static av_cold int smvjpeg_decode_end(AVCodecContext *avctx)
     jpg->picture_ptr = NULL;
     av_frame_free(&s->picture[0]);
     av_frame_free(&s->picture[1]);
-    avcodec_free_context(&s->avctx);
+    ret = avcodec_close(s->avctx);
+    av_freep(&s->avctx);
     return ret;
 }
 
