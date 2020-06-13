c48922ea73159d51ffe716cf6e465633436d08f1
liujinye <liujinye@xiaomi.com>
2020-06-13 22:34:19 +0800

    avformat/matroskadec: Move AVBufferRef instead of copying, fix memleak
    EBML binary elements are already made reference-counted when read;
    so when populating the AVStream.attached_pic, one does not need to
    allocate a new buffer for the data; instead the current code just
    creates a new reference to the underlying AVBuffer. But this can be
    improved even further: Just move the already existing reference.
    
    This also fixes a memleak that happens upon error because
    matroska_read_close has not been called in this scenario.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
    (cherry picked from commit cbe336c9e81e2d9de3a18abef887c9255a9b9da5)

--- libavformat/microdvddec.c
+++ libavformat/microdvddec.c
@@ -81,7 +81,7 @@ static int microdvd_read_header(AVFormatContext *s)
     AVRational pts_info = (AVRational){ 2997, 125 };  /* default: 23.976 fps */
     MicroDVDContext *microdvd = s->priv_data;
     AVStream *st = avformat_new_stream(s, NULL);
-    int i = 0, ret;
+    int i = 0;
     char line_buf[MAX_LINESIZE];
     int has_real_fps = 0;
 
@@ -117,10 +117,10 @@ static int microdvd_read_header(AVFormatContext *s)
                 continue;
             }
             if (!st->codecpar->extradata && sscanf(line, "{DEFAULT}{}%c", &c) == 1) {
-                int size = strlen(line + 11);
+                int ret, size = strlen(line + 11);
                 ret = ff_alloc_extradata(st->codecpar, size);
                 if (ret < 0)
-                    goto fail;
+                    return ret;
                 memcpy(st->codecpar->extradata, line + 11, size);
                 continue;
             }
@@ -138,10 +138,8 @@ static int microdvd_read_header(AVFormatContext *s)
         if (!*p)
             continue;
         sub = ff_subtitles_queue_insert(&microdvd->q, p, strlen(p), 0);
-        if (!sub) {
-            ret = AVERROR(ENOMEM);
-            goto fail;
-        }
+        if (!sub)
+            return AVERROR(ENOMEM);
         sub->pos = pos;
         sub->pts = get_pts(line);
         sub->duration = get_duration(line);
@@ -158,9 +156,6 @@ static int microdvd_read_header(AVFormatContext *s)
     st->codecpar->codec_type = AVMEDIA_TYPE_SUBTITLE;
     st->codecpar->codec_id   = AV_CODEC_ID_MICRODVD;
     return 0;
-fail:
-    ff_subtitles_queue_clean(&microdvd->q);
-    return ret;
 }
 
 static int microdvd_read_packet(AVFormatContext *s, AVPacket *pkt)
