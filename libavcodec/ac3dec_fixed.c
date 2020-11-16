55b022ec0336167c2911cfed6d5c8b89cebb20fa
openvela-robot <openvela-robot@xiaomi.com>
2020-11-16 09:38:39 +0800

    avutil/{avstring,bprint}: add XML escaping from ffprobe to avutil
    Base escaping only escapes values required for base character data
    according to part 2.4 of XML, and if additional flags are added
    single and double quotes can additionally be escaped in order
    to handle single and double quoted attributes.
    
    Co-authored-by: Jan Ekström <jan.ekstrom@24i.com>
    Signed-off-by: Jan Ekström <jan.ekstrom@24i.com>

--- libavcodec/ac3dec_fixed.c
+++ libavcodec/ac3dec_fixed.c
@@ -151,10 +151,7 @@ static void ac3_downmix_c_fixed16(int16_t **samples, int16_t **matrix,
     }
 }
 
-#ifdef CONFIG_EAC3_DECODER
 #include "eac3dec.c"
-#endif
-
 #include "ac3dec.c"
 
 static const AVOption options[] = {
@@ -179,9 +176,11 @@ AVCodec ff_ac3_fixed_decoder = {
     .init           = ac3_decode_init,
     .close          = ac3_decode_end,
     .decode         = ac3_decode_frame,
-    .capabilities   = AV_CODEC_CAP_DR1,
+    .capabilities   = AV_CODEC_CAP_CHANNEL_CONF |
+                      AV_CODEC_CAP_DR1,
     .long_name      = NULL_IF_CONFIG_SMALL("ATSC A/52A (AC-3)"),
     .sample_fmts    = (const enum AVSampleFormat[]) { AV_SAMPLE_FMT_S16P,
                                                       AV_SAMPLE_FMT_NONE },
     .priv_class     = &ac3_decoder_class,
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
