c48e70afbfe62bfb376709753b2df5781fb59e76
liujinye <liujinye@xiaomi.com>
2020-11-17 23:20:03 +0800

    avcodec/mpegaudio_tablegen: Avoid write-only buffers
    The mpegaudio_tablegen header contains code to initialize several
    tables; it is included in both the fixed as well as the floating point
    mpegaudio decoders and some of these tables are only used by the fixed
    resp. floating point decoders; yet both types are always initialized,
    leaving the compiler to figure out that one of them is unused.
    
    GCC 9.3 fails at this (even with -O3):
    $ readelf -s mpegaudiodec_fixed.o|grep _float
        28: 0000000000001660 32768 OBJECT  LOCAL  DEFAULT    4 expval_table_float
    An actually unused table (expval_table_fixed/float) of size 32KiB is kept
    and initialized (the reason for this is probably that this table is read
    from, namely to initialize another table: exp_table_fixed/float; of course
    the float resp. fixed tables are not used in the fixed resp. floating point
    decoder).
    
    Therefore #ifdef the unneeded tables away.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

--- libavformat/hashenc.c
+++ libavformat/hashenc.c
@@ -156,7 +156,6 @@ static int hash_write_trailer(struct AVFormatContext *s)
 
     return 0;
 }
-#endif
 
 static void hash_free(struct AVFormatContext *s)
 {
@@ -169,6 +168,7 @@ static void hash_free(struct AVFormatContext *s)
     }
     av_freep(&c->hashes);
 }
+#endif
 
 #if CONFIG_HASH_MUXER
 static const AVClass hashenc_class = {
@@ -326,6 +326,14 @@ static int framehash_write_packet(struct AVFormatContext *s, AVPacket *pkt)
     avio_printf(s->pb, "\n");
     return 0;
 }
+
+static void framehash_free(struct AVFormatContext *s)
+{
+    struct HashContext *c = s->priv_data;
+    if (c->hashes)
+        av_hash_freep(&c->hashes[0]);
+    av_freep(&c->hashes);
+}
 #endif
 
 #if CONFIG_FRAMEHASH_MUXER
@@ -345,7 +353,7 @@ AVOutputFormat ff_framehash_muxer = {
     .init              = framehash_init,
     .write_header      = framehash_write_header,
     .write_packet      = framehash_write_packet,
-    .deinit            = hash_free,
+    .deinit            = framehash_free,
     .flags             = AVFMT_VARIABLE_FPS | AVFMT_TS_NONSTRICT |
                          AVFMT_TS_NEGATIVE,
     .priv_class        = &framehash_class,
@@ -369,7 +377,7 @@ AVOutputFormat ff_framemd5_muxer = {
     .init              = framehash_init,
     .write_header      = framehash_write_header,
     .write_packet      = framehash_write_packet,
-    .deinit            = hash_free,
+    .deinit            = framehash_free,
     .flags             = AVFMT_VARIABLE_FPS | AVFMT_TS_NONSTRICT |
                          AVFMT_TS_NEGATIVE,
     .priv_class        = &framemd5_class,
--- libavformat/rtsp.h
+++ libavformat/rtsp.h
@@ -27,7 +27,6 @@
 #include "rtpdec.h"
 #include "network.h"
 #include "httpauth.h"
-#include "internal.h"
 
 #include "libavutil/log.h"
 #include "libavutil/opt.h"
@@ -79,7 +78,6 @@ enum RTSPControlTransport {
 #define RTSP_DEFAULT_AUDIO_SAMPLERATE 44100
 #define RTSP_RTP_PORT_MIN 5000
 #define RTSP_RTP_PORT_MAX 65000
-#define SDP_MAX_SIZE 16384
 
 /**
  * This describes a single item in the "Transport:" line of one stream as
@@ -317,7 +315,7 @@ typedef struct RTSPState {
     /** some MS RTSP streams contain a URL in the SDP that we need to use
      * for all subsequent RTSP requests, rather than the input URI; in
      * other cases, this is a copy of AVFormatContext->filename. */
-    char control_uri[MAX_URL_SIZE];
+    char control_uri[1024];
 
     /** The following are used for parsing raw mpegts in udp */
     //@{
@@ -445,7 +443,7 @@ typedef struct RTSPStream {
      * for the selected transport. Only used for TCP. */
     int interleaved_min, interleaved_max;
 
-    char control_url[MAX_URL_SIZE];   /**< url for this stream (from SDP) */
+    char control_url[1024];   /**< url for this stream (from SDP) */
 
     /** The following are used only in SDP, not RTSP */
     //@{
