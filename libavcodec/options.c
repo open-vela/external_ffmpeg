55b2a0d54cd49ea05b68975a0ebf3319630f15b4
openvela-robot <openvela-robot@xiaomi.com>
2021-01-05 15:07:40 +0800

    ffmpeg: fix compile error with -o0 when open ac3 (no eac3)
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- libavcodec/options.c
+++ libavcodec/options.c
@@ -53,7 +53,6 @@ static void *codec_child_next(void *obj, void *prev)
     return NULL;
 }
 
-#if FF_API_CHILD_CLASS_NEXT
 static const AVClass *codec_child_class_next(const AVClass *prev)
 {
     void *iter = NULL;
@@ -66,17 +65,6 @@ static const AVClass *codec_child_class_next(const AVClass *prev)
 
     /* find next codec with priv options */
     while (c = av_codec_iterate(&iter))
-        if (c->priv_class)
-            return c->priv_class;
-    return NULL;
-}
-#endif
-
-static const AVClass *codec_child_class_iterate(void **iter)
-{
-    const AVCodec *c;
-    /* find next codec with priv options */
-    while (c = av_codec_iterate(iter))
         if (c->priv_class)
             return c->priv_class;
     return NULL;
@@ -96,10 +84,7 @@ static const AVClass av_codec_context_class = {
     .version                 = LIBAVUTIL_VERSION_INT,
     .log_level_offset_offset = offsetof(AVCodecContext, log_level_offset),
     .child_next              = codec_child_next,
-#if FF_API_CHILD_CLASS_NEXT
     .child_class_next        = codec_child_class_next,
-#endif
-    .child_class_iterate     = codec_child_class_iterate,
     .category                = AV_CLASS_CATEGORY_ENCODER,
     .get_category            = get_category,
 };
@@ -327,7 +312,6 @@ static const AVOption frame_options[]={
 {NULL},
 };
 
-#if FF_API_COPY_CONTEXT
 static const AVClass av_frame_class = {
     .class_name              = "AVFrame",
     .item_name               = NULL,
@@ -339,7 +323,6 @@ const AVClass *avcodec_get_frame_class(void)
 {
     return &av_frame_class;
 }
-#endif
 
 #define SROFFSET(x) offsetof(AVSubtitleRect,x)
 
@@ -354,7 +337,6 @@ static const AVOption subtitle_rect_options[]={
 {NULL},
 };
 
-#if FF_API_COPY_CONTEXT
 static const AVClass av_subtitle_rect_class = {
     .class_name             = "AVSubtitleRect",
     .item_name              = NULL,
@@ -366,4 +348,3 @@ const AVClass *avcodec_get_subtitle_rect_class(void)
 {
     return &av_subtitle_rect_class;
 }
-#endif
