1550ea0c1a685f912989d58034e88094c5643099
openvela-robot <openvela-robot@xiaomi.com>
2021-01-27 19:06:36 +0800

    get_bits: move check_marker() to mpegvideodec.h
    It is only used by mpegvideo-based decoders - specifically mpeg12, intelh263,
    ituh263, mpeg4video.

--- libavutil/file_open.c
+++ libavutil/file_open.c
@@ -45,7 +45,7 @@ static int win32_open(const char *filename_utf8, int oflag, int pmode)
     wchar_t *filename_w;
 
     /* convert UTF-8 to wide chars */
-    if (utf8towchar(filename_utf8, &filename_w))
+    if (get_extended_win32_path(filename_utf8, &filename_w))
         return -1;
     if (!filename_w)
         goto fallback;
@@ -112,14 +112,14 @@ int avpriv_tempfile(const char *prefix, char **filename, int log_offset, void *l
     FileLogContext file_log_ctx = { &file_log_ctx_class, log_offset, log_ctx };
     int fd = -1;
 #if !HAVE_MKSTEMP
-    void *ptr= tempnam(FFMPEG_TMPDIR, prefix);
+    void *ptr= tempnam(NULL, prefix);
     if(!ptr)
         ptr= tempnam(".", prefix);
     *filename = av_strdup(ptr);
 #undef free
     free(ptr);
 #else
-    size_t len = strlen(prefix) + strlen(FFMPEG_TMPDIR) + 8; /* room for FFMPEG_TMPDIR and "XXXXXX\0" */
+    size_t len = strlen(prefix) + 12; /* room for "/tmp/" and "XXXXXX\0" */
     *filename  = av_malloc(len);
 #endif
     /* -----common section-----*/
@@ -136,7 +136,7 @@ int avpriv_tempfile(const char *prefix, char **filename, int log_offset, void *l
 #   endif
     fd = open(*filename, O_RDWR | O_BINARY | O_CREAT | O_EXCL, 0600);
 #else
-    snprintf(*filename, len, "%s/%sXXXXXX", FFMPEG_TMPDIR, prefix);
+    snprintf(*filename, len, "/tmp/%sXXXXXX", prefix);
     fd = mkstemp(*filename);
 #if defined(_WIN32) || defined (__ANDROID__) || defined(__DJGPP__)
     if (fd < 0) {
@@ -155,7 +155,7 @@ int avpriv_tempfile(const char *prefix, char **filename, int log_offset, void *l
     return fd; /* success */
 }
 
-FILE *av_fopen_utf8(const char *path, const char *mode)
+FILE *avpriv_fopen_utf8(const char *path, const char *mode)
 {
     int fd;
     int access;
@@ -188,3 +188,10 @@ FILE *av_fopen_utf8(const char *path, const char *mode)
         return NULL;
     return fdopen(fd, mode);
 }
+
+#if FF_API_AV_FOPEN_UTF8
+FILE *av_fopen_utf8(const char *path, const char *mode)
+{
+    return avpriv_fopen_utf8(path, mode);
+}
+#endif
