8f445dbfb86acf34c693c544b4e6748c9ea24992
danguanghua <danguanghua@xiaomi.com>
2021-03-01 09:25:41 +0800

    ffmpeg: support SBC packed stream

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -502,6 +502,7 @@ enum AVCodecID {
     AV_CODEC_ID_MPEGH_3D_AUDIO,
     AV_CODEC_ID_SIREN,
     AV_CODEC_ID_HCA,
+    AV_CODEC_ID_SBC_PACKED,
 
     /* subtitle codecs */
     AV_CODEC_ID_FIRST_SUBTITLE = 0x17000,          ///< A dummy ID pointing at the start of subtitle codecs.
