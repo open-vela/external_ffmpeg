3c9982f1b6510291d56364efc1c6799a6d0dfb3a
ligd <liguiding1@xiaomi.com>
2021-04-16 18:25:52 +0800

    ffmpeg: add uorb remote device support
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -51,6 +51,7 @@ extern AVOutputFormat ff_pulse_muxer;
 extern AVOutputFormat ff_sdl2_muxer;
 extern AVInputFormat  ff_sndio_demuxer;
 extern AVOutputFormat ff_sndio_muxer;
+extern AVOutputFormat ff_uorb_muxer;
 extern AVInputFormat  ff_v4l2_demuxer;
 extern AVOutputFormat ff_v4l2_muxer;
 extern AVInputFormat  ff_vfwcap_demuxer;
