6db4bab90412afacd0ec8c450891e8a97015f5bc
openvela-robot <openvela-robot@xiaomi.com>
2021-10-12 23:01:17 +0800

    avfiltergraph: update runtime renegoation support
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- libavdevice/alsa_enc.c
+++ libavdevice/alsa_enc.c
@@ -37,6 +37,8 @@
  * which gives a low latency suitable for real-time playback.
  */
 
+#define _POSIX_C_SOURCE 2
+
 #include <alsa/asoundlib.h>
 
 #include "libavutil/internal.h"
@@ -157,7 +159,7 @@ static const AVClass alsa_muxer_class = {
     .category       = AV_CLASS_CATEGORY_DEVICE_AUDIO_OUTPUT,
 };
 
-const AVOutputFormat ff_alsa_muxer = {
+AVOutputFormat ff_alsa_muxer = {
     .name           = "alsa",
     .long_name      = NULL_IF_CONFIG_SMALL("ALSA audio output"),
     .priv_data_size = sizeof(AlsaData),
