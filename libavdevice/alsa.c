b9cc78ad40c578b5c48e1e54bdc9de04f11e76e7
liujinye <liujinye@xiaomi.com>
2021-10-12 22:48:23 +0800

    Revert "filtergraph: fix negotiation error when processing EOF"
    This reverts commit 753bab9236de303028afef7fbeda9aca9e541597.

--- libavdevice/alsa.c
+++ libavdevice/alsa.c
@@ -28,6 +28,8 @@
  * @author Nicolas George ( nicolas george normalesup org )
  */
 
+#define _POSIX_C_SOURCE 2
+
 #include <alsa/asoundlib.h>
 #include "avdevice.h"
 #include "libavutil/avassert.h"
@@ -286,10 +288,6 @@ av_cold int ff_alsa_open(AVFormatContext *ctx, snd_pcm_stream_t mode,
         }
     }
 
-    s->pkt = av_packet_alloc();
-    if (!s->pkt)
-        goto fail1;
-
     s->h = h;
     return 0;
 
@@ -312,7 +310,6 @@ av_cold int ff_alsa_close(AVFormatContext *s1)
     if (CONFIG_ALSA_INDEV)
         ff_timefilter_destroy(s->timefilter);
     snd_pcm_close(s->h);
-    av_packet_free(&s->pkt);
     return 0;
 }
 
