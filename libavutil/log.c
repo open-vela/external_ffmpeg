a423e6fa3e72ba147a7eb3418f41a8d3d9f804f2
Paul B Mahol <onemda@gmail.com>
2021-10-26 12:34:30 +0200

    avfilter: split negate filter from lut filter
    Using luts for negating is suboptimal.
    
    FATE test changes because filter no longer clips values into limited color range.
    
    Change-Id: I43e6b09cbb5589ccdc9f587e406dd9df0ae383a2

--- libavutil/log.c
+++ libavutil/log.c
@@ -359,6 +359,8 @@ void av_log_default_callback(void* ptr, int level, const char* fmt, va_list vl)
         level &= 0xff;
     }
 
+    if (level > av_log_level)
+        return;
     ff_mutex_lock(&mutex);
 
     format_line(ptr, level, fmt, vl, part, &print_prefix, type);
@@ -426,8 +428,6 @@ void av_vlog(void* avcl, int level, const char *fmt, va_list vl)
     if (avc && avc->version >= (50 << 16 | 15 << 8 | 2) &&
         avc->log_level_offset_offset && level >= AV_LOG_FATAL)
         level += *(int *) (((uint8_t *) avcl) + avc->log_level_offset_offset);
-    if (level > av_log_level)
-        return;
     if (log_callback)
         log_callback(avcl, level, fmt, vl);
 }
