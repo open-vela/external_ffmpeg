8c678ec374242ca653018c1ef20491edf5e4ae33
Alan Kelly <alankelly-at-google.com@ffmpeg.org>
2021-12-21 20:56:41 +0100

    libavutil/cpu: Add AV_CPU_FLAG_SLOW_GATHER.
    This flag is set on Haswell and earlier and all AMD cpus.
    
    Change-Id: I3f79dfeb1f4b9e89116e8893f80c1165e9437cdb

--- libavcodec/bsf_internal.h
+++ libavcodec/bsf_internal.h
@@ -42,6 +42,6 @@ int ff_bsf_get_packet(AVBSFContext *ctx, AVPacket **pkt);
  */
 int ff_bsf_get_packet_ref(AVBSFContext *ctx, AVPacket *pkt);
 
-const AVClass *ff_bsf_child_class_next(const AVClass *prev);
+const AVClass *ff_bsf_child_class_iterate(void **opaque);
 
 #endif /* AVCODEC_BSF_INTERNAL_H */
