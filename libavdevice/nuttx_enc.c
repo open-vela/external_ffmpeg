d7e4a3010f255fe03d93a7f2ff14682cb92e64bf
fangzhenwei <fangzhenwei@xiaomi.com>
2021-12-28 11:18:42 +0800

    ffmepg/nuttx:nuttx device add start/pause cmd for btsco
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- libavdevice/nuttx_enc.c
+++ libavdevice/nuttx_enc.c
@@ -171,6 +171,11 @@ static int nuttx_control_message(struct AVFormatContext *s1, int type,
 
             return ret;
         }
+        case AV_APP_TO_DEV_PLAY: {
+            avdevice_dev_to_app_control_message(s1, AV_DEV_TO_APP_BUFFER_WRITABLE, NULL, 0);
+
+            return 0;
+        }
         case AV_APP_TO_DEV_DUMP:
             snprintf(data, data_size, "%d|%d|%d|%d|%d",
                      priv->running, priv->flushing,
