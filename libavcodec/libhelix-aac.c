6fff796f8c04737710aed1df9eb65f15b376ebde
qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
2022-01-18 09:50:10 +0800

    FFMPEG: add raw aac support in libhelix_aac dec
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- libavcodec/libhelix-aac.c
+++ libavcodec/libhelix-aac.c
@@ -29,6 +29,9 @@
 
 #include <aacdec.h>
 
+#define SYNCWORDH 0xff
+#define SYNCWORDL 0xf0
+
 #define LIBHELIX_AAC_MAX_CHANNELS 2
 #define LIBHELIX_AAC_MAX_NSAMPS   2048
 
@@ -48,9 +51,6 @@ static int aac_decode_init(AVCodecContext *avctx)
     if (!aac->context)
         return AVERROR(ENOMEM);
 
-    ret = AACSetFormat(aac->context, AAC_FF_ADTS);
-    if (ret < 0)
-        return ret;
 
     avctx->sample_fmt = AV_SAMPLE_FMT_S16;
     aac->pcm_size = LIBHELIX_AAC_MAX_NSAMPS * LIBHELIX_AAC_MAX_CHANNELS *
@@ -79,15 +79,35 @@ static int aac_decode_frame(AVCodecContext *avctx,
     in_data = avpkt->data;
     in_size = avpkt->size;
 
+    if ((in_data[0] & SYNCWORDH) == SYNCWORDH && (in_data[1] & SYNCWORDL) == SYNCWORDL) {
+        ret = AACSetFormat(aac->context, AAC_FF_ADTS);
+        if (ret < 0)
+            return 0;
+    } else {
+        info.sampRateCore = avctx->sample_rate;
+        info.nChans  = avctx->channels;
+        info.profile = AAC_PROFILE_LC;
+        ret = AACSetRawBlockParams(aac->context, 0, &info);
+        if (ret < 0)
+            return ret;
+    }
+
     ret = AACDecode(aac->context, &in_data, &in_size, (int16_t *)aac->pcm);
     if (ret < 0)
         return ret;
 
     AACGetLastFrameInfo(aac->context, &info);
-    avctx->sample_rate = info.sampRateOut;
-    avctx->channels    = info.nChans;
-    avctx->frame_size  = info.outputSamps / info.nChans;
-    avctx->channel_layout = av_get_default_channel_layout(avctx->channels);
+
+    if (!avctx->sample_rate)
+         avctx->sample_rate = info.sampRateOut;
+
+    if (!avctx->channels)
+         avctx->channels = info.nChans;
+
+    if (!avctx->channel_layout)
+         avctx->channel_layout = av_get_default_channel_layout(avctx->channels);
+
+    avctx->frame_size = info.outputSamps / info.nChans;
 
     frame->nb_samples = avctx->frame_size;
     if ((ret = ff_get_buffer(avctx, frame, 0)) < 0)
