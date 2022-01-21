3cbb71dc19454d88458e5f3037bce456c6fa22b3
openvela-robot <openvela-robot@xiaomi.com>
2022-01-21 12:36:11 +0800

    FFMPEG: send silent frame after EVENT_COMPLETE.
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- libavcodec/mpegpicture.h
+++ libavcodec/mpegpicture.h
@@ -29,7 +29,6 @@
 #include "motion_est.h"
 #include "thread.h"
 
-#define MPEGVIDEO_MAX_PLANES 4
 #define MAX_PICTURE_COUNT 36
 #define EDGE_WIDTH 16
 
@@ -70,7 +69,6 @@ typedef struct Picture {
 
     int alloc_mb_width;         ///< mb_width used to allocate tables
     int alloc_mb_height;        ///< mb_height used to allocate tables
-    int alloc_mb_stride;        ///< mb_stride used to allocate tables
 
     AVBufferRef *mb_mean_buf;
     uint8_t *mb_mean;           ///< Table for MB luminance
@@ -89,7 +87,7 @@ typedef struct Picture {
     int reference;
     int shared;
 
-    uint64_t encoding_error[MPEGVIDEO_MAX_PLANES];
+    uint64_t encoding_error[AV_NUM_DATA_POINTERS];
 } Picture;
 
 /**
