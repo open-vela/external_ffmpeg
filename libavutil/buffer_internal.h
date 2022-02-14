c6c208a906c8e8e8e59881189a4072e44f91c313
openvela-robot <openvela-robot@xiaomi.com>
2022-02-14 20:00:21 +0800

    Revert "FFMPEG: use av_opt_set api when set options."
    This reverts commit 3c587680fb30d29433e6fdcd086693a299405a31.

--- libavutil/buffer_internal.h
+++ libavutil/buffer_internal.h
@@ -22,7 +22,6 @@
 #include <stdatomic.h>
 #include <stdint.h>
 
-#include "internal.h"
 #include "buffer.h"
 #include "thread.h"
 
@@ -30,15 +29,10 @@
  * The buffer was av_realloc()ed, so it is reallocatable.
  */
 #define BUFFER_FLAG_REALLOCATABLE (1 << 0)
-/**
- * The AVBuffer structure is part of a larger structure
- * and should not be freed.
- */
-#define BUFFER_FLAG_NO_FREE       (1 << 1)
 
 struct AVBuffer {
     uint8_t *data; /**< data described by this buffer */
-    size_t size; /**< size of data in bytes */
+    int      size; /**< size of data in bytes */
 
     /**
      *  number of existing AVBufferRef instances referring to this buffer
@@ -78,12 +72,6 @@ typedef struct BufferPoolEntry {
 
     AVBufferPool *pool;
     struct BufferPoolEntry *next;
-
-    /*
-     * An AVBuffer structure to (re)use as AVBuffer for subsequent uses
-     * of this BufferPoolEntry.
-     */
-    AVBuffer buffer;
 } BufferPoolEntry;
 
 struct AVBufferPool {
@@ -101,10 +89,10 @@ struct AVBufferPool {
      */
     atomic_uint refcount;
 
-    size_t size;
+    int size;
     void *opaque;
-    AVBufferRef* (*alloc)(size_t size);
-    AVBufferRef* (*alloc2)(void *opaque, size_t size);
+    AVBufferRef* (*alloc)(int size);
+    AVBufferRef* (*alloc2)(void *opaque, int size);
     void         (*pool_free)(void *opaque);
 };
 
