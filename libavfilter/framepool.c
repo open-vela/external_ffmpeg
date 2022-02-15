743a39aa5bfda9bc83ecf0bde0bbeaccbf94ed90
openvela-robot <openvela-robot@xiaomi.com>
2022-02-15 17:28:03 +0800

    FFMPEG: flush codec when error occured.
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- libavfilter/framepool.c
+++ libavfilter/framepool.c
@@ -48,7 +48,7 @@ struct FFFramePool {
 
 };
 
-FFFramePool *ff_frame_pool_video_init(AVBufferRef* (*alloc)(size_t size),
+FFFramePool *ff_frame_pool_video_init(AVBufferRef* (*alloc)(int size),
                                       int width,
                                       int height,
                                       enum AVPixelFormat format,
@@ -76,31 +76,34 @@ FFFramePool *ff_frame_pool_video_init(AVBufferRef* (*alloc)(size_t size),
     }
 
     if (!pool->linesize[0]) {
-        ret = av_image_fill_linesizes(pool->linesize, pool->format,
-                                      FFALIGN(pool->width, align));
-        if (ret < 0) {
-            goto fail;
+        for(i = 1; i <= align; i += i) {
+            ret = av_image_fill_linesizes(pool->linesize, pool->format,
+                                          FFALIGN(pool->width, i));
+            if (ret < 0) {
+                goto fail;
+            }
+            if (!(pool->linesize[0] & (pool->align - 1)))
+                break;
         }
 
         for (i = 0; i < 4 && pool->linesize[i]; i++) {
             pool->linesize[i] = FFALIGN(pool->linesize[i], pool->align);
-            if ((pool->linesize[i] & (pool->align - 1)))
-                goto fail;
         }
     }
 
     for (i = 0; i < 4 && pool->linesize[i]; i++) {
-        int h = pool->height;
+        int h = FFALIGN(pool->height, 32);
         if (i == 1 || i == 2)
             h = AV_CEIL_RSHIFT(h, desc->log2_chroma_h);
 
-        pool->pools[i] = av_buffer_pool_init(pool->linesize[i] * h + align,
+        pool->pools[i] = av_buffer_pool_init(pool->linesize[i] * h + 16 + 16 - 1,
                                              alloc);
         if (!pool->pools[i])
             goto fail;
     }
 
-    if (desc->flags & AV_PIX_FMT_FLAG_PAL) {
+    if (desc->flags & AV_PIX_FMT_FLAG_PAL ||
+        desc->flags & FF_PSEUDOPAL) {
         pool->pools[1] = av_buffer_pool_init(AVPALETTE_SIZE, alloc);
         if (!pool->pools[1])
             goto fail;
@@ -113,7 +116,7 @@ fail:
     return NULL;
 }
 
-FFFramePool *ff_frame_pool_audio_init(AVBufferRef* (*alloc)(size_t size),
+FFFramePool *ff_frame_pool_audio_init(AVBufferRef* (*alloc)(int size),
                                       int channels,
                                       int nb_samples,
                                       enum AVSampleFormat format,
@@ -223,7 +226,8 @@ AVFrame *ff_frame_pool_get(FFFramePool *pool)
             frame->data[i] = frame->buf[i]->data;
         }
 
-        if (desc->flags & AV_PIX_FMT_FLAG_PAL) {
+        if (desc->flags & AV_PIX_FMT_FLAG_PAL ||
+            desc->flags & FF_PSEUDOPAL) {
             enum AVPixelFormat format =
                 pool->format == AV_PIX_FMT_PAL8 ? AV_PIX_FMT_BGR8 : pool->format;
 
@@ -241,11 +245,11 @@ AVFrame *ff_frame_pool_get(FFFramePool *pool)
         frame->linesize[0] = pool->linesize[0];
 
         if (pool->planes > AV_NUM_DATA_POINTERS) {
-            frame->extended_data = av_calloc(pool->planes,
-                                             sizeof(*frame->extended_data));
+            frame->extended_data = av_mallocz_array(pool->planes,
+                                                    sizeof(*frame->extended_data));
             frame->nb_extended_buf = pool->planes - AV_NUM_DATA_POINTERS;
-            frame->extended_buf  = av_calloc(frame->nb_extended_buf,
-                                             sizeof(*frame->extended_buf));
+            frame->extended_buf = av_mallocz_array(frame->nb_extended_buf,
+                                                   sizeof(*frame->extended_buf));
             if (!frame->extended_data || !frame->extended_buf)
                 goto fail;
         } else {
