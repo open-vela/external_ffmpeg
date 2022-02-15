02631d92398a34b9571b91ff15667e53d4f760a3
qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
2022-02-15 17:28:03 +0800

    FFMPEG: flush codec when error occured.

--- libavcodec/flvdec.c
+++ libavcodec/flvdec.c
@@ -20,9 +20,8 @@
 
 #include "libavutil/imgutils.h"
 
-#include "flvdec.h"
-#include "h263dec.h"
-#include "internal.h"
+#include "flv.h"
+#include "h263.h"
 #include "mpegvideo.h"
 #include "mpegvideodata.h"
 
@@ -91,6 +90,7 @@ int ff_flv_decode_picture_header(MpegEncContext *s)
 
     s->h263_plus = 0;
 
+    s->unrestricted_mv   = 1;
     s->h263_long_vectors = 0;
 
     /* PEI */
@@ -113,7 +113,7 @@ int ff_flv_decode_picture_header(MpegEncContext *s)
     return 0;
 }
 
-const AVCodec ff_flv_decoder = {
+AVCodec ff_flv_decoder = {
     .name           = "flv",
     .long_name      = NULL_IF_CONFIG_SMALL("FLV / Sorenson Spark / Sorenson H.263 (Flash Video)"),
     .type           = AVMEDIA_TYPE_VIDEO,
@@ -123,8 +123,7 @@ const AVCodec ff_flv_decoder = {
     .close          = ff_h263_decode_end,
     .decode         = ff_h263_decode_frame,
     .capabilities   = AV_CODEC_CAP_DRAW_HORIZ_BAND | AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE |
-                      FF_CODEC_CAP_SKIP_FRAME_FILL_PARAM,
+    .caps_internal  = FF_CODEC_CAP_SKIP_FRAME_FILL_PARAM,
     .max_lowres     = 3,
     .pix_fmts       = (const enum AVPixelFormat[]) { AV_PIX_FMT_YUV420P,
                                                      AV_PIX_FMT_NONE },
--- libavcodec/mss3.c
+++ libavcodec/mss3.c
@@ -844,13 +844,20 @@ static av_cold int mss3_decode_init(AVCodecContext *avctx)
                                             b_width * b_height);
         if (!c->dct_coder[i].prev_dc) {
             av_log(avctx, AV_LOG_ERROR, "Cannot allocate buffer\n");
+            av_frame_free(&c->pic);
+            while (i >= 0) {
+                av_freep(&c->dct_coder[i].prev_dc);
+                i--;
+            }
             return AVERROR(ENOMEM);
         }
     }
 
     c->pic = av_frame_alloc();
-    if (!c->pic)
+    if (!c->pic) {
+        mss3_decode_end(avctx);
         return AVERROR(ENOMEM);
+    }
 
     avctx->pix_fmt     = AV_PIX_FMT_YUV420P;
 
@@ -859,7 +866,7 @@ static av_cold int mss3_decode_init(AVCodecContext *avctx)
     return 0;
 }
 
-const AVCodec ff_msa1_decoder = {
+AVCodec ff_msa1_decoder = {
     .name           = "msa1",
     .long_name      = NULL_IF_CONFIG_SMALL("MS ATC Screen"),
     .type           = AVMEDIA_TYPE_VIDEO,
@@ -869,5 +876,4 @@ const AVCodec ff_msa1_decoder = {
     .close          = mss3_decode_end,
     .decode         = mss3_decode_frame,
     .capabilities   = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
 };
