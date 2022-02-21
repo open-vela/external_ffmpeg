5840f11b0b794def5a85d618c434b31916194456
ligd <liguiding1@xiaomi.com>
2022-02-21 23:46:39 +0800

    ffmpeg: make pthread mutex enable to fix crash issue

--- libavcodec/fic.c
+++ libavcodec/fic.c
@@ -22,7 +22,6 @@
  */
 
 #include "libavutil/common.h"
-#include "libavutil/mem_internal.h"
 #include "libavutil/opt.h"
 #include "avcodec.h"
 #include "internal.h"
@@ -483,7 +482,7 @@ static const AVClass fic_decoder_class = {
     .version    = LIBAVUTIL_VERSION_INT,
 };
 
-const AVCodec ff_fic_decoder = {
+AVCodec ff_fic_decoder = {
     .name           = "fic",
     .long_name      = NULL_IF_CONFIG_SMALL("Mirillis FIC"),
     .type           = AVMEDIA_TYPE_VIDEO,
@@ -494,5 +493,4 @@ const AVCodec ff_fic_decoder = {
     .close          = fic_decode_close,
     .capabilities   = AV_CODEC_CAP_DR1 | AV_CODEC_CAP_SLICE_THREADS,
     .priv_class     = &fic_decoder_class,
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
