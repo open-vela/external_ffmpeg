7cabcfbbe38cfcde6e9e3f07095e1f0a2bd4ffc8
fangzhenwei <fangzhenwei@xiaomi.com>
2022-02-28 20:08:54 +0800

    ffmpeg/bsf:add aac rawtolatm bsf, for support a2dp aac
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- libavcodec/bitstream_filters.c
+++ libavcodec/bitstream_filters.c
@@ -26,6 +26,7 @@
 
 extern const AVBitStreamFilter ff_a2dp_rechunk_bsf;
 extern const AVBitStreamFilter ff_aac_adtstoasc_bsf;
+extern const AVBitStreamFilter ff_aac_rawtolatm_bsf;
 extern const AVBitStreamFilter ff_av1_frame_merge_bsf;
 extern const AVBitStreamFilter ff_av1_frame_split_bsf;
 extern const AVBitStreamFilter ff_av1_metadata_bsf;
