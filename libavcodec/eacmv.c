9c3b9833c8812b9893089b226325c93db99d7bab
liujinye <liujinye@xiaomi.com>
2022-04-01 16:47:52 +0800

    ffmpeg/bluelet: rename write action, dec and enc common name
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- libavcodec/eacmv.c
+++ libavcodec/eacmv.c
@@ -32,7 +32,6 @@
 #include "libavutil/intreadwrite.h"
 #include "libavutil/imgutils.h"
 #include "avcodec.h"
-#include "codec_internal.h"
 #include "internal.h"
 
 typedef struct CmvContext {
@@ -51,8 +50,11 @@ static av_cold int cmv_decode_init(AVCodecContext *avctx){
 
     s->last_frame  = av_frame_alloc();
     s->last2_frame = av_frame_alloc();
-    if (!s->last_frame || !s->last2_frame)
+    if (!s->last_frame || !s->last2_frame) {
+        av_frame_free(&s->last_frame);
+        av_frame_free(&s->last2_frame);
         return AVERROR(ENOMEM);
+    }
 
     return 0;
 }
@@ -231,15 +233,14 @@ static av_cold int cmv_decode_end(AVCodecContext *avctx){
     return 0;
 }
 
-const FFCodec ff_eacmv_decoder = {
-    .p.name         = "eacmv",
-    .p.long_name    = NULL_IF_CONFIG_SMALL("Electronic Arts CMV video"),
-    .p.type         = AVMEDIA_TYPE_VIDEO,
-    .p.id           = AV_CODEC_ID_CMV,
+AVCodec ff_eacmv_decoder = {
+    .name           = "eacmv",
+    .long_name      = NULL_IF_CONFIG_SMALL("Electronic Arts CMV video"),
+    .type           = AVMEDIA_TYPE_VIDEO,
+    .id             = AV_CODEC_ID_CMV,
     .priv_data_size = sizeof(CmvContext),
     .init           = cmv_decode_init,
     .close          = cmv_decode_end,
     .decode         = cmv_decode_frame,
-    .p.capabilities = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE | FF_CODEC_CAP_INIT_CLEANUP,
+    .capabilities   = AV_CODEC_CAP_DR1,
 };
