1d92a4839d98d55923cb09fd4764455a2a7bbe18
liujinye <liujinye@xiaomi.com>
2022-04-20 14:20:26 +0800

    modify xiami aec time of frame:15ms
    update xiaomi aec time of frame
    
    Signed-off-by: genghuihui <genghuihui@xiaomi.com>

--- libavformat/av1.h
+++ libavformat/av1.h
@@ -46,14 +46,12 @@ typedef struct AV1SequenceParameters {
  * the resulting bitstream to the provided AVIOContext.
  *
  * @param pb pointer to the AVIOContext where the filtered bitstream shall be
- *           written; may be NULL, in which case nothing is written.
+ *           written
  * @param buf input data buffer
  * @param size size of the input data buffer
  *
- * @return the amount of bytes written (or would have been written in case
- *         pb had been supplied) in case of success, a negative AVERROR
+ * @return the amount of bytes written in case of success, a negative AVERROR
  *         code in case of failure
- * @note   One can use NULL for pb to just get the output size.
  */
 int ff_av1_filter_obus(AVIOContext *pb, const uint8_t *buf, int size);
 
