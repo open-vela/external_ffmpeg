c4fe34617a2682892f59a504b12fd58d2893d8c4
openvela-robot <openvela-robot@xiaomi.com>
2022-04-24 15:45:04 +0800

    FFMPEG: fix gcc warning
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- libavcodec/opus_pvq.c
+++ libavcodec/opus_pvq.c
@@ -23,8 +23,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "config_components.h"
-
 #include "opustab.h"
 #include "opus_pvq.h"
 
--- libavcodec/qcelpdec.c
+++ libavcodec/qcelpdec.c
@@ -33,7 +33,6 @@
 #include "libavutil/channel_layout.h"
 #include "libavutil/float_dsp.h"
 #include "avcodec.h"
-#include "codec_internal.h"
 #include "internal.h"
 #include "get_bits.h"
 #include "qcelpdata.h"
@@ -88,8 +87,8 @@ static av_cold int qcelp_decode_init(AVCodecContext *avctx)
     QCELPContext *q = avctx->priv_data;
     int i;
 
-    av_channel_layout_uninit(&avctx->ch_layout);
-    avctx->ch_layout      = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
+    avctx->channels       = 1;
+    avctx->channel_layout = AV_CH_LAYOUT_MONO;
     avctx->sample_fmt     = AV_SAMPLE_FMT_FLT;
 
     for (i = 0; i < 10; i++)
@@ -684,12 +683,13 @@ static void postfilter(QCELPContext *q, float *samples, float *lpc)
                              160, 0.9375, &q->postfilter_agc_mem);
 }
 
-static int qcelp_decode_frame(AVCodecContext *avctx, AVFrame *frame,
+static int qcelp_decode_frame(AVCodecContext *avctx, void *data,
                               int *got_frame_ptr, AVPacket *avpkt)
 {
     const uint8_t *buf = avpkt->data;
     int buf_size       = avpkt->size;
     QCELPContext *q    = avctx->priv_data;
+    AVFrame *frame     = data;
     float *outbuffer;
     int   i, ret;
     float quantized_lspf[10], lpc[10];
@@ -790,14 +790,13 @@ erasure:
     return buf_size;
 }
 
-const FFCodec ff_qcelp_decoder = {
-    .p.name         = "qcelp",
-    .p.long_name    = NULL_IF_CONFIG_SMALL("QCELP / PureVoice"),
-    .p.type         = AVMEDIA_TYPE_AUDIO,
-    .p.id           = AV_CODEC_ID_QCELP,
+AVCodec ff_qcelp_decoder = {
+    .name           = "qcelp",
+    .long_name      = NULL_IF_CONFIG_SMALL("QCELP / PureVoice"),
+    .type           = AVMEDIA_TYPE_AUDIO,
+    .id             = AV_CODEC_ID_QCELP,
     .init           = qcelp_decode_init,
-    FF_CODEC_DECODE_CB(qcelp_decode_frame),
-    .p.capabilities = AV_CODEC_CAP_DR1 | AV_CODEC_CAP_CHANNEL_CONF,
+    .decode         = qcelp_decode_frame,
+    .capabilities   = AV_CODEC_CAP_DR1,
     .priv_data_size = sizeof(QCELPContext),
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
 };
