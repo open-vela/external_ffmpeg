0a87560ca684bf32b02019447cf2dd5e9cd701dc
openvela-robot <openvela-robot@xiaomi.com>
2022-05-25 12:54:01 +0800

    avcodec/mfenc: Dynamically load MFPlat.DLL
    Allows non-UWP builds of FFmpeg with MediaFoundation to work on
    N editions of Windows which are without MediaFoundation by default.
    
    On UWP target, FFmpeg is linked directly against MediaFoundation since
    LoadLibrary is not available.
    
    This commit adresses https://trac.ffmpeg.org/ticket/9788
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- libavformat/bfi.c
+++ libavformat/bfi.c
@@ -29,6 +29,7 @@
 #include "libavutil/channel_layout.h"
 #include "libavutil/intreadwrite.h"
 #include "avformat.h"
+#include "demux.h"
 #include "internal.h"
 
 typedef struct BFIContext {
@@ -69,7 +70,12 @@ static int bfi_read_header(AVFormatContext * s)
     /* Set the total number of frames. */
     avio_skip(pb, 8);
     chunk_header           = avio_rl32(pb);
+    if (chunk_header < 3)
+        return AVERROR_INVALIDDATA;
+
     bfi->nframes           = avio_rl32(pb);
+    if (bfi->nframes < 0)
+        return AVERROR_INVALIDDATA;
     avio_rl32(pb);
     avio_rl32(pb);
     avio_rl32(pb);
@@ -101,8 +107,7 @@ static int bfi_read_header(AVFormatContext * s)
     /* Set up the audio codec now... */
     astream->codecpar->codec_type      = AVMEDIA_TYPE_AUDIO;
     astream->codecpar->codec_id        = AV_CODEC_ID_PCM_U8;
-    astream->codecpar->channels        = 1;
-    astream->codecpar->channel_layout  = AV_CH_LAYOUT_MONO;
+    astream->codecpar->ch_layout       = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
     astream->codecpar->bits_per_coded_sample = 8;
     astream->codecpar->bit_rate        =
         (int64_t)astream->codecpar->sample_rate * astream->codecpar->bits_per_coded_sample;
@@ -171,7 +176,7 @@ static int bfi_read_packet(AVFormatContext * s, AVPacket * pkt)
     return ret;
 }
 
-AVInputFormat ff_bfi_demuxer = {
+const AVInputFormat ff_bfi_demuxer = {
     .name           = "bfi",
     .long_name      = NULL_IF_CONFIG_SMALL("Brute Force & Ignorance"),
     .priv_data_size = sizeof(BFIContext),
