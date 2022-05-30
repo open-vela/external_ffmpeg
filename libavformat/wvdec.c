b56bcdcaeb0f3b111b21ae49b06476aef1d0d37b
liujinye <liujinye@xiaomi.com>
2022-05-30 08:15:03 +0800

    FFMPEG: reset flushing flag after RELEASE ioctl
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- libavformat/wvdec.c
+++ libavformat/wvdec.c
@@ -79,9 +79,8 @@ static int wv_read_block_header(AVFormatContext *ctx, AVIOContext *pb)
 {
     WVContext *wc = ctx->priv_data;
     int ret;
-    int rate, bpp, chan;
+    int rate, rate_x, bpp, chan;
     uint32_t chmask, flags;
-    unsigned rate_x;
 
     wc->pos = avio_tell(pb);
 
@@ -193,7 +192,7 @@ static int wv_read_block_header(AVFormatContext *ctx, AVIOContext *pb)
             if (id & 0x40)
                 avio_skip(pb, 1);
         }
-        if (rate == -1 || rate * (uint64_t)rate_x >= INT_MAX) {
+        if (rate == -1) {
             av_log(ctx, AV_LOG_ERROR,
                    "Cannot determine custom sampling rate\n");
             return AVERROR_INVALIDDATA;
@@ -256,7 +255,8 @@ static int wv_read_header(AVFormatContext *s)
     AV_WL16(st->codecpar->extradata, wc->header.version);
     st->codecpar->codec_type            = AVMEDIA_TYPE_AUDIO;
     st->codecpar->codec_id              = AV_CODEC_ID_WAVPACK;
-    av_channel_layout_from_mask(&st->codecpar->ch_layout, wc->chmask);
+    st->codecpar->channels              = wc->chan;
+    st->codecpar->channel_layout        = wc->chmask;
     st->codecpar->sample_rate           = wc->rate;
     st->codecpar->bits_per_coded_sample = wc->bpp;
     avpriv_set_pts_info(st, 64, 1, wc->rate);
@@ -328,7 +328,7 @@ static int wv_read_packet(AVFormatContext *s, AVPacket *pkt)
     return 0;
 }
 
-const AVInputFormat ff_wv_demuxer = {
+AVInputFormat ff_wv_demuxer = {
     .name           = "wv",
     .long_name      = NULL_IF_CONFIG_SMALL("WavPack"),
     .priv_data_size = sizeof(WVContext),
