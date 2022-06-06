ab6d596dbf704eec4e8c82caf823ad1b01178b0e
liujinye <liujinye@xiaomi.com>
2022-06-06 16:10:01 +0800

    FFMPEG: do fadeout when pause.
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- tests/checkasm/vf_threshold.c
+++ tests/checkasm/vf_threshold.c
@@ -18,9 +18,8 @@
 
 #include <string.h>
 #include "checkasm.h"
-#include "libavfilter/vf_threshold_init.h"
+#include "libavfilter/threshold.h"
 #include "libavutil/intreadwrite.h"
-#include "libavutil/mem_internal.h"
 
 #define WIDTH 256
 #define WIDTH_PADDED 256 + 32
