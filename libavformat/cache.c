4f0e0b7898811b1c792086e799a13e57f5ec7341
qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
2022-06-22 20:51:14 +0800

    FFMPEG: calculate average bps in cache protocol.
    1, calculate average bps.
    2, log when EOF.
    
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- libavformat/cache.c
+++ libavformat/cache.c
@@ -33,7 +33,8 @@
 #include "libavutil/mem.h"
 #include "libavutil/opt.h"
 #include "libavutil/tree.h"
-#include "avio.h"
+#include "libavutil/time.h"
+#include "avformat.h"
 #include <fcntl.h>
 #if HAVE_IO_H
 #include <io.h>
@@ -101,9 +102,12 @@ static void *cache_thread(void *arg)
 {
     URLContext *h = arg;
     Context *c = h->priv_data;
-    int64_t offset = 0;
     uint8_t buf[CACHE_BUFSIZE];
+    int64_t time, bps, diff;
+    int64_t offset = 0;
     int ret;
+
+    time = av_gettime_relative();
 
     while (1) {
         pthread_mutex_lock(&c->mutex);
@@ -122,6 +126,10 @@ static void *cache_thread(void *arg)
         else if (ret != AVERROR(EINTR))
             break;
     }
+
+    diff = (av_gettime_relative() - time) / AV_TIME_BASE;
+    bps = diff ? offset * 8 / diff : 0;
+    av_log(h, AV_LOG_INFO, "%s end ret %d bytes %lld %lldbps\n", __func__, ret, offset, bps);
 
     return NULL;
 }
@@ -142,7 +150,7 @@ static int cache_open(URLContext *h, const char *arg, int flags, AVDictionary **
 
     c->fd = avpriv_tempfile("ffcache", &buffername, 0, h);
     if (c->fd < 0){
-        av_log(h, AV_LOG_ERROR, "Failed to create tempfile\n");
+        av_log(h, AV_LOG_ERROR, "Failed to create tempfile %d\n", c->fd);
         return c->fd;
     }
 
