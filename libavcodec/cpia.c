441a7d6f62bfae142932f4deabfc2b33b4392597
liujinye <liujinye@xiaomi.com>
2022-06-27 18:06:51 +0800

    FFMPEG: add debug log.
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- libavcodec/cpia.c
+++ libavcodec/cpia.c
@@ -23,7 +23,6 @@
  */
 
 #include "avcodec.h"
-#include "codec_internal.h"
 #include "get_bits.h"
 #include "internal.h"
 
@@ -48,14 +47,14 @@ typedef struct {
 } CpiaContext;
 
 
-static int cpia_decode_frame(AVCodecContext *avctx, AVFrame *rframe,
-                             int *got_frame, AVPacket* avpkt)
+static int cpia_decode_frame(AVCodecContext *avctx,
+                             void *data, int *got_frame, AVPacket* avpkt)
 {
     CpiaContext* const cpia = avctx->priv_data;
     int i,j,ret;
 
-    const uint8_t *const header = avpkt->data;
-    const uint8_t *src;
+    uint8_t* const header = avpkt->data;
+    uint8_t* src;
     int src_size;
     uint16_t linelength;
     uint8_t skip;
@@ -112,7 +111,6 @@ static int cpia_decode_frame(AVCodecContext *avctx, AVFrame *rframe,
         // Read line length, two byte little endian
         linelength = AV_RL16(src);
         src += 2;
-        src_size -= 2;
 
         if (src_size < linelength) {
             frame->decode_error_flags = FF_DECODE_ERROR_INVALID_BITSTREAM;
@@ -185,7 +183,7 @@ static int cpia_decode_frame(AVCodecContext *avctx, AVFrame *rframe,
     }
 
     *got_frame = 1;
-    if ((ret = av_frame_ref(rframe, cpia->frame)) < 0)
+    if ((ret = av_frame_ref(data, cpia->frame)) < 0)
         return ret;
 
     return avpkt->size;
@@ -222,15 +220,14 @@ static av_cold int cpia_decode_end(AVCodecContext *avctx)
     return 0;
 }
 
-const FFCodec ff_cpia_decoder = {
-    .p.name         = "cpia",
-    .p.long_name    = NULL_IF_CONFIG_SMALL("CPiA video format"),
-    .p.type         = AVMEDIA_TYPE_VIDEO,
-    .p.id           = AV_CODEC_ID_CPIA,
+AVCodec ff_cpia_decoder = {
+    .name           = "cpia",
+    .long_name      = NULL_IF_CONFIG_SMALL("CPiA video format"),
+    .type           = AVMEDIA_TYPE_VIDEO,
+    .id             = AV_CODEC_ID_CPIA,
     .priv_data_size = sizeof(CpiaContext),
     .init           = cpia_decode_init,
     .close          = cpia_decode_end,
-    FF_CODEC_DECODE_CB(cpia_decode_frame),
-    .p.capabilities = AV_CODEC_CAP_DR1,
-    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE,
+    .decode         = cpia_decode_frame,
+    .capabilities   = AV_CODEC_CAP_DR1,
 };
