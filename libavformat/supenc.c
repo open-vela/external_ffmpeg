c3c5ba6b27fd1be9dc9ae32a2c8d5fd6c06e9190
openvela-robot <openvela-robot@xiaomi.com>
2022-07-04 21:40:36 +0800

    asink_adevsink: foward param to priv
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- libavformat/supenc.c
+++ libavformat/supenc.c
@@ -27,7 +27,7 @@
 
 static int sup_write_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    const uint8_t *data = pkt->data;
+    uint8_t *data = pkt->data;
     size_t size = pkt->size;
     uint32_t pts = 0, dts = 0;
 
@@ -84,7 +84,7 @@ static int sup_write_header(AVFormatContext *s)
     return 0;
 }
 
-const AVOutputFormat ff_sup_muxer = {
+AVOutputFormat ff_sup_muxer = {
     .name           = "sup",
     .long_name      = NULL_IF_CONFIG_SMALL("raw HDMV Presentation Graphic Stream subtitles"),
     .extensions     = "sup",
