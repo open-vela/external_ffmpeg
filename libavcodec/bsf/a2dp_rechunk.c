4eca3ef26d47fb114019d43f31c5865907da3485
fangzhenwei <fangzhenwei@xiaomi.com>
2022-07-05 23:11:52 +0800

    ffmpeg/bsf:a2dp rechunk bdf support AV_CODEC_ID_AAC_LATM_A2DP
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- libavcodec/bsf/a2dp_rechunk.c
+++ libavcodec/bsf/a2dp_rechunk.c
@@ -140,6 +140,7 @@ static const enum AVCodecID codec_ids[] = {
     AV_CODEC_ID_SBC_PACKED_A2DP,
     AV_CODEC_ID_AAC,
     AV_CODEC_ID_AAC_LATM,
+    AV_CODEC_ID_AAC_LATM_A2DP,
     AV_CODEC_ID_NONE,
 };
 
