ac776c1934def7589b5f39316d440b16cd73e941
openvela-robot <openvela-robot@xiaomi.com>
2022-07-21 11:03:14 +0800

    src_movie_async: use av_asprintf instead
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- libavcodec/pnm_parser.c
+++ libavcodec/pnm_parser.c
@@ -109,12 +109,8 @@ retry:
         if (next == END_NOT_FOUND)
             pnmpc->ascii_scan = sync - pnmctx.bytestream + skip;
     } else {
-        int ret = av_image_get_buffer_size(avctx->pix_fmt, avctx->width, avctx->height, 1);
-        next = pnmctx.bytestream - pnmctx.bytestream_start + skip;
-        if (ret > 0 && pnmctx.half)
-            ret >>= 1;
-        if (ret >= 0 && next + (uint64_t)ret <= INT_MAX)
-            next += ret;
+        next = pnmctx.bytestream - pnmctx.bytestream_start + skip
+               + av_image_get_buffer_size(avctx->pix_fmt, avctx->width, avctx->height, 1);
     }
     if (next != END_NOT_FOUND && pnmctx.bytestream_start != buf + skip)
         next -= pc->index;
@@ -133,10 +129,9 @@ end:
     return next;
 }
 
-const AVCodecParser ff_pnm_parser = {
+AVCodecParser ff_pnm_parser = {
     .codec_ids      = { AV_CODEC_ID_PGM, AV_CODEC_ID_PGMYUV, AV_CODEC_ID_PPM,
-                        AV_CODEC_ID_PBM, AV_CODEC_ID_PAM, AV_CODEC_ID_PFM,
-                        AV_CODEC_ID_PHM },
+                        AV_CODEC_ID_PBM, AV_CODEC_ID_PAM },
     .priv_data_size = sizeof(PNMParseContext),
     .parser_parse   = pnm_parse,
     .parser_close   = ff_parse_close,
