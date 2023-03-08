c811cdb72d5c101bc87259300826883cd4f42311
jihandong <jihandong@xiaomi.com>
2023-03-08 11:53:14 +0800

    ffmpeg: nuttx_dec support temparory pause
    Signed-off-by: jihandong <jihandong@xiaomi.com>

--- libavfilter/asrc_adevsrc.c
+++ libavfilter/asrc_adevsrc.c
@@ -243,7 +243,7 @@ static int adevsrc_process_command(AVFilterContext *ctx, const char *cmd, const 
         return avdevice_app_to_dev_control_message(
                 priv->fmt_ctx,
                 AV_APP_TO_DEV_PAUSE,
-                res, res_len);
+                (char *)args, 0);
     } else if (!strcmp(cmd, "get_pollfd")) {
         return avdevice_app_to_dev_control_message(
                 priv->fmt_ctx,
