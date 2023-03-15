46e725a6c5769241d99022feef17fcc20e407681
yangliming <yangliming@xiaomi.com>
2023-03-15 15:49:05 +0800

    ffmpeg : add ldac demuxer and decoder.
    add ldac demuxer and decoder in ffmpeg base on the libldac provided by sony
    
    Signed-off-by: yangliming <yangliming@xiaomi.com>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -238,6 +238,7 @@ extern const AVOutputFormat ff_kvag_muxer;
 extern const AVOutputFormat ff_latm_muxer;
 extern const AVInputFormat  ff_lc3_demuxer;
 extern const AVOutputFormat ff_lc3_muxer;
+extern const AVInputFormat  ff_ldac_demuxer;
 extern const AVInputFormat  ff_lmlm4_demuxer;
 extern const AVInputFormat  ff_loas_demuxer;
 extern const AVInputFormat  ff_luodat_demuxer;
