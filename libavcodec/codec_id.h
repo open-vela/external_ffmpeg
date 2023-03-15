2e480b30f422fe08a59a30d376bc4e82c742595d
yangliming <yangliming@xiaomi.com>
2023-03-15 15:49:05 +0800

    ffmpeg : add ldac demuxer and decoder.

--- libavcodec/codec_id.h
+++ libavcodec/codec_id.h
@@ -525,6 +525,7 @@ enum AVCodecID {
     AV_CODEC_ID_MSNSIREN,
     AV_CODEC_ID_DFPWM,
     AV_CODEC_ID_LC3,
+    AV_CODEC_ID_LDAC,
 
     /* subtitle codecs */
     AV_CODEC_ID_FIRST_SUBTITLE = 0x17000,          ///< A dummy ID pointing at the start of subtitle codecs.
