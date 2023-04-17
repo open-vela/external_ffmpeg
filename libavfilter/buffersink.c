dd2f03d0d8c48960a00073eb7f8982d83b93ce81
hanqiyuan <hanqiyuan@xiaomi.com>
2023-04-17 11:28:05 +0800

    ffmpeg: codesize optimize for filters asink_abuffer,asrc_abuffer,vsink_buffer,vsrc_buffer.

--- libavfilter/buffersink.c
+++ libavfilter/buffersink.c
@@ -406,7 +406,7 @@ static const AVFilterPad avfilter_vsink_buffer_inputs[] = {
     },
 };
 
-const AVFilter ff_vsink_buffer = {
+const AVFilter ff_vf_vsink_buffer = {
     .name          = "buffersink",
     .description   = NULL_IF_CONFIG_SMALL("Buffer video frames, and make them available to the end of the filter graph."),
     .priv_size     = sizeof(BufferSinkContext),
@@ -425,7 +425,7 @@ static const AVFilterPad avfilter_asink_abuffer_inputs[] = {
     },
 };
 
-const AVFilter ff_asink_abuffer = {
+const AVFilter ff_af_asink_abuffer = {
     .name          = "abuffersink",
     .description   = NULL_IF_CONFIG_SMALL("Buffer audio frames, and make them available to the end of the filter graph."),
     .priv_class    = &abuffersink_class,
