38e5e2939783b6183cbe4f71ab8d71c4befc101b
renjianguang <renjianguang@xiaomi.com>
2023-05-30 14:55:45 +0800

    FFMPEG: Add amrnb amrwb muxer, don't add file header to amr file
    Signed-off-by: renjianguang <renjianguang@xiaomi.com>

--- libavformat/rawenc.c
+++ libavformat/rawenc.c
@@ -87,6 +87,36 @@ const FFOutputFormat ff_adx_muxer = {
 };
 #endif
 
+#if CONFIG_AMRNB_MUXER
+const FFOutputFormat ff_amrnb_muxer = {
+    .p.name            = "amrnb",
+    .p.long_name       = NULL_IF_CONFIG_SMALL("raw AMR-NB"),
+    .p.mime_type       = "audio/amrnb",
+    .p.extensions      = "amr",
+    .p.audio_codec     = AV_CODEC_ID_AMR_NB,
+    .p.video_codec     = AV_CODEC_ID_NONE,
+    .flags_internal    = FF_OFMT_FLAG_MAX_ONE_OF_EACH |
+                         FF_OFMT_FLAG_ONLY_DEFAULT_CODECS,
+    .write_packet      = ff_raw_write_packet,
+    .p.flags           = AVFMT_NOTIMESTAMPS,
+};
+#endif
+
+#if CONFIG_AMRWB_MUXER
+const FFOutputFormat ff_amrwb_muxer = {
+    .p.name            = "amrwb",
+    .p.long_name       = NULL_IF_CONFIG_SMALL("raw AMR-WB"),
+    .p.mime_type       = "audio/amrwb",
+    .p.extensions      = "amr",
+    .p.audio_codec     = AV_CODEC_ID_AMR_WB,
+    .p.video_codec     = AV_CODEC_ID_NONE,
+    .flags_internal    = FF_OFMT_FLAG_MAX_ONE_OF_EACH |
+                         FF_OFMT_FLAG_ONLY_DEFAULT_CODECS,
+    .write_packet      = ff_raw_write_packet,
+    .p.flags             = AVFMT_NOTIMESTAMPS,
+};
+#endif
+
 #if CONFIG_APTX_MUXER
 const FFOutputFormat ff_aptx_muxer = {
     .p.name            = "aptx",
