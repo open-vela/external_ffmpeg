7c76b27b03ea55ea750b0e6dc59547bda82dd3bb
zhangchendong1 <zhangchendong1@xiaomi.com>
2023-06-19 17:19:11 +0800

    external/ffmpeg: add Simplified Abstraction Stream Protocol demuxer

---
+++ libavformat/sasp_common.h
@@ -0,0 +1,64 @@
+/*
+ * Simplified Abstraction Stream Protocol Common Header
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * Simplified Abstraction Stream Protocol Common Header
+ */
+
+#ifndef AVFORMAT_SASP_COMMON_H
+#define AVFORMAT_SASP_COMMON_H
+
+#include "avformat.h"
+
+typedef struct SASPStreamHeader {
+    uint16_t version;
+    uint16_t header_len;
+    uint32_t magic;
+    uint32_t video_codec_id;
+    uint32_t audio_codec_id;
+
+    /* video specific info */
+    uint32_t width;
+    uint32_t height;
+    uint32_t fps;
+
+    /* audio specific info */
+    uint32_t sample_rate;
+    uint32_t channel;
+} SASPStreamHeader;
+
+typedef struct SASPFrameHeader {
+    uint16_t version;
+    uint16_t header_len;
+    uint32_t magic;
+    uint32_t body_len;
+    uint32_t codec_id;
+    uint32_t sequence;
+    uint32_t timestamp_ms;    /* the 'ms' part of the total time */
+    uint64_t timestamp_s;     /* the 's' part of the total time */
+} SASPFrameHeader;
+
+int ff_sasp_read_stream_header(AVFormatContext *ic, SASPStreamHeader *header);
+int ff_sasp_write_stream_header(char *header_buf, const SASPStreamHeader *header);
+int ff_sasp_read_frame_header(AVFormatContext *ic, SASPFrameHeader *header);
+int ff_sasp_write_frame_header(char *frame_buf, const SASPFrameHeader *header);
+
+#endif /* AVFORMAT_SASP_COMMON_H */
---
+++ libavformat/sasp_dec.c
@@ -0,0 +1,192 @@
+/*
+ * Simplified Abstraction Stream Protocol Demuxer
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * Simplified Abstraction Stream Protocol Demuxer
+ */
+
+#include "avformat.h"
+#include "internal.h"
+#include "libavutil/avstring.h"
+#include "libavutil/opt.h"
+#include "libavutil/intreadwrite.h"
+
+#include "sasp_common.h"
+
+typedef struct SASPDecContext {
+    AVClass *class;
+    enum AVCodecID audio_codec_id;
+    enum AVCodecID video_codec_id;
+    int audio_stream_idx;
+    int video_stream_idx;
+    uint64_t audio_pos;
+    uint64_t video_pos;
+} SASPDecContext;
+
+static int sasp_probe(const AVProbeData *probe)
+{
+    if (probe->buf_size >= 4 &&
+        AV_RB32(probe->buf) == MKBETAG('s', 'a', 's', 'p'))
+            return AVPROBE_SCORE_MAX;
+
+    return 0;
+}
+
+static void sasp_add_video(AVFormatContext *ic, SASPStreamHeader *stream_header)
+{
+    SASPDecContext *s = ic->priv_data;
+    AVStream *st;
+    FFStream *sti;
+
+    st = avformat_new_stream(ic, NULL);
+    sti = ffstream(st);
+
+    s->video_codec_id   = stream_header->video_codec_id;
+    s->video_stream_idx = st->index;
+
+    st->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
+    st->codecpar->codec_id   = stream_header->video_codec_id;
+
+    /* the following parameters can also be extracted
+     * from the video data in avformat_find_stream_info
+     */
+    st->r_frame_rate.num = stream_header->fps;
+    st->r_frame_rate.den = 1;
+    st->avg_frame_rate   = st->r_frame_rate;
+    st->codecpar->width  = stream_header->width;
+    st->codecpar->height = stream_header->height;
+
+    avpriv_set_pts_info(st, 64, 1, AV_TIME_BASE);
+
+    /* force h264/h265 parsing, in case there are
+     * network problems and the pkts maybe lossed
+     */
+    sti->need_parsing = AVSTREAM_PARSE_FULL_RAW;
+}
+
+static void sasp_add_audio(AVFormatContext *ic, SASPStreamHeader *stream_header)
+{
+    SASPDecContext *s = ic->priv_data;
+    AVStream *st;
+    FFStream *sti;
+
+    st = avformat_new_stream(ic, NULL);
+    sti = ffstream(st);
+
+    s->audio_codec_id   = stream_header->audio_codec_id;
+    s->audio_stream_idx = st->index;
+
+    st->codecpar->codec_type  = AVMEDIA_TYPE_AUDIO;
+    st->codecpar->codec_id    = stream_header->audio_codec_id;
+    st->codecpar->sample_rate = stream_header->sample_rate;
+
+    av_channel_layout_default(&st->codecpar->ch_layout, stream_header->channel);
+    avpriv_set_pts_info(st, 64, 1, st->codecpar->sample_rate);
+
+    sti->need_parsing = AVSTREAM_PARSE_FULL_RAW;
+}
+
+static int sasp_read_header(AVFormatContext *ic)
+{
+    SASPStreamHeader stream_header = { 0 };
+    SASPDecContext *s = ic->priv_data;
+    AVStream *st;
+    FFStream *sti;
+    int ret;
+
+    ret = ff_sasp_read_stream_header(ic, &stream_header);
+    if (ret < 0)
+        return ret;
+
+    avio_skip(ic->pb, stream_header.header_len - ret);
+
+    if (stream_header.video_codec_id)
+        sasp_add_video(ic, &stream_header);
+
+    if (stream_header.audio_codec_id)
+        sasp_add_audio(ic, &stream_header);
+
+    return ret;
+}
+
+static int sasp_read_packet(AVFormatContext *ic, AVPacket *pkt)
+{
+    SASPFrameHeader frame_header = { 0 };
+    SASPDecContext *s = ic->priv_data;
+    AVStream *st;
+    int ret;
+
+    ret = ff_sasp_read_frame_header(ic, &frame_header);
+    if (ret  < 0)
+        return ret;
+
+    avio_skip(ic->pb, frame_header.header_len - ret);
+
+    ret = av_get_packet(ic->pb, pkt, frame_header.body_len);
+    if (ret < 0) {
+        av_log(s, AV_LOG_ERROR, "Error when get packet from sasp stream.\n");
+        return ret;
+    }
+
+    if (frame_header.codec_id == s->video_codec_id) {
+        pkt->pos          = s->video_pos;
+        s->video_pos     += frame_header.body_len;
+        pkt->stream_index = s->video_stream_idx;
+    } else if (frame_header.codec_id == s->audio_codec_id) {
+        pkt->pos          = s->audio_pos;
+        s->audio_pos     += frame_header.body_len;
+        pkt->stream_index = s->audio_stream_idx;
+    } else {
+        av_log(s, AV_LOG_ERROR, "Unknown id in frame header.\n");
+        return AVERROR_INVALIDDATA;
+    }
+
+    st = ic->streams[pkt->stream_index];
+
+    pkt->pts = av_rescale(frame_header.timestamp_s * 1000 + frame_header.timestamp_ms, st->time_base.den, 1000);
+
+    av_log(ic, AV_LOG_TRACE, "Sasp stream %s pkt: len %"PRIu32", pts %"PRIu64"s%"PRIu32"ms, seqnum %"PRIu32"\n",
+           frame_header.codec_id == s->video_codec_id ? "video" : "audio",
+           frame_header.body_len, frame_header.timestamp_s, frame_header.timestamp_ms,
+           frame_header.sequence);
+
+    return ret;
+}
+
+av_cold static int sasp_init(AVFormatContext *ic)
+{
+    /* disable any fps probe */
+    ic->fps_probe_size = 0;
+    return 0;
+}
+
+AVInputFormat ff_sasp_demuxer = {
+        .name           = "sasp",
+        .long_name      = NULL_IF_CONFIG_SMALL("Simplified Abstraction Stream Protocol Demuxer"),
+        .flags          = AVFMT_TS_DISCONT,
+        .flags_internal = FF_FMT_INIT_CLEANUP,
+        .priv_data_size = sizeof(SASPDecContext),
+        .read_probe     = sasp_probe,
+        .read_header    = sasp_read_header,
+        .read_packet    = sasp_read_packet,
+        .init           = sasp_init,
+        .extensions     = "sasp",
+};
