7b54a3ada6f4539c39199b5fb59e475dbb878c78
zhangchendong1 <zhangchendong1@xiaomi.com>
2023-06-19 17:19:11 +0800

    external/ffmpeg: add Simplified Abstraction Stream Protocol demuxer
    Simplified Abstraction Stream Protocol(SASP) demuxer is a common
    stream demuxer which can be used to parse miss stream data and
    generate ffmpeg compatible audio/video packets.
    
    Signed-off-by: zhangchendong1 <zhangchendong1@xiaomi.com>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -417,6 +417,7 @@ extern const FFInputFormat  ff_s337m_demuxer;
 extern const FFInputFormat  ff_sami_demuxer;
 extern const FFInputFormat  ff_sap_demuxer;
 extern const FFOutputFormat ff_sap_muxer;
+extern const FFInputFormat  ff_sasp_demuxer;
 extern const FFInputFormat  ff_sbc_demuxer;
 extern const FFOutputFormat ff_sbc_muxer;
 extern const FFInputFormat  ff_sbg_demuxer;
