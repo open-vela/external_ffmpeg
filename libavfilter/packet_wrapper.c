24f80caf70ccbb11bfdfc665a76633e1cb174eaf
jihandong <jihandong@xiaomi.com>
2023-06-27 17:20:43 +0800

    ffmpeg: packet wrapper for src => dec, enc => sink, and src => sink (offload)
    Signed-off-by: jihandong <jihandong@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

---
+++ libavfilter/packet_wrapper.c
@@ -0,0 +1,99 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "packet_wrapper.h"
+
+static void wrap_free_packet(void *opaque, uint8_t *data)
+{
+    AVPacket *packet = (AVPacket *)data;
+
+    av_packet_free(&packet);
+}
+
+static void wrap_free_params(void *opaque, uint8_t *data)
+{
+    AVCodecParameters *params = (AVCodecParameters *)data;
+
+    avcodec_parameters_free(&params);
+}
+
+AVFrame* wrap_frame(AVPacket* packet, AVCodecParameters* params)
+{
+    AVCodecParameters *dup = NULL;
+    AVFrame *frame = NULL;
+    int ret;
+
+    frame = av_frame_alloc();
+    if (!frame)
+        return NULL;
+
+    /* Packet field. */
+    frame->buf[0] = av_buffer_create((void *)packet,
+                                     sizeof(AVPacket) + AV_INPUT_BUFFER_PADDING_SIZE,
+                                     wrap_free_packet, NULL, 0);
+    if (!frame->buf[0])
+        goto failed;
+
+    frame->linesize[0] = frame->buf[0]->size;
+    frame->data[0]     = frame->buf[0]->data;
+
+    /* Paramters field. */
+    if (params) {
+        dup = avcodec_parameters_alloc();
+        if (!dup)
+           goto failed;
+
+        ret = avcodec_parameters_copy(dup, params);
+        if (ret < 0)
+            goto failed;
+
+        frame->opaque_ref = av_buffer_create((void *)dup,
+                                             sizeof(AVCodecParameters) + AV_INPUT_BUFFER_PADDING_SIZE,
+                                             wrap_free_params, NULL, 0);
+        if (!frame->opaque_ref)
+            goto failed;
+    }
+
+    frame->pts = packet->pts;
+    return frame;
+
+failed:
+    av_frame_free(&frame);
+
+    if (dup)
+        avcodec_parameters_free(&dup);
+
+    return NULL;
+}
+
+void unwrap_frame(AVFrame* frame, AVPacket** packetptr, AVCodecParameters** paramsptr)
+{
+    if (packetptr) {
+        if (frame->linesize[0])
+            *packetptr = (AVPacket *)frame->data[0];
+        else
+            *packetptr = NULL;
+    }
+
+    if (paramsptr) {
+        if (frame->opaque_ref)
+            *paramsptr = (AVCodecParameters *)frame->opaque_ref->data;
+        else
+            *paramsptr = NULL;
+    }
+}
