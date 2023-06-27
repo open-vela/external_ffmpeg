3a0b85d7f6838dc9be708ff9b8d7a0276f92b5aa
jihandong <jihandong@xiaomi.com>
2023-06-27 17:20:43 +0800

    ffmpeg: packet wrapper for src => dec, enc => sink, and src => sink (offload)

---
+++ libavfilter/packet_wrapper.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libavutil/opt.h"
+#include "libavcodec/avcodec.h"
+
+/**
+ * Wrap packet and its format paramters into a special frame.
+ */
+AVFrame* wrap_frame(AVPacket* packet, AVCodecParameters* params);
+
+/**
+ * Unwrap a special frame into a packet and its format paramters.
+ */
+void unwrap_frame(AVFrame* frame, AVPacket** packetptr, AVCodecParameters** paramsptr);
