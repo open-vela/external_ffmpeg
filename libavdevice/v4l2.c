ea81def0aca3f0a3922eac1a40962a119be3705c
jinxiuxu <jinxiuxu@xiaomi.com>
2023-06-27 13:19:21 +0800

    FFMPEG: avoid v4l2 ioctl warning
    ffmpeg/libavdevice/v4l2.c:137:17: warning: assignment to 'int (*)(int,
    long unsigned int,  ...)' from incompatible pointer type ' (*)(int,  int,  ...)
      137 |     s->ioctl_f  = prefix ## ioctl;      \
          |                 ^
    ffmpeg/libavdevice/v4l2.c:151:9: note: in expansion of macro 'SET_WRAPPERS'
      151 |         SET_WRAPPERS();
          |         ^~~~~~~~~~~~
    
    Signed-off-by: jinxiuxu <jinxiuxu@xiaomi.com>

--- libavdevice/v4l2.c
+++ libavdevice/v4l2.c
@@ -107,7 +107,7 @@ struct video_data {
     int (*open_f)(const char *file, int oflag, ...);
     int (*close_f)(int fd);
     int (*dup_f)(int fd);
-#ifdef __GLIBC__
+#if defined(__GLIBC__) && !defined(__NuttX__)
     int (*ioctl_f)(int fd, unsigned long int request, ...);
 #else
     int (*ioctl_f)(int fd, int request, ...);
