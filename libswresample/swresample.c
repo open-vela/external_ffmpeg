521c4bce28aca6dc5c1a4e392891f583cea8c72c
shizhenghui <shizhenghui@xiaomi.com>
2023-07-24 09:28:35 +0800

    Revert "FFmpeg: make swr_resampler optional"
    This reverts commit 6ed50380e06aac5d47d98604f6de3f23544bfee8.
    
    Reason for revert: <specific product-Dev build failed>
    Signed-off-by: shizhenghui <shizhenghui@xiaomi.com>

--- libswresample/swresample.c
+++ libswresample/swresample.c
@@ -278,10 +278,9 @@ av_cold int swr_init(struct SwrContext *s){
         case SWR_ENGINE_SOXR: s->resampler = &swri_soxr_resampler; break;
 #endif
 #if CONFIG_HIFI4SRC
-        case SWR_ENGINE_SWR: s->resampler = &swri_hifi4_resampler; break;
-#endif
-#if CONFIG_SWR_RESAMPLER
-        case SWR_ENGINE_SWR: s->resampler = &swri_resampler; break;
+        case SWR_ENGINE_SWR : s->resampler = &swri_hifi4_resampler; break;
+#else
+        case SWR_ENGINE_SWR : s->resampler = &swri_resampler; break;
 #endif
         default:
             av_log(s, AV_LOG_ERROR, "Requested resampling engine is unavailable\n");
