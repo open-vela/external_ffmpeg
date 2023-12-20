dc540203e9e7efa9087b4d0e898df93e47607848
yangsen5 <yangsen5@xiaomi.com>
2023-12-20 15:25:42 +0800

    ffmpeg: Fix warning when compiling goldfish 32-bit mjpegdec.c print format

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -1836,7 +1836,7 @@ static int mjpeg_decode_app(MJpegDecodeContext *s)
     if (len < 6) {
         if (s->bayer) {
             // Pentax K-1 (digital camera) JPEG images embedded in DNG images contain unknown APP0 markers
-            av_log(s->avctx, AV_LOG_WARNING, "skipping APPx (len=%"PRId32") for bayer-encoded image\n", len);
+            av_log(s->avctx, AV_LOG_WARNING, "skipping APPx (len=%d) for bayer-encoded image\n", len);
             skip_bits(&s->gb, len);
             return 0;
         } else
