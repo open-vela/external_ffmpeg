848e9dc5951efda93d3e2f3acd6bc0f8c0fb013a
rongyichang <rongyichang@xiaomi.com>
2024-01-11 14:54:10 +0800

    ffmpeg: set vtun default queue size to 1
    Signed-off-by: rongyichang <rongyichang@xiaomi.com>

--- libavdevice/vtun_enc.c
+++ libavdevice/vtun_enc.c
@@ -421,7 +421,7 @@ static int vtun_control_message(struct AVFormatContext *h, int type,
 #define OFFSET(x) offsetof(VtunCtx, x)
 #define ENC AV_OPT_FLAG_ENCODING_PARAM
 static const AVOption options[] = {
-    { "frame_count", "Set frame count", OFFSET(frame_count), AV_OPT_TYPE_INT, {.i64 = 2}, 1, 4, ENC },
+    { "frame_count", "Set frame count", OFFSET(frame_count), AV_OPT_TYPE_INT, {.i64 = 1}, 1, 4, ENC },
     { "server_path", "Set server path", OFFSET(server_path), AV_OPT_TYPE_STRING, { .str = NULL }, 0, 0, ENC },
     { NULL }
 };
