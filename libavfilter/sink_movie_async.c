f7185ebae9fae0aec4cad98043acf88874129585
cenzhanquan1 <cenzhanquan1@xiaomi.com>
2024-03-01 15:31:46 +0800

    ffmpeg: adjust log level is warning
    Signed-off-by: cenzhanquan1 <cenzhanquan1@xiaomi.com>

--- libavfilter/sink_movie_async.c
+++ libavfilter/sink_movie_async.c
@@ -474,7 +474,7 @@ static int moviesink_proc_dat(AVFilterContext *ctx)
         /* @deprecated naive avsync. */
         ret = moviesink_init_stream(ctx, i, frame);
         if (ret < 0) {
-            av_log(ctx, AV_LOG_ERROR, "moviesink_init_stream error %d\n.", ret);
+            av_log(ctx, AV_LOG_WARNING, "moviesink_init_stream error %d\n.", ret);
             av_frame_free(&frame);
             return ret;
         } else if (priv->format_ctx->nb_streams < ctx->nb_inputs) {
