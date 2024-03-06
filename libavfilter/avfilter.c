0a01904abf1b6fd491cc489c1b19b14d7e589b13
cuiziwei <cuiziwei@xiaomi.com>
2024-03-06 14:14:27 +0800

    sim/m64:fix build warning.

--- libavfilter/avfilter.c
+++ libavfilter/avfilter.c
@@ -340,7 +340,7 @@ void ff_avfilter_trace_status(AVFilterLink *link, int status_in, int status_out)
 
     len1 = avfilter_process_command(src, "dump", NULL, dump1 + len1, sizeof(dump1) - len1, 0);
     len2 = avfilter_process_command(dst, "dump", NULL, dump2 + len2, sizeof(dump2) - len2, 0);
-    av_log(NULL, AV_LOG_TRACE, "[%24s:%-24s] w:%d f:%lld p:%lld st:%d/%d=>%d/%d%s%s\n",
+    av_log(NULL, AV_LOG_TRACE, "[%24s:%-24s] w:%d f:%" PRId64 " p:%" PRId64 " st:%d/%d=>%d/%d%s%s\n",
         src->name, dst->name,
         link->frame_wanted_out, link->frame_count_in, link->sample_count_in,
         link->status_in, link->status_out, status_in, status_out,
