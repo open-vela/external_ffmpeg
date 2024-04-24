9c3adf4eefb65aeb362f71fea32cafcf0b511542
shizhenghui <shizhenghui@xiaomi.com>
2024-04-24 13:29:18 +0800

    FFmpeg: fix build warning in v4l2.c
    ffmpeg/libavdevice/v4l2.c:571:20: warning: format '%x' expects argument of type 'unsigned int', but argument 6 has type 'uint32_t' {aka 'long unsigned int'} [-Wformat=]
      571 |                    "Dequeued v4l2 buffer contains %"PRIu32" bytes, but %d were expected. Flags: 0x%08"PRIx16".\n",
          |                    ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      572 |                    buf.bytesused, s->frame_size, buf.flags);
          |                                                  ~~~~~~~~~
          |                                                     |
          |                                                     uint32_t {aka long unsigned int}
    
    Signed-off-by: shizhenghui <shizhenghui@xiaomi.com>

--- libavdevice/v4l2.c
+++ libavdevice/v4l2.c
@@ -568,7 +568,7 @@ static int mmap_read_frame(AVFormatContext *ctx, AVPacket *pkt)
 
         if (s->frame_size > 0 && buf.bytesused != s->frame_size) {
             av_log(ctx, AV_LOG_WARNING,
-                   "Dequeued v4l2 buffer contains %"PRIu32" bytes, but %d were expected. Flags: 0x%08"PRIx16".\n",
+                   "Dequeued v4l2 buffer contains %"PRIu32" bytes, but %d were expected. Flags: 0x%08"PRIx32".\n",
                    buf.bytesused, s->frame_size, buf.flags);
             buf.bytesused = 0;
         }
