e9891102b9250ed39f185ac061096fcbffbe6b32
wangxiaoxin <wangxiaoxin@xiaomi.com>
2024-07-26 15:35:26 +0800

    Fix logic infinite loop code

--- libavdevice/nuttx.c
+++ libavdevice/nuttx.c
@@ -294,7 +294,8 @@ static int ff_nuttx_fmt_to_avcodec(int *codec_id, int *formats)
     } else if (*formats & (1 << (AUDIO_FMT_OTHER - 1))) {
         format    = AUDIO_FMT_OTHER;
         *formats &= ~(1 << (AUDIO_FMT_OTHER - 1));
-    } else if (format & (1 << (AUDIO_FMT_AAC - 1))) {
+    } else if (*formats & (1 << (AUDIO_FMT_AAC - 1))) {
+        codec     = AV_CODEC_ID_AAC;
         format    = AUDIO_FMT_AAC;
         *formats &= ~(1 << (AUDIO_FMT_AAC - 1));
     } else if (*formats & (1 << (AUDIO_FMT_OPUS - 1))) {
