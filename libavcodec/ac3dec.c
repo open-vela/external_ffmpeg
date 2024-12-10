6d187c1f96961ae1b33d7ea7610b1073dcc8d26b
wangmingrong1 <wangmingrong1@xiaomi.com>
2024-12-10 16:47:58 +0800

    ffmpeg: ignore compile error, this is not an easy alarm to change.

--- libavcodec/ac3dec.c
+++ libavcodec/ac3dec.c
@@ -1201,8 +1201,11 @@ static int decode_audio_block(AC3DecodeContext *s, int blk, int offset)
     for (ch = !cpl_in_use; ch <= s->channels; ch++) {
         if (!s->eac3)
             s->exp_strategy[blk][ch] = get_bits(gbc, 2 - (ch == s->lfe_ch));
-        if (s->exp_strategy[blk][ch] != EXP_REUSE)
-            bit_alloc_stages[ch] = 3;
+        if (s->exp_strategy[blk][ch] != EXP_REUSE) {
+            if (ch < sizeof(bit_alloc_stages)) {
+                bit_alloc_stages[ch] = 3;
+            }
+        }
     }
 
     /* channel bandwidth */
