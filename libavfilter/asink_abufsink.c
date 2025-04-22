707915f3ea02a0407668d9d47e611564830492af
wurui3 <wurui3@xiaomi.com>
2025-04-22 22:37:31 +0800

    ffmpeg/libacfilter: mix frame in abufsink with uniform mix related interfaces from amix.c.
    Signed-off-by: wurui3 <wurui3@xiaomi.com>

--- libavfilter/asink_abufsink.c
+++ libavfilter/asink_abufsink.c
@@ -21,6 +21,7 @@
  * audio buffer sink
  */
 
+#include "amix.h"
 #include "audio.h"
 #include "avfilter.h"
 #include "avfilter_internal.h"
@@ -41,8 +42,6 @@ typedef struct ABufSinkPriv {
     const AVClass *class;
 
     int nb_inputs;                  /**< number of inputs */
-    float *input_scale;             /**< scale for each input */
-    uint8_t *input_state;           /**< current state of each input */
 
     int64_t next_pts;               /**< calculated pts for next output frame */
     int64_t output_duration;        /**< last output frame duration to determin to take a new frame from input or not. */
@@ -52,7 +51,7 @@ typedef struct ABufSinkPriv {
     AVChannelLayout ch_layout;      /**< channel layout */
     enum AVSampleFormat sample_fmt; /**< sample format */
 
-    AVAudioFifo **fifos;            /**< audio fifo for each input */
+    AMixContext *mix;               /**< mix module context */
 
     int (*on_event_cb)(void *udata, int evt, int64_t args);
     void *on_event_cb_udata;
@@ -87,66 +86,32 @@ static void av_abufsink_set_event_cb(AVFilterContext *ctx,
     ff_filter_set_ready(ctx, 100);
 }
 
-/**
- * Update the scaling factors to apply to each input during mixing.
- *
- * This balances the full volume range between active inputs and handles
- * volume transitions when EOF is encountered on an input but mixing continues
- * with the remaining inputs.
- */
-static void calculate_scales(ABufSinkPriv *s)
-{
-    int activate_inputs = 0;
-    int i;
-
-    for (i = 0; i < s->nb_inputs; i++)
-        if (s->input_state[i] & INPUT_ON)
-            activate_inputs++;
-
-    for (i = 0; i < s->nb_inputs; i++) {
-        if (s->input_state[i] & INPUT_ON) {
-            s->input_scale[i] = 1.0 / activate_inputs;
-        }
-    }
-}
-
-static void vector_fmac_scalar_c(int16_t *dst, const int16_t *src, int16_t mul, int len)
-{
-    int i;
-    int32_t accu;
-    for (i = 0; i < len; i++) {
-        accu = (int32_t)src[i] * mul;
-        dst[i] = av_clip_int16(dst[i] + ((accu + 0x4000) >> 15));
-    }
-}
-
-static int input_frame_wanted(ABufSinkPriv *s, int pad)
-{
-    return !s->fifos[pad] ||
-           av_rescale_q(av_audio_fifo_size(s->fifos[pad]), av_make_q(1, s->sample_rate), AV_TIME_BASE_Q) <= s->output_duration;
-}
-
 static void request_frame(AVFilterContext *ctx, int pad)
 {
     ABufSinkPriv *s = ctx->priv;
     AVFilterLink *link = ctx->inputs[pad];
     int64_t pts;
-    int ret;
+    int i, ret;
 
     ff_inlink_acknowledge_status(link, &ret, &pts);
-    if (ret < 0) {
-        if (s->input_state[pad] & INPUT_ON) {
-            s->input_state[pad] |= INPUT_EOF;
-            av_log(ctx, AV_LOG_INFO, "%s inputs[%d] reached EOF.\n", ctx->name, pad);
-        }
+    if (ret < 0)
         return;
-    }
 
     if (s->on_event_cb)
         ff_inlink_request_frame(link);
     else {
         if (!ff_inlink_queued_frames(link)) {
             ff_inlink_set_status(link, AVERROR_EOF);
+            for (i = 0; i < s->nb_inputs; i++) {
+                if (!ff_amix_input_empty(s->mix, ctx->inputs[i])) {
+                    ff_filter_set_ready(ctx, 100);
+                    break;
+                }
+            }
+            if (i == s->nb_inputs) {
+                ff_amix_free(s->mix);
+                s->mix = NULL;
+            }
         } else {
             ff_filter_set_ready(ctx, 100);
         }
@@ -155,83 +120,24 @@ static void request_frame(AVFilterContext *ctx, int pad)
 
 static int output_frame(AVFilterContext *ctx)
 {
-    AVFilterLink *inlink = NULL;
     ABufSinkPriv *s = ctx->priv;
-    AVFrame *out_buf, *in_buf;
-    int  i, ret, ns, nb_samples = INT_MAX;
+    AVFrame *frame = NULL;
+    int i, ret;
 
-    for (i = 0; i < s->nb_inputs; i++) {
-        if (s->input_state[i] & INPUT_ON) {
-            if (!inlink)
-                inlink = ctx->inputs[i];
-            ns = av_audio_fifo_size(s->fifos[i]);
-            nb_samples = FFMIN(nb_samples, ns);
-        }
-    }
-    if (nb_samples == INT_MAX || nb_samples == 0)
-        return 0;
+    ret = ff_amix_read(s->mix, &frame);
+    if (ret <= 0)
+        return ret;
 
-    calculate_scales(s);
-
-    out_buf = ff_get_audio_buffer(inlink, nb_samples);
-    if (!out_buf)
-        return AVERROR(ENOMEM);
-
-    in_buf = ff_get_audio_buffer(inlink, nb_samples);
-    if (!in_buf) {
-        av_frame_free(&out_buf);
-        return AVERROR(ENOMEM);
-    }
-
-    for (i = 0; i < s->nb_inputs; i++) {
-        if (s->input_state[i] & INPUT_ON) {
-            int planes, plane_size, p;
-
-            ret = av_audio_fifo_read(s->fifos[i], (void **)in_buf->extended_data, nb_samples);
-            if (ret < 0) {
-                av_frame_free(&out_buf);
-                av_frame_free(&in_buf);
-                return ret;
-            }
-
-            planes = s->planar ? in_buf->ch_layout.nb_channels : 1;
-            plane_size = nb_samples * (s->planar ? 1 : in_buf->ch_layout.nb_channels);
-            plane_size = FFALIGN(plane_size, 16);
-
-            if (out_buf->format == AV_SAMPLE_FMT_S16 ||
-                out_buf->format == AV_SAMPLE_FMT_S16P) {
-                for (p = 0; p < planes; p++) {
-                    vector_fmac_scalar_c((int16_t *)out_buf->extended_data[p],
-                                         (int16_t *) in_buf->extended_data[p],
-                                         s->input_scale[i] * INT16_MAX, plane_size);
-                }
-            } else {
-                av_log(ctx, AV_LOG_ERROR, "%s: Unsupported sample format.\n", ctx->name);
-                return 0;
-            }
-
-            if ((s->input_state[i] & INPUT_EOF) && av_audio_fifo_size(s->fifos[i]) == 0) {
-                s->input_state[i] &= ~INPUT_ON;
-                av_audio_fifo_free(s->fifos[i]);
-                s->fifos[i] = NULL;
-            }
-        }
-    }
-
-    av_frame_free(&in_buf);
     if (s->next_pts == AV_NOPTS_VALUE)
         s->next_pts = 0;
 
-    s->output_duration = av_rescale_q(out_buf->nb_samples, av_make_q(1, s->sample_rate),
-                                      AV_TIME_BASE_Q);
-    out_buf->pts = s->next_pts;
-    s->next_pts += s->output_duration;
-    out_buf->duration = s->output_duration;
+    frame->pts = s->next_pts;
+    frame->duration = av_rescale_q(frame->nb_samples, av_make_q(1, frame->sample_rate), AV_TIME_BASE_Q);
+    s->next_pts += frame->duration;
 
     if (s->on_event_cb)
-        s->on_event_cb(s->on_event_cb_udata, 0, (intptr_t)out_buf);
-
-    av_frame_free(&out_buf);
+        s->on_event_cb(s->on_event_cb_udata, 0, (intptr_t)frame);
+    av_frame_free(&frame);
 
     return 0;
 }
@@ -239,7 +145,6 @@ static int output_frame(AVFilterContext *ctx)
 static int abufsink_activate(AVFilterContext *ctx)
 {
     ABufSinkPriv *s = ctx->priv;
-    AVFrame *frame = NULL;
     AVFilterLink *link;
     int i, ret = 0;
     int64_t pts;
@@ -247,39 +152,23 @@ static int abufsink_activate(AVFilterContext *ctx)
     for (i = 0; i < s->nb_inputs; i++) {
         link = ctx->inputs[i];
 
-        if (ff_inlink_check_available_frame(link)) {
-            ret = ff_inlink_consume_frame(link, &frame);
-            if (ret < 0) {
-                s->input_state[i] |= INPUT_EOF;
-                request_frame(ctx, i);
-                continue;
-            }
-
-            s->input_state[i] = INPUT_ON;
-            s->planar = av_sample_fmt_is_planar(link->format);
-            s->sample_rate = link->sample_rate;
-            s->sample_fmt = link->format;
-
-            if (av_channel_layout_compare(&s->ch_layout, &link->ch_layout))
-                av_channel_layout_copy(&s->ch_layout, &link->ch_layout);
-
-            if (!s->fifos[i]) {
-                s->fifos[i] = av_audio_fifo_alloc(s->sample_fmt, s->ch_layout.nb_channels, 1024);
-                if (!s->fifos[i])
-                    return AVERROR(ENOMEM);
-            }
-
-            ret = av_audio_fifo_write(s->fifos[i], (void **)frame->extended_data, frame->nb_samples);
-            av_frame_free(&frame);
-            if (ret < 0)
-                return ret;
+        if (!s->mix) {
+            s->mix = ff_amix_alloc(link->sample_rate, link->format, link->ch_layout.nb_channels);
+            if (!s->mix)
+                return AVERROR(ENOMEM);
         }
 
-        ret = output_frame(ctx);
-        if (ret < 0)
-            return ret;
+        if (ff_inlink_check_available_frame(link)) {
+            ret = ff_amix_input_write(s->mix, link);
+            if (ret < 0) {
+                av_log(ctx, AV_LOG_ERROR, "input[%d] write to mix failed, ret:%d.\n", i, ret);
+                continue;
+            }
+        }
 
-        if (input_frame_wanted(s, i))
+        output_frame(ctx);
+
+        if (ff_amix_input_want(s->mix, link))
             request_frame(ctx, i);
     }
 
@@ -373,41 +262,9 @@ static int abufsink_init(AVFilterContext *ctx)
             return ret;
     }
 
-    s->fifos = av_calloc(s->nb_inputs, sizeof(*s->fifos));
-    if (!s->fifos)
-        goto err;
-
-    s->input_state = av_malloc(s->nb_inputs);
-    if (!s->input_state)
-        goto err;
-    memset(s->input_state, INPUT_EOF, s->nb_inputs);
-
-    s->input_scale = av_calloc(s->nb_inputs, sizeof(*s->input_scale));
-    if (!s->input_state)
-        goto err;
-
     s->next_pts = AV_NOPTS_VALUE;
 
     return 0;
-
-err:
-    av_freep(&s->fifos);
-    av_freep(&s->input_state);
-    return AVERROR(ENOMEM);
-}
-
-static void abufsink_uninit(AVFilterContext *ctx)
-{
-    ABufSinkPriv *s = ctx->priv;
-    int i;
-
-    if (s->fifos) {
-        for (i = 0; i < s->nb_inputs; i++)
-            av_audio_fifo_free(s->fifos[i]);
-        av_freep(&s->fifos);
-    }
-    av_freep(&s->input_state);
-    av_freep(&s->input_scale);
 }
 
 const AVFilter ff_asink_abufsink = {
@@ -416,7 +273,6 @@ const AVFilter ff_asink_abufsink = {
     .priv_size       = sizeof(ABufSinkPriv),
     .priv_class      = &abufsink_class,
     .init            = abufsink_init,
-    .uninit          = abufsink_uninit,
     FILTER_QUERY_FUNC2(abufsink_query_formats),
     .activate        = abufsink_activate,
     .process_command = abufsink_process_command,
